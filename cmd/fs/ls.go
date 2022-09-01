5beabc121a447210a9ffd3a124bdd54885166a57
Shreyas Goenka <shreyas.goenka@databricks.com>
2022-09-01 13:38:56 +0200

    Revert "Using go sdk for ./bricks fs ls DIR_NAME command"
    This reverts commit 6217d20e57dba9f698df73cae00b751c994a753f.

--- cmd/fs/ls.go
+++ cmd/fs/ls.go
@@ -3,9 +3,10 @@ package fs
 import (
 	"fmt"
 
+	"github.com/databricks/bricks/project"
 	"github.com/spf13/cobra"
-	"github.com/databricks/databricks-sdk-go/workspaces"	
-	"github.com/databricks/databricks-sdk-go/service/dbfs"
+
+	"github.com/databrickslabs/terraform-provider-databricks/storage"
 )
 
 // lsCmd represents the ls command
@@ -15,17 +16,11 @@ var lsCmd = &cobra.Command{
 	Long:  `Lists files`,
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		// Question (shreyas): Where does the client pick up the login creds from ? Context ?
-		// Maybe this client can be added on a higher level ?
-		// Create issue to add tests for this ? How to write those for cli ?
-		workspacesClient := workspaces.New()
-		listStatusResponse, err := workspacesClient.Dbfs.ListStatus(cmd.Context(), 
-			dbfs.ListStatusRequest{Path: args[0]},
-		)
+		api := storage.NewDbfsAPI(cmd.Context(), project.Current.Client())
+		files, err := api.List(args[0], false)
 		if err != nil {
 			panic(err)
 		}
-		files := listStatusResponse.Files
 		// TODO: output formatting: JSON, CSV, tables and default
 		for _, v := range files {
 			fmt.Printf("[-] %s (%d, %v)\n", v.Path, v.FileSize, v.IsDir)
