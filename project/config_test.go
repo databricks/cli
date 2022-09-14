f9edebda1f9d2e51552aa4b0fd97a6824bfa748a
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2022-09-14 15:04:57 +0200

    Function to detect project root

--- project/config_test.go
+++ project/config_test.go
@@ -1,32 +1,11 @@
 package project
 
 import (
-	"fmt"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-
-func TestFindProjectRoot(t *testing.T) {
-	wd, _ := os.Getwd()
-	defer os.Chdir(wd)
-	err := os.Chdir("testdata/a/b/c")
-	assert.NoError(t, err)
-	root, err := findProjectRoot()
-	assert.NoError(t, err)
-
-	assert.Equal(t, fmt.Sprintf("%s/testdata", wd), root)
-}
-
-func TestFindProjectRootInRoot(t *testing.T) {
-	wd, _ := os.Getwd()
-	defer os.Chdir(wd)
-	err := os.Chdir("/tmp")
-	assert.NoError(t, err)
-	_, err = findProjectRoot()
-	assert.EqualError(t, err, "cannot find databricks.yml anywhere")
-}
 
 func TestLoadProjectConf(t *testing.T) {
 	wd, _ := os.Getwd()
