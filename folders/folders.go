f12e525c03edebfcd72e0a246e7cb0ed100629fa
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2022-09-14 10:43:39 +0200

    Don't depend on working directory in folders.FindDirWithLeaf

--- folders/folders.go
+++ folders/folders.go
@@ -2,30 +2,33 @@ package folders
 
 import (
 	"errors"
-	"fmt"
 	"os"
-	"path"
+	"path/filepath"
 )
 
-func FindDirWithLeaf(leaf string) (string, error) {
-	dir, err := os.Getwd()
-	if err != nil {
-		return "", fmt.Errorf("cannot find $PWD: %s", err)
-	}
+// FindDirWithLeaf returns the first directory that holds `leaf`,
+// traversing up to the root of the filesystem, starting at `dir`.
+func FindDirWithLeaf(dir string, leaf string) (string, error) {
 	for {
-		_, err = os.Stat(fmt.Sprintf("%s/%s", dir, leaf))
+		_, err := os.Stat(filepath.Join(dir, leaf))
+
+		// No error means we found the leaf in dir.
+		if err == nil {
+			return dir, nil
+		}
+
+		// ErrNotExist means we continue traversal up the tree.
 		if errors.Is(err, os.ErrNotExist) {
-			// TODO: test on windows
-			next := path.Dir(dir)
-			if dir == next { // or stop at $HOME?..
-				return "", fmt.Errorf("cannot find %s anywhere", leaf)
+			next := filepath.Dir(dir)
+			if dir == next {
+				// Return if we cannot continue traversal.
+				return "", err
 			}
+
 			dir = next
 			continue
 		}
-		if err != nil {
-			return "", err
-		}
-		return dir, nil
+
+		return "", err
 	}
 }
---
+++ folders/folders_test.go
@@ -0,0 +1,38 @@
+package folders
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFindDirWithLeaf(t *testing.T) {
+	wd, err := os.Getwd()
+	require.NoError(t, err)
+
+	root := filepath.Join(wd, "..")
+
+	// Find from working directory should work.
+	{
+		out, err := FindDirWithLeaf(wd, ".git")
+		assert.NoError(t, err)
+		assert.Equal(t, out, root)
+	}
+
+	// Find from project root itself should work.
+	{
+		out, err := FindDirWithLeaf(root, ".git")
+		assert.NoError(t, err)
+		assert.Equal(t, out, root)
+	}
+
+	// Find for something that doesn't exist should work.
+	{
+		out, err := FindDirWithLeaf(root, "this-leaf-doesnt-exist-anywhere")
+		assert.ErrorIs(t, err, os.ErrNotExist)
+		assert.Equal(t, out, "")
+	}
+}
