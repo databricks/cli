b8f9caa2bba78b1449c9bce31cfaeaa18d842f91
Shreyas Goenka <shreyas.goenka@databricks.com>
2022-10-04 14:29:05 +0200

    use set limit for concurrent wait group

--- cmd/sync/watchdog.go
+++ cmd/sync/watchdog.go
@@ -27,6 +27,8 @@ type watchdog struct {
 	failure error // data race? make channel?
 }
 
+const MaxRequestLimit = 30
+
 func putFile(ctx context.Context, path string, content io.Reader) error {
 	wsc := project.Get(ctx).WorkspacesClient()
 	// workspace mkdirs is idempotent
@@ -41,101 +43,60 @@ func putFile(ctx context.Context, path string, content io.Reader) error {
 	return apiClient.Post(ctx, apiPath, content, nil)
 }
 
-func deleteFilesInParallel(ctx context.Context, paths []string, numParallelRequests int, remoteRoot string) error {
-	// Outer loop, deletes numParallelRequests in parallel
-	for i := 0; i < len(paths); i += numParallelRequests {
-		// select min of len(paths)-i, numParallelRequests
-		var numFilesToDeleteInParallel int
-		if len(paths)-i < numParallelRequests {
-			numFilesToDeleteInParallel = len(paths) - i
-		} else {
-			numFilesToDeleteInParallel = numParallelRequests
-		}
+func getRemoteSyncCallback(ctx context.Context, root, remoteDir string, wsc *workspaces.WorkspacesClient) func(localDiff diff) error {
+	return func(d diff) error {
 
 		// Abstraction over wait groups which allows you to get the errors
 		// returned in goroutines
 		var g errgroup.Group
-		// Inner loop, spawns goroutines to delete a single file
-		for j := 0; j < numFilesToDeleteInParallel; j++ {
-			fileName := paths[i+j]
+
+		// Allow MaxRequestLimit maxumim concurrent api calls
+		g.SetLimit(MaxRequestLimit)
+
+		for _, fileName := range d.delete {
+			// Copy of fileName created to make this safe for concurrent use.
+			// directly using fileName can cause race conditions since the loop
+			// might iterate over to the next fileName before the go routine function
+			// is evaluated
+			localFileName := fileName
 			g.Go(func() error {
 				wsc := project.Get(ctx).WorkspacesClient()
 				err := wsc.Workspace.Delete(ctx,
 					workspace.Delete{
-						Path:      path.Join(remoteRoot, fileName),
+						Path:      path.Join(remoteDir, localFileName),
 						Recursive: true,
 					},
 				)
 				if err != nil {
 					return err
 				}
-				log.Printf("[INFO] Deleted %s", fileName)
+				log.Printf("[INFO] Deleted %s", localFileName)
 				return nil
 			})
 		}
-
-		// wait for goroutines to finish and return first non-nil error return
-		// if any
-		if err := g.Wait(); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func putFilesInParallel(ctx context.Context, paths []string, numParallelRequests int, localRoot string, remoteRoot string) error {
-	// Outer loop, uploads numParallelRequests in parallel
-	for i := 0; i < len(paths); i += numParallelRequests {
-		// select min of len(paths)-i, numParallelRequests
-		var numFilesToUploadInParallel int
-		if len(paths)-i < numParallelRequests {
-			numFilesToUploadInParallel = len(paths) - i
-		} else {
-			numFilesToUploadInParallel = numParallelRequests
-		}
-
-		// Abstraction over wait groups which allows you to get the errors
-		// returned in goroutines
-		var g errgroup.Group
-		// Inner loop, spawns goroutines to upload a single file
-		for j := 0; j < numFilesToUploadInParallel; j++ {
-			fileName := paths[i+j]
+		for _, fileName := range d.put {
+			localFileName := fileName
 			g.Go(func() error {
-				f, err := os.Open(filepath.Join(localRoot, fileName))
+				f, err := os.Open(filepath.Join(root, localFileName))
 				if err != nil {
 					return err
 				}
-				err = putFile(ctx, path.Join(remoteRoot, fileName), f)
+				err = putFile(ctx, path.Join(remoteDir, localFileName), f)
 				if err != nil {
 					return fmt.Errorf("failed to upload file: %s", err)
 				}
 				err = f.Close()
 				if err != nil {
-					return err // TODO: fmt.Errorf
+					return err
 				}
-				log.Printf("[INFO] Uploaded %s", fileName)
+				log.Printf("[INFO] Uploaded %s", localFileName)
 				return nil
 			})
 		}
-
 		// wait for goroutines to finish and return first non-nil error return
 		// if any
 		if err := g.Wait(); err != nil {
 			return err
-		}
-	}
-	return nil
-}
-
-func getRemoteSyncCallback(ctx context.Context, root, remoteDir string, wsc *workspaces.WorkspacesClient) func(localDiff diff) error {
-	return func(d diff) error {
-		err := deleteFilesInParallel(ctx, d.delete, 25, remoteDir)
-		if err != nil {
-			return fmt.Errorf("failed to delete files: %s", err)
-		}
-		err = putFilesInParallel(ctx, d.put, 25, root, remoteDir)
-		if err != nil {
-			return fmt.Errorf("failed to upload files: %s", err)
 		}
 		return nil
 	}
