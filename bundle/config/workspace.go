da74dba4a1eaae2f6d58dd9c504c202664545561
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2022-11-18 15:54:41 +0100

    Add unified authentication attributes to workspace struct

--- bundle/config/workspace.go
+++ bundle/config/workspace.go
@@ -7,15 +7,46 @@ import (
 
 // Workspace defines configurables at the workspace level.
 type Workspace struct {
-	// TODO: Add all unified authentication configurables.
+	// Unified authentication attributes.
+	//
+	// We omit sensitive attributes as they should never be hardcoded.
+	// They must be specified through environment variables instead.
+	//
+	// For example: token, password, Google credentials, Azure client secret, etc.
+	//
+
+	// Generic attributes.
 	Host    string `json:"host,omitempty"`
 	Profile string `json:"profile,omitempty"`
+
+	// Google specific attributes.
+	GoogleServiceAccount string `json:"google_service_account,omitempty"`
+
+	// Azure specific attributes.
+	AzureResourceID  string `json:"azure_workspace_resource_id,omitempty"`
+	AzureUseMSI      bool   `json:"azure_use_msi,omitempty"`
+	AzureClientID    string `json:"azure_client_id,omitempty"`
+	AzureTenantID    string `json:"azure_tenant_id,omitempty"`
+	AzureEnvironment string `json:"azure_environment,omitempty"`
+	AzureLoginAppID  string `json:"azure_login_app_id,omitempty"`
 }
 
 func (w *Workspace) Client() *workspaces.WorkspacesClient {
 	config := databricks.Config{
+		// Generic
 		Host:    w.Host,
 		Profile: w.Profile,
+
+		// Google
+		GoogleServiceAccount: w.GoogleServiceAccount,
+
+		// Azure
+		AzureResourceID:  w.AzureResourceID,
+		AzureUseMSI:      w.AzureUseMSI,
+		AzureClientID:    w.AzureClientID,
+		AzureTenantID:    w.AzureTenantID,
+		AzureEnvironment: w.AzureEnvironment,
+		AzureLoginAppID:  w.AzureLoginAppID,
 	}
 
 	return workspaces.New(&config)
