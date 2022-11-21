3b351d3b00e83588e4611bbb4b7cfd01bb18cd71
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2022-11-21 15:39:53 +0100

    Add command that writes the materialized bundle configuration to stdout (#95)
    Used to inspect the bundle configuration after loading and merging all
    files.
    
    Once we add variable interpolation this command could show the result
    after interpolation as well.
    
    Each of the mutations to this configuration is observable, so we could
    add a mode that writes each of the intermediate versions to disk for
    even more fine grained introspection.

---
+++ cmd/bundle/root.go
@@ -0,0 +1,36 @@
+package bundle
+
+import (
+	"github.com/databricks/bricks/bundle"
+	"github.com/databricks/bricks/cmd/root"
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the root command for the bundle subcommand.
+var rootCmd = &cobra.Command{
+	Use:   "bundle",
+	Short: "Databricks Application Bundles",
+}
+
+// ConfigureBundle loads the bundle configuration
+// and configures it on the command's context.
+func ConfigureBundle(cmd *cobra.Command, args []string) error {
+	ctx, err := bundle.ConfigureForEnvironment(cmd.Context(), getEnvironment(cmd))
+	if err != nil {
+		return err
+	}
+
+	cmd.SetContext(ctx)
+	return nil
+}
+
+func AddCommand(cmd *cobra.Command) {
+	rootCmd.AddCommand(cmd)
+}
+
+func init() {
+	// All bundle commands take an "environment" parameter.
+	rootCmd.PersistentFlags().StringP("environment", "e", "", "Environment to use")
+	// Add to top level root.
+	root.RootCmd.AddCommand(rootCmd)
+}
