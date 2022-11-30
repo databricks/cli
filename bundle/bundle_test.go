4064a21797a7b0c85c2d262d247793ae93541f1d
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2022-11-30 14:40:41 +0100

    Function to return bundle's cache directory (#109)
    Parallel of `project.CacheDir()` introduced in
    https://github.com/databricks/bricks/pull/82.

--- bundle/bundle_test.go
+++ bundle/bundle_test.go
@@ -2,6 +2,8 @@ package bundle
 
 import (
 	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -18,4 +20,22 @@ func TestLoadExists(t *testing.T) {
 	b, err := Load("./tests/basic")
 	require.Nil(t, err)
 	assert.Equal(t, "basic", b.Config.Bundle.Name)
+}
+
+func TestBundleCacheDir(t *testing.T) {
+	projectDir := t.TempDir()
+	f1, err := os.Create(filepath.Join(projectDir, "bundle.yml"))
+	require.NoError(t, err)
+	f1.Close()
+
+	bundle, err := Load(projectDir)
+	require.NoError(t, err)
+
+	// Artificially set environment.
+	// This is otherwise done by [mutators.SelectEnvironment].
+	bundle.Config.Bundle.Environment = "default"
+
+	cacheDir, err := bundle.CacheDir()
+	assert.NoError(t, err)
+	assert.True(t, strings.HasPrefix(cacheDir, projectDir))
 }
