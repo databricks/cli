398fc739a6aa7c83c1db4dc3aac4f00920154fcf
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2022-12-14 15:27:07 +0100

    Update integration test

--- internal/filer_test.go
+++ internal/filer_test.go
@@ -6,11 +6,10 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"path"
 	"strings"
 	"testing"
 
-	"github.com/databricks/bricks/utilities"
+	"github.com/databricks/bricks/libs/filer"
 	"github.com/databricks/databricks-sdk-go"
 	"github.com/databricks/databricks-sdk-go/apierr"
 	"github.com/databricks/databricks-sdk-go/service/workspace"
@@ -20,7 +19,7 @@ import (
 
 type filerTest struct {
 	*testing.T
-	utilities.Filer
+	filer.Filer
 }
 
 func (f filerTest) assertContents(ctx context.Context, name string, contents string) {
@@ -37,59 +36,77 @@ func (f filerTest) assertContents(ctx context.Context, name string, contents str
 	assert.Equal(f, contents, string(body))
 }
 
+func temporaryWorkspaceDir(t *testing.T, w *databricks.WorkspaceClient) string {
+	ctx := context.Background()
+	me, err := w.CurrentUser.Me(ctx)
+	require.NoError(t, err)
+
+	path := fmt.Sprintf("/Users/%s/%s", me.UserName, RandomName("wsfs-files-"))
+
+	// Ensure directory exists, but doesn't exist YET!
+	// Otherwise we could inadvertently remove a directory that already exists on cleanup.
+	t.Logf("mkdir %s", path)
+	err = w.Workspace.MkdirsByPath(ctx, path)
+	require.NoError(t, err)
+
+	// Remove test directory on test completion.
+	t.Cleanup(func() {
+		t.Logf("rm -rf %s", path)
+		err := w.Workspace.Delete(ctx, workspace.Delete{
+			Path:      path,
+			Recursive: true,
+		})
+		if err == nil || apierr.IsMissing(err) {
+			return
+		}
+		t.Logf("unable to remove temporary workspace path %s: %#v", path, err)
+	})
+
+	return path
+}
+
 func TestAccFilerWorkspaceFiles(t *testing.T) {
 	t.Log(GetEnvOrSkipTest(t, "CLOUD_ENV"))
 
 	ctx := context.Background()
 	w := databricks.Must(databricks.NewWorkspaceClient())
-	me, err := w.CurrentUser.Me(ctx)
-	require.NoError(t, err)
-
-	tmpdir := fmt.Sprintf("/Users/%s/%s", me.UserName, RandomName("wsfs-files-"))
-	f, err := utilities.NewWorkspaceFilesClient(w, tmpdir)
+	tmpdir := temporaryWorkspaceDir(t, w)
+	f, err := filer.NewWorkspaceFilesClient(w, tmpdir)
 	require.NoError(t, err)
 
 	// Check if we can use this API here, skip test if we cannot.
-	_, err = f.Read(ctx, "/foo")
+	_, err = f.Read(ctx, "we_use_this_call_to_test_if_this_api_is_enabled")
 	if apierr, ok := err.(apierr.APIError); ok && apierr.StatusCode == http.StatusBadRequest {
 		t.Skip(apierr.Message)
 	}
 
-	// Remove test directory on test completion.
-	t.Cleanup(func() {
-		err := w.Workspace.Delete(ctx, workspace.Delete{
-			Path: tmpdir,
-		})
-		if err != nil {
-			t.Logf("unable to remove %s: %s", tmpdir, err)
-		}
-	})
-
 	// Write should fail because the root path doesn't yet exist.
-	err = f.Write(ctx, "/foo", strings.NewReader(`"hello world"`))
-	assert.True(t, errors.As(err, &utilities.NoSuchDirectoryError{}))
+	err = f.Write(ctx, "/foo/bar", strings.NewReader(`"hello world"`))
+	assert.True(t, errors.As(err, &filer.NoSuchDirectoryError{}))
 
 	// Read should fail because the root path doesn't yet exist.
-	_, err = f.Read(ctx, "/foo")
+	_, err = f.Read(ctx, "/foo/bar")
 	assert.True(t, apierr.IsMissing(err))
 
 	// Write with CreateParentDirectories flag should succeed.
-	err = f.Write(ctx, "/foo", strings.NewReader(`"hello world"`), utilities.CreateParentDirectories)
+	err = f.Write(ctx, "/foo/bar", strings.NewReader(`"hello world"`), filer.CreateParentDirectories)
 	assert.NoError(t, err)
-	filerTest{t, f}.assertContents(ctx, "/foo", `"hello world"`)
+	filerTest{t, f}.assertContents(ctx, "/foo/bar", `"hello world"`)
 
 	// Write should fail because there is an existing file at the specified path.
-	err = f.Write(ctx, "/foo", strings.NewReader(`"hello universe"`))
-	assert.True(t, errors.As(err, &utilities.FileAlreadyExistsError{}))
+	err = f.Write(ctx, "/foo/bar", strings.NewReader(`"hello universe"`))
+	assert.True(t, errors.As(err, &filer.FileAlreadyExistsError{}))
 
 	// Write with OverwriteIfExists should succeed.
-	err = f.Write(ctx, "/foo", strings.NewReader(`"hello universe"`), utilities.OverwriteIfExists)
+	err = f.Write(ctx, "/foo/bar", strings.NewReader(`"hello universe"`), filer.OverwriteIfExists)
 	assert.NoError(t, err)
-	filerTest{t, f}.assertContents(ctx, "/foo", `"hello universe"`)
+	filerTest{t, f}.assertContents(ctx, "/foo/bar", `"hello universe"`)
 
-	// Remove file.
-	err = w.Workspace.Delete(ctx, workspace.Delete{
-		Path: path.Join(tmpdir, "foo"),
-	})
+	// Delete should fail if the file doesn't exist.
+	err = f.Delete(ctx, "/doesnt_exist")
+	assert.True(t, apierr.IsMissing(err))
+
+	// Delete should succeed for file that does exist.
+	err = f.Delete(ctx, "/foo/bar")
 	assert.NoError(t, err)
 }
