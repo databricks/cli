d5d8b2045186aa811b61bf43baaac22e32ef696e
Shreyas Goenka <shreyas.goenka@databricks.com>
2022-12-22 00:12:34 +0530

    added some comments

--- cmd/sync/snapshot.go
+++ cmd/sync/snapshot.go
@@ -228,19 +228,24 @@ func (s *Snapshot) diff(all []git.File) (change diff, err error) {
 		if !seen || modified.After(lastSeenModified) {
 			lastModifiedTimes[f.Relative] = modified
 
-			// Keep track of remote names of local files so we can delete them later
+			// change separators to '/' for file paths in remote store
 			unixFileName := filepath.ToSlash(f.Relative)
+
+			// put file in databricks workspace
 			change.put = append(change.put, unixFileName)
 
-			// strip `.py` for python notebook
-			remoteName := unixFileName
+			// get file metadata about whether it's a notebook
 			isNotebook, typeOfNotebook, err := getNotebookDetails(f.Absolute)
 			if err != nil {
 				return change, err
 			}
+
+			// strip `.py` for python notebooks
+			remoteName := unixFileName
 			if isNotebook && typeOfNotebook == "PYTHON" {
 				remoteName = strings.TrimSuffix(remoteName, `.py`)
 			}
+
 			// If the remote handle of a file changes, we want to delete the old
 			// remote version of that file to avoid duplicates.
 			// This can happen if a python notebook is converted to a python
