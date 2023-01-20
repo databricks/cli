b3a30166f69259ee2378fc7a4fdfa04f7b8f4f06
shreyas-goenka <88374338+shreyas-goenka@users.noreply.github.com>
2023-01-20 16:55:44 +0100

    JSON Schema generator for golang types (#167)
    This PR contains a struct to allow you to generate JSON schemas from
    Golang types and a struct to allow injecting documentation into the json
    schema. This will support autocomplete for DABs

---
+++ bundle/schema/tracker.go
@@ -0,0 +1,54 @@
+package schema
+
+import (
+	"container/list"
+	"fmt"
+	"reflect"
+)
+
+type tracker struct {
+	// Types encountered in current path during the recursive traversal. Used to
+	// check for cycles
+	seenTypes map[reflect.Type]struct{}
+
+	// List of field names encountered in current path during the recursive traversal.
+	// Used to hydrate errors with path to the exact node where error occured.
+	//
+	// The field names here are the first tag in the json tags of struct field.
+	debugTrace *list.List
+}
+
+func newTracker() *tracker {
+	return &tracker{
+		seenTypes:  map[reflect.Type]struct{}{},
+		debugTrace: list.New(),
+	}
+}
+
+func (t *tracker) errWithTrace(prefix string) error {
+	traceString := "root"
+	curr := t.debugTrace.Front()
+	for curr != nil {
+		if curr.Value.(string) != "" {
+			traceString += " -> " + curr.Value.(string)
+		}
+		curr = curr.Next()
+	}
+	return fmt.Errorf("[ERROR] " + prefix + ". traversal trace: " + traceString)
+}
+
+func (t *tracker) hasCycle(golangType reflect.Type) bool {
+	_, ok := t.seenTypes[golangType]
+	return ok
+}
+
+func (t *tracker) push(nodeType reflect.Type, jsonName string) {
+	t.seenTypes[nodeType] = struct{}{}
+	t.debugTrace.PushBack(jsonName)
+}
+
+func (t *tracker) pop(nodeType reflect.Type) {
+	back := t.debugTrace.Back()
+	t.debugTrace.Remove(back)
+	delete(t.seenTypes, nodeType)
+}
