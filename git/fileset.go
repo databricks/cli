c6630a936ec35c5ca6c474c903e8e0d8d016b356
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2023-01-27 09:02:48 +0100

    Use git.View in existing git.FileSet
    This makes FileSet aware of gitignore files in parent directories up to the
    repository root, as well as gitignore files in underlying directories.

--- git/fileset.go
+++ git/fileset.go
@@ -1,15 +1,10 @@
 package git
 
 import (
-	"fmt"
 	"io/fs"
 	"os"
 	"path/filepath"
-	"strings"
 	"time"
-
-	ignore "github.com/sabhiram/go-gitignore"
-	"golang.org/x/exp/slices"
 )
 
 type File struct {
@@ -34,49 +29,42 @@ func (f File) Modified() (ts time.Time) {
 //
 //	We do not sync files that match this pattern
 type FileSet struct {
-	root   string
-	ignore *ignore.GitIgnore
+	root string
+	view *View
 }
 
 // Retuns FileSet for the git repo located at `root`
 func NewFileSet(root string) *FileSet {
-	syncIgnoreLines := append(getGitIgnoreLines(root), getSyncIgnoreLines()...)
-	return &FileSet{
-		root:   root,
-		ignore: ignore.CompileIgnoreLines(syncIgnoreLines...),
+	w := &FileSet{
+		root: root,
 	}
+	err := w.createView()
+	if err != nil {
+		panic(err)
+	}
+	return w
 }
 
-func getGitIgnoreLines(root string) []string {
-	gitIgnoreLines := []string{}
-	gitIgnorePath := fmt.Sprintf("%s/.gitignore", root)
-	rawIgnore, err := os.ReadFile(gitIgnorePath)
-
-	if err == nil {
-		// add entries from .gitignore if the file exists (did read correctly)
-		for _, line := range strings.Split(string(rawIgnore), "\n") {
-			// underlying library doesn't behave well with Rule 5 of .gitignore,
-			// hence this workaround
-			gitIgnoreLines = append(gitIgnoreLines, strings.Trim(line, "/"))
-		}
+// createView instantiates the view for this FileSet.
+// Separate function because it needs to be recreated
+// after adding a .gitignore entry in the below function.
+func (w *FileSet) createView() error {
+	view, err := NewView(w.root)
+	if err != nil {
+		return err
 	}
-	return gitIgnoreLines
-}
-
-// This contains additional files/dirs to be ignored while syncing that are
-// not mentioned in .gitignore
-func getSyncIgnoreLines() []string {
-	return []string{".git"}
+	w.view = view
+	return nil
 }
 
 // Only call this function for a bricks project root
 // since it will create a .gitignore file if missing
 func (w *FileSet) EnsureValidGitIgnoreExists() error {
-	gitIgnoreLines := getGitIgnoreLines(w.root)
-	gitIgnorePath := fmt.Sprintf("%s/.gitignore", w.root)
-	if slices.Contains(gitIgnoreLines, ".databricks") {
+	if w.view.Ignore(".databricks") {
 		return nil
 	}
+
+	gitIgnorePath := filepath.Join(w.root, ".gitignore")
 	f, err := os.OpenFile(gitIgnorePath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
 		return err
@@ -86,9 +74,7 @@ func (w *FileSet) EnsureValidGitIgnoreExists() error {
 	if err != nil {
 		return err
 	}
-	gitIgnoreLines = append(gitIgnoreLines, ".databricks")
-	w.ignore = ignore.CompileIgnoreLines(append(gitIgnoreLines, getSyncIgnoreLines()...)...)
-	return nil
+	return w.createView()
 }
 
 // Return root for fileset.
@@ -102,53 +88,35 @@ func (w *FileSet) All() ([]File, error) {
 }
 
 func (w *FileSet) IsGitIgnored(pattern string) bool {
-	return w.ignore.MatchesPath(pattern)
+	return w.view.Ignore(pattern)
 }
 
 // Recursively traverses dir in a depth first manner and returns a list of all files
 // that are being tracked in the FileSet (ie not being ignored for matching one of the
 // patterns in w.ignore)
 func (w *FileSet) RecursiveListFiles(dir string) (fileList []File, err error) {
-	queue, err := readDir(dir, w.root)
-	if err != nil {
-		return nil, err
-	}
-	for len(queue) > 0 {
-		current := queue[0]
-		queue = queue[1:]
-		if w.ignore.MatchesPath(current.Relative) {
-			continue
-		}
-		if !current.IsDir() {
-			fileList = append(fileList, current)
-			continue
-		}
-		children, err := readDir(current.Absolute, w.root)
+	err = filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
-			return nil, err
+			return err
 		}
-		queue = append(queue, children...)
-	}
-	return fileList, nil
-}
 
-func readDir(dir, root string) (queue []File, err error) {
-	f, err := os.Open(dir)
-	if err != nil {
-		return
-	}
-	defer f.Close()
-	dirs, err := f.ReadDir(-1)
-	if err != nil {
-		return
-	}
-	for _, v := range dirs {
-		absolute := filepath.Join(dir, v.Name())
-		relative, err := filepath.Rel(root, absolute)
+		relPath, err := filepath.Rel(w.root, path)
 		if err != nil {
-			return nil, err
+			return err
 		}
-		queue = append(queue, File{v, absolute, relative})
-	}
+
+		if w.view.Ignore(relPath) {
+			if d.IsDir() {
+				return filepath.SkipDir
+			}
+			return nil
+		}
+
+		if !d.IsDir() {
+			fileList = append(fileList, File{d, path, relPath})
+		}
+
+		return nil
+	})
 	return
 }
