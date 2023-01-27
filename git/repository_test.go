a1525c8ac05841c678043e7d84eed9b873bab8aa
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2023-01-27 14:18:22 +0100

    Add test for repository

--- git/repository_test.go
+++ git/repository_test.go
@@ -1,1 +1,28 @@
 package git
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRepository(t *testing.T) {
+	// Load this repository as test.
+	repo, err := NewRepository("..")
+	require.NoError(t, err)
+
+	// Load all .gitignore files in this repository.
+	err = repo.includeIgnoreFilesForPath(".")
+	require.NoError(t, err)
+
+	// Check that top level ignores work.
+	assert.True(t, repo.Ignore(".DS_Store"))
+	assert.True(t, repo.Ignore("foo.pyc"))
+	assert.False(t, repo.Ignore("vendor"))
+	assert.True(t, repo.Ignore("vendor/"))
+
+	// Check that ignores under testdata work.
+	assert.True(t, repo.Ignore(filepath.Join("git", "testdata", "root.ignoreme")))
+}
