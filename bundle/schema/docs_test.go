7e8b71e75bfce0539cf86f218cc1fe8685f9087f
Shreyas Goenka <shreyas.goenka@databricks.com>
2023-03-01 15:18:10 +0100

    added TestSchemaToDocs

--- bundle/schema/docs_test.go
+++ bundle/schema/docs_test.go
@@ -1,34 +1,39 @@
 package schema
 
-// func TestSchemaToDocs(t *testing.T) {
-// 	type Bar struct {
-// 		A int    `json:"a"`
-// 		B string `json:"b,string"`
-// 	}
+import (
+	"encoding/json"
+	"testing"
 
-// 	type Foo struct {
-// 		Bar Bar `json:"bar"`
-// 	}
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
-// 	type MyStruct struct {
-// 		Foo map[string]Bar `json:"foo"`
-// 	}
+func TestSchemaToDocs(t *testing.T) {
+	schema := &Schema{
+		Type:        "object",
+		Description: "root doc",
+		Properties: map[string]*Schema{
+			"foo": {Type: "number", Description: "foo doc"},
+			"bar": {Type: "string"},
+		},
+	}
+	docs := schemaToDocs(schema)
+	docsJson, err := json.MarshalIndent(docs, "		", "	")
+	require.NoError(t, err)
 
-// 	elem := MyStruct{}
-
-// 	schema, err := New(reflect.TypeOf(elem), nil)
-// 	require.NoError(t, err)
-
-// 	docs, err := newDocs(reflect.TypeOf(elem))
-// 	require.NoError(t, err)
-
-// 	jsonSchema, err := json.MarshalIndent(schema, "		", "	")
-// 	assert.NoError(t, err)
-
-// 	jsonDocs, err := json.MarshalIndent(docs, "		", "	")
-// 	assert.NoError(t, err)
-
-// 	t.Log("[DEBUG] schema: ", string(jsonSchema))
-// 	t.Log("[DEBUG] docs: ", string(jsonDocs))
-// 	assert.False(t, true)
-// }
+	expected :=
+		`{
+			"description": "root doc",
+			"properties": {
+				"bar": {
+					"description": ""
+				},
+				"foo": {
+					"description": "foo doc"
+				}
+			}
+		}`
+	t.Log("[DEBUG] actual: ", string(docsJson))
+	t.Log("[DEBUG] expected: ", expected)
+	assert.Equal(t, expected, string(docsJson))
+}
