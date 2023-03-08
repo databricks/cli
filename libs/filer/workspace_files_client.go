6a3a41b2befb2756c151c77efb61cbe84a40fe1e
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2023-03-08 14:16:17 +0100

    Escape URL in filer

--- libs/filer/workspace_files_client.go
+++ libs/filer/workspace_files_client.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"path"
 	"strings"
 
@@ -53,7 +54,7 @@ func (w *WorkspaceFilesClient) Write(ctx context.Context, name string, reader io
 	overwrite := slices.Contains(mode, OverwriteIfExists)
 	urlPath := fmt.Sprintf(
 		"/api/2.0/workspace-files/import-file/%s?overwrite=%t",
-		strings.TrimLeft(absPath, "/"),
+		url.QueryEscape(strings.TrimLeft(absPath, "/")),
 		overwrite,
 	)
 
