46cfa747aca13d7594d882f4aa1f70b17467a3ec
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2023-03-09 10:26:56 +0100

    Move and hide launch and test commands (#222)
    Semantics in the context of a bundle are not yet clearly defined. Moving
    and hiding these commands until then.

---
+++ cmd/bundle/launch.go
@@ -0,0 +1,37 @@
+package bundle
+
+import (
+	"fmt"
+
+	"github.com/databricks/bricks/cmd/root"
+	"github.com/spf13/cobra"
+)
+
+var launchCmd = &cobra.Command{
+	Use:   "launch",
+	Short: "Launches a notebook on development cluster",
+	Long:  `Reads a file and executes it on dev cluster`,
+	Args:  cobra.ExactArgs(1),
+
+	// We're not ready to expose this command until we specify its semantics.
+	Hidden: true,
+
+	PreRunE: root.MustConfigureBundle,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return fmt.Errorf("TODO")
+		// contents, err := os.ReadFile(args[0])
+		// if err != nil {
+		// 	return err
+		// }
+		// results := project.RunPythonOnDev(cmd.Context(), string(contents))
+		// if results.Failed() {
+		// 	return results.Err()
+		// }
+		// fmt.Fprintf(cmd.OutOrStdout(), "Success: %s", results.Text())
+		// return nil
+	},
+}
+
+func init() {
+	AddCommand(launchCmd)
+}
--- main.go
+++ main.go
@@ -9,10 +9,8 @@ import (
 	_ "github.com/databricks/bricks/cmd/configure"
 	_ "github.com/databricks/bricks/cmd/fs"
 	_ "github.com/databricks/bricks/cmd/init"
-	_ "github.com/databricks/bricks/cmd/launch"
 	"github.com/databricks/bricks/cmd/root"
 	_ "github.com/databricks/bricks/cmd/sync"
-	_ "github.com/databricks/bricks/cmd/test"
 	_ "github.com/databricks/bricks/cmd/version"
 )
 
