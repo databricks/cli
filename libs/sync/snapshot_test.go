103c7c0e34db1c2b9f4baa29b9b6f83bb96a00b5
Shreyas Goenka <shreyas.goenka@databricks.com>
2023-03-10 11:33:06 +0100

    nit

--- libs/sync/snapshot_test.go
+++ libs/sync/snapshot_test.go
@@ -272,7 +272,7 @@ func TestNoErrorRenameWithIdenticalRemoteName(t *testing.T) {
 	assert.Len(t, change.put, 1)
 	assert.Contains(t, change.put, "foo.py")
 
-	pythonFoo.Close()
+	pythonFoo.Close(t)
 	pythonFoo.Remove(t)
 	sqlFoo := testfile.CreateFile(t, filepath.Join(projectDir, "foo.sql"))
 	sqlFoo.Overwrite(t, "-- Databricks notebook source\n")
