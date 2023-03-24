63c16175a8556863cae9216866ed95d75ed0195a
Shreyas Goenka <shreyas.goenka@databricks.com>
2023-03-24 16:50:43 +0100

    added string test

---
+++ bundle/run/job_progress_test.go
@@ -0,0 +1,25 @@
+package run
+
+import (
+	"testing"
+	"time"
+
+	"github.com/databricks/databricks-sdk-go/service/jobs"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestJobProgressEventString(t *testing.T) {
+	event := &JobProgressEvent{
+		Timestamp: time.Date(0, 0, 0, 0, 0, 0, 0, &time.Location{}),
+		JobId:     123,
+		RunId:     456,
+		RunName:   "run_name",
+		State: jobs.RunState{
+			LifeCycleState: jobs.RunLifeCycleStateTerminated,
+			ResultState:    jobs.RunResultStateSuccess,
+			StateMessage:   "state_message",
+		},
+		RunPageURL: "run_url",
+	}
+	assert.Equal(t, "-0001-11-30 00:00:00 run_name TERMINATED SUCCESS state_message run_url", event.String())
+}
