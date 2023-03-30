04e77102c90bffd49de389b2d390cbe392f39f7f
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2023-03-30 12:01:09 +0200

    Add mutators to pull and push Terraform state (#288)
    ## Changes
    
    Pull state before deploying and push state after deploying.
    
    Note: the run command was missing mutators to initialize Terraform. This
    is necessary if the cache directory is removed between running "deploy"
    and "run" (which is valid now that we synchronize state).
    
    ## Tests
    
    Manually.

---
+++ bundle/deploy/terraform/state_pull.go
@@ -0,0 +1,62 @@
+package terraform
+
+import (
+	"context"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/databricks/bricks/bundle"
+	"github.com/databricks/bricks/libs/filer"
+	"github.com/databricks/bricks/libs/log"
+	"github.com/databricks/databricks-sdk-go/apierr"
+)
+
+type statePull struct{}
+
+func (l *statePull) Name() string {
+	return "terraform:state-pull"
+}
+
+func (l *statePull) Apply(ctx context.Context, b *bundle.Bundle) ([]bundle.Mutator, error) {
+	f, err := filer.NewWorkspaceFilesClient(b.WorkspaceClient(), b.Config.Workspace.StatePath.Workspace)
+	if err != nil {
+		return nil, err
+	}
+
+	dir, err := Dir(b)
+	if err != nil {
+		return nil, err
+	}
+
+	// Download state file from filer to local cache directory.
+	log.Infof(ctx, "Opening remote state file")
+	remote, err := f.Read(ctx, TerraformStateFileName)
+	if err != nil {
+		// On first deploy this state file doesn't yet exist.
+		if apierr.IsMissing(err) {
+			log.Infof(ctx, "Remote state file does not exist")
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	// Expect the state file to live under dir.
+	local, err := os.OpenFile(filepath.Join(dir, TerraformStateFileName), os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
+	if err != nil {
+		return nil, err
+	}
+
+	// Write file to disk.
+	log.Infof(ctx, "Writing remote state file to local cache directory")
+	_, err = io.Copy(local, remote)
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+func StatePull() bundle.Mutator {
+	return &statePull{}
+}
---
+++ bundle/deploy/terraform/state_push.go
@@ -0,0 +1,48 @@
+package terraform
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+
+	"github.com/databricks/bricks/bundle"
+	"github.com/databricks/bricks/libs/filer"
+	"github.com/databricks/bricks/libs/log"
+)
+
+type statePush struct{}
+
+func (l *statePush) Name() string {
+	return "terraform:state-push"
+}
+
+func (l *statePush) Apply(ctx context.Context, b *bundle.Bundle) ([]bundle.Mutator, error) {
+	f, err := filer.NewWorkspaceFilesClient(b.WorkspaceClient(), b.Config.Workspace.StatePath.Workspace)
+	if err != nil {
+		return nil, err
+	}
+
+	dir, err := Dir(b)
+	if err != nil {
+		return nil, err
+	}
+
+	// Expect the state file to live under dir.
+	local, err := os.Open(filepath.Join(dir, TerraformStateFileName))
+	if err != nil {
+		return nil, err
+	}
+
+	// Upload state file from local cache directory to filer.
+	log.Infof(ctx, "Writing local state file to remote state directory")
+	err = f.Write(ctx, TerraformStateFileName, local, filer.CreateParentDirectories, filer.OverwriteIfExists)
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+func StatePush() bundle.Mutator {
+	return &statePush{}
+}
