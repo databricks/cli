8b445c90630840c60a922a8764f32646331bcf52
Shreyas Goenka <shreyas.goenka@databricks.com>
2023-04-11 12:52:49 +0200

    clean

--- cmd/bundle/destroy.go
+++ cmd/bundle/destroy.go
@@ -46,8 +46,6 @@ var destroyCmd = &cobra.Command{
 		// Inplace logging not supported for bundle destroy
 		ctx = cmdio.DisableInplace(ctx)
 
-		// TODO: protect against inplace logging here
-
 		return bundle.Apply(ctx, b, []bundle.Mutator{
 			phases.Initialize(),
 			phases.Build(),
