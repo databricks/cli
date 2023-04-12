b5f77c03700e4e2706c2c5d3c604b4b91e2ff0b9
Shreyas Goenka <shreyas.goenka@databricks.com>
2023-04-12 23:03:03 +0200

    add url progress logging'

---
+++ bundle/run/pipeline/events.go
@@ -0,0 +1,23 @@
+package pipeline
+
+import "fmt"
+
+type UpdateUrlEvent struct {
+	Type       string `json:"type"`
+	UpdateId   string `json:"update_id"`
+	PipelineId string `json:"pipeline_id"`
+	Url        string `json:"url"`
+}
+
+func NewUpdateUrlEvent(host, updateId, pipelineId string) *UpdateUrlEvent {
+	return &UpdateUrlEvent{
+		Type:       "update_url",
+		UpdateId:   updateId,
+		PipelineId: pipelineId,
+		Url:        fmt.Sprintf("%s/#joblist/pipelines/%s/updates/%s", host, pipelineId, updateId),
+	}
+}
+
+func (event *UpdateUrlEvent) String() string {
+	return fmt.Sprintf("The update can be found at %s\n", event.Url)
+}
