3894d5796d57f0677611a2038cc8e38397e42a24
shreyas-goenka <88374338+shreyas-goenka@users.noreply.github.com>
2023-04-14 11:11:30 +0200

    Add progress logging event for pipeline update URLs (#331)
    ## Changes
    <!-- Summary of your changes that are easy to understand -->
    Output now: 
    ```
    shreyas.goenka@THW32HFW6T pipeline-progress % bricks bundle run foo
    The update can be found at https://e2-dogfood.staging.cloud.databricks.com/#joblist/pipelines/1cc605db-daab-4218-b38a-a63030e3eb03/updates/f92f2159-1141-47de-b1e2-1ca854b7238f
    
    2023-04-12T20:41:19.813Z update_progress INFO "Update f92f21 is INITIALIZING."
    2023-04-12T20:41:19.841Z update_progress INFO "Update f92f21 is SETTING_UP_TABLES."
    2023-04-12T20:41:21.270Z update_progress INFO "Update f92f21 is RUNNING."
    2023-04-12T20:41:21.271Z flow_progress   INFO "Flow 'sales_orders_raw' is QUEUED."
    2023-04-12T20:41:21.349Z flow_progress   INFO "Flow 'sales_orders_raw' is STARTING."
    2023-04-12T20:41:21.480Z flow_progress   INFO "Flow 'sales_orders_raw' is RUNNING."
    2023-04-12T20:41:23.493Z flow_progress   INFO "Flow 'sales_orders_raw' has COMPLETED."
    2023-04-12T20:41:25.484Z update_progress INFO "Update f92f21 is COMPLETED."
    ```
    
    ## Tests
    <!-- How is this tested? -->

--- bundle/run/pipeline.go
+++ bundle/run/pipeline.go
@@ -172,8 +172,7 @@ func (r *pipelineRunner) Run(ctx context.Context, opts *Options) (RunOutput, err
 	}
 
 	// Log the pipeline update URL as soon as it is available.
-	updateUrl := fmt.Sprintf("%s/#joblist/pipelines/%s/updates/%s", w.Config.Host, pipelineID, updateID)
-	log.Infof(ctx, "Update available at %s", updateUrl)
+	progressLogger.Log(pipeline.NewUpdateUrlEvent(w.Config.Host, updateID, pipelineID))
 
 	// Poll update for completion and post status.
 	// Note: there is no "StartUpdateAndWait" wrapper for this API.
