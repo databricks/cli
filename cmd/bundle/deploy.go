adca97e56bc675980182e7ab7f7e9d0df94d152f
Shreyas Goenka <shreyas.goenka@databricks.com>
2023-04-18 15:59:47 +0200

    remove logger mode modification

--- cmd/bundle/deploy.go
+++ cmd/bundle/deploy.go
@@ -1,13 +1,9 @@
 package bundle
 
 import (
-	"fmt"
-
 	"github.com/databricks/bricks/bundle"
 	"github.com/databricks/bricks/bundle/phases"
 	"github.com/databricks/bricks/cmd/root"
-	"github.com/databricks/bricks/libs/cmdio"
-	"github.com/databricks/bricks/libs/flags"
 	"github.com/spf13/cobra"
 )
 
@@ -17,22 +13,12 @@ var deployCmd = &cobra.Command{
 
 	PreRunE: root.MustConfigureBundle,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		ctx := cmd.Context()
-		b := bundle.Get(ctx)
+		b := bundle.Get(cmd.Context())
 
 		// If `--force` is specified, force acquisition of the deployment lock.
 		b.Config.Bundle.Lock.Force = force
 
-		// TODO: remove once state for inplace support is moved to events
-		logger, ok := cmdio.FromContext(ctx)
-		if !ok {
-			return fmt.Errorf("progress logger not found")
-		}
-		if logger.Mode == flags.ModeInplace {
-			logger.Mode = flags.ModeAppend
-		}
-
-		return bundle.Apply(ctx, b, []bundle.Mutator{
+		return bundle.Apply(cmd.Context(), b, []bundle.Mutator{
 			phases.Initialize(),
 			phases.Build(),
 			phases.Deploy(),
