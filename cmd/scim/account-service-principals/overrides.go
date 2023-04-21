ea65606b32e6ea267b31e4d1b6b0cc56b3d39ed5
Serge Smertin <serge.smertin@databricks.com>
2023-04-21 12:24:08 +0200

    some renames

--- cmd/scim/account-service-principals/overrides.go
+++
@@ -1,9 +0,0 @@
-package account_service_principals
-
-import "github.com/databricks/bricks/lib/ui"
-
-func init() {
-	listCmd.Annotations["template"] = ui.Heredoc(`
-	{{range .}}{{.Id|green}}	{{.ApplicationId}}	{{.DisplayName}}	{{range .Roles}}{{.Display}} {{end}}	{{if .Active}}{{"ACTIVE"|green}}{{else}}DISABLED{{end}}
-	{{end}}`)
-}
