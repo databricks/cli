83268ffbda4d3f90eea9dfe00572faf7877c460f
Serge Smertin <serge.smertin@databricks.com>
2023-04-21 12:34:09 +0200

    progress

--- cmd/dbfs/overrides.go
+++
@@ -1,9 +0,0 @@
-package dbfs
-
-import "github.com/databricks/bricks/lib/ui"
-
-func init() {
-	listCmd.Annotations["template"] = ui.Heredoc(`
-	{{range .}}{{if .IsDir}}📂{{else}}📄{{end}}	{{.FileSize}}	{{.Path|green}}
-	{{end}}`)
-}
