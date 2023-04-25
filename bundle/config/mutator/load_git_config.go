4c821e651547922ddcd2fd3fcd90210af66e19fa
Shreyas Goenka <shreyas.goenka@databricks.com>
2023-04-25 16:16:27 +0200

    do not fail on loading git config error

--- bundle/config/mutator/load_git_config.go
+++ bundle/config/mutator/load_git_config.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/databricks/bricks/bundle"
 	"github.com/databricks/bricks/libs/git"
+	"github.com/databricks/bricks/libs/log"
 )
 
 type loadGitConfig struct{}
@@ -17,7 +18,7 @@ func (m *loadGitConfig) Name() string {
 	return "LoadGitConfig"
 }
 
-func (m *loadGitConfig) Apply(_ context.Context, b *bundle.Bundle) ([]bundle.Mutator, error) {
+func (m *loadGitConfig) Apply(ctx context.Context, b *bundle.Bundle) ([]bundle.Mutator, error) {
 	// Load relevant git repository
 	repo, err := git.NewRepository(b.Config.Path)
 	if err != nil {
@@ -27,25 +28,28 @@ func (m *loadGitConfig) Apply(_ context.Context, b *bundle.Bundle) ([]bundle.Mut
 	if b.Config.Bundle.Git.Branch == "" {
 		branch, err := repo.CurrentBranch()
 		if err != nil {
-			return nil, err
+			log.Warnf(ctx, "failed to load current branch: %s", err)
+		} else {
+			b.Config.Bundle.Git.Branch = branch
 		}
-		b.Config.Bundle.Git.Branch = branch
 	}
 	// load commit hash if undefined
 	if b.Config.Bundle.Git.Commit == "" {
 		commit, err := repo.LatestCommit()
 		if err != nil {
-			return nil, err
+			log.Warnf(ctx, "failed to load latest commit: %s", err)
+		} else {
+			b.Config.Bundle.Git.Commit = commit
 		}
-		b.Config.Bundle.Git.Commit = commit
 	}
 	// load origin url if undefined
 	if b.Config.Bundle.Git.RemoteURL == "" {
 		remoteUrl, err := repo.OriginUrl()
 		if err != nil {
-			return nil, err
+			log.Warnf(ctx, "failed to load remote url: %s", err)
+		} else {
+			b.Config.Bundle.Git.RemoteURL = remoteUrl
 		}
-		b.Config.Bundle.Git.RemoteURL = remoteUrl
 	}
 	return nil, nil
 }
