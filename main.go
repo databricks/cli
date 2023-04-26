66c6eef3e54b9f084707f85a51fc0e28a0ffea01
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2023-04-26 08:10:35 +0200

    Reinstate configure command (#361)
    It was removed by accident in #321.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	_ "github.com/databricks/bricks/cmd/auth"
 	_ "github.com/databricks/bricks/cmd/bundle"
 	_ "github.com/databricks/bricks/cmd/bundle/debug"
+	_ "github.com/databricks/bricks/cmd/configure"
 	_ "github.com/databricks/bricks/cmd/fs"
 	"github.com/databricks/bricks/cmd/root"
 	_ "github.com/databricks/bricks/cmd/sync"
