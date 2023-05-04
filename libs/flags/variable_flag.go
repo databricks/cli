4f2307de163d1689c9ac2dd4c415a943f930116d
Shreyas Goenka <shreyas.goenka@databricks.com>
2023-05-04 14:14:03 +0200

    -

--- libs/flags/variable_flag.go
+++
@@ -1,9 +0,0 @@
-package flags
-
-// import "github.com/spf13/cobra"
-
-// const variableFlagDescription =
-
-// func AddVariableFlag(cmd *cobra.Command, p *[]string) {
-// 	cmd.Flags().StringSliceVar(p, "var", []string{}, variableFlagDescription)
-// }
