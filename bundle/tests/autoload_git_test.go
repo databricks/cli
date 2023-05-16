6b743c2eff87d170f926521626b3624428d0f108
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2023-05-16 18:56:27 +0200

    Fix tests for new repository name

--- bundle/tests/autoload_git_test.go
+++ bundle/tests/autoload_git_test.go
@@ -9,7 +9,7 @@ import (
 func TestGitConfig(t *testing.T) {
 	b := load(t, "./autoload_git")
 	assert.Equal(t, "foo", b.Config.Bundle.Git.Branch)
-	sshUrl := "git@github.com:databricks/bricks.git"
-	httpsUrl := "https://github.com/databricks/bricks"
+	sshUrl := "git@github.com:databricks/cli.git"
+	httpsUrl := "https://github.com/databricks/cli"
 	assert.Contains(t, []string{sshUrl, httpsUrl}, b.Config.Bundle.Git.OriginURL)
 }
