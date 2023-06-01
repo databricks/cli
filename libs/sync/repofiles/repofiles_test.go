5f4b8fbd6d94d25353cb337853f63d5f2527ed44
Shreyas Goenka <shreyas.goenka@databricks.com>
2023-06-02 00:40:18 +0200

    -

--- libs/sync/repofiles/repofiles_test.go
+++ libs/sync/repofiles/repofiles_test.go
@@ -14,66 +14,66 @@ func TestRepoFilesRemotePath(t *testing.T) {
 	repoFiles, err := Create(repoRoot, "/doraemon/foo/bar", nil, nil)
 	require.NoError(t, err)
 
-	remotePath, err := repoFiles.RemotePath("a/b/c")
+	remotePath, err := repoFiles.remotePath("a/b/c")
 	assert.NoError(t, err)
 	assert.Equal(t, repoRoot+"/a/b/c", remotePath)
 
-	remotePath, err = repoFiles.RemotePath("a/b/../d")
+	remotePath, err = repoFiles.remotePath("a/b/../d")
 	assert.NoError(t, err)
 	assert.Equal(t, repoRoot+"/a/d", remotePath)
 
-	remotePath, err = repoFiles.RemotePath("a/../c")
+	remotePath, err = repoFiles.remotePath("a/../c")
 	assert.NoError(t, err)
 	assert.Equal(t, repoRoot+"/c", remotePath)
 
-	remotePath, err = repoFiles.RemotePath("a/b/c/.")
+	remotePath, err = repoFiles.remotePath("a/b/c/.")
 	assert.NoError(t, err)
 	assert.Equal(t, repoRoot+"/a/b/c", remotePath)
 
-	remotePath, err = repoFiles.RemotePath("a/b/c/d/./../../f/g")
+	remotePath, err = repoFiles.remotePath("a/b/c/d/./../../f/g")
 	assert.NoError(t, err)
 	assert.Equal(t, repoRoot+"/a/b/f/g", remotePath)
 
-	_, err = repoFiles.RemotePath("..")
+	_, err = repoFiles.remotePath("..")
 	assert.ErrorContains(t, err, `relative file path is not inside repo root: ..`)
 
-	_, err = repoFiles.RemotePath("a/../..")
+	_, err = repoFiles.remotePath("a/../..")
 	assert.ErrorContains(t, err, `relative file path is not inside repo root: a/../..`)
 
-	_, err = repoFiles.RemotePath("./../.")
+	_, err = repoFiles.remotePath("./../.")
 	assert.ErrorContains(t, err, `relative file path is not inside repo root: ./../.`)
 
-	_, err = repoFiles.RemotePath("/./.././..")
+	_, err = repoFiles.remotePath("/./.././..")
 	assert.ErrorContains(t, err, `relative file path is not inside repo root: /./.././..`)
 
-	_, err = repoFiles.RemotePath("./../.")
+	_, err = repoFiles.remotePath("./../.")
 	assert.ErrorContains(t, err, `relative file path is not inside repo root: ./../.`)
 
-	_, err = repoFiles.RemotePath("./..")
+	_, err = repoFiles.remotePath("./..")
 	assert.ErrorContains(t, err, `relative file path is not inside repo root: ./..`)
 
-	_, err = repoFiles.RemotePath("./../../..")
+	_, err = repoFiles.remotePath("./../../..")
 	assert.ErrorContains(t, err, `relative file path is not inside repo root: ./../../..`)
 
-	_, err = repoFiles.RemotePath("./../a/./b../../..")
+	_, err = repoFiles.remotePath("./../a/./b../../..")
 	assert.ErrorContains(t, err, `relative file path is not inside repo root: ./../a/./b../../..`)
 
-	_, err = repoFiles.RemotePath("../..")
+	_, err = repoFiles.remotePath("../..")
 	assert.ErrorContains(t, err, `relative file path is not inside repo root: ../..`)
 
-	_, err = repoFiles.RemotePath(".//a/..//./b/..")
+	_, err = repoFiles.remotePath(".//a/..//./b/..")
 	assert.ErrorContains(t, err, `file path relative to repo root cannot be empty`)
 
-	_, err = repoFiles.RemotePath("a/b/../..")
+	_, err = repoFiles.remotePath("a/b/../..")
 	assert.ErrorContains(t, err, "file path relative to repo root cannot be empty")
 
-	_, err = repoFiles.RemotePath("")
+	_, err = repoFiles.remotePath("")
 	assert.ErrorContains(t, err, "file path relative to repo root cannot be empty")
 
-	_, err = repoFiles.RemotePath(".")
+	_, err = repoFiles.remotePath(".")
 	assert.ErrorContains(t, err, "file path relative to repo root cannot be empty")
 
-	_, err = repoFiles.RemotePath("/")
+	_, err = repoFiles.remotePath("/")
 	assert.ErrorContains(t, err, "file path relative to repo root cannot be empty")
 }
 
