25aa5b667cb93309d6cc0b4716ae6e49b037e1c9
Shreyas Goenka <shreyas.goenka@databricks.com>
2023-06-05 15:42:56 +0200

    -

--- cmd/fs/rm.go
+++ cmd/fs/rm.go
@@ -2,13 +2,12 @@ package fs
 
 import (
 	"github.com/databricks/cli/cmd/root"
-	"github.com/databricks/cli/libs/filer"
 	"github.com/databricks/databricks-sdk-go/service/files"
 	"github.com/spf13/cobra"
 )
 
 var rmCmd = &cobra.Command{
-	Use:     "rm <dir-name>",
+	Use:     "rm DIR_PATH",
 	Short:   "Remove files and directories from dbfs.",
 	Long:    `Remove files and directories from dbfs.`,
 	Args:    cobra.ExactArgs(1),
@@ -18,7 +17,7 @@ var rmCmd = &cobra.Command{
 		ctx := cmd.Context()
 		w := root.WorkspaceClient(ctx)
 
-		path, err := filer.ResolveDbfsPath(args[0])
+		path, err := resolveDbfsPath(args[0])
 		if err != nil {
 			return err
 		}
