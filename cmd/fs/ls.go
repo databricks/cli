19a20512e9870859113f1c28755d39c7695e7fd5
Shreyas Goenka <shreyas.goenka@databricks.com>
2023-06-05 13:49:33 +0200

    comments

--- cmd/fs/ls.go
+++ cmd/fs/ls.go
@@ -1,13 +1,36 @@
 package fs
 
 import (
+	"io/fs"
 	"sort"
+	"time"
 
 	"github.com/databricks/cli/cmd/root"
 	"github.com/databricks/cli/libs/cmdio"
 	"github.com/databricks/cli/libs/filer"
 	"github.com/spf13/cobra"
 )
+
+type jsonDirEntry struct {
+	Name    string    `json:"name"`
+	IsDir   bool      `json:"is_directory"`
+	Size    int64     `json:"size"`
+	ModTime time.Time `json:"last_modified"`
+}
+
+func toJsonDirEntry(f fs.DirEntry) (*jsonDirEntry, error) {
+	info, err := f.Info()
+	if err != nil {
+		return nil, err
+	}
+
+	return &jsonDirEntry{
+		Name:    f.Name(),
+		IsDir:   f.IsDir(),
+		Size:    info.Size(),
+		ModTime: info.ModTime(),
+	}, nil
+}
 
 // lsCmd represents the ls command
 var lsCmd = &cobra.Command{
@@ -17,10 +40,6 @@ var lsCmd = &cobra.Command{
 	Args:    cobra.ExactArgs(1),
 	PreRunE: root.MustWorkspaceClient,
 	Annotations: map[string]string{
-		"template_long": cmdio.Heredoc(`
-		{{range .}}{{if .IsDir}}DIRECTORY {{else}}FILE      {{end}}{{.Size}} {{.ModTime|pretty_date}} {{.Name}}
-		{{end}}
-		`),
 		"template": cmdio.Heredoc(`
 		{{range .}}{{.Name}}
 		{{end}}
@@ -46,9 +65,9 @@ var lsCmd = &cobra.Command{
 			return err
 		}
 
-		lsOutputs := make([]lsOutput, 0)
+		lsOutputs := make([]jsonDirEntry, 0)
 		for _, entry := range entries {
-			parsedEntry, err := toLsOutput(entry)
+			parsedEntry, err := toJsonDirEntry(entry)
 			if err != nil {
 				return err
 			}
@@ -60,7 +79,10 @@ var lsCmd = &cobra.Command{
 
 		// Use template for long mode if the flag is set
 		if longMode {
-			return cmdio.RenderWithTemplate(ctx, lsOutputs, "template_long")
+			return cmdio.RenderWithTemplate(ctx, lsOutputs, cmdio.Heredoc(`
+			{{range .}}{{if .IsDir}}DIRECTORY {{else}}FILE      {{end}}{{.Size}} {{.ModTime|pretty_date}} {{.Name}}
+			{{end}}
+			`))
 		}
 		return cmdio.Render(ctx, lsOutputs)
 	},
