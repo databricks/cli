f14cebae4813aeea0f1ed5a1296958529944ccb7
Shreyas Goenka <shreyas.goenka@databricks.com>
2023-06-05 02:36:10 +0200

    Add fs mkdirs command for dbfs

---
+++ cmd/fs/mkdirs.go
@@ -0,0 +1,36 @@
+package fs
+
+import (
+	"github.com/databricks/cli/cmd/root"
+	"github.com/databricks/cli/libs/filer"
+	"github.com/spf13/cobra"
+)
+
+var mkdirsCmd = &cobra.Command{
+	Use:     "mkdirs <dir-path>",
+	Short:   "Make directories",
+	Long:    `Mkdirs will create directories along the path to the argument directory.`,
+	Args:    cobra.ExactArgs(1),
+	PreRunE: root.MustWorkspaceClient,
+
+	RunE: func(cmd *cobra.Command, args []string) error {
+		ctx := cmd.Context()
+		w := root.WorkspaceClient(ctx)
+
+		path, err := filer.ResolveDbfsPath(args[0])
+		if err != nil {
+			return err
+		}
+
+		f, err := filer.NewDbfsClient(w, "/")
+		if err != nil {
+			return err
+		}
+
+		return f.Mkdir(ctx, path)
+	},
+}
+
+func init() {
+	fsCmd.AddCommand(mkdirsCmd)
+}
