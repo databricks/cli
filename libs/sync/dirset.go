16bb2241087d8a003d6bc650ea81516fa893b90f
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2023-06-12 13:44:00 +0200

    Add directory tracking to sync (#425)
    ## Changes
    
    This change replaces usage of the `repofiles` package with the `filer`
    package to consolidate WSFS code paths.
    
    The `repofiles` package implemented the following behavior. If a file at
    `foo/bar.txt` was created and removed, the directory `foo` was kept
    around because we do not perform directory tracking. If subsequently, a
    file at `foo` was created, it resulted in an `fs.ErrExist` because it is
    impossible to overwrite a directory. It would then perform a recursive
    delete of the path if this happened and retry the file write.
    
    To make this use case work without resorting to a recursive delete on
    conflict, we need to implement directory tracking as part of sync. The
    approach in this commit is as follows:
    
    1. Maintain set of directories needed for current set of files. Compare
    to previous set of files. This results in mkdir of added directories and
    rmdir of removed directories.
    2. Creation of new directories should happen prior to writing files.
    Otherwise, many file writes may race to create the same parent
    directories, resulting in additional API calls. Removal of existing
    directories should happen after removing files.
    3. Making new directories can be deduped across common prefixes where
    only the longest prefix is created recursively.
    4. Removing existing directories must happen sequentially, starting with
    the longest prefix.
    5. Removal of directories is a best effort. It fails only if the
    directory is not empty, and if this happens we know something placed a
    file or directory manually, outside of sync.
    
    ## Tests
    
    * Existing integration tests pass (modified where it used to assert
    directories weren't cleaned up)
    * New integration test to confirm the inability to remove a directory
    doesn't fail the sync run

---
+++ libs/sync/dirset.go
@@ -0,0 +1,54 @@
+package sync
+
+import (
+	"path"
+	"path/filepath"
+	"sort"
+)
+
+// DirSet is a set of directories.
+type DirSet map[string]struct{}
+
+// MakeDirSet turns a list of file paths into the complete set of directories
+// that is needed to store them (including parent directories).
+func MakeDirSet(files []string) DirSet {
+	out := map[string]struct{}{}
+
+	// Iterate over all files.
+	for _, f := range files {
+		// Get the directory of the file in /-separated form.
+		dir := filepath.ToSlash(filepath.Dir(f))
+
+		// Add this directory and its parents until it is either "." or already in the set.
+		for dir != "." {
+			if _, ok := out[dir]; ok {
+				break
+			}
+			out[dir] = struct{}{}
+			dir = path.Dir(dir)
+		}
+	}
+
+	return out
+}
+
+// Slice returns a sorted copy of the dirset elements as a slice.
+func (dirset DirSet) Slice() []string {
+	out := make([]string, 0, len(dirset))
+	for dir := range dirset {
+		out = append(out, dir)
+	}
+	sort.Strings(out)
+	return out
+}
+
+// Remove returns the set difference of two DirSets.
+func (dirset DirSet) Remove(other DirSet) DirSet {
+	out := map[string]struct{}{}
+	for dir := range dirset {
+		if _, ok := other[dir]; !ok {
+			out[dir] = struct{}{}
+		}
+	}
+	return out
+}
