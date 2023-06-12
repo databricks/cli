6abe944905ec85a663bba885b5e69c8615e29703
Shreyas Goenka <shreyas.goenka@databricks.com>
2023-06-12 18:24:09 +0200

    -

--- cmd/fs/cp.go
+++ cmd/fs/cp.go
@@ -2,28 +2,52 @@ package fs
 
 import (
 	"context"
+	"errors"
 	"io/fs"
 	"path"
 	"path/filepath"
 
 	"github.com/databricks/cli/cmd/root"
+	"github.com/databricks/cli/libs/cmdio"
 	"github.com/databricks/cli/libs/filer"
 	"github.com/spf13/cobra"
 )
 
-func dbfsWriteCallback(ctx context.Context, sourceFiler filer.Filer, sourceDir, targetDir string) func(string, fs.DirEntry, error) error {
-	return func(sourcePath string, d fs.DirEntry, err error) error {
+func cpWriteCallback(ctx context.Context, sourceFiler, targetFiler filer.Filer, sourceDir, targetDir string) func(string, fs.DirEntry, error) error {
+	return func(relPath string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
 
-		sourceName := 
+		sourcePath := path.Join(sourceDir, relPath)
+		targetPath := filepath.Join(targetDir, relPath)
 
+		// create directory and return early
 		if d.IsDir() {
-
+			return targetFiler.Mkdir(ctx, relPath)
 		}
+
+		// get reader for source file
+		r, err := sourceFiler.Read(ctx, relPath)
+		if err != nil {
+			return err
+		}
+
+		// write to target file
+		err = targetFiler.Write(ctx, relPath, r)
+		if errors.Is(err, fs.ErrExist) {
+			fileSkippedEvent := newFileSkippedEvent(sourcePath, targetPath)
+			template := "{{.SourcePath}} -> {{.TargetPath}} (skipped; already exists)\n"
+			return cmdio.RenderWithTemplate(ctx, fileSkippedEvent, template)
+		}
+
 	}
 }
+
+var cpOverwrite bool
+var cpRecursive bool
+
+// TODO: error out if source is a directory and recursive is not specified
 
 // cpCmd represents the fs cp command
 var cpCmd = &cobra.Command{
@@ -39,5 +63,7 @@ var cpCmd = &cobra.Command{
 }
 
 func init() {
+	cpCmd.Flags().BoolVar(&cpOverwrite, "overwrite", false, "overwrite existing files")
+	cpCmd.Flags().BoolVarP(&cpRecursive, "recursive", "r", false, "recursively copy files from directory")
 	fsCmd.AddCommand(cpCmd)
 }
