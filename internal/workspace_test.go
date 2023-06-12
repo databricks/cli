ba065f087ef627c7709adf19568687de29aa06b0
Shreyas Goenka <shreyas.goenka@databricks.com>
2023-06-12 15:13:27 +0200

    -

--- internal/workspace_test.go
+++ internal/workspace_test.go
@@ -40,7 +40,7 @@ func TestWorkpaceGetStatusErrorWhenNoArguments(t *testing.T) {
 }
 
 func setupWorkspaceImportExportTest(t *testing.T) (context.Context, filer.Filer, string) {
-	// t.Log(GetEnvOrSkipTest(t, "CLOUD_ENV"))
+	t.Log(GetEnvOrSkipTest(t, "CLOUD_ENV"))
 
 	ctx := context.Background()
 	w := databricks.Must(databricks.NewWorkspaceClient())
