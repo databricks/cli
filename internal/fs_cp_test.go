388f4cdb23eb2195ad33b27c158b1bfc123e9d52
Shreyas Goenka <shreyas.goenka@databricks.com>
2023-06-14 16:21:52 +0200

    tablize testS

--- internal/fs_cp_test.go
+++ internal/fs_cp_test.go
@@ -78,140 +78,195 @@ func setupDbfsFiler(t *testing.T) (filer.Filer, string) {
 	return f, "dbfs:" + tmpDir
 }
 
-func TestFsCpDirLocalToDbfs(t *testing.T) {
-	ctx := context.Background()
-	sourceFiler, sourceDir := setupLocalFiler(t)
-	targetFiler, targetDir := setupDbfsFiler(t)
-	setupSourceDir(t, ctx, sourceFiler)
-
-	RequireSuccessfulRun(t, "fs", "cp", "-r", sourceDir, targetDir)
-
-	assertTargetDir(t, ctx, targetFiler)
+type cpTest struct {
+	setupSource func(*testing.T) (filer.Filer, string)
+	setupTarget func(*testing.T) (filer.Filer, string)
 }
 
-func TestFsCpDirDbfsToDbfs(t *testing.T) {
-	ctx := context.Background()
-	sourceFiler, sourceDir := setupDbfsFiler(t)
-	targetFiler, targetDir := setupDbfsFiler(t)
-	setupSourceDir(t, ctx, sourceFiler)
-
-	RequireSuccessfulRun(t, "fs", "cp", "-r", sourceDir, targetDir)
-
-	assertTargetDir(t, ctx, targetFiler)
+func setupTable() []cpTest {
+	return []cpTest{
+		{setupSource: setupLocalFiler, setupTarget: setupLocalFiler},
+		{setupSource: setupLocalFiler, setupTarget: setupDbfsFiler},
+		{setupSource: setupDbfsFiler, setupTarget: setupLocalFiler},
+		{setupSource: setupDbfsFiler, setupTarget: setupDbfsFiler},
+	}
 }
 
-func TestFsCpDirDbfsToLocal(t *testing.T) {
+func TestFsCpDir(t *testing.T) {
 	ctx := context.Background()
-	sourceFiler, sourceDir := setupDbfsFiler(t)
-	targetFiler, targetDir := setupLocalFiler(t)
-	setupSourceDir(t, ctx, sourceFiler)
+	table := setupTable()
 
-	RequireSuccessfulRun(t, "fs", "cp", "-r", sourceDir, targetDir)
+	for _, row := range table {
+		sourceFiler, sourceDir := row.setupSource(t)
+		targetFiler, targetDir := row.setupTarget(t)
+		setupSourceDir(t, ctx, sourceFiler)
 
-	assertTargetDir(t, ctx, targetFiler)
+		RequireSuccessfulRun(t, "fs", "cp", "-r", sourceDir, targetDir)
+
+		assertTargetDir(t, ctx, targetFiler)
+	}
 }
 
-func TestFsCpDirLocalToLocal(t *testing.T) {
+func TestFsCpFileToFile(t *testing.T) {
 	ctx := context.Background()
-	sourceFiler, sourceDir := setupLocalFiler(t)
-	targetFiler, targetDir := setupLocalFiler(t)
-	setupSourceDir(t, ctx, sourceFiler)
+	table := setupTable()
 
-	RequireSuccessfulRun(t, "fs", "cp", "-r", sourceDir, targetDir)
+	for _, row := range table {
+		sourceFiler, sourceDir := row.setupSource(t)
+		targetFiler, targetDir := row.setupTarget(t)
+		setupSourceFile(t, ctx, sourceFiler)
 
-	assertTargetDir(t, ctx, targetFiler)
+		RequireSuccessfulRun(t, "fs", "cp", path.Join(sourceDir, "foo.txt"), path.Join(targetDir, "bar.txt"))
+
+		assertTargetFile(t, ctx, targetFiler, "bar.txt")
+	}
+}
+
+func TestFsCpFileToDir(t *testing.T) {
+	ctx := context.Background()
+	table := setupTable()
+	for _, row := range table {
+		sourceFiler, sourceDir := row.setupSource(t)
+		targetFiler, targetDir := row.setupTarget(t)
+		setupSourceFile(t, ctx, sourceFiler)
+
+		RequireSuccessfulRun(t, "fs", "cp", path.Join(sourceDir, "foo.txt"), targetDir)
+
+		assertTargetFile(t, ctx, targetFiler, "foo.txt")
+	}
+}
+
+func TestFsCpDirToDirFileNotOverwritten(t *testing.T) {
+	ctx := context.Background()
+	table := setupTable()
+
+	for _, row := range table {
+		sourceFiler, sourceDir := row.setupSource(t)
+		targetFiler, targetDir := row.setupTarget(t)
+		setupSourceDir(t, ctx, sourceFiler)
+
+		// Write a conflicting file to target
+		err := targetFiler.Write(ctx, "a/b/c/hello.txt", strings.NewReader("this should not be overwritten"), filer.CreateParentDirectories)
+		require.NoError(t, err)
+
+		RequireSuccessfulRun(t, "fs", "cp", sourceDir, targetDir, "--recursive")
+		assertFileContent(t, ctx, targetFiler, "a/b/c/hello.txt", "this should not be overwritten")
+		assertFileContent(t, ctx, targetFiler, "query.sql", "SELECT 1")
+		assertFileContent(t, ctx, targetFiler, "pyNb.py", "# Databricks notebook source\nprint(123)")
+	}
+}
+
+func TestFsCpFileToDirFileNotOverwritten(t *testing.T) {
+	ctx := context.Background()
+	table := setupTable()
+
+	for _, row := range table {
+		sourceFiler, sourceDir := row.setupSource(t)
+		targetFiler, targetDir := row.setupTarget(t)
+		setupSourceDir(t, ctx, sourceFiler)
+
+		// Write a conflicting file to target
+		err := targetFiler.Write(ctx, "a/b/c/hello.txt", strings.NewReader("this should not be overwritten"), filer.CreateParentDirectories)
+		require.NoError(t, err)
+
+		RequireSuccessfulRun(t, "fs", "cp", path.Join(sourceDir, "a/b/c/hello.txt"), path.Join(targetDir, "a/b/c"))
+		assertFileContent(t, ctx, targetFiler, "a/b/c/hello.txt", "this should not be overwritten")
+	}
+}
+
+func TestFsCpFileToFileFileNotOverwritten(t *testing.T) {
+	ctx := context.Background()
+	table := setupTable()
+
+	for _, row := range table {
+		sourceFiler, sourceDir := row.setupSource(t)
+		targetFiler, targetDir := row.setupTarget(t)
+		setupSourceDir(t, ctx, sourceFiler)
+
+		// Write a conflicting file to target
+		err := targetFiler.Write(ctx, "a/b/c/hola.txt", strings.NewReader("this should not be overwritten"), filer.CreateParentDirectories)
+		require.NoError(t, err)
+
+		RequireSuccessfulRun(t, "fs", "cp", path.Join(sourceDir, "a/b/c/hello.txt"), path.Join(targetDir, "a/b/c/hola.txt"), "--recursive")
+		assertFileContent(t, ctx, targetFiler, "a/b/c/hola.txt", "this should not be overwritten")
+	}
+}
+
+func TestFsCpDirToDirWithOverwriteFlag(t *testing.T) {
+	ctx := context.Background()
+	table := setupTable()
+
+	for _, row := range table {
+		sourceFiler, sourceDir := row.setupSource(t)
+		targetFiler, targetDir := row.setupTarget(t)
+		setupSourceDir(t, ctx, sourceFiler)
+
+		// Write a conflicting file to target
+		err := targetFiler.Write(ctx, "a/b/c/hello.txt", strings.NewReader("this will be overwritten"), filer.CreateParentDirectories)
+		require.NoError(t, err)
+
+		RequireSuccessfulRun(t, "fs", "cp", sourceDir, targetDir, "--recursive", "--overwrite")
+		assertTargetDir(t, ctx, targetFiler)
+	}
+}
+
+func TestFsCpFileToFileWithOverwriteFlag(t *testing.T) {
+	ctx := context.Background()
+	table := setupTable()
+
+	for _, row := range table {
+		sourceFiler, sourceDir := row.setupSource(t)
+		targetFiler, targetDir := row.setupTarget(t)
+		setupSourceDir(t, ctx, sourceFiler)
+
+		// Write a conflicting file to target
+		err := targetFiler.Write(ctx, "a/b/c/hola.txt", strings.NewReader("this will be overwritten. Such is life."), filer.CreateParentDirectories)
+		require.NoError(t, err)
+
+		RequireSuccessfulRun(t, "fs", "cp", path.Join(sourceDir, "a/b/c/hello.txt"), path.Join(targetDir, "a/b/c/hola.txt"), "--overwrite")
+		assertFileContent(t, ctx, targetFiler, "a/b/c/hola.txt", "hello, world\n")
+	}
+}
+
+func TestFsCpFileToDirWithOverwriteFlag(t *testing.T) {
+	ctx := context.Background()
+	table := setupTable()
+
+	for _, row := range table {
+		sourceFiler, sourceDir := row.setupSource(t)
+		targetFiler, targetDir := row.setupTarget(t)
+		setupSourceDir(t, ctx, sourceFiler)
+
+		// Write a conflicting file to target
+		err := targetFiler.Write(ctx, "a/b/c/hello.txt", strings.NewReader("this will be overwritten :') "), filer.CreateParentDirectories)
+		require.NoError(t, err)
+
+		RequireSuccessfulRun(t, "fs", "cp", path.Join(sourceDir, "a/b/c/hello.txt"), path.Join(targetDir, "a/b/c"), "--recursive", "--overwrite")
+		assertFileContent(t, ctx, targetFiler, "a/b/c/hello.txt", "hello, world\n")
+	}
+}
+
+func TestFsCpErrorsWhenSourceIsDirWithoutRecursiveFlag(t *testing.T) {
+	type test struct {
+		path   string
+		scheme string
+	}
+
+	w, err := databricks.NewWorkspaceClient()
+	require.NoError(t, err)
+
+	table := []test{
+		{path: temporaryDbfsDir(t, w), scheme: "dbfs"},
+		{path: t.TempDir(), scheme: "file"},
+	}
+
+	for _, row := range table {
+		_, _, err := RequireErrorRun(t, "fs", "cp", row.scheme+":"+row.path, "file:/a")
+		assert.Equal(t, fmt.Sprintf("source path %s is a directory. Please specify the --recursive flag", row.path), err.Error())
+	}
 }
 
 // TODO: test out all the error cases
-
-func TestFsCpFileDbfsToDbfs(t *testing.T) {
-	ctx := context.Background()
-	sourceFiler, sourceDir := setupDbfsFiler(t)
-	targetFiler, targetDir := setupDbfsFiler(t)
-	setupSourceFile(t, ctx, sourceFiler)
-
-	RequireSuccessfulRun(t, "fs", "cp", path.Join(sourceDir, "foo.txt"), path.Join(targetDir, "bar.txt"))
-
-	assertTargetFile(t, ctx, targetFiler, "bar.txt")
-}
-
-func TestFsCpFileLocalToDbfs(t *testing.T) {
-	ctx := context.Background()
-	sourceFiler, sourceDir := setupLocalFiler(t)
-	targetFiler, targetDir := setupDbfsFiler(t)
-	setupSourceFile(t, ctx, sourceFiler)
-
-	RequireSuccessfulRun(t, "fs", "cp", path.Join(sourceDir, "foo.txt"), path.Join(targetDir, "bar.txt"))
-
-	assertTargetFile(t, ctx, targetFiler, "bar.txt")
-}
-
-func TestFsCpFileDbfsToLocal(t *testing.T) {
-	ctx := context.Background()
-	sourceFiler, sourceDir := setupDbfsFiler(t)
-	targetFiler, targetDir := setupLocalFiler(t)
-	setupSourceFile(t, ctx, sourceFiler)
-
-	RequireSuccessfulRun(t, "fs", "cp", path.Join(sourceDir, "foo.txt"), path.Join(targetDir, "bar.txt"))
-
-	assertTargetFile(t, ctx, targetFiler, "bar.txt")
-}
-
-func TestFsCpFileLocalToLocal(t *testing.T) {
-	ctx := context.Background()
-	sourceFiler, sourceDir := setupLocalFiler(t)
-	targetFiler, targetDir := setupLocalFiler(t)
-	setupSourceFile(t, ctx, sourceFiler)
-
-	RequireSuccessfulRun(t, "fs", "cp", path.Join(sourceDir, "foo.txt"), path.Join(targetDir, "bar.txt"))
-
-	assertTargetFile(t, ctx, targetFiler, "bar.txt")
-}
-
-func TestFsCpFileToDirDbfsToDbfs(t *testing.T) {
-	ctx := context.Background()
-	sourceFiler, sourceDir := setupDbfsFiler(t)
-	targetFiler, targetDir := setupDbfsFiler(t)
-	setupSourceFile(t, ctx, sourceFiler)
-
-	RequireSuccessfulRun(t, "fs", "cp", path.Join(sourceDir, "foo.txt"), targetDir)
-
-	assertTargetFile(t, ctx, targetFiler, "foo.txt")
-}
-
-func TestFsCpFileToDirLocalToDbfs(t *testing.T) {
-	ctx := context.Background()
-	sourceFiler, sourceDir := setupLocalFiler(t)
-	targetFiler, targetDir := setupDbfsFiler(t)
-	setupSourceFile(t, ctx, sourceFiler)
-
-	RequireSuccessfulRun(t, "fs", "cp", path.Join(sourceDir, "foo.txt"), targetDir)
-
-	assertTargetFile(t, ctx, targetFiler, "foo.txt")
-}
-
-func TestFsCpFileToDirDbfsToLocal(t *testing.T) {
-	ctx := context.Background()
-	sourceFiler, sourceDir := setupDbfsFiler(t)
-	targetFiler, targetDir := setupLocalFiler(t)
-	setupSourceFile(t, ctx, sourceFiler)
-
-	RequireSuccessfulRun(t, "fs", "cp", path.Join(sourceDir, "foo.txt"), targetDir)
-
-	assertTargetFile(t, ctx, targetFiler, "foo.txt")
-}
-
-func TestFsCpFileToDirLocalToLocal(t *testing.T) {
-	ctx := context.Background()
-	sourceFiler, sourceDir := setupLocalFiler(t)
-	targetFiler, targetDir := setupLocalFiler(t)
-	setupSourceFile(t, ctx, sourceFiler)
-
-	RequireSuccessfulRun(t, "fs", "cp", path.Join(sourceDir, "foo.txt"), targetDir)
-
-	assertTargetFile(t, ctx, targetFiler, "foo.txt")
-}
-
 // TODO: Test cp works for relative local paths
 
 func TestFsCpErrorsOnNoScheme(t *testing.T) {
@@ -222,106 +277,6 @@ func TestFsCpErrorsOnNoScheme(t *testing.T) {
 func TestFsCpErrorsOnInvalidScheme(t *testing.T) {
 	_, _, err := RequireErrorRun(t, "fs", "cp", "file:/a", "https:/b")
 	assert.Equal(t, "unsupported scheme https specified for path https:/b. Please specify scheme \"dbfs\" or \"file\". Example: file:/foo/bar", err.Error())
-}
-
-func TestFsCpErrorWhenSourceIsDbfsDirWithoutRecursiveFlag(t *testing.T) {
-	w, err := databricks.NewWorkspaceClient()
-	require.NoError(t, err)
-	tmpDir := temporaryDbfsDir(t, w)
-	_, _, err = RequireErrorRun(t, "fs", "cp", "dbfs:"+tmpDir, "file:/a")
-	assert.Equal(t, fmt.Sprintf("source path %s is a directory. Please specify the --recursive flag", tmpDir), err.Error())
-}
-
-func TestFsCpErrorWhenSourceIsLocalDirWithoutRecursiveFlag(t *testing.T) {
-	tmpDir := t.TempDir()
-	_, _, err := RequireErrorRun(t, "fs", "cp", "file:"+tmpDir, "file:/a")
-	assert.Equal(t, fmt.Sprintf("source path %s is a directory. Please specify the --recursive flag", tmpDir), err.Error())
-}
-
-func TestFsCpDbfsDirToDbfsDirFileNotOverwritten(t *testing.T) {
-	ctx := context.Background()
-	sourceFiler, sourceDir := setupDbfsFiler(t)
-	targetFiler, targetDir := setupDbfsFiler(t)
-	setupSourceDir(t, ctx, sourceFiler)
-
-	// Write a conflicting file to target
-	err := targetFiler.Write(ctx, "a/b/c/hello.txt", strings.NewReader("this should not be overwritten"), filer.CreateParentDirectories)
-	require.NoError(t, err)
-
-	RequireSuccessfulRun(t, "fs", "cp", sourceDir, targetDir, "--recursive")
-	assertFileContent(t, ctx, targetFiler, "a/b/c/hello.txt", "this should not be overwritten")
-	assertFileContent(t, ctx, targetFiler, "query.sql", "SELECT 1")
-	assertFileContent(t, ctx, targetFiler, "pyNb.py", "# Databricks notebook source\nprint(123)")
-}
-
-func TestFsCpDbfsDirToDbfsDirFileWithOverwriteFlag(t *testing.T) {
-	ctx := context.Background()
-	sourceFiler, sourceDir := setupDbfsFiler(t)
-	targetFiler, targetDir := setupDbfsFiler(t)
-	setupSourceDir(t, ctx, sourceFiler)
-
-	// Write a conflicting file to target
-	err := targetFiler.Write(ctx, "a/b/c/hello.txt", strings.NewReader("this will be overwritten"), filer.CreateParentDirectories)
-	require.NoError(t, err)
-
-	RequireSuccessfulRun(t, "fs", "cp", sourceDir, targetDir, "--recursive", "--overwrite")
-	assertTargetDir(t, ctx, targetFiler)
-}
-
-func TestFsCpDbfsFileToLocalDirFileNotOverwritten(t *testing.T) {
-	ctx := context.Background()
-	sourceFiler, sourceDir := setupDbfsFiler(t)
-	targetFiler, targetDir := setupLocalFiler(t)
-	setupSourceDir(t, ctx, sourceFiler)
-
-	// Write a conflicting file to target
-	err := targetFiler.Write(ctx, "a/b/c/hello.txt", strings.NewReader("this should not be overwritten"), filer.CreateParentDirectories)
-	require.NoError(t, err)
-
-	RequireSuccessfulRun(t, "fs", "cp", path.Join(sourceDir, "a/b/c/hello.txt"), path.Join(targetDir, "a/b/c"), "--recursive")
-	assertFileContent(t, ctx, targetFiler, "a/b/c/hello.txt", "this should not be overwritten")
-}
-
-func TestFsCpDbfsFileToLocalDirFileWithOverwriteFlag(t *testing.T) {
-	ctx := context.Background()
-	sourceFiler, sourceDir := setupDbfsFiler(t)
-	targetFiler, targetDir := setupLocalFiler(t)
-	setupSourceDir(t, ctx, sourceFiler)
-
-	// Write a conflicting file to target
-	err := targetFiler.Write(ctx, "a/b/c/hello.txt", strings.NewReader("this will be overwritten :') "), filer.CreateParentDirectories)
-	require.NoError(t, err)
-
-	RequireSuccessfulRun(t, "fs", "cp", path.Join(sourceDir, "a/b/c/hello.txt"), path.Join(targetDir, "a/b/c"), "--recursive", "--overwrite")
-	assertFileContent(t, ctx, targetFiler, "a/b/c/hello.txt", "hello, world\n")
-}
-
-func TestFsCpLocalFileToDbfsFileNotOverwritten(t *testing.T) {
-	ctx := context.Background()
-	sourceFiler, sourceDir := setupLocalFiler(t)
-	targetFiler, targetDir := setupDbfsFiler(t)
-	setupSourceDir(t, ctx, sourceFiler)
-
-	// Write a conflicting file to target
-	err := targetFiler.Write(ctx, "a/b/c/hola.txt", strings.NewReader("this should not be overwritten"), filer.CreateParentDirectories)
-	require.NoError(t, err)
-
-	RequireSuccessfulRun(t, "fs", "cp", path.Join(sourceDir, "a/b/c/hello.txt"), path.Join(targetDir, "a/b/c/hola.txt"), "--recursive")
-	assertFileContent(t, ctx, targetFiler, "a/b/c/hola.txt", "this should not be overwritten")
-}
-
-func TestFsCpLocalFileToDbfsFileWithOverwriteFlag(t *testing.T) {
-	ctx := context.Background()
-	sourceFiler, sourceDir := setupDbfsFiler(t)
-	targetFiler, targetDir := setupLocalFiler(t)
-	setupSourceDir(t, ctx, sourceFiler)
-
-	// Write a conflicting file to target
-	err := targetFiler.Write(ctx, "a/b/c/hola.txt", strings.NewReader("this will be overwritten. Such is life."), filer.CreateParentDirectories)
-	require.NoError(t, err)
-
-	RequireSuccessfulRun(t, "fs", "cp", path.Join(sourceDir, "a/b/c/hello.txt"), path.Join(targetDir, "a/b/c/hola.txt"), "--recursive", "--overwrite")
-	assertFileContent(t, ctx, targetFiler, "a/b/c/hola.txt", "hello, world\n")
 }
 
 func TestFsCpSourceIsDirectoryButTargetIsFile(t *testing.T) {
