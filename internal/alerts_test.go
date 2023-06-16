e4060592e3daf0e2bad9c1bad395a17386997f93
Andrew Nester <andrew.nester@databricks.com>
2023-06-16 14:02:24 +0200

    mark the test acceptance

--- internal/alerts_test.go
+++ internal/alerts_test.go
@@ -6,7 +6,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAlertsCreateErrWhenNoArguments(t *testing.T) {
+func TestAccAlertsCreateErrWhenNoArguments(t *testing.T) {
+	t.Log(GetEnvOrSkipTest(t, "CLOUD_ENV"))
+
 	_, _, err := RequireErrorRun(t, "alerts", "create")
 	assert.Equal(t, "provide command input in JSON format by specifying --json option", err.Error())
 }
