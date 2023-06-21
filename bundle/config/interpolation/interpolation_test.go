d3644c3fd502c3341c48ac343fa85af600ec7ae7
Sri Tikkireddy <sri.tikkireddy@databricks.com>
2023-06-21 08:21:16 -0400

    fixed the tests to include explanations on why it fails

--- bundle/config/interpolation/interpolation_test.go
+++ bundle/config/interpolation/interpolation_test.go
@@ -76,30 +76,33 @@ func TestValidMatches(t *testing.T) {
 		"${hello_world.world_world}": "hello_world.world_world",
 		"${helloworld.world-world}":  "helloworld.world-world",
 		"${hello-world.world-world}": "hello-world.world-world",
-		"${a-a.a-_a-a.id}":           "a-a.a-_a-a.id",
 	}
-	for key, expectedMatch := range expectedMatches {
-		match := re.FindStringSubmatch(key)
-		assert.Equal(t, expectedMatch, match[1])
+	for interpolationStr, expectedMatch := range expectedMatches {
+		match := re.FindStringSubmatch(interpolationStr)
+		assert.True(t, len(match) > 0,
+			"Failed to match %s and find %s", interpolationStr, expectedMatch)
+		assert.Equal(t, expectedMatch, match[1],
+			"Failed to match the exact pattern %s and find %s", interpolationStr, expectedMatch)
 	}
 }
 
 func TestInValidMatches(t *testing.T) {
 	invalidMatches := []string{
-		"${hello_world-.world_world}",
-		"${hello_world-_.world_world}",
-		"${helloworld.world-world-}",
-		"${helloworld-.world-world}",
-		"${helloworld.-world-world}",
-		"${-hello-world.-world-world-}",
-		"${_-_._-_.id}",
-		"${0helloworld.world-world}",
-		"${helloworld.9world-world}",
+		"${hello_world-.world_world}",   // the first segment ending must not end with hyphen (-)
+		"${hello_world-_.world_world}",  // the first segment ending must not end with underscore (_)
+		"${helloworld.world-world-}",    // second segment must not end with hyphen (-)
+		"${helloworld-.world-world}",    // first segment must not end with hyphen (-)
+		"${helloworld.-world-world}",    // second segment must not start with hyphen (-)
+		"${-hello-world.-world-world-}", // must not start or end with hyphen (-)
+		"${_-_._-_.id}",                 // cannot use _- in sequence
+		"${0helloworld.world-world}",    // interpolated first section shouldn't start with number
+		"${helloworld.9world-world}",    // interpolated second section shouldn't start with number
+		"${a-a.a-_a-a.id}",              // fails because of -_ in the second segment
+		"${a-a.a--a-a.id}",              // fails because of -- in the second segment
 	}
 	for _, invalidMatch := range invalidMatches {
 		match := re.FindStringSubmatch(invalidMatch)
-		// no match
-		assert.Equal(t, 0, len(match))
+		assert.True(t, len(match) == 0, "Should be invalid interpolation: %s", invalidMatch)
 	}
 }
 
