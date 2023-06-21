b911327d621b74b26c19297d25b1e75dad5dfbf0
Andrew Nester <andrew.nester@databricks.com>
2023-06-21 17:51:59 +0200

    Added configure-cluster flag for auth login (#500)
    ## Changes
    Added configure-cluster flag for auth login which will allow to
    configure cluster ID and save it in Databricks profile
    
    Note: the build will fail until this one is merged and released
    https://github.com/databricks/databricks-sdk-go/pull/524
    
    ## Tests
    
    ```
    andrew.nester@HFW9Y94129 cli % ./cli auth login https://xxxxxxx.databricks.com --configure-cluster
    ✔ Databricks Profile Name: my-profile█
    Search: █
    ? Choose cluster:
      10.1 ML beta (1029-yyyyy-xxxxxx)
      10.5 ML standard cluster
      12.2 LTS
    ↓ 13.1 free for all
    
    
    andrew.nester@HFW9Y94129 cli % cat ~/.databrickscfg
    [DEFAULT]
    host       = https://xxxxx.databricks.com
    cluster_id = 1029-xxxxx-yyyyy
    auth_type  = databricks-cli
    ```

--- cmd/auth/login.go
+++ cmd/auth/login.go
@@ -8,11 +8,14 @@ import (
 	"github.com/databricks/cli/libs/auth"
 	"github.com/databricks/cli/libs/cmdio"
 	"github.com/databricks/cli/libs/databrickscfg"
+	"github.com/databricks/databricks-sdk-go"
 	"github.com/databricks/databricks-sdk-go/config"
+	"github.com/databricks/databricks-sdk-go/service/compute"
 	"github.com/spf13/cobra"
 )
 
 var loginTimeout time.Duration
+var configureCluster bool
 
 var loginCmd = &cobra.Command{
 	Use:   "login [HOST]",
@@ -21,6 +24,7 @@ var loginCmd = &cobra.Command{
 		if perisistentAuth.Host == "" && len(args) == 1 {
 			perisistentAuth.Host = args[0]
 		}
+
 		defer perisistentAuth.Close()
 		ctx, cancel := context.WithTimeout(cmd.Context(), loginTimeout)
 		defer cancel()
@@ -45,13 +49,35 @@ var loginCmd = &cobra.Command{
 			return err
 		}
 
-		err = databrickscfg.SaveToProfile(ctx, &config.Config{
+		cfg := config.Config{
 			Host:      perisistentAuth.Host,
 			AccountID: perisistentAuth.AccountID,
 			AuthType:  "databricks-cli",
 			Profile:   profileName,
-		})
+		}
 
+		if configureCluster {
+			w, err := databricks.NewWorkspaceClient((*databricks.Config)(&cfg))
+			if err != nil {
+				return err
+			}
+			ctx := cmd.Context()
+
+			promptSpinner := cmdio.Spinner(ctx)
+			promptSpinner <- "Loading list of clusters to select from"
+			names, err := w.Clusters.ClusterInfoClusterNameToClusterIdMap(ctx, compute.ListClustersRequest{})
+			close(promptSpinner)
+			if err != nil {
+				return fmt.Errorf("failed to load clusters list. Original error: %w", err)
+			}
+			clusterId, err := cmdio.Select(ctx, names, "Choose cluster")
+			if err != nil {
+				return err
+			}
+			cfg.ClusterID = clusterId
+		}
+
+		err = databrickscfg.SaveToProfile(ctx, &cfg)
 		if err != nil {
 			return err
 		}
@@ -65,4 +91,7 @@ func init() {
 	authCmd.AddCommand(loginCmd)
 	loginCmd.Flags().DurationVar(&loginTimeout, "timeout", auth.DefaultTimeout,
 		"Timeout for completing login challenge in the browser")
+
+	loginCmd.Flags().BoolVar(&configureCluster, "configure-cluster", false,
+		"Prompts to configure cluster")
 }
