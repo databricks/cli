5eddeb42eb63c913f2e547c9b4a14913c804eb49
Shreyas Goenka <shreyas.goenka@databricks.com>
2023-06-30 02:08:06 +0200

    remove private repo download test

--- internal/git_clone_test.go
+++ internal/git_clone_test.go
@@ -6,7 +6,6 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/databricks/cli/libs/cmdio"
 	"github.com/databricks/cli/libs/git"
 	"github.com/stretchr/testify/assert"
 )
@@ -61,28 +60,4 @@ func TestAccGitClonePublicRepositoryForTagReference(t *testing.T) {
 	b, err := os.ReadFile(filepath.Join(tmpDir, "cli-snapshot/NOTICE"))
 	assert.NoError(t, err)
 	assert.Contains(t, string(b), "Copyright (2023) Databricks, Inc.")
-}
-
-// TODO(file an issue before merge): create a dedicated databricks private repository
-// and test this for branches and tags
-func TestAccGitClonePrivateRepository(t *testing.T) {
-	t.Log(GetEnvOrSkipTest(t, "CLOUD_ENV"))
-
-	tmpDir := t.TempDir()
-	cmdIO := cmdio.NewIO("text", os.Stdin, os.Stdout, os.Stderr, "")
-	ctx := cmdio.InContext(context.Background(), cmdIO)
-
-	// This is a private repository only accessible to databricks employees
-	err := git.Clone(ctx, git.CloneOptions{
-		Provider:       "github",
-		Organization:   "databricks",
-		RepositoryName: "bundle-samples-internal",
-		Reference:      "main",
-		TargetDir:      tmpDir,
-	})
-	assert.NoError(t, err)
-
-	// assert examples from the private repository
-	assert.DirExists(t, filepath.Join(tmpDir, "bundle-samples-internal-main", "shark_sightings"))
-	assert.DirExists(t, filepath.Join(tmpDir, "bundle-samples-internal-main", "wikipedia_clickstream"))
 }
