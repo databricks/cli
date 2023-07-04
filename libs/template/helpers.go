4a53e0ce5ab1cb6521692102c811b80baa081e52
Shreyas Goenka <shreyas.goenka@databricks.com>
2023-07-04 16:01:47 +0200

    remove panic from skipThisFile

--- libs/template/helpers.go
+++ libs/template/helpers.go
@@ -8,8 +8,9 @@ import (
 var errSkipThisFile = errors.New("skip generating this file")
 
 var HelperFuncs = template.FuncMap{
+	// Text template execution returns the error only if it's the second return
+	// value from a function: https://pkg.go.dev/text/template#hdr-Pipelines
 	"skipThisFile": func() (any, error) {
 		return nil, errSkipThisFile
-		// panic(errSkipThisFile)
 	},
 }
