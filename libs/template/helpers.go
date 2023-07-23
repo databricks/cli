bb37f60cf299c24d4b1ce02a53c912b5582dfb79
Shreyas Goenka <shreyas.goenka@databricks.com>
2023-07-24 00:44:36 +0200

    added comment

--- libs/template/helpers.go
+++ libs/template/helpers.go
@@ -17,6 +17,8 @@ var helperFuncs = template.FuncMap{
 	"fail": func(format string, args ...any) (any, error) {
 		return nil, ErrFail{fmt.Sprintf(format, args...)}
 	},
+	// Returns the input string as is. Useful for printing text that would otherwise
+	// get interpreted as a template.
 	"raw": func(s string) string {
 		return s
 	},
