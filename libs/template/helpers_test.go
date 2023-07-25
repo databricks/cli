34f196bb4eada73b97d468d121f6fca55317417d
shreyas-goenka <88374338+shreyas-goenka@users.noreply.github.com>
2023-07-25 17:18:43 +0200

    Add unit test that raw strings are printed as is (#599)
    ## Changes
    Add unit test that raw strings are printed as is. This method is useful
    to print text that would otherwise be interpreted a go text template.

--- libs/template/helpers_test.go
+++ libs/template/helpers_test.go
@@ -2,11 +2,27 @@ package template
 
 import (
 	"context"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func TestTemplatePrintStringWithoutProcessing(t *testing.T) {
+	ctx := context.Background()
+	tmpDir := t.TempDir()
+
+	r, err := newRenderer(ctx, nil, "./testdata/print-without-processing/template", "./testdata/print-without-processing/library", tmpDir)
+	require.NoError(t, err)
+
+	err = r.walk()
+	assert.NoError(t, err)
+
+	assert.Len(t, r.files, 1)
+	cleanContent := strings.Trim(string(r.files[0].content), "\n\r")
+	assert.Equal(t, `{{ fail "abc" }}`, cleanContent)
+}
 
 func TestTemplateRegexpCompileFunction(t *testing.T) {
 	ctx := context.Background()
