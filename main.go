6bb4ac0d0e18dac75d50ea2e56a03a3978480c19
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2023-07-26 14:47:12 +0200

    Remove anonymous imports from main.go

--- main.go
+++ main.go
@@ -2,11 +2,7 @@ package main
 
 import (
 	"github.com/databricks/cli/cmd"
-	_ "github.com/databricks/cli/cmd/configure"
-	_ "github.com/databricks/cli/cmd/fs"
 	"github.com/databricks/cli/cmd/root"
-	_ "github.com/databricks/cli/cmd/sync"
-	_ "github.com/databricks/cli/cmd/version"
 )
 
 func main() {
