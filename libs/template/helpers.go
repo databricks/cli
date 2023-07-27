ed972f7ae02979b0b3822871ce10a4755a0502b6
shreyas-goenka <88374338+shreyas-goenka@users.noreply.github.com>
2023-07-27 11:51:31 +0200

    Add url parse helper function for templates (#600)
    ## Tests
    unit test

--- libs/template/helpers.go
+++ libs/template/helpers.go
@@ -2,6 +2,7 @@ package template
 
 import (
 	"fmt"
+	"net/url"
 	"regexp"
 	"text/template"
 )
@@ -17,6 +18,10 @@ func (err ErrFail) Error() string {
 var helperFuncs = template.FuncMap{
 	"fail": func(format string, args ...any) (any, error) {
 		return nil, ErrFail{fmt.Sprintf(format, args...)}
+	},
+	// Alias for https://pkg.go.dev/net/url#Parse. Allows usage of all methods of url.URL
+	"url": func(rawUrl string) (*url.URL, error) {
+		return url.Parse(rawUrl)
 	},
 	// Alias for https://pkg.go.dev/regexp#Compile. Allows usage of all methods of regexp.Regexp
 	"regexp": func(expr string) (*regexp.Regexp, error) {
