1a8e9e7ec0319a57165e4ab51c6d56c57ce48757
monalisa <shreyas.goenka@databricks.com>
2023-07-27 11:34:48 +0200

    -

--- libs/template/helpers_test.go
+++ libs/template/helpers_test.go
@@ -16,6 +16,11 @@ func TestTemplatePrintStringWithoutProcessing(t *testing.T) {
 	r, err := newRenderer(ctx, nil, "./testdata/print-without-processing/template", "./testdata/print-without-processing/library", tmpDir)
 	require.NoError(t, err)
 
+	err = r.walk()
+	assert.NoError(t, err)
+
+	assert.Len(t, r.files, 1)
+
 	cleanContent := strings.Trim(string(r.files[0].content), "\n\r")
 	assert.Equal(t, `{{ fail "abc" }}`, cleanContent)
 }
