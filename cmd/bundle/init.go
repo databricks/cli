e03cc741a71acb1e618bc3f80526c7fae2ce678d
monalisa <shreyas.goenka@databricks.com>
2023-08-03 15:33:58 +0200

    use repo name as the dir name for temp download of tmeplate

--- cmd/bundle/init.go
+++ cmd/bundle/init.go
@@ -15,6 +15,26 @@ var gitUrlPrefixes = []string{
 	"git@",
 }
 
+// TODO: add tests for these functions
+func isRepoUrl(url string) bool {
+	result := false
+	for _, prefix := range gitUrlPrefixes {
+		if strings.HasPrefix(url, prefix) {
+			result = true
+			break
+		}
+	}
+	return result
+}
+
+// Computes the repo name from the repo URL. Treats the last non empty word
+// when splitting at '/' as the repo name. For example: for url git@github.com:databricks/cli.git
+// the name would be "cli.git"
+func repoName(url string) string {
+	parts := strings.Split(strings.TrimRight(url, "/"), "/")
+	return parts[len(parts)-1]
+}
+
 func newInitCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "init TEMPLATE_PATH",
@@ -29,33 +49,28 @@ func newInitCommand() *cobra.Command {
 	cmd.MarkFlagRequired("output-dir")
 
 	cmd.RunE = func(cmd *cobra.Command, args []string) error {
-		templateURL := args[0]
+		templatePathOrURL := args[0]
 		ctx := cmd.Context()
 
-		isRepo := false
-		for _, prefix := range gitUrlPrefixes {
-			if strings.HasPrefix(templateURL, prefix) {
-				isRepo = true
-				break
-			}
-		}
-		if !isRepo {
+		if !isRepoUrl(templatePathOrURL) {
 			// skip downloading the repo because input arg is not a URL. We assume
 			// it's a path on the local file system in that case
-			return template.Materialize(ctx, configFile, templateURL, projectDir)
+			return template.Materialize(ctx, configFile, templatePathOrURL, projectDir)
 		}
 
 		// Download the template in a temporary directory
 		tmpDir := os.TempDir()
-		templateDir := filepath.Join(tmpDir, templateURL)
+		templateDir := filepath.Join(tmpDir, repoName(templatePathOrURL))
 		err := os.MkdirAll(templateDir, 0755)
 		if err != nil {
 			return err
 		}
-		err = git.Clone(ctx, templateURL, "", templateDir)
+		// TODO: test the downloaded repo is cleaned up
+		err = git.Clone(ctx, templatePathOrURL, "", templateDir)
 		if err != nil {
 			return err
 		}
+		defer os.RemoveAll(templateDir)
 
 		return template.Materialize(ctx, configFile, templateDir, projectDir)
 	}
