b101ec280e7b2335b4d2653d8914440d1dafed52
monalisa <shreyas.goenka@databricks.com>
2023-08-07 11:49:02 +0200

    -

--- cmd/bundle/init_test.go
+++ cmd/bundle/init_test.go
@@ -16,10 +16,12 @@ func TestBundleInitIsRepoUrl(t *testing.T) {
 
 func TestBundleInitRepoName(t *testing.T) {
 	// Test valid URLs
-	assert.Equal(t, repoName("git@github.com:databricks/cli.git"), "cli.git")
-	assert.Equal(t, repoName("https://github.com/databricks/cli/"), "cli")
+	assert.Equal(t, "cli.git", repoName("git@github.com:databricks/cli.git"))
+	assert.Equal(t, "cli", repoName("https://github.com/databricks/cli/"))
 
-	// test invalid URLs
+	// test invalid URLs. In these cases the error would be floated when the
+	// git clone operation fails.
 	assert.Equal(t, "git@github.com:databricks", repoName("git@github.com:databricks"))
 	assert.Equal(t, "invalid-url", repoName("invalid-url"))
+	assert.Equal(t, "www.github.com", repoName("https://www.github.com"))
 }
