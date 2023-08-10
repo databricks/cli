2a58253d20ad6d48c55f138f0c92f62544fa7c28
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2023-08-10 11:36:42 +0200

    Consolidate functions in libs/git (#652)
    ## Changes
    
    The functions in `libs/git/git.go` assumed global state (e.g. working
    directory) and were no longer used.
    
    This change consolidates the functionality to turn an origin URL into an
    HTTPS URL.
    
    Closes #187.
    
    ## Tests
    
    Expanded existing unit test.

--- libs/git/utils.go
+++ libs/git/utils.go
@@ -6,23 +6,23 @@ import (
 	giturls "github.com/whilp/git-urls"
 )
 
+// Return an origin URL as an HTTPS URL.
+// The transformations in this function are not guaranteed to work for all
+// Git providers. They are only guaranteed to work for GitHub.
 func ToHttpsUrl(url string) (string, error) {
-	originUrl, err := giturls.Parse(url)
+	origin, err := giturls.Parse(url)
 	if err != nil {
 		return "", err
 	}
-	if originUrl.Scheme == "https" {
-		return originUrl.String(), nil
+	// If this repository is checked out over SSH
+	if origin.Scheme != "https" {
+		origin.Scheme = "https"
 	}
-	// if current repo is checked out with a SSH key
-	if originUrl.Scheme != "https" {
-		originUrl.Scheme = "https"
-	}
-	// `git@` is not required for HTTPS
-	if originUrl.User != nil {
-		originUrl.User = nil
+	// Basic auth is not applicable for an HTTPS URL.
+	if origin.User != nil {
+		origin.User = nil
 	}
 	// Remove `.git` suffix, if present.
-	originUrl.Path = strings.TrimSuffix(originUrl.Path, ".git")
-	return originUrl.String(), nil
+	origin.Path = strings.TrimSuffix(origin.Path, ".git")
+	return origin.String(), nil
 }
