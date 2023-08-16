f0a11e5191566d632637beab2a3ec6e42847e46e
Andrew Nester <andrew.nester@databricks.com>
2023-08-16 15:39:36 +0200

    added glob test

---
+++ libs/fileset/glob_test.go
@@ -0,0 +1,41 @@
+package fileset
+
+import (
+	"io/fs"
+	"os"
+	"slices"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGlobFileset(t *testing.T) {
+	root, err := os.Getwd()
+	require.NoError(t, err)
+
+	entries, err := os.ReadDir(root)
+	require.NoError(t, err)
+
+	g := NewGlobSet(root, []string{
+		"./*.go",
+	})
+
+	files, err := g.All()
+	require.NoError(t, err)
+
+	require.Equal(t, len(files), len(entries))
+	for _, f := range files {
+		exists := slices.ContainsFunc(entries, func(de fs.DirEntry) bool {
+			return de.Name() == f.Name()
+		})
+		require.True(t, exists)
+	}
+
+	g = NewGlobSet(root, []string{
+		"./*.js",
+	})
+
+	files, err = g.All()
+	require.NoError(t, err)
+	require.Equal(t, len(files), 0)
+}
