ed585cdd2d37efcbecfb19f387645434397db0ae
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2023-08-16 11:56:45 +0200

    Confirm that override with a zero value doesn't work
    This is not desirable and will be addressed by representing our configuration
    in a different structure (e.g. with cty), instead of Go structs.

--- bundle/tests/environment_overrides_test.go
+++ bundle/tests/environment_overrides_test.go
@@ -19,9 +19,20 @@ func TestEnvironmentOverridesWorkspaceStaging(t *testing.T) {
 func TestEnvironmentOverridesResourcesDev(t *testing.T) {
 	b := loadEnvironment(t, "./environment_overrides/resources", "development")
 	assert.Equal(t, "base job", b.Config.Resources.Jobs["job1"].Name)
+
+	// Base values are preserved in the development environment.
+	assert.Equal(t, true, b.Config.Resources.Pipelines["boolean1"].Photon)
+	assert.Equal(t, false, b.Config.Resources.Pipelines["boolean2"].Photon)
 }
 
 func TestEnvironmentOverridesResourcesStaging(t *testing.T) {
 	b := loadEnvironment(t, "./environment_overrides/resources", "staging")
 	assert.Equal(t, "staging job", b.Config.Resources.Jobs["job1"].Name)
+
+	// Overrides are only applied if they are not zero-valued.
+	// This means that in its current form, we cannot override a true value with a false value.
+	// Note: this is not desirable and will be addressed by representing our configuration
+	// in a different structure (e.g. with cty), instead of Go structs.
+	assert.Equal(t, true, b.Config.Resources.Pipelines["boolean1"].Photon)
+	assert.Equal(t, true, b.Config.Resources.Pipelines["boolean2"].Photon)
 }
