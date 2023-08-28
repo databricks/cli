771c3093bc29dad16886a5e2e003fc0439b67528
Andrew Nester <andrew.nester@databricks.com>
2023-08-28 18:20:36 +0200

    bring back missing test case

--- bundle/tests/bundle/wheel_test.go
+++ bundle/tests/bundle/wheel_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/databricks/cli/bundle"
+	"github.com/databricks/cli/bundle/libraries"
 	"github.com/databricks/cli/bundle/phases"
 	"github.com/stretchr/testify/require"
 )
@@ -22,6 +23,10 @@ func TestBundlePythonWheelBuild(t *testing.T) {
 	matches, err := filepath.Glob("python_wheel/my_test_code/dist/my_test_code-*.whl")
 	require.NoError(t, err)
 	require.Equal(t, 1, len(matches))
+
+	match := libraries.MatchWithArtifacts()
+	err = match.Apply(ctx, b)
+	require.NoError(t, err)
 }
 
 func TestBundlePythonWheelBuildAutoDetect(t *testing.T) {
@@ -36,6 +41,10 @@ func TestBundlePythonWheelBuildAutoDetect(t *testing.T) {
 	matches, err := filepath.Glob("python_wheel/my_test_code/dist/my_test_code-*.whl")
 	require.NoError(t, err)
 	require.Equal(t, 1, len(matches))
+
+	match := libraries.MatchWithArtifacts()
+	err = match.Apply(ctx, b)
+	require.NoError(t, err)
 }
 
 func TestBundlePythonWheelWithDBFSLib(t *testing.T) {
@@ -45,5 +54,9 @@ func TestBundlePythonWheelWithDBFSLib(t *testing.T) {
 
 	m := phases.Build()
 	err = m.Apply(ctx, b)
+	require.NoError(t, err)
+
+	match := libraries.MatchWithArtifacts()
+	err = match.Apply(ctx, b)
 	require.NoError(t, err)
 }
