899de692198bccd8d9efa4995241bd38792f75e5
Andrew Nester <andrew.nester@databricks.com>
2023-08-29 16:44:19 +0200

    fixed tests

--- bundle/tests/relative_path_with_includes_test.go
+++ bundle/tests/relative_path_with_includes_test.go
@@ -2,6 +2,7 @@ package config_tests
 
 import (
 	"context"
+	"path/filepath"
 	"testing"
 
 	"github.com/databricks/cli/bundle"
@@ -17,11 +18,11 @@ func TestRelativePathsWithIncludes(t *testing.T) {
 	assert.NoError(t, err)
 
 	assert.Equal(t, "artifact_a", b.Config.Artifacts["test_a"].Path)
-	assert.Equal(t, "subfolder/artifact_b", b.Config.Artifacts["test_b"].Path)
+	assert.Equal(t, filepath.Join("subfolder", "artifact_b"), b.Config.Artifacts["test_b"].Path)
 
-	assert.ElementsMatch(t, []string{"./folder_a/*.*", "subfolder/folder_c/*.*"}, b.Config.Sync.Include)
-	assert.ElementsMatch(t, []string{"./folder_b/*.*", "subfolder/folder_d/*.*"}, b.Config.Sync.Exclude)
+	assert.ElementsMatch(t, []string{"./folder_a/*.*", filepath.Join("subfolder", "folder_c", "*.*")}, b.Config.Sync.Include)
+	assert.ElementsMatch(t, []string{"./folder_b/*.*", filepath.Join("subfolder", "folder_d", "*.*")}, b.Config.Sync.Exclude)
 
-	assert.Equal(t, "dist/job_a.whl", b.Config.Resources.Jobs["job_a"].Tasks[0].Libraries[0].Whl)
-	assert.Equal(t, "subfolder/dist/job_b.whl", b.Config.Resources.Jobs["job_b"].Tasks[0].Libraries[0].Whl)
+	assert.Equal(t, filepath.Join("dist", "job_a.whl"), b.Config.Resources.Jobs["job_a"].Tasks[0].Libraries[0].Whl)
+	assert.Equal(t, filepath.Join("subfolder", "dist", "job_b.whl"), b.Config.Resources.Jobs["job_b"].Tasks[0].Libraries[0].Whl)
 }
