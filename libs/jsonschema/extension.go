d8c87007dee926c329708c5f4d5ecd6e1f10c0ff
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2023-09-05 10:45:37 +0200

    Add property ordering to the jsonschema package

---
+++ libs/jsonschema/extension.go
@@ -0,0 +1,14 @@
+package jsonschema
+
+// Extension defines our custom JSON schema extensions.
+//
+// JSON schema supports custom extensions through vocabularies:
+// https://json-schema.org/understanding-json-schema/reference/schema.html#vocabularies.
+// We don't (yet?) define a meta-schema for the extensions below.
+// It's not a big issue because the reach/scope of these extensions is limited.
+type Extension struct {
+	// Order defines the order of a field with respect to other fields.
+	// If not defined, the field is ordered alphabetically after all fields
+	// that do have an order defined.
+	Order int `json:"order,omitempty"`
+}
--- libs/jsonschema/schema.go
+++ libs/jsonschema/schema.go
@@ -4,7 +4,17 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"slices"
+	"strings"
 )
+
+// Property defines a single property of a struct schema.
+// This type is not used in the schema itself but rather to
+// return the pair of a property name and its schema.
+type Property struct {
+	Name   string
+	Schema *Schema
+}
 
 // defines schema for a json object
 type Schema struct {
@@ -40,6 +50,9 @@ type Schema struct {
 
 	// Default value for the property / object
 	Default any `json:"default,omitempty"`
+
+	// Extension embeds our custom JSON schema extensions.
+	Extension
 }
 
 type Type string
@@ -70,6 +83,30 @@ func (schema *Schema) validate() error {
 		}
 	}
 	return nil
+}
+
+// PropertiesOrdered returns the items of the `prope`
+func (s *Schema) PropertiesOrdered() []Property {
+	order := make(map[string]int)
+	out := make([]Property, len(s.Properties))
+	for key, property := range s.Properties {
+		order[key] = property.Order
+		out = append(out, Property{
+			Name:   key,
+			Schema: property,
+		})
+	}
+
+	// Sort the properties by order and then by name.
+	slices.SortFunc(out, func(a, b Property) int {
+		k := order[a.Name] - order[b.Name]
+		if k != 0 {
+			return k
+		}
+		return strings.Compare(a.Name, b.Name)
+	})
+
+	return out
 }
 
 func Load(path string) (*Schema, error) {
