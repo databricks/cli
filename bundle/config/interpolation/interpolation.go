ccc27c255438287389e60ae684cbbc0ea0672201
monalisa <shreyas.goenka@databricks.com>
2023-09-11 14:29:10 +0200

    -

--- bundle/config/interpolation/interpolation.go
+++ bundle/config/interpolation/interpolation.go
@@ -184,7 +184,7 @@ func (a *accumulator) Resolve(path string, seenPaths []string, fns ...LookupFunc
 	// fetch the string node to resolve
 	field, ok := a.strings[path]
 	if !ok {
-		return fmt.Errorf("no value found for interpolation query: ${%s}", path)
+		return fmt.Errorf("no value found for interpolation reference: ${%s}", path)
 	}
 
 	// return early if the string field has no variables to interpolate
--- bundle/config/interpolation/interpolation_test.go
+++ bundle/config/interpolation/interpolation_test.go
@@ -247,5 +247,5 @@ func TestInterpolationInvalidVariableReference(t *testing.T) {
 	}
 
 	err := expand(&config)
-	assert.ErrorContains(t, err, "no value found for interpolation query: ${vars.foo}")
+	assert.ErrorContains(t, err, "no value found for interpolation reference: ${vars.foo}")
 }
