2455a562ec37549bd7a94de258833a8210face04
monalisa <shreyas.goenka@databricks.com>
2023-09-13 19:08:42 +0200

    added other integratoin test

--- internal/bundle/empty_bundle_test.go
+++ internal/bundle/empty_bundle_test.go
@@ -1,11 +1,13 @@
 package bundle
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/databricks/cli/internal"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 )
 
@@ -13,15 +15,18 @@ func TestAccEmptyBundleDeploy(t *testing.T) {
 	env := internal.GetEnvOrSkipTest(t, "CLOUD_ENV")
 	t.Log(env)
 
+	// create empty bundle
 	tmpDir := t.TempDir()
 	f, err := os.Create(filepath.Join(tmpDir, "databricks.yml"))
 	require.NoError(t, err)
 
-	_, err = f.WriteString(
-		`bundle:
-  name: abc`)
+	bundleRoot := fmt.Sprintf(`bundle:
+  name: %s`, uuid.New().String())
+	_, err = f.WriteString(bundleRoot)
 	require.NoError(t, err)
+	f.Close()
 
+	// deploy empty bundle
 	err = deployBundle(t, tmpDir)
 	require.NoError(t, err)
 
