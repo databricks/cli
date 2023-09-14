745e861e919dc40be92e389edd593ea2326213fb
Andrew Nester <andrew.nester@databricks.com>
2023-09-14 12:02:24 +0200

    moved to experimental.go

--- bundle/config/experimental.go
+++ bundle/config/experimental.go
@@ -3,3 +3,16 @@ package config
 type Experimental struct {
 	Scripts map[ScriptHook]Command `json:"scripts,omitempty"`
 }
+
+type Command string
+type ScriptHook string
+
+// These hook names are subject to change and currently experimental
+const (
+	ScriptPreInit    ScriptHook = "preinit"
+	ScriptPostInit   ScriptHook = "postinit"
+	ScriptPreBuild   ScriptHook = "prebuild"
+	ScriptPostBuild  ScriptHook = "postbuild"
+	ScriptPreDeploy  ScriptHook = "predeploy"
+	ScriptPostDeploy ScriptHook = "postdeploy"
+)
