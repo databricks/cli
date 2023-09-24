1e2e71c4d63367240295ed844a29ebec105a0ab2
monalisa <shreyas.goenka@databricks.com>
2023-09-25 01:56:33 +0200

    Support import by specifying file path

--- cmd/workspace/workspace/overrides.go
+++ cmd/workspace/workspace/overrides.go
@@ -1,7 +1,15 @@
 package workspace
 
 import (
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
 	"github.com/databricks/cli/libs/cmdio"
+	"github.com/databricks/databricks-sdk-go/apierr"
 	"github.com/databricks/databricks-sdk-go/service/workspace"
 	"github.com/spf13/cobra"
 )
@@ -19,7 +27,36 @@ func exportOverride(exportCmd *cobra.Command, exportReq *workspace.ExportRequest
 	exportCmd.Annotations["template"] = `{{.Content | b64_decode}}`
 }
 
+func importOverride(importCmd *cobra.Command, importReq *workspace.Import) {
+	var filePath string
+	importCmd.Use = "import TARGET_PATH"
+	importCmd.Flags().StringVar(&filePath, "file", "", `Path of local file to import`)
+	importCmd.MarkFlagsMutuallyExclusive("content", "file")
+
+	originalRunE := importCmd.RunE
+	importCmd.RunE = func(cmd *cobra.Command, args []string) error {
+		if filePath != "" {
+			b, err := os.ReadFile(filePath)
+			if err != nil {
+				return err
+			}
+			importReq.Content = base64.StdEncoding.EncodeToString(b)
+		}
+		err := originalRunE(cmd, args)
+		target := &apierr.APIError{}
+		if errors.As(err, &target) &&
+			(importReq.Format == workspace.ImportFormatSource || importReq.Format == "") &&
+			target.StatusCode == http.StatusBadRequest &&
+			strings.Contains(target.Message, "The zip file may not be valid or may be an unsupported version.") {
+			return fmt.Errorf("%w Hint: Objects imported using source format are assumed to be zip encoded by default. Please specify a language using the --language flag if you are trying to import a notebook instead", err)
+		}
+		return err
+	}
+
+}
+
 func init() {
 	listOverrides = append(listOverrides, listOverride)
 	exportOverrides = append(exportOverrides, exportOverride)
+	importOverrides = append(importOverrides, importOverride)
 }
