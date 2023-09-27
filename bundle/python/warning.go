3ee89c41dac53f2969a5fbbd6d09fb30c10ee7e0
Andrew Nester <andrew.nester@databricks.com>
2023-09-27 10:26:59 +0200

    Added a warning when Python wheel wrapper needs to be used (#807)
    ## Changes
    Added a warning when Python wheel wrapper needs to be used
    
    ## Tests
    Added unit tests + manual run with different bundle configurations

---
+++ bundle/python/warning.go
@@ -0,0 +1,65 @@
+package python
+
+import (
+	"context"
+	"strings"
+
+	"github.com/databricks/cli/bundle"
+	"github.com/databricks/cli/bundle/libraries"
+	"github.com/databricks/cli/libs/cmdio"
+	"golang.org/x/mod/semver"
+)
+
+type wrapperWarning struct {
+}
+
+func WrapperWarning() bundle.Mutator {
+	return &wrapperWarning{}
+}
+
+func (m *wrapperWarning) Apply(ctx context.Context, b *bundle.Bundle) error {
+	if hasIncompatibleWheelTasks(ctx, b) {
+		cmdio.LogString(ctx, "Python wheel tasks with local libraries require compute with DBR 13.1+. Please change your cluster configuration or set experimental 'python_wheel_wrapper' setting to 'true'")
+	}
+	return nil
+}
+
+func hasIncompatibleWheelTasks(ctx context.Context, b *bundle.Bundle) bool {
+	tasks := libraries.FindAllWheelTasksWithLocalLibraries(b)
+	for _, task := range tasks {
+		if task.NewCluster != nil {
+			if lowerThanExpectedVersion(ctx, task.NewCluster.SparkVersion) {
+				return true
+			}
+		}
+
+		if task.JobClusterKey != "" {
+			for _, job := range b.Config.Resources.Jobs {
+				for _, cluster := range job.JobClusters {
+					if task.JobClusterKey == cluster.JobClusterKey && cluster.NewCluster != nil {
+						if lowerThanExpectedVersion(ctx, cluster.NewCluster.SparkVersion) {
+							return true
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return false
+}
+
+func lowerThanExpectedVersion(ctx context.Context, sparkVersion string) bool {
+	parts := strings.Split(sparkVersion, ".")
+	if len(parts) < 2 {
+		return false
+	}
+
+	v := "v" + parts[0] + "." + parts[1]
+	return semver.Compare(v, "v13.1") < 0
+}
+
+// Name implements bundle.Mutator.
+func (m *wrapperWarning) Name() string {
+	return "PythonWrapperWarning"
+}
