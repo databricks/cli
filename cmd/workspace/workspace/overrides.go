be4f94d883e918218473a653fca3c3736ece2353
monalisa <shreyas.goenka@databricks.com>
2023-10-05 14:52:41 +0200

    remove prompt logic

--- cmd/workspace/workspace/overrides.go
+++ cmd/workspace/workspace/overrides.go
@@ -30,21 +30,6 @@ func exportOverride(exportCmd *cobra.Command, exportReq *workspace.ExportRequest
 	exportCmd.RunE = func(cmd *cobra.Command, args []string) error {
 		ctx := cmd.Context()
 		w := root.WorkspaceClient(ctx)
-
-		if len(args) == 0 {
-			promptSpinner := cmdio.Spinner(ctx)
-			promptSpinner <- "No SOURCE_PATH argument specified. Loading names for Workspace drop-down."
-			names, err := w.Workspace.ObjectInfoPathToObjectIdMap(ctx, workspace.ListWorkspaceRequest{})
-			close(promptSpinner)
-			if err != nil {
-				return fmt.Errorf("failed to load names for Workspace drop-down. Please manually specify required arguments. Original error: %w", err)
-			}
-			id, err := cmdio.Select(ctx, names, "The absolute path of the object or directory")
-			if err != nil {
-				return err
-			}
-			args = append(args, id)
-		}
 		if len(args) != 1 {
 			return fmt.Errorf("expected to have the absolute path of the object or directory")
 		}
