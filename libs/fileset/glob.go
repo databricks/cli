bf332221ca792a5cd37834385c985ba1c7415fa2
Andrew Nester <andrew.nester@databricks.com>
2023-10-09 19:05:08 +0200

    refactoring

--- libs/fileset/glob.go
+++ libs/fileset/glob.go
@@ -4,30 +4,19 @@ import (
 	"path/filepath"
 )
 
-type GlobSet struct {
-	fs       *FileSet
-	root     string
-	patterns []string
-}
-
-func NewGlobSet(root string, includes []string) (*GlobSet, error) {
+func NewGlobSet(root string, includes []string) (*FileSet, error) {
 	absRoot, err := filepath.Abs(root)
 	if err != nil {
 		return nil, err
 	}
 
 	for k := range includes {
-		includes[k] = filepath.Clean(filepath.FromSlash(includes[k]))
+		includes[k] = filepath.ToSlash(filepath.Clean(includes[k]))
 	}
 
 	fs := &FileSet{
 		absRoot,
 		newIncluder(includes),
 	}
-	return &GlobSet{fs, absRoot, includes}, nil
-}
-
-// Return all files which matches defined glob patterns
-func (s *GlobSet) All() ([]File, error) {
-	return s.fs.All()
+	return fs, nil
 }
