2cd07b69bc2ca231ad041176d03611ef7536adb5
Andrew Nester <andrew.nester@databricks.com>
2023-10-09 17:07:58 +0200

    Support .gitignore syntax in sync section and make sure it works recursively

--- libs/fileset/glob.go
+++ libs/fileset/glob.go
@@ -1,12 +1,11 @@
 package fileset
 
 import (
-	"io/fs"
-	"os"
 	"path/filepath"
 )
 
 type GlobSet struct {
+	fs       *FileSet
 	root     string
 	patterns []string
 }
@@ -16,34 +15,19 @@ func NewGlobSet(root string, includes []string) (*GlobSet, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	for k := range includes {
-		includes[k] = filepath.Join(absRoot, filepath.FromSlash(includes[k]))
+		includes[k] = filepath.Clean(includes[k])
 	}
-	return &GlobSet{absRoot, includes}, nil
+
+	fs := &FileSet{
+		absRoot,
+		newIncluder(includes),
+	}
+	return &GlobSet{fs, absRoot, includes}, nil
 }
 
 // Return all files which matches defined glob patterns
 func (s *GlobSet) All() ([]File, error) {
-	files := make([]File, 0)
-	for _, pattern := range s.patterns {
-		matches, err := filepath.Glob(pattern)
-		if err != nil {
-			return files, err
-		}
-
-		for _, match := range matches {
-			matchRel, err := filepath.Rel(s.root, match)
-			if err != nil {
-				return files, err
-			}
-
-			stat, err := os.Stat(match)
-			if err != nil {
-				return files, err
-			}
-			files = append(files, File{fs.FileInfoToDirEntry(stat), match, matchRel})
-		}
-	}
-
-	return files, nil
+	return s.fs.All()
 }
--- libs/fileset/ignorer.go
+++ libs/fileset/ignorer.go
@@ -1,5 +1,9 @@
 package fileset
 
+import (
+	ignore "github.com/sabhiram/go-gitignore"
+)
+
 // Ignorer is the interface for what determines if a path
 // in the [FileSet] must be ignored or not.
 type Ignorer interface {
@@ -16,4 +20,29 @@ func (nopIgnorer) IgnoreFile(path string) (bool, error) {
 
 func (nopIgnorer) IgnoreDirectory(path string) (bool, error) {
 	return false, nil
+}
+
+type includer struct {
+	matcher *ignore.GitIgnore
+}
+
+func newIncluder(includes []string) *includer {
+	matcher := ignore.CompileIgnoreLines(includes...)
+	return &includer{
+		matcher,
+	}
+}
+
+func (i *includer) IgnoreFile(path string) (bool, error) {
+	return i.ignore(path), nil
+}
+
+func (i *includer) IgnoreDirectory(path string) (bool, error) {
+	return false, nil
+}
+
+func (i *includer) ignore(path string) bool {
+	matched := i.matcher.MatchesPath(path)
+	// If matched, do not ignore the file because we want to include it
+	return !matched
 }
