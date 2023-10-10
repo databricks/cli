2963c75caf131ef7c7f366f36fbc4184f7889542
Andrew Nester <andrew.nester@databricks.com>
2023-10-10 10:38:30 +0200

    added a comment

--- libs/fileset/ignorer.go
+++ libs/fileset/ignorer.go
@@ -37,6 +37,15 @@ func (i *includer) IgnoreFile(path string) (bool, error) {
 	return i.ignore(path), nil
 }
 
+// In the context of 'include' functionality, the Ignorer logic appears to be reversed:
+// For patterns like 'foo/bar/' which intends to match directories only, we still need to traverse into the directory for potential file matches.
+// Ignoring the directory entirely isn't an option, especially when dealing with patterns like 'foo/bar/*.go'.
+// While this pattern doesn't target directories, it does match all Go files within them and ignoring directories not matching the pattern
+// Will result in missing file matches.
+// During the tree traversal process, we call 'IgnoreDirectory' on ".", "./foo", and "./foo/bar",
+// all while applying the 'foo/bar/*.go' pattern. To handle this situation effectively, it requires to make the code more complex.
+// This could mean generating various prefix patterns to facilitate the exclusion of directories from traversal.
+// It's worth noting that, in this particular case, opting for a simpler logic results in a performance trade-off.
 func (i *includer) IgnoreDirectory(path string) (bool, error) {
 	return false, nil
 }
