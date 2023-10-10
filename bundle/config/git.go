a214973ca6d851ebfb3ce4f18bbd5cfff007fe78
monalisa <shreyas.goenka@databricks.com>
2023-10-10 19:37:39 +0200

    wip adding bundle root metadata

--- bundle/config/git.go
+++ bundle/config/git.go
@@ -5,6 +5,10 @@ type Git struct {
 	OriginURL string `json:"origin_url,omitempty"`
 	Commit    string `json:"commit,omitempty" bundle:"readonly"`
 
+	// Path to bundle root (ie directory containing databricks.yml) relative to
+	// the git repository root.
+	BundleRoot string `json:"bundle_root,omitempty" bundle:"readonly"`
+
 	// Inferred is set to true if the Git details were inferred and weren't set explicitly
 	Inferred bool `json:"-" bundle:"readonly"`
 
