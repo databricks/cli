4b1f6ba1124a360db4b1c13d58332c6cade13f30
monalisa <shreyas.goenka@databricks.com>
2023-10-17 18:11:45 +0200

    Add metadata path to jobs deployed in a bundle

---
+++ bundle/deploy/metadata/annotate_jobs.go
@@ -0,0 +1,30 @@
+package metadata
+
+import (
+	"context"
+	"path"
+
+	"github.com/databricks/cli/bundle"
+	"github.com/databricks/databricks-sdk-go/service/jobs"
+)
+
+type annotateJobs struct{}
+
+func AnnotateJobs() bundle.Mutator {
+	return &annotateJobs{}
+}
+
+func (m *annotateJobs) Name() string {
+	return "metadata.SetInJobs"
+}
+
+func (m *annotateJobs) Apply(_ context.Context, b *bundle.Bundle) error {
+	for _, job := range b.Config.Resources.Jobs {
+		job.JobSettings.Deployment = &jobs.JobDeployment{
+			Kind:             jobs.JobDeploymentKindBundle,
+			MetadataFilePath: path.Join(b.Config.Workspace.StatePath, MetadataFileName),
+		}
+	}
+
+	return nil
+}
