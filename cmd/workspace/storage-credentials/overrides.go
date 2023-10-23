b3659817b7acf3ef9ba7e14c3ef21cf97b5fcd61
Christian Höltje <docwhat@gerf.org>
2023-10-23 17:45:25 -0400

    fix: gcp_service_account_key is not available
    GcpServiceAccountKey doesn't exist on StorageCredentialInfo in the
    current SDKs

--- cmd/workspace/storage-credentials/overrides.go
+++ cmd/workspace/storage-credentials/overrides.go
@@ -8,7 +8,7 @@ import (
 func listOverride(listCmd *cobra.Command) {
 	listCmd.Annotations["template"] = cmdio.Heredoc(`
 	{{header "ID"}}	{{header "Name"}}	{{header "Credentials"}}
-	{{range .}}{{.Id|green}}	{{.Name|cyan}}	{{if .AwsIamRole}}{{.AwsIamRole.RoleArn}}{{end}}{{if .AzureServicePrincipal}}{{.AzureServicePrincipal.ApplicationId}}{{end}}{{if .GcpServiceAccountKey}}{{.Email}}{{end}}
+	{{range .}}{{.Id|green}}	{{.Name|cyan}}	{{if .AwsIamRole}}{{.AwsIamRole.RoleArn}}{{end}}{{if .AzureServicePrincipal}}{{.AzureServicePrincipal.ApplicationId}}{{end}}
 	{{end}}`)
 }
 
