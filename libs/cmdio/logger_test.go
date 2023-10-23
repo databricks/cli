fc3164e4be8e61f27558649bae805300b84b755a
monalisa <shreyas.goenka@databricks.com>
2023-10-23 23:08:37 +0200

    add tests for log raw

--- libs/cmdio/logger_test.go
+++ libs/cmdio/logger_test.go
@@ -21,3 +21,19 @@ func TestAskChoiceFailsInJsonMode(t *testing.T) {
 	_, err := AskSelect(ctx, "what is a question?", []string{"b", "c", "a"})
 	assert.EqualError(t, err, "question prompts are not supported in json mode")
 }
+
+func TestLogRawErrorForJsonMode(t *testing.T) {
+	l := NewLogger(flags.ModeJson)
+	ctx := NewContext(context.Background(), l)
+
+	err := LogRaw(ctx, "hello world")
+	assert.EqualError(t, err, "logging raw strings is only supported in append mode. Failed to log: \"hello world\"")
+}
+
+func TestLogRawErrorForInplaceMode(t *testing.T) {
+	l := NewLogger(flags.ModeInplace)
+	ctx := NewContext(context.Background(), l)
+
+	err := LogRaw(ctx, "hello world")
+	assert.EqualError(t, err, "logging raw strings is only supported in append mode. Failed to log: \"hello world\"")
+}
