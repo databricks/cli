46090a4f9ff1824f4a5e183da9268ef0594a6290
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2023-10-24 20:59:17 +0200

    Add configuration normalization code
    This is similar to #904 but instead of converting the dynamic configuration to
    Go structs, this normalizes a `config.Value` according to the type of a Go struct
    and returns the new, normalized `config.Value`.
    
    This will be used to ensure that two config.Value trees are type-compatible
    before we can merge them (i.e. instances from different files).
    
    Warnings and errors during normalization are accumulated and returned as a
    `diag.Diagnostics` structure. We can use this to surface warnings about unknown
    fields, or errors about invalid types, in aggregate instead of one-by-one.

---
+++ libs/diag/diagnostic.go
@@ -0,0 +1,76 @@
+package diag
+
+import (
+	"fmt"
+
+	"github.com/databricks/cli/libs/config"
+)
+
+type Diagnostic struct {
+	Severity Severity
+
+	// Summary is a short description of the diagnostic.
+	// This is expected to be a single line and always present.
+	Summary string
+
+	// Detail is a longer description of the diagnostic.
+	// This may be multiple lines and may be nil.
+	Detail string
+
+	// Location is a source code location associated with the diagnostic message.
+	// It may be zero if there is no associated location.
+	Location config.Location
+}
+
+// Errorf creates a new error diagnostic.
+func Errorf(format string, args ...any) Diagnostics {
+	return []Diagnostic{
+		{
+			Severity: Error,
+			Summary:  fmt.Sprintf(format, args...),
+		},
+	}
+}
+
+// Warningf creates a new warning diagnostic.
+func Warningf(format string, args ...any) Diagnostics {
+	return []Diagnostic{
+		{
+			Severity: Warning,
+			Summary:  fmt.Sprintf(format, args...),
+		},
+	}
+}
+
+// Infof creates a new info diagnostic.
+func Infof(format string, args ...any) Diagnostics {
+	return []Diagnostic{
+		{
+			Severity: Info,
+			Summary:  fmt.Sprintf(format, args...),
+		},
+	}
+}
+
+// Diagsnostics holds zero or more instances of [Diagnostic].
+type Diagnostics []Diagnostic
+
+// Append adds a new diagnostic to the end of the list.
+func (ds Diagnostics) Append(d Diagnostic) Diagnostics {
+	return append(ds, d)
+}
+
+// Extend adds all diagnostics from another list to the end of the list.
+func (ds Diagnostics) Extend(other Diagnostics) Diagnostics {
+	return append(ds, other...)
+}
+
+// HasError returns true if any of the diagnostics are errors.
+func (ds Diagnostics) HasError() bool {
+	for _, d := range ds {
+		if d.Severity == Error {
+			return true
+		}
+	}
+	return false
+}
