9e66350dc505a3cfd1201735d7d48ad829680cb8
monalisa <shreyas.goenka@databricks.com>
2023-10-27 15:08:56 +0200

    cleanup

--- bundle/config/paths/paths.go
+++ bundle/config/paths/paths.go
@@ -9,10 +9,6 @@ type Paths struct {
 	// Absolute path on the local file system to the configuration file that holds
 	// the definition of this resource.
 	ConfigFilePath string `json:"-" bundle:"readonly"`
-
-	// Relative path from the bundle root to the configuration file that holds
-	// the definition of this resource.
-	RelativePath string `json:"relative_path,omitempty" bundle:"readonly"`
 }
 
 func (p *Paths) ConfigFileDirectory() (string, error) {
