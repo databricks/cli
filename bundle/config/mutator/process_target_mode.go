0ab125c10931341c114124ffbf47da3180fa121f
Lennart Kats (databricks) <lennart.kats@databricks.com>
2023-11-13 20:50:39 +0100

    Allow jobs to be manually unpaused in development mode (#885)
    Partly mitigates #859. It's still not clear to me if there is an actual
    use case or if users are trying to use "development" mode jobs for
    production, but making this overridable is reasonable.
    
    Beyond this fix I think we could do something in the Jobs schedule UI,
    but it would help to better understand the use case (or actual reason of
    confusion). I expect we should hint customers to move away from dev mode
    rather than unpause.

--- bundle/config/mutator/process_target_mode.go
+++ bundle/config/mutator/process_target_mode.go
@@ -47,13 +47,17 @@ func transformDevelopmentMode(b *bundle.Bundle) error {
 		if r.Jobs[i].MaxConcurrentRuns == 0 {
 			r.Jobs[i].MaxConcurrentRuns = developmentConcurrentRuns
 		}
-		if r.Jobs[i].Schedule != nil {
+
+		// Pause each job. As an exception, we don't pause jobs that are explicitly
+		// marked as "unpaused". This allows users to override the default behavior
+		// of the development mode.
+		if r.Jobs[i].Schedule != nil && r.Jobs[i].Schedule.PauseStatus != jobs.PauseStatusUnpaused {
 			r.Jobs[i].Schedule.PauseStatus = jobs.PauseStatusPaused
 		}
-		if r.Jobs[i].Continuous != nil {
+		if r.Jobs[i].Continuous != nil && r.Jobs[i].Schedule.PauseStatus != jobs.PauseStatusUnpaused {
 			r.Jobs[i].Continuous.PauseStatus = jobs.PauseStatusPaused
 		}
-		if r.Jobs[i].Trigger != nil {
+		if r.Jobs[i].Trigger != nil && r.Jobs[i].Schedule.PauseStatus != jobs.PauseStatusUnpaused {
 			r.Jobs[i].Trigger.PauseStatus = jobs.PauseStatusPaused
 		}
 	}
