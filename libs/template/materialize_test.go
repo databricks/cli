99c31471d03808730d45993d3d3fda14ac386498
monalisa <shreyas.goenka@databricks.com>
2023-11-14 17:40:54 +0100

    -

--- libs/template/materialize_test.go
+++ libs/template/materialize_test.go
@@ -12,5 +12,5 @@ import (
 func TestMaterializeForNonTemplateDirectory(t *testing.T) {
 	tmpDir := t.TempDir()
 	err := Materialize(context.Background(), "", tmpDir, "")
-	assert.EqualError(t, err, fmt.Sprintf("Expected to find a template schema file at %s. Valid bundle templates are expected to contain a schema file", filepath.Join(tmpDir, schemaFileName)))
+	assert.EqualError(t, err, fmt.Sprintf("expected to find a template schema file at %s. Valid bundle templates are expected to contain a schema file", filepath.Join(tmpDir, schemaFileName)))
 }
