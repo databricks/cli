0cdf78413de91a2e98a6f44a8dd671bb9849f17d
monalisa <shreyas.goenka@databricks.com>
2023-11-22 01:13:31 +0100

    remove unrelated code

--- libs/template/materialize_test.go
+++ libs/template/materialize_test.go
@@ -6,11 +6,19 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/databricks/cli/cmd/root"
+	"github.com/databricks/databricks-sdk-go"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestMaterializeForNonTemplateDirectory(t *testing.T) {
 	tmpDir := t.TempDir()
-	err := Materialize(context.Background(), "", tmpDir, "")
+	w, err := databricks.NewWorkspaceClient(&databricks.Config{})
+	require.NoError(t, err)
+	ctx := root.SetWorkspaceClient(context.Background(), w)
+
+	// Try to materialize a non-template directory.
+	err = Materialize(ctx, "", tmpDir, "")
 	assert.EqualError(t, err, fmt.Sprintf("not a bundle template: expected to find a template schema file at %s", filepath.Join(tmpDir, schemaFileName)))
 }
