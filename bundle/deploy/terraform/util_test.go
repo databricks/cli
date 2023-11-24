618780300752c2190c76fb2c667244491e006691
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2023-11-24 12:15:46 +0100

    Correctly overwrite local state if remote state is newer (#1008)
    ## Changes
    
    A bug in the code that pulls the remote state could cause the local
    state to be empty instead of a copy of the remote state. This happened
    only if the local state was present and stale when compared to the
    remote version.
    
    We correctly checked for the state serial to see if the local state had
    to be replaced but didn't seek back on the remote state before writing
    it out. Because the staleness check would read the remote state in full,
    copying from the same reader would immediately yield an EOF.
    
    ## Tests
    
    * Unit tests for state pull and push mutators that rely on a mocked
    filer.
    * An integration test that deploys the same bundle from multiple paths,
    triggering the staleness logic.
    
    Both failed prior to the fix and now pass.

--- bundle/deploy/terraform/util_test.go
+++ bundle/deploy/terraform/util_test.go
@@ -2,92 +2,39 @@ package terraform
 
 import (
 	"fmt"
-	"io"
+	"strings"
 	"testing"
 	"testing/iotest"
 
 	"github.com/stretchr/testify/assert"
 )
 
-type mockedReader struct {
-	content string
-}
-
-func (r *mockedReader) Read(p []byte) (n int, err error) {
-	content := []byte(r.content)
-	n = copy(p, content)
-	return n, io.EOF
-}
-
 func TestLocalStateIsNewer(t *testing.T) {
-	local := &mockedReader{content: `
-{
-	"serial": 5
-}
-`}
-	remote := &mockedReader{content: `
-{
-	"serial": 4
-}
-`}
-
-	stale := IsLocalStateStale(local, remote)
-
-	assert.False(t, stale)
+	local := strings.NewReader(`{"serial": 5}`)
+	remote := strings.NewReader(`{"serial": 4}`)
+	assert.False(t, IsLocalStateStale(local, remote))
 }
 
 func TestLocalStateIsOlder(t *testing.T) {
-	local := &mockedReader{content: `
-{
-	"serial": 5
-}
-`}
-	remote := &mockedReader{content: `
-{
-	"serial": 6
-}
-`}
-
-	stale := IsLocalStateStale(local, remote)
-	assert.True(t, stale)
+	local := strings.NewReader(`{"serial": 5}`)
+	remote := strings.NewReader(`{"serial": 6}`)
+	assert.True(t, IsLocalStateStale(local, remote))
 }
 
 func TestLocalStateIsTheSame(t *testing.T) {
-	local := &mockedReader{content: `
-{
-	"serial": 5
-}
-`}
-	remote := &mockedReader{content: `
-{
-	"serial": 5
-}
-`}
-
-	stale := IsLocalStateStale(local, remote)
-	assert.False(t, stale)
+	local := strings.NewReader(`{"serial": 5}`)
+	remote := strings.NewReader(`{"serial": 5}`)
+	assert.False(t, IsLocalStateStale(local, remote))
 }
 
 func TestLocalStateMarkStaleWhenFailsToLoad(t *testing.T) {
 	local := iotest.ErrReader(fmt.Errorf("Random error"))
-	remote := &mockedReader{content: `
-{
-	"serial": 5
-}
-`}
-
-	stale := IsLocalStateStale(local, remote)
-	assert.True(t, stale)
+	remote := strings.NewReader(`{"serial": 5}`)
+	assert.True(t, IsLocalStateStale(local, remote))
 }
 
 func TestLocalStateMarkNonStaleWhenRemoteFailsToLoad(t *testing.T) {
-	local := &mockedReader{content: `
-{
-	"serial": 5
-}
-`}
+	local := strings.NewReader(`{"serial": 5}`)
 	remote := iotest.ErrReader(fmt.Errorf("Random error"))
-
-	stale := IsLocalStateStale(local, remote)
-	assert.False(t, stale)
+	assert.False(t, IsLocalStateStale(local, remote))
 }
