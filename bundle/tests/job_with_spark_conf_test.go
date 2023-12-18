64ab2024680593bdf941cad5c8d308fe195403d7
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2023-12-18 11:49:23 +0100

    Test

---
+++ bundle/tests/job_with_spark_conf_test.go
@@ -0,0 +1,24 @@
+package config_tests
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestJobWithSparkConf(t *testing.T) {
+	b := loadTarget(t, "./job_with_spark_conf", "default")
+	assert.Len(t, b.Config.Resources.Jobs, 1)
+
+	job := b.Config.Resources.Jobs["job_with_spark_conf"]
+	assert.Len(t, job.JobClusters, 1)
+
+	jobCluster := job.JobClusters[0]
+	assert.Equal(t, "test_cluster", jobCluster.JobClusterKey)
+
+	// Existing behavior is such that including non-string values
+	// in the spark_conf map will cause the job to fail to load.
+	// This is expected to be solved once we switch to the custom YAML loader.
+	tasks := job.Tasks
+	assert.Len(t, tasks, 0, "see https://github.com/databricks/cli/issues/992")
+}
