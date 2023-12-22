938eb1600c9682b33b8de0b74273faf0687b9192
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2023-12-22 14:20:45 +0100

    Rename libs/config -> libs/dyn (#1086)
    ## Changes
    
    The name "dynamic value", or "dyn" for short, is more descriptive than
    the opaque "config". Also, it conveniently does not alias with other
    packages in the repository, or (popular ones) elsewhere.
    
    (discussed with @andrewnester)
    
    ## Tests
    
    n/a

---
+++ libs/dyn/location.go
@@ -0,0 +1,13 @@
+package dyn
+
+import "fmt"
+
+type Location struct {
+	File   string
+	Line   int
+	Column int
+}
+
+func (l Location) String() string {
+	return fmt.Sprintf("%s:%d:%d", l.File, l.Line, l.Column)
+}
