14bc1fa2597bc0b7dac8bca21b53b75aac991814
Lennart Kats <lennart.kats@databricks.com>
2024-01-20 22:11:14 +0100

    Add conditional

--- libs/template/templates/dbt-sql/template/{{.project_name}}/models/example/orders_daily.sql
+++ libs/template/templates/dbt-sql/template/{{.project_name}}/models/example/orders_daily.sql
@@ -5,6 +5,12 @@
 
 
 select order_date, count(*) AS number_of_orders
+
 from {{ ref('orders_raw') }}
-where order_date is not null
+
+-- Process a smaller range unless we're in 'prod' mode
+{% if var('bundle_target', 'default') != 'prod' %}
+where created_at >= '2019-08-01' and created_at < '2019-09-01'
+{% endif %}
+
 group by order_date
