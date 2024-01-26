959af0788342ba678a0c913fe5feb88197f1fb11
Lennart Kats <lennart.kats@databricks.com>
2024-01-26 17:08:35 +0100

    Fix keyword

--- default_sql/src/orders_raw.sql
+++ default_sql/src/orders_raw.sql
@@ -4,7 +4,7 @@
 -- See also https://docs.databricks.com/sql/language-manual/sql-ref-syntax-ddl-create-streaming-table.html
 
 CREATE OR REFRESH STREAMING TABLE
-  SYMBOL(CONCAT({{catalog}}, '.', {{schema}}, '.', 'raw_orders'))
+  IDENTIFIER(CONCAT({{catalog}}, '.', {{schema}}, '.', 'raw_orders'))
 AS SELECT
   customer_name,
   DATE(TIMESTAMP(FROM_UNIXTIME(TRY_CAST(order_datetime AS BIGINT)))) AS order_date,
