c445d198d02c266e245a01b6f596fa91604ac6d6
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2024-02-05 20:43:34 +0100

    Revert commented out scripts section

--- bundle/config/mutator/mutator.go
+++ bundle/config/mutator/mutator.go
@@ -2,11 +2,13 @@ package mutator
 
 import (
 	"github.com/databricks/cli/bundle"
+	"github.com/databricks/cli/bundle/config"
+	"github.com/databricks/cli/bundle/scripts"
 )
 
 func DefaultMutators() []bundle.Mutator {
 	return []bundle.Mutator{
-		// scripts.Execute(config.ScriptPreInit),
+		scripts.Execute(config.ScriptPreInit),
 		ProcessRootIncludes(),
 		RewriteSyncPaths(),
 		EnvironmentsToTargets(),
