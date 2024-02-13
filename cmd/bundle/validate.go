727beca545edee81fff59064dbf7b0cf66c7ee85
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2024-02-13 15:11:54 +0100

    Add debug logging of configuration diagnostics on validate

--- cmd/bundle/validate.go
+++ cmd/bundle/validate.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/databricks/cli/bundle"
 	"github.com/databricks/cli/bundle/phases"
+	"github.com/databricks/cli/libs/log"
 	"github.com/spf13/cobra"
 )
 
@@ -22,6 +23,12 @@ func newValidateCommand() *cobra.Command {
 		err := bundle.Apply(cmd.Context(), b, phases.Initialize())
 		if err != nil {
 			return err
+		}
+
+		// Until we change up the output of this command to be a text representation,
+		// we'll just output all diagnostics as debug logs.
+		for _, diag := range b.Config.Diagnostics() {
+			log.Debugf(cmd.Context(), "[%s]: %s", diag.Location, diag.Summary)
 		}
 
 		buf, err := json.MarshalIndent(b.Config, "", "  ")
