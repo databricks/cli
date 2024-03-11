965f6e94b4030691fb477eb0d4dc25813b0e2f92
Andrew Nester <andrew.nester@databricks.com>
2024-03-11 17:33:38 +0100

    fixed tests

--- internal/workspace_test.go
+++ internal/workspace_test.go
@@ -34,12 +34,12 @@ func TestAccWorkspaceList(t *testing.T) {
 
 func TestWorkpaceListErrorWhenNoArguments(t *testing.T) {
 	_, _, err := RequireErrorRun(t, "workspace", "list")
-	assert.Equal(t, "accepts 1 arg(s), received 0", err.Error())
+	assert.Contains(t, err.Error(), "accepts 1 arg(s), received 0")
 }
 
 func TestWorkpaceGetStatusErrorWhenNoArguments(t *testing.T) {
 	_, _, err := RequireErrorRun(t, "workspace", "get-status")
-	assert.Equal(t, "accepts 1 arg(s), received 0", err.Error())
+	assert.Contains(t, err.Error(), "accepts 1 arg(s), received 0")
 }
 
 func TestWorkpaceExportPrintsContents(t *testing.T) {
