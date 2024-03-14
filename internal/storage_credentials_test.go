bc27e1496110cc46505cdcab97653fd5befccfa6
Andrew Nester <andrew.nester@databricks.com>
2024-03-14 12:48:45 +0100

    Fixed TestAccStorageCredentialsListRendersResponse test

--- internal/storage_credentials_test.go
+++ internal/storage_credentials_test.go
@@ -9,6 +9,10 @@ import (
 
 func TestAccStorageCredentialsListRendersResponse(t *testing.T) {
 	_, _ = acc.WorkspaceTest(t)
+
+	// Check if metastore is assigned for the workspace, otherwise test will fail
+	t.Log(GetEnvOrSkipTest(t, "TEST_METASTORE_ID"))
+
 	stdout, stderr := RequireSuccessfulRun(t, "storage-credentials", "list")
 	assert.NotEmpty(t, stdout)
 	assert.Empty(t, stderr)
