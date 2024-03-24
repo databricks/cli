4e1789bbab5697c79988d36f9e99489376291878
aravind-segu <aravind.segu@databricks.com>
2024-03-24 11:51:59 -0700

    clean up

--- libs/dyn/convert/normalize.go
+++ libs/dyn/convert/normalize.go
@@ -165,7 +165,6 @@ func (n normalizeOptions) normalizeStruct(typ reflect.Type, src dyn.Value, seen 
 				out.Set(dyn.V(k), v)
 			}
 		}
-		print("HELLLOOOOOO")
 		return dyn.NewValue(out, src.Location()), diags
 	case dyn.KindNil:
 		return src, diags
