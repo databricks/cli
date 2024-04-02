7413253c18f19968a4b65308f4c49c541316ffc4
Ilia Babanov <ilia.babanov@databricks.com>
2024-04-02 10:23:53 +0200

    Update bundle/deploy/terraform/init.go
    Co-authored-by: shreyas-goenka <88374338+shreyas-goenka@users.noreply.github.com>

--- bundle/deploy/terraform/init.go
+++ bundle/deploy/terraform/init.go
@@ -132,8 +132,8 @@ func inheritEnvVars(ctx context.Context, environ map[string]string) error {
 }
 
 // Example: this function will return a value of TF_EXEC_PATH only if the path exists and if TF_VERSION matches the TerraformVersion.
-// This function is used for env vars set by the Databricks VSCode extension. The variables are intented to be used by the CLI
-// bundled with the extension, but users can use different CLI version in the VSCode terminals, in which case we want to ignore
+// This function is used for env vars set by the Databricks VSCode extension. The variables are intended to be used by the CLI
+// bundled with the Databricks VSCode extension, but users can use different CLI versions in the VSCode terminals, in which case we want to ignore
 // the variables if that CLI uses different versions of the dependencies.
 func getEnvVarWithMatchingVersion(ctx context.Context, envVarName string, versionVarName string, currentVersion string) (string, error) {
 	envValue := env.Get(ctx, envVarName)
