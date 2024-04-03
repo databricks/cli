7df82a3b53307db061dab51e788a3d9e2e255a6f
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2024-04-03 14:00:56 +0200

    Work

--- cmd/bundle/validate.go
+++ cmd/bundle/validate.go
@@ -1,29 +1,122 @@
 package bundle
 
 import (
+	"encoding/json"
+	"fmt"
 	"path/filepath"
+	"strings"
+	"text/template"
 
 	"github.com/databricks/cli/bundle"
 	"github.com/databricks/cli/bundle/phases"
 	"github.com/databricks/cli/cmd/bundle/utils"
 	"github.com/databricks/cli/cmd/root"
-	"github.com/databricks/cli/libs/cmdio"
 	"github.com/databricks/cli/libs/diag"
+	"github.com/databricks/cli/libs/flags"
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 )
 
-// "red":     color.RedString,
-// "green":   color.GreenString,
-// "blue":    color.BlueString,
-// "yellow":  color.YellowString,
-// "magenta": color.MagentaString,
-// "cyan":    color.CyanString,
+var validateFuncMap = template.FuncMap{
+	"red":     color.RedString,
+	"green":   color.GreenString,
+	"blue":    color.BlueString,
+	"yellow":  color.YellowString,
+	"magenta": color.MagentaString,
+	"cyan":    color.CyanString,
+	"bold": func(format string, a ...interface{}) string {
+		return color.New(color.Bold).Sprintf(format, a...)
+	},
+	"italic": func(format string, a ...interface{}) string {
+		return color.New(color.Italic).Sprintf(format, a...)
+	},
+}
 
-const tmpl = `
-  {{ "Warning" | yellow }}: {{ .Summary }}
+const errorTemplate = `{{ "Error" | red }}: {{ .Summary }}
   {{ "At " }}{{ .Path.String | green }}
   {{ "In " }}{{ .Location.String | cyan }}
+
 `
+
+const warningTemplate = `{{ "Warning" | yellow }}: {{ .Summary }}
+  {{ "At " }}{{ .Path.String | green }}
+  {{ "In " }}{{ .Location.String | cyan }}
+
+`
+
+const summaryTemplate = `Name: {{ .Config.Bundle.Name | bold }}
+Target: {{ .Config.Bundle.Target | bold }}
+Workspace:
+  Host: {{ .Config.Workspace.Host | bold }}
+  User: {{ .Config.Workspace.CurrentUser.UserName | bold }}
+  Path: {{ .Config.Workspace.RootPath | bold }}
+
+{{ .Trailer }}
+`
+
+func pluralize(n int, singular, plural string) string {
+	if n == 1 {
+		return fmt.Sprintf("%d %s", n, singular)
+	}
+	return fmt.Sprintf("%d %s", n, plural)
+}
+
+func renderTextOutput(cmd *cobra.Command, b *bundle.Bundle, diags diag.Diagnostics) error {
+	// Print errors and warnings.
+	for _, d := range diags {
+		var t *template.Template
+		switch d.Severity {
+		case diag.Error:
+			t = template.Must(template.New("error").Funcs(validateFuncMap).Parse(errorTemplate))
+		case diag.Warning:
+			t = template.Must(template.New("warning").Funcs(validateFuncMap).Parse(warningTemplate))
+		}
+
+		// Make file relative to bundle root
+		if d.Location.File != "" {
+			out, _ := filepath.Rel(b.RootPath, d.Location.File)
+			d.Location.File = out
+		}
+
+		// Render the diagnostic with the appropriate template.
+		err := t.Execute(cmd.OutOrStdout(), d)
+		if err != nil {
+			return err
+		}
+	}
+
+	trailer := []string{}
+	if errors := len(diags.Filter(diag.Error)); errors > 0 {
+		trailer = append(trailer, color.RedString(pluralize(errors, "error", "errors")))
+	}
+	if warnings := len(diags.Filter(diag.Warning)); warnings > 0 {
+		trailer = append(trailer, color.YellowString(pluralize(warnings, "warning", "warnings")))
+	}
+	if len(trailer) == 0 {
+		trailer = append(trailer, color.GreenString("Validation OK!"))
+	}
+
+	// Print validation summary.
+	t := template.Must(template.New("summary").Funcs(validateFuncMap).Parse(summaryTemplate))
+	err := t.Execute(cmd.OutOrStdout(), map[string]any{
+		"Config":  b.Config,
+		"Trailer": strings.Join(trailer, ", "),
+	})
+	if err != nil {
+		return err
+	}
+
+	return diags.Error()
+}
+
+func renderJsonOutput(cmd *cobra.Command, b *bundle.Bundle, diags diag.Diagnostics) error {
+	buf, err := json.MarshalIndent(b.Config, "", "  ")
+	if err != nil {
+		return err
+	}
+	cmd.OutOrStdout().Write(buf)
+	return diags.Error()
+}
 
 func newValidateCommand() *cobra.Command {
 	cmd := &cobra.Command{
@@ -44,29 +137,14 @@ func newValidateCommand() *cobra.Command {
 			return err
 		}
 
-		// Until we change up the output of this command to be a text representation,
-		// we'll just output all diagnostics as debug logs.
-		for _, d := range diags {
-			switch d.Severity {
-			case diag.Warning:
-				// Make file relative to bundle root
-				out, _ := filepath.Rel(b.RootPath, d.Location.File)
-				d.Location.File = out
-				err := cmdio.RenderWithTemplate(ctx, d, "", tmpl)
-				if err != nil {
-					return err
-				}
-			}
-
-			// log.Debugf(cmd.Context(), "[%s]: %s", diag.Location, diag.Summary)
+		switch root.OutputType(cmd) {
+		case flags.OutputText:
+			return renderTextOutput(cmd, b, diags)
+		case flags.OutputJSON:
+			return renderJsonOutput(cmd, b, diags)
+		default:
+			return fmt.Errorf("unknown output type %s", root.OutputType(cmd))
 		}
-
-		// buf, err := json.MarshalIndent(b.Config, "", "  ")
-		// if err != nil {
-		// 	return err
-		// }
-		// cmd.OutOrStdout().Write(buf)
-		return nil
 	}
 
 	return cmd
