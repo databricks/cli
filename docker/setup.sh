51da5c639e1f57ff668b91b6302c815540ce575b
Shreyas Goenka <shreyas.goenka@databricks.com>
2024-04-10 18:41:42 +0200

    more comment cleanup

--- docker/setup.sh
+++ docker/setup.sh
@@ -15,18 +15,3 @@ mv zip/terraform/terraform /app/bin/terraform
 TF_PROVIDER_NAME=terraform-provider-databricks_${DATABRICKS_TF_PROVIDER_VERSION}_linux_${ARCH}.zip
 mkdir -p /app/providers/registry.terraform.io/databricks/databricks
 wget https://github.com/databricks/terraform-provider-databricks/releases/download/v${DATABRICKS_TF_PROVIDER_VERSION}/${TF_PROVIDER_NAME} -O /app/providers/registry.terraform.io/databricks/databricks/${TF_PROVIDER_NAME}
-
-
-# TODO: document both the interactive and the non interactive workflows for
-# working with DABs with the docker container. Execing into the container
-# allows for a better iteration loop
-
-# For the interactive devloop:
-# docker run -it --entrypoint /bin/sh cli ...
-
-# For the non-interactive devloop:
-# docker run cli ...
-
-# TODO: End to end test for this image?
-
-# TODO: Final sanity check that the docker image is indeed airgapped.
