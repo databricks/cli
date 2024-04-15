562bb9f54cc59a0d0454e621eeaee5301fad49f7
Shreyas Goenka <shreyas.goenka@databricks.com>
2024-04-15 18:03:05 +0200

    Add URLs for unified auth docs to the auth command help

--- cmd/auth/auth.go
+++ cmd/auth/auth.go
@@ -12,6 +12,13 @@ func New() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "auth",
 		Short: "Authentication related commands",
+		Long: `Authentication related commands. For more information regarding how
+authentication for tooling works at Databricks, please refer to the documentation
+linked below.
+
+AWS: https://docs.databricks.com/en/dev-tools/auth/index.html
+Azure: https://learn.microsoft.com/en-us/azure/databricks/dev-tools/auth
+GCP: https://docs.gcp.databricks.com/en/dev-tools/auth/index.html`,
 	}
 
 	var perisistentAuth auth.PersistentAuth
