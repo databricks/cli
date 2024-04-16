082b33a1a53caa91c45f692948717ca2a3120978
Shreyas Goenka <shreyas.goenka@databricks.com>
2024-04-16 14:37:56 +0200

    Followup improvements to the docker setup script

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM alpine:3.19 as builder
 
 RUN ["apk", "add", "jq"]
+RUN ["apk", "add", "bash"]
 
 WORKDIR /build
 
@@ -9,7 +10,7 @@ COPY ./databricks /app/databricks
 COPY ./docker/config.tfrc /app/config/config.tfrc
 
 ARG ARCH
-RUN /build/docker/setup.sh
+RUN ["/bin/bash", "/build/docker/setup.sh"]
 
 # Start from a fresh base image, to remove any build artifacts and scripts.
 FROM alpine:3.19
--- docker/setup.sh
+++ docker/setup.sh
@@ -1,12 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 set -euo pipefail
 
 DATABRICKS_TF_VERSION=$(/app/databricks bundle debug terraform --output json | jq -r .terraform.version)
 DATABRICKS_TF_PROVIDER_VERSION=$(/app/databricks bundle debug terraform --output json | jq -r .terraform.providerVersion)
 
+# Checksums for the terraform binary version 1.5.5. The checksums are used to verify the integrity of the downloaded binary.
+# The checksums are optained from https://releases.hashicorp.com/terraform/1.5.5/
+EXPECTED_CHECKSUM="invalid"
+if [ $ARCH == "arm64" ]; then
+  EXPECTED_CHECKSUM=b055aefe343d0b710d8a7afd31aeb702b37bbf4493bb9385a709991e48dfbcd2 # linux/arm64
+elif [ $ARCH == "amd64" ]; then
+  EXPECTED_CHECKSUM=ad0c696c870c8525357b5127680cd79c0bdf58179af9acd091d43b1d6482da4a # linux/amd64
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
+
 # Download the terraform binary
 mkdir -p zip
 wget https://releases.hashicorp.com/terraform/${DATABRICKS_TF_VERSION}/terraform_${DATABRICKS_TF_VERSION}_linux_${ARCH}.zip -O zip/terraform.zip
+
+# Verify the checksum
+COMPUTED_HASH=$(sha256sum zip/terraform.zip | awk '{ print $1 }')
+if [ "$COMPUTED_HASH" != "$EXPECTED_CHECKSUM" ]; then
+    echo "Checksum mismatch for terraform binary. Version: $DATABRICKS_TF_VERSION, Arch: $ARCH, Expected checksum: $EXPECTED_CHECKSUM, Computed checksum: $COMPUTED_HASH."
+    exit 1
+fi
+
+# Unzip the terraform binary. It's safe to do so because we have already verified the checksum.
 unzip zip/terraform.zip -d zip/terraform
 mkdir -p /app/bin
 mv zip/terraform/terraform /app/bin/terraform
