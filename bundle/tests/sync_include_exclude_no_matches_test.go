053678440c3d4974562aeeb36facda1949ff396f
Andrew Nester <andrew.nester@databricks.com>
2024-04-17 14:28:17 +0200

    fix test

--- bundle/tests/sync_include_exclude_no_matches_test.go
+++ bundle/tests/sync_include_exclude_no_matches_test.go
@@ -2,6 +2,7 @@ package config_tests
 
 import (
 	"context"
+	"path/filepath"
 	"testing"
 
 	"github.com/databricks/cli/bundle"
@@ -18,21 +19,21 @@ func TestSyncIncludeExcludeNoMatchesTest(t *testing.T) {
 	require.NoError(t, diags.Error())
 	require.Equal(t, diags[0].Severity, diag.Warning)
 	require.Equal(t, diags[0].Summary, "Exclude pattern dist does not match any files")
-	require.Equal(t, diags[0].Location.File, "override_sync/databricks.yml")
+	require.Equal(t, diags[0].Location.File, filepath.Join("override_sync", "databricks.yml"))
 	require.Equal(t, diags[0].Location.Line, 17)
 	require.Equal(t, diags[0].Location.Column, 11)
 	require.Equal(t, diags[0].Path.String(), "sync.exclude[0]")
 
 	require.Equal(t, diags[1].Severity, diag.Warning)
 	require.Equal(t, diags[1].Summary, "Include pattern src/* does not match any files")
-	require.Equal(t, diags[1].Location.File, "override_sync/databricks.yml")
+	require.Equal(t, diags[1].Location.File, filepath.Join("override_sync", "databricks.yml"))
 	require.Equal(t, diags[1].Location.Line, 9)
 	require.Equal(t, diags[1].Location.Column, 7)
 	require.Equal(t, diags[1].Path.String(), "sync.include[0]")
 
 	require.Equal(t, diags[2].Severity, diag.Warning)
 	require.Equal(t, diags[2].Summary, "Include pattern tests/* does not match any files")
-	require.Equal(t, diags[2].Location.File, "override_sync/databricks.yml")
+	require.Equal(t, diags[2].Location.File, filepath.Join("override_sync", "databricks.yml"))
 	require.Equal(t, diags[2].Location.Line, 15)
 	require.Equal(t, diags[2].Location.Column, 11)
 	require.Equal(t, diags[2].Path.String(), "sync.include[1]")
