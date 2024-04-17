2368525753e41af2c03f4e98ee5377b6f8c8a2df
Andrew Nester <andrew.nester@databricks.com>
2024-04-17 14:50:42 +0200

    parallel sync validate

--- bundle/config/validate/validate_sync_patterns.go
+++ bundle/config/validate/validate_sync_patterns.go
@@ -3,10 +3,12 @@ package validate
 import (
 	"context"
 	"fmt"
+	"sync"
 
 	"github.com/databricks/cli/bundle"
 	"github.com/databricks/cli/libs/diag"
 	"github.com/databricks/cli/libs/fileset"
+	"golang.org/x/sync/errgroup"
 )
 
 func ValidateSyncPatterns() bundle.ReadOnlyMutator {
@@ -21,59 +23,57 @@ func (v *validateSyncPatterns) Name() string {
 }
 
 func (v *validateSyncPatterns) Apply(ctx context.Context, rb bundle.ReadOnlyBundle) diag.Diagnostics {
-	sync := rb.Config().Sync()
-	if len(sync.Exclude) == 0 && len(sync.Include) == 0 {
+	s := rb.Config().Sync()
+	if len(s.Exclude) == 0 && len(s.Include) == 0 {
 		return nil
 	}
 
-	diags := diag.Diagnostics{}
-	if len(sync.Exclude) != 0 {
-		for i, exclude := range sync.Exclude {
-			fs, err := fileset.NewGlobSet(rb.RootPath(), []string{exclude})
+	diags, err := checkPatterns(s.Exclude, "sync.exclude", rb)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	includeDiags, err := checkPatterns(s.Include, "sync.include", rb)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return diags.Extend(includeDiags)
+}
+
+func checkPatterns(patterns []string, path string, rb bundle.ReadOnlyBundle) (diag.Diagnostics, error) {
+	var mu sync.Mutex
+	var errs errgroup.Group
+	var diags diag.Diagnostics
+
+	for i, pattern := range patterns {
+		index := i
+		p := pattern
+		errs.Go(func() error {
+			fs, err := fileset.NewGlobSet(rb.RootPath(), []string{p})
 			if err != nil {
-				return diag.FromErr(err)
+				return err
 			}
 
 			all, err := fs.All()
 			if err != nil {
-				return diag.FromErr(err)
+				return err
 			}
 
 			if len(all) == 0 {
-				loc := location{path: fmt.Sprintf("sync.exclude[%d]", i), rb: rb}
+				loc := location{path: fmt.Sprintf("%s[%d]", path, index), rb: rb}
+				mu.Lock()
 				diags = diags.Append(diag.Diagnostic{
 					Severity: diag.Warning,
-					Summary:  fmt.Sprintf("Exclude pattern %s does not match any files", exclude),
+					Summary:  fmt.Sprintf("Pattern %s does not match any files", p),
 					Location: loc.Location(),
 					Path:     loc.Path(),
 				})
+				mu.Unlock()
 			}
-		}
+			return nil
+		})
 	}
 
-	if len(sync.Include) != 0 {
-		for i, include := range sync.Include {
-			fs, err := fileset.NewGlobSet(rb.RootPath(), []string{include})
-			if err != nil {
-				return diag.FromErr(err)
-			}
-
-			all, err := fs.All()
-			if err != nil {
-				return diag.FromErr(err)
-			}
-
-			if len(all) == 0 {
-				loc := location{path: fmt.Sprintf("sync.include[%d]", i), rb: rb}
-				diags = diags.Append(diag.Diagnostic{
-					Severity: diag.Warning,
-					Summary:  fmt.Sprintf("Include pattern %s does not match any files", include),
-					Location: loc.Location(),
-					Path:     loc.Path(),
-				})
-			}
-		}
-	}
-
-	return diags
+	return diags, errs.Wait()
 }
--- bundle/tests/sync_include_exclude_no_matches_test.go
+++ bundle/tests/sync_include_exclude_no_matches_test.go
@@ -17,24 +17,22 @@ func TestSyncIncludeExcludeNoMatchesTest(t *testing.T) {
 	diags := bundle.ApplyReadOnly(context.Background(), bundle.ReadOnly(b), validate.ValidateSyncPatterns())
 	require.Len(t, diags, 3)
 	require.NoError(t, diags.Error())
+
 	require.Equal(t, diags[0].Severity, diag.Warning)
-	require.Equal(t, diags[0].Summary, "Exclude pattern dist does not match any files")
+	require.Equal(t, diags[0].Summary, "Pattern dist does not match any files")
 	require.Equal(t, diags[0].Location.File, filepath.Join("override_sync", "databricks.yml"))
 	require.Equal(t, diags[0].Location.Line, 17)
 	require.Equal(t, diags[0].Location.Column, 11)
 	require.Equal(t, diags[0].Path.String(), "sync.exclude[0]")
 
+	summaries := []string{
+		diags[1].Summary,
+		diags[2].Summary,
+	}
+
 	require.Equal(t, diags[1].Severity, diag.Warning)
-	require.Equal(t, diags[1].Summary, "Include pattern src/* does not match any files")
-	require.Equal(t, diags[1].Location.File, filepath.Join("override_sync", "databricks.yml"))
-	require.Equal(t, diags[1].Location.Line, 9)
-	require.Equal(t, diags[1].Location.Column, 7)
-	require.Equal(t, diags[1].Path.String(), "sync.include[0]")
+	require.Contains(t, summaries, diags[1].Summary)
 
 	require.Equal(t, diags[2].Severity, diag.Warning)
-	require.Equal(t, diags[2].Summary, "Include pattern tests/* does not match any files")
-	require.Equal(t, diags[2].Location.File, filepath.Join("override_sync", "databricks.yml"))
-	require.Equal(t, diags[2].Location.Line, 15)
-	require.Equal(t, diags[2].Location.Column, 11)
-	require.Equal(t, diags[2].Path.String(), "sync.include[1]")
+	require.Contains(t, summaries, diags[2].Summary)
 }
