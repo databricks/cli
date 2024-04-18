c3a7d17d1d97db17a585cbb06abfa3ff6a58d4e0
Lennart Kats (databricks) <lennart.kats@databricks.com>
2024-04-17 18:59:39 -0700

    Disable locking for development mode (#1302)
    ## Changes
    
    This changes `databricks bundle deploy` so that it skips the lock
    acquisition/release step for a `mode: development` target:
    * This saves about 2 seconds (measured over 100 runs on a quiet/busy
    workspace).
    * This helps avoid the `deploy lock acquired by lennart@company.com at
    2024-02-28 15:48:38.40603 +0100 CET. Use --force-lock to override` error
    * Risk: this may cause deployment conflicts, but since dev mode
    deployments are always scoped to a user, that risk should be minimal
    
    Update after discussion:
    * This behavior can now be disabled via a setting.
    * Docs PR: https://github.com/databricks/docs/pull/15873
    
    ## Measurements
    
    ### 100 deployments of the "python_default" project to an empty
    workspace
    
    _Before this branch:_
    p50 time: 11.479 seconds
    p90 time: 11.757 seconds
    
    _After this branch:_
    p50 time: 9.386 seconds
    p90 time: 9.599 seconds
    
    ### 100 deployments of the "python_default" project to a busy (staging)
    workspace
    
    _Before this branch:_
    * p50 time: 13.335 seconds
    * p90 time: 15.295 seconds
    
    _After this branch:_
    * p50 time: 11.397 seconds
    * p90 time: 11.743 seconds
    
    ### Typical duration of deployment steps
    
    * Acquiring Deployment Lock: 1.096 seconds
    * Deployment Preparations and Operations: 1.477 seconds
    * Uploading Artifacts: 1.26 seconds
    * Finalizing Deployment: 9.699 seconds
    * Releasing Deployment Lock: 1.198 seconds
    
    ---------
    
    Co-authored-by: Pieter Noordhuis <pcnoordhuis@gmail.com>
    Co-authored-by: Andrew Nester <andrew.nester.dev@gmail.com>

--- bundle/config/lock.go
+++ bundle/config/lock.go
@@ -1,7 +1,7 @@
 package config
 
 type Lock struct {
-	// Enabled toggles deployment lock. True by default.
+	// Enabled toggles deployment lock. True by default except in development mode.
 	// Use a pointer value so that only explicitly configured values are set
 	// and we don't merge configuration with zero-initialized values.
 	Enabled *bool `json:"enabled,omitempty"`
@@ -11,9 +11,20 @@ type Lock struct {
 	Force bool `json:"force,omitempty"`
 }
 
+// IsEnabled checks if the deployment lock is enabled.
 func (lock Lock) IsEnabled() bool {
 	if lock.Enabled != nil {
 		return *lock.Enabled
 	}
 	return true
+}
+
+// IsExplicitlyEnabled checks if the deployment lock is explicitly enabled.
+// Only returns true if locking is explicitly set using a command-line
+// flag or configuration file.
+func (lock Lock) IsExplicitlyEnabled() bool {
+	if lock.Enabled != nil {
+		return *lock.Enabled
+	}
+	return false
 }
