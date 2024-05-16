0b68093d52493eec259f909bae98606149016cc8
Miles Yucht <miles@databricks.com>
2024-05-16 10:20:38 +0200

    better error

--- cmd/auth/token.go
+++ cmd/auth/token.go
@@ -61,7 +61,7 @@ func newTokenCommand(persistentAuth *auth.PersistentAuth) *cobra.Command {
 			profileName = profileFlag.Value.String()
 			// If a profile is provided we read the host from the .databrickscfg file
 			if profileName != "" && len(args) > 0 {
-				return errors.New("providing both a profile and the host parameter is not supported")
+				return errors.New("providing both a profile and host is not supported")
 			}
 		}
 
