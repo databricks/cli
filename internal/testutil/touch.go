d08a650670589976b609c31f3acba5706309a7d1
Shreyas Goenka <shreyas.goenka@databricks.com>
2024-06-17 18:57:34 +0200

    Serialize dynamic value for bundle validate output

--- internal/testutil/touch.go
+++ internal/testutil/touch.go
@@ -24,3 +24,15 @@ func Touch(t *testing.T, elems ...string) string {
 	f.Close()
 	return path
 }
+
+func TouchWithContent(t *testing.T, content string, elems ...string) string {
+	path := filepath.Join(elems...)
+	os.MkdirAll(filepath.Dir(path), 0755)
+	f, err := os.Create(path)
+	require.NoError(t, err)
+	_, err = f.WriteString(content)
+	require.NoError(t, err)
+	err = f.Close()
+	require.NoError(t, err)
+	return path
+}
