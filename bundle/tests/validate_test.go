441a76f7e9aff0e3a240cb0134ba0e3507e8f0ee
Shreyas Goenka <shreyas.goenka@databricks.com>
2024-06-18 16:12:38 +0200

    -

--- bundle/tests/validate_test.go
+++ bundle/tests/validate_test.go
@@ -21,7 +21,7 @@ func TestConflictingConfigurationValidate(t *testing.T) {
 	assert.Len(t, diags, 2)
 	assert.Contains(t, diags, diag.Diagnostic{
 		Severity: diag.Warning,
-		Summary:  "Multiple values found for the same configuration variables.baz.default. Only the value from location resources2.yml:3:14 will be used. Locations found: [resources2.yml:3:14 resources1.yml:9:14]",
+		Summary:  "Multiple values found for the same configuration variables.baz.default. Only the value from location resources2.yml:3:14 will be used. Locations found: [resources2.yml:3:14 resources1.yml:6:14]",
 		Location: dyn.Location{
 			File:   filepath.FromSlash("validate/conflicting_configuration/resources2.yml"),
 			Line:   3,
@@ -29,12 +29,12 @@ func TestConflictingConfigurationValidate(t *testing.T) {
 		},
 		Path: dyn.MustPathFromString("variables.baz.default"),
 	})
-	assert.Equal(t, diags[0], diag.Diagnostic{
+	assert.Contains(t, diags, diag.Diagnostic{
 		Severity: diag.Warning,
-		Summary:  "Multiple values found for the same configuration variables.bar.default. Only the value from location resources1.yml:6:14 will be used. Locations found: [resources1.yml:6:14 databricks.yml:9:14]",
+		Summary:  "Multiple values found for the same configuration variables.bar.default. Only the value from location resources1.yml:3:14 will be used. Locations found: [resources1.yml:3:14 databricks.yml:9:14]",
 		Location: dyn.Location{
 			File:   filepath.FromSlash("validate/conflicting_configuration/resources1.yml"),
-			Line:   6,
+			Line:   3,
 			Column: 14,
 		},
 		Path: dyn.MustPathFromString("variables.bar.default"),
