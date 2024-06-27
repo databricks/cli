7e964f7f09344569ffcf44e70413a58add0e9c6a
Gleb Kanterov <gleb.kanterov@databricks.com>
2024-06-27 15:16:12 +0200

    Fix tests

--- bundle/tests/suggest_target_test.go
+++ bundle/tests/suggest_target_test.go
@@ -1,17 +1,21 @@
 package config_tests
 
 import (
+	"github.com/databricks/cli/cmd/root"
+	assert "github.com/databricks/cli/libs/dyn/dynassert"
 	"path/filepath"
 	"testing"
 
 	"github.com/databricks/cli/internal"
-	"github.com/stretchr/testify/require"
 )
 
 func TestSuggestTargetIfWrongPassed(t *testing.T) {
 	t.Setenv("BUNDLE_ROOT", filepath.Join("target_overrides", "workspace"))
-	_, _, err := internal.RequireErrorRun(t, "bundle", "validate", "-e", "incorrect")
-	require.ErrorContains(t, err, "Available targets:")
-	require.ErrorContains(t, err, "development")
-	require.ErrorContains(t, err, "staging")
+	stdoutBytes, _, err := internal.RequireErrorRun(t, "bundle", "validate", "-e", "incorrect")
+	stdout := stdoutBytes.String()
+
+	assert.Error(t, root.ErrAlreadyPrinted, err)
+	assert.Contains(t, stdout, "Available targets:")
+	assert.Contains(t, stdout, "development")
+	assert.Contains(t, stdout, "staging")
 }
