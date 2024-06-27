a461d9378b313198e498517b27e29265d7a50f9d
Gleb Kanterov <gleb.kanterov@databricks.com>
2024-06-27 13:40:29 +0200

    Fix build

--- cmd/bundle/validate.go
+++ cmd/bundle/validate.go
@@ -7,7 +7,7 @@ import (
 	"github.com/databricks/cli/bundle"
 	"github.com/databricks/cli/bundle/config/validate"
 	"github.com/databricks/cli/bundle/phases"
-	"github.com/databricks/cli/cmd/bundle/render"
+	"github.com/databricks/cli/bundle/render"
 	"github.com/databricks/cli/cmd/bundle/utils"
 	"github.com/databricks/cli/cmd/root"
 	"github.com/databricks/cli/libs/diag"
