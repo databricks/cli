13eb5fb524dfd48d9b4da37405f9bc3dbf14d92f
Andrew Nester <andrew.nester@databricks.com>
2024-07-02 13:40:29 +0200

    Update bundle/config/mutator/resolve_variable_references.go
    Co-authored-by: Pieter Noordhuis <pieter.noordhuis@databricks.com>

--- bundle/config/mutator/resolve_variable_references.go
+++ bundle/config/mutator/resolve_variable_references.go
@@ -75,9 +75,12 @@ func lookupForComplexVariables(v dyn.Value, path dyn.Path) (dyn.Value, error) {
 }
 
 func skipResolvingInNonComplexVariables(v dyn.Value) bool {
-	_, okMap := v.AsMap()
-	_, okSeq := v.AsSequence()
-	return !okMap && !okSeq
+	switch v.Kind() {
+	case dyn.KindMap, dyn.KindSequence:
+		return false
+	default:
+		return true
+	}
 }
 
 func lookupForVariables(v dyn.Value, path dyn.Path) (dyn.Value, error) {
