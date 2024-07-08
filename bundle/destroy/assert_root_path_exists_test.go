34c4d975e7729f016dd249f6e368764ebe85c5bb
Shreyas Goenka <shreyas.goenka@databricks.com>
2024-07-08 19:58:00 +0200

    add test for the mutator

---
+++ bundle/destroy/assert_root_path_exists_test.go
@@ -0,0 +1,34 @@
+package destroy
+
+import (
+	"context"
+	"testing"
+
+	"github.com/databricks/cli/bundle"
+	"github.com/databricks/cli/bundle/config"
+	"github.com/databricks/databricks-sdk-go/apierr"
+	"github.com/databricks/databricks-sdk-go/experimental/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestAssertRootPathExists(t *testing.T) {
+	b := &bundle.Bundle{
+		Config: config.Root{
+			Workspace: config.Workspace{
+				RootPath: "/path/to/root",
+			},
+		},
+	}
+
+	m := mocks.NewMockWorkspaceClient(t)
+	b.SetWorkpaceClient(m.WorkspaceClient)
+	workspaceApi := m.GetMockWorkspaceAPI()
+	workspaceApi.EXPECT().GetStatusByPath(mock.Anything, "/path/to/root").Return(nil, &apierr.APIError{
+		StatusCode: 404,
+		ErrorCode:  "RESOURCE_DOES_NOT_EXIST",
+	})
+
+	diags := bundle.Apply(context.Background(), b, AssertRootPathExists())
+	assert.Equal(t, bundle.DiagnosticBreakSequence, diags)
+}
