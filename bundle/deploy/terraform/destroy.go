4f15fe9d38fb6f26598b2a1bb0aeaa685d198a1f
Shreyas Goenka <shreyas.goenka@databricks.com>
2024-07-09 20:01:40 +0200

    -

--- bundle/deploy/terraform/destroy.go
+++ bundle/deploy/terraform/destroy.go
@@ -19,7 +19,7 @@ func (w *destroy) Name() string {
 func (w *destroy) Apply(ctx context.Context, b *bundle.Bundle) diag.Diagnostics {
 	// return early if plan is empty
 	if b.Plan.IsEmpty {
-		cmdio.LogString(ctx, "No resources to destroy`")
+		cmdio.LogString(ctx, "No resources to destroy")
 		return nil
 	}
 
