abbb891992f1e4941dfa0d5c78dbe022745716c6
Shreyas Goenka <shreyas.goenka@databricks.com>
2024-07-09 11:42:06 +0200

    more coverage for assert root path

--- bundle/destroy/assert_root_path_exists.go
+++ bundle/destroy/assert_root_path_exists.go
@@ -3,6 +3,7 @@ package destroy
 import (
 	"context"
 	"errors"
+	"fmt"
 	"net/http"
 
 	"github.com/databricks/cli/bundle"
@@ -26,13 +27,11 @@ func (m *assertRootPathExists) Apply(ctx context.Context, b *bundle.Bundle) diag
 	w := b.WorkspaceClient()
 	_, err := w.Workspace.GetStatusByPath(ctx, b.Config.Workspace.RootPath)
 
-	if err != nil {
-		var aerr *apierr.APIError
-		if errors.As(err, &aerr) && aerr.StatusCode == http.StatusNotFound {
-			log.Infof(ctx, "No active deployment found. %s does not exist. Skipping destroy.", b.Config.Workspace.RootPath)
-			cmdio.LogString(ctx, "No active deployment found to destroy!")
-			return bundle.DiagnosticSequenceBreak
-		}
+	var aerr *apierr.APIError
+	if errors.As(err, &aerr) && aerr.StatusCode == http.StatusNotFound {
+		log.Infof(ctx, "No active deployment found. %s does not exist. Skipping destroy.", b.Config.Workspace.RootPath)
+		cmdio.LogString(ctx, "No active deployment found to destroy!")
+		return bundle.DiagnosticSequenceBreak
 	}
-	return nil
+	return diag.FromErr(fmt.Errorf("cannot assert root path exists: %w", err))
 }
--- bundle/destroy/assert_root_path_exists_test.go
+++ bundle/destroy/assert_root_path_exists_test.go
@@ -2,6 +2,7 @@ package destroy
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/databricks/cli/bundle"
@@ -30,5 +31,23 @@ func TestAssertRootPathExists(t *testing.T) {
 	})
 
 	diags := bundle.Apply(context.Background(), b, AssertRootPathExists())
-	assert.Equal(t, bundle.DiagnosticSequenceBreak, diags)
+	assert.Equal(t, bundle.ErrorSequenceBreak, diags.Error())
+}
+
+func TestAssertRootPathExistsIgnoresNon404Errors(t *testing.T) {
+	b := &bundle.Bundle{
+		Config: config.Root{
+			Workspace: config.Workspace{
+				RootPath: "/path/to/root",
+			},
+		},
+	}
+
+	m := mocks.NewMockWorkspaceClient(t)
+	b.SetWorkpaceClient(m.WorkspaceClient)
+	workspaceApi := m.GetMockWorkspaceAPI()
+	workspaceApi.EXPECT().GetStatusByPath(mock.Anything, "/path/to/root").Return(nil, errors.New("wsfs API failed"))
+
+	diags := bundle.Apply(context.Background(), b, AssertRootPathExists())
+	assert.EqualError(t, diags.Error(), "cannot assert root path exists: wsfs API failed")
 }
