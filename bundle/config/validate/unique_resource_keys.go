a48909598bf9f9b2ba540e87c1e450076b608480
Shreyas Goenka <shreyas.goenka@databricks.com>
2024-07-23 15:15:46 +0200

    first version

--- bundle/config/validate/unique_resource_keys.go
+++ bundle/config/validate/unique_resource_keys.go
@@ -14,31 +14,19 @@ func UniqueResourceKeys() bundle.ReadOnlyMutator {
 }
 
 // TODO: Might need to enforce sorted walk on dyn.Walk
-// TODO: return multiple locations with diagnostics. Can be a followup.
 type uniqueResourceKeys struct{}
 
 func (m *uniqueResourceKeys) Name() string {
 	return "validate:unique_resource_keys"
 }
 
-func conflictingResourceKeysErr(key string, p1 dyn.Path, l1 dyn.Location, p2 dyn.Path, l2 dyn.Location) diag.Diagnostic {
-	return diag.Diagnostic{
-		Severity: diag.Error,
-		Summary:  fmt.Sprintf("multiple resources have been defined with the same key: %s (%s at %s, %s at %s)", key, p1, l1, p2, l2),
-		Location: l1,
-	}
-}
-
 func (m *uniqueResourceKeys) Apply(ctx context.Context, rb bundle.ReadOnlyBundle) diag.Diagnostics {
 	diags := diag.Diagnostics{}
 
-	type r struct {
-		p dyn.Path
-		l dyn.Location
-	}
-	seenResource := make(map[string]r)
+	// Map of resource key to the paths and locations the resource is defined at.
+	paths := map[string][]dyn.Path{}
+	locations := map[string][]dyn.Location{}
 
-	seenResources := make(map[string]dyn.Location)
 	_, err := dyn.Walk(rb.Config().Value().Get("resources"), func(p dyn.Path, v dyn.Value) (dyn.Value, error) {
 		// The path is expected to be of length 2, and of the form <resource_type>.<resource_identifier>.
 		// Eg: jobs.my_job, pipelines.my_pipeline, etc.
@@ -49,39 +37,36 @@ func (m *uniqueResourceKeys) Apply(ctx context.Context, rb bundle.ReadOnlyBundle
 			return v, dyn.ErrSkip
 		}
 
-		// Each resource should be completely defined in a single YAML file. We
-		// do not allow users to split the definition of a single resource across
-		// multiple files.
-		// Users can use simple / complex variables to modularize their configuration.
-		if locations := v.Locations(); len(locations) >= 2 {
-			diags = append(diags, conflictingResourceKeysErr(p[1].Key(), p, locations[0], p, locations[1]))
-		}
+		// The key for the resource. Eg: "my_job" for jobs.my_job.
+		k := p[1].Key()
 
-		// l, ok := seenResources[p[1].Key()]
-		// if ok {
-		// 	diags = append(diags, conflictingResourceKeysErr(p[1].Key(), p, l, p, v.Locations()[0]))
-		// } else {
-		// 	seenResources[p[1].Key()] = v.Locations()[0]
-		// }
-
-		// // key for the source. Eg: "my_job" for jobs.my_job.
-		// key := p[1].Key()
-		// info, ok := seenResources[key]
-
-		// for _, l := range v.Locations() {
-		// 	info, ok := seenResources[p[1].Key()]
-		// 	if !ok {
-		// 		seenResources[p[1].Key()] = resourceInfo{p, l}
-		// 		continue
-		// 	}
-
-		// 	diags = append(diags)
-		// }
+		paths[k] = append(paths[k], p)
+		locations[k] = append(locations[k], v.Locations()...)
 		return v, nil
 	})
 	if err != nil {
 		diags = append(diags, diag.FromErr(err)...)
 	}
 
+	for k, ps := range paths {
+		if len(ps) <= 1 {
+			continue
+		}
+
+		// TODO: What happens on target overrides? Ensure they do not misbehave.
+		// 1. What was the previous behaviour for target overrides?
+		// 2. What if a completely new resource with a conflicting key is defined
+		// in a target override.
+		//
+		// If there are multiple resources with the same key, report an error.
+		// NOTE: This includes if the same resource is defined in multiple files as
+		// TODO: continue this comment.
+		diags = append(diags, diag.Diagnostic{
+			Severity:  diag.Error,
+			Summary:   fmt.Sprintf("multiple resources have been defined with the same key: %s", k),
+			Locations: locations[k],
+			Paths:     ps,
+		})
+	}
 	return diags
 }
