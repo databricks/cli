e6241e196fe2ae37bcf6709243eabdeeb94f261a
shreyas-goenka <88374338+shreyas-goenka@users.noreply.github.com>
2024-07-24 18:32:19 +0530

    Move to a single prompt during bundle destroy (#1583)
    ## Changes
    Right now we ask users for two confirmations when destroying a bundle.
    One to destroy the resources and one to delete the files. This PR
    consolidates the two prompts into one.
    
    ## Tests
    Manually
    
    Destroying a bundle with no resources:
    ```
    ➜  bundle-playground git:(master) ✗ cli bundle destroy
    All files and directories at the following location will be deleted: /Users/shreyas.goenka@databricks.com/.bundle/bundle-playground/default
    
    Would you like to proceed? [y/n]: y
    No resources to destroy
    Updating deployment state...
    Deleting files...
    Destroy complete!
    ```
    
    Destroying a bundle with no remote state:
    ```
    ➜  bundle-playground git:(master) ✗ cli bundle destroy
    No active deployment found to destroy!
    ```
    
    When a user cancells a deployment:
    ```
    ➜  bundle-playground git:(master) ✗ cli bundle destroy
    The following resources will be deleted:
      delete job job_1
      delete job job_2
      delete pipeline foo
    
    All files and directories at the following location will be deleted: /Users/shreyas.goenka@databricks.com/.bundle/bundle-playground/default
    
    Would you like to proceed? [y/n]: n
    Destroy cancelled!
    ```
    
    When a user destroys resources:
    ```
    ➜  bundle-playground git:(master) ✗ cli bundle destroy
    The following resources will be deleted:
      delete job job_1
      delete job job_2
      delete pipeline foo
    
    All files and directories at the following location will be deleted: /Users/shreyas.goenka@databricks.com/.bundle/bundle-playground/default
    
    Would you like to proceed? [y/n]: y
    Updating deployment state...
    Deleting files...
    Destroy complete!
    ```

--- bundle/deploy/files/delete.go
+++ bundle/deploy/files/delete.go
@@ -12,7 +12,6 @@ import (
 	"github.com/databricks/cli/libs/diag"
 	"github.com/databricks/cli/libs/sync"
 	"github.com/databricks/databricks-sdk-go/service/workspace"
-	"github.com/fatih/color"
 )
 
 type delete struct{}
@@ -22,24 +21,7 @@ func (m *delete) Name() string {
 }
 
 func (m *delete) Apply(ctx context.Context, b *bundle.Bundle) diag.Diagnostics {
-	// Do not delete files if terraform destroy was not consented
-	if !b.Plan.IsEmpty && !b.Plan.ConfirmApply {
-		return nil
-	}
-
-	cmdio.LogString(ctx, "Starting deletion of remote bundle files")
-	cmdio.LogString(ctx, fmt.Sprintf("Bundle remote directory is %s", b.Config.Workspace.RootPath))
-
-	red := color.New(color.FgRed).SprintFunc()
-	if !b.AutoApprove {
-		proceed, err := cmdio.AskYesOrNo(ctx, fmt.Sprintf("\n%s and all files in it will be %s Proceed?", b.Config.Workspace.RootPath, red("deleted permanently!")))
-		if err != nil {
-			return diag.FromErr(err)
-		}
-		if !proceed {
-			return nil
-		}
-	}
+	cmdio.LogString(ctx, "Deleting files...")
 
 	err := b.WorkspaceClient().Workspace.Delete(ctx, workspace.Delete{
 		Path:      b.Config.Workspace.RootPath,
@@ -54,8 +36,6 @@ func (m *delete) Apply(ctx context.Context, b *bundle.Bundle) diag.Diagnostics {
 	if err != nil {
 		return diag.FromErr(err)
 	}
-
-	cmdio.LogString(ctx, "Successfully deleted files!")
 	return nil
 }
 
