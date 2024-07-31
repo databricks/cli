databricks/cli

commit 89c0af5bdcee82bc01a27930c86d550402eedc96

Add resource for UC schemas to DABs (#1413)
## Changes
This PR adds support for UC Schemas to DABs. This allows users to define
schemas for tables and other assets their pipelines/workflows create as
part of the DAB, thus managing the life-cycle in the DAB.

The first version has a couple of intentional limitations:
1. The owner of the schema will be the deployment user. Changing the
owner of the schema is not allowed (yet). `run_as` will not be
restricted for DABs containing UC schemas. Let's limit the scope of
run_as to the compute identity used instead of ownership of data assets
like UC schemas.
2. API fields that are present in the update API but not the create API.
For example: enabling predictive optimization is not supported in the
create schema API and thus is not available in DABs at the moment.

## Tests
Manually and integration test. Manually verified the following work:
1. Development mode adds a "dev_" prefix.
2. Modified status is correctly computed in the `bundle summary`
command.
3. Grants work as expected, for assigning privileges.
4. Variable interpolation works for the schema ID.

bundle/phases/deploy.go
@@ -1,6 +1,9 @@
 package phases
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/databricks/cli/bundle"
 	"github.com/databricks/cli/bundle/artifacts"
 	"github.com/databricks/cli/bundle/config"
@@ -14,10 +17,91 @@ import (
 	"github.com/databricks/cli/bundle/permissions"
 	"github.com/databricks/cli/bundle/python"
 	"github.com/databricks/cli/bundle/scripts"
+	"github.com/databricks/cli/libs/cmdio"
+	terraformlib "github.com/databricks/cli/libs/terraform"
 )
+
+func approvalForUcSchemaDelete(ctx context.Context, b *bundle.Bundle) (bool, error) {
+	tf := b.Terraform
+	if tf == nil {
+		return false, fmt.Errorf("terraform not initialized")
+	}
+
+	// read plan file
+	plan, err := tf.ShowPlanFile(ctx, b.Plan.Path)
+	if err != nil {
+		return false, err
+	}
+
+	actions := make([]terraformlib.Action, 0)
+	for _, rc := range plan.ResourceChanges {
+		// We only care about destructive actions on UC schema resources.
+		if rc.Type != "databricks_schema" {
+			continue
+		}
+
+		var actionType terraformlib.ActionType
+
+		switch {
+		case rc.Change.Actions.Delete():
+			actionType = terraformlib.ActionTypeDelete
+		case rc.Change.Actions.Replace():
+			actionType = terraformlib.ActionTypeRecreate
+		default:
+			// We don't need a prompt for non-destructive actions like creating
+			// or updating a schema.
+			continue
+		}
+
+		actions = append(actions, terraformlib.Action{
+			Action:       actionType,
+			ResourceType: rc.Type,
+			ResourceName: rc.Name,
+		})
+	}
+
+	// No restricted actions planned. No need for approval.
+	if len(actions) == 0 {
+		return true, nil
+	}
+
+	cmdio.LogString(ctx, "The following UC schemas will be deleted or recreated. Any underlying data may be lost:")
+	for _, action := range actions {
+		cmdio.Log(ctx, action)
+	}
+
+	if b.AutoApprove {
+		return true, nil
+	}
+
+	if !cmdio.IsPromptSupported(ctx) {
+		return false, fmt.Errorf("the deployment requires destructive actions, but current console does not support prompting. Please specify --auto-approve if you would like to skip prompts and proceed")
+	}
+
+	cmdio.LogString(ctx, "")
+	approved, err := cmdio.AskYesOrNo(ctx, "Would you like to proceed?")
+	if err != nil {
+		return false, err
+	}
+
+	return approved, nil
+}
 
 // The deploy phase deploys artifacts and resources.
 func Deploy() bundle.Mutator {
+	// Core mutators that CRUD resources and modify deployment state. These
+	// mutators need informed consent if they are potentially destructive.
+	deployCore := bundle.Defer(
+		terraform.Apply(),
+		bundle.Seq(
+			terraform.StatePush(),
+			terraform.Load(),
+			metadata.Compute(),
+			metadata.Upload(),
+			bundle.LogString("Deployment complete!"),
+		),
+	)
+
 	deployMutator := bundle.Seq(
 		scripts.Execute(config.ScriptPreDeploy),
 		lock.Acquire(),
@@ -37,20 +121,16 @@ func Deploy() bundle.Mutator {
 				terraform.Interpolate(),
 				terraform.Write(),
 				terraform.CheckRunningResource(),
-				bundle.Defer(
-					terraform.Apply(),
-					bundle.Seq(
-						terraform.StatePush(),
-						terraform.Load(),
-						metadata.Compute(),
-						metadata.Upload(),
-					),
+				terraform.Plan(terraform.PlanGoal("deploy")),
+				bundle.If(
+					approvalForUcSchemaDelete,
+					deployCore,
+					bundle.LogString("Deployment cancelled!"),
 				),
 			),
 			lock.Release(lock.GoalDeploy),
 		),
 		scripts.Execute(config.ScriptPostDeploy),
-		bundle.LogString("Deployment complete!"),
 	)
 
 	return newPhase(

cmd/bundle/deploy.go
@@ -24,10 +24,12 @@ func newDeployCommand() *cobra.Command {
 	var forceLock bool
 	var failOnActiveRuns bool
 	var computeID string
+	var autoApprove bool
 	cmd.Flags().BoolVar(&force, "force", false, "Force-override Git branch validation.")
 	cmd.Flags().BoolVar(&forceLock, "force-lock", false, "Force acquisition of deployment lock.")
 	cmd.Flags().BoolVar(&failOnActiveRuns, "fail-on-active-runs", false, "Fail if there are running jobs or pipelines in the deployment.")
 	cmd.Flags().StringVarP(&computeID, "compute-id", "c", "", "Override compute in the deployment with the given compute ID.")
+	cmd.Flags().BoolVar(&autoApprove, "auto-approve", false, "Skip interactive approvals that might be required for deployment.")
 
 	cmd.RunE = func(cmd *cobra.Command, args []string) error {
 		ctx := cmd.Context()
@@ -37,10 +39,11 @@ func newDeployCommand() *cobra.Command {
 			bundle.ApplyFunc(ctx, b, func(context.Context, *bundle.Bundle) diag.Diagnostics {
 				b.Config.Bundle.Force = force
 				b.Config.Bundle.Deployment.Lock.Force = forceLock
+				b.AutoApprove = autoApprove
+
 				if cmd.Flag("compute-id").Changed {
 					b.Config.Bundle.ComputeID = computeID
 				}
-
 				if cmd.Flag("fail-on-active-runs").Changed {
 					b.Config.Bundle.Deployment.FailOnActiveRuns = failOnActiveRuns
 				}

bundle/config/mutator/process_target_mode.go
@@ -112,6 +112,13 @@ func transformDevelopmentMode(ctx context.Context, b *bundle.Bundle) diag.Diagno
 		}
 	}
 
+	for i := range r.Schemas {
+		prefix = "dev_" + b.Config.Workspace.CurrentUser.ShortName + "_"
+		r.Schemas[i].Name = prefix + r.Schemas[i].Name
+		// HTTP API for schemas doesn't yet support tags. It's only supported in
+		// the Databricks UI and via the SQL API.
+	}
+
 	return nil
 }
 

bundle/config/mutator/process_target_mode_test.go
@@ -114,6 +114,9 @@ func mockBundle(mode config.Mode) *bundle.Bundle {
 						},
 					},
 				},
+				Schemas: map[string]*resources.Schema{
+					"schema1": {CreateSchema: &catalog.CreateSchema{Name: "schema1"}},
+				},
 			},
 		},
 		// Use AWS implementation for testing.
@@ -167,6 +170,9 @@ func TestProcessTargetModeDevelopment(t *testing.T) {
 	assert.Equal(t, "qualityMonitor1", b.Config.Resources.QualityMonitors["qualityMonitor1"].TableName)
 	assert.Nil(t, b.Config.Resources.QualityMonitors["qualityMonitor2"].Schedule)
 	assert.Equal(t, catalog.MonitorCronSchedulePauseStatusUnpaused, b.Config.Resources.QualityMonitors["qualityMonitor3"].Schedule.PauseStatus)
+
+	// Schema 1
+	assert.Equal(t, "dev_lennart_schema1", b.Config.Resources.Schemas["schema1"].Name)
 }
 
 func TestProcessTargetModeDevelopmentTagNormalizationForAws(t *testing.T) {

bundle/config/mutator/run_as_test.go
@@ -39,6 +39,7 @@ func allResourceTypes(t *testing.T) []string {
 		"pipelines",
 		"quality_monitors",
 		"registered_models",
+		"schemas",
 	},
 		resourceTypes,
 	)
@@ -136,6 +137,7 @@ func TestRunAsErrorForUnsupportedResources(t *testing.T) {
 		"models",
 		"registered_models",
 		"experiments",
+		"schemas",
 	}
 
 	base := config.Root{

bundle/config/resources.go
@@ -18,6 +18,7 @@ type Resources struct {
 	ModelServingEndpoints map[string]*resources.ModelServingEndpoint `json:"model_serving_endpoints,omitempty"`
 	RegisteredModels      map[string]*resources.RegisteredModel      `json:"registered_models,omitempty"`
 	QualityMonitors       map[string]*resources.QualityMonitor       `json:"quality_monitors,omitempty"`
+	Schemas               map[string]*resources.Schema               `json:"schemas,omitempty"`
 }
 
 type resource struct {

bundle/config/resources/schema.go
@@ -1,0 +1,27 @@
+package resources
+
+import (
+	"github.com/databricks/databricks-sdk-go/marshal"
+	"github.com/databricks/databricks-sdk-go/service/catalog"
+)
+
+type Schema struct {
+	// List of grants to apply on this schema.
+	Grants []Grant `json:"grants,omitempty"`
+
+	// Full name of the schema (catalog_name.schema_name). This value is read from
+	// the terraform state after deployment succeeds.
+	ID string `json:"id,omitempty" bundle:"readonly"`
+
+	*catalog.CreateSchema
+
+	ModifiedStatus ModifiedStatus `json:"modified_status,omitempty" bundle:"internal"`
+}
+
+func (s *Schema) UnmarshalJSON(b []byte) error {
+	return marshal.Unmarshal(b, s)
+}
+
+func (s Schema) MarshalJSON() ([]byte, error) {
+	return marshal.Marshal(s)
+}

bundle/deploy/terraform/convert.go
@@ -66,8 +66,10 @@ func convGrants(acl []resources.Grant) *schema.ResourceGrants {
 // BundleToTerraform converts resources in a bundle configuration
 // to the equivalent Terraform JSON representation.
 //
-// NOTE: THIS IS CURRENTLY A HACK. WE NEED A BETTER WAY TO
-// CONVERT TO/FROM TERRAFORM COMPATIBLE FORMAT.
+// Note: This function is an older implementation of the conversion logic. It is
+// no longer used in any code paths. It is kept around to be used in tests.
+// New resources do not need to modify this function and can instead can define
+// the conversion login in the tfdyn package.
 func BundleToTerraform(config *config.Root) *schema.Root {
 	tfroot := schema.NewRoot()
 	tfroot.Provider = schema.NewProviders()
@@ -382,6 +384,16 @@ func TerraformToBundle(state *resourcesState, config *config.Root) error {
 				}
 				cur.ID = instance.Attributes.ID
 				config.Resources.QualityMonitors[resource.Name] = cur
+			case "databricks_schema":
+				if config.Resources.Schemas == nil {
+					config.Resources.Schemas = make(map[string]*resources.Schema)
+				}
+				cur := config.Resources.Schemas[resource.Name]
+				if cur == nil {
+					cur = &resources.Schema{ModifiedStatus: resources.ModifiedStatusDeleted}
+				}
+				cur.ID = instance.Attributes.ID
+				config.Resources.Schemas[resource.Name] = cur
 			case "databricks_permissions":
 			case "databricks_grants":
 				// Ignore; no need to pull these back into the configuration.
@@ -422,6 +434,11 @@ func TerraformToBundle(state *resourcesState, config *config.Root) error {
 		}
 	}
 	for _, src := range config.Resources.QualityMonitors {
+		if src.ModifiedStatus == "" && src.ID == "" {
+			src.ModifiedStatus = resources.ModifiedStatusCreated
+		}
+	}
+	for _, src := range config.Resources.Schemas {
 		if src.ModifiedStatus == "" && src.ID == "" {
 			src.ModifiedStatus = resources.ModifiedStatusCreated
 		}

commit c454c2fd10534abe666c879b23de3859b738af79

Use precomputed terraform plan for `bundle deploy` (#1640)
# Changes
With https://github.com/databricks/cli/pull/1413 we started to compute
and partially print the plan if it contained deletion of UC schemas.
This PR uses the precomputed plan to avoid double planning when actually
doing the terraform plan.

This fixes a performance regression introduced in
https://github.com/databricks/cli/pull/1413.

# Tests

Tested manually.
1. Verified bundle deployment still works and deploys resources.
2. Verified that the precomputed plan is indeed being used by attaching
a debugger and removing the plan file right before the terraform apply
process is spawned and asserting that terraform apply fails because the
plan is not found.

bundle/deploy/terraform/apply.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	"github.com/databricks/cli/bundle"
-	"github.com/databricks/cli/libs/cmdio"
 	"github.com/databricks/cli/libs/diag"
 	"github.com/databricks/cli/libs/log"
 	"github.com/hashicorp/terraform-exec/tfexec"
@@ -17,28 +16,32 @@ func (w *apply) Name() string {
 }
 
 func (w *apply) Apply(ctx context.Context, b *bundle.Bundle) diag.Diagnostics {
+	// return early if plan is empty
+	if b.Plan.IsEmpty {
+		log.Debugf(ctx, "No changes in plan. Skipping terraform apply.")
+		return nil
+	}
+
 	tf := b.Terraform
 	if tf == nil {
 		return diag.Errorf("terraform not initialized")
 	}
 
-	cmdio.LogString(ctx, "Deploying resources...")
-
-	err := tf.Init(ctx, tfexec.Upgrade(true))
-	if err != nil {
-		return diag.Errorf("terraform init: %v", err)
+	if b.Plan.Path == "" {
+		return diag.Errorf("no plan found")
 	}
 
-	err = tf.Apply(ctx)
+	// Apply terraform according to the computed plan
+	err := tf.Apply(ctx, tfexec.DirOrPlan(b.Plan.Path))
 	if err != nil {
 		return diag.Errorf("terraform apply: %v", err)
 	}
 
-	log.Infof(ctx, "Resource deployment completed")
+	log.Infof(ctx, "terraform apply completed")
 	return nil
 }
 
-// Apply returns a [bundle.Mutator] that runs the equivalent of `terraform apply`
+// Apply returns a [bundle.Mutator] that runs the equivalent of `terraform apply ./plan`
 // from the bundle's ephemeral working directory for Terraform.
 func Apply() bundle.Mutator {
 	return &apply{}

bundle/deploy/terraform/destroy.go
@@ -1,46 +1,0 @@
-package terraform
-
-import (
-	"context"
-
-	"github.com/databricks/cli/bundle"
-	"github.com/databricks/cli/libs/diag"
-	"github.com/databricks/cli/libs/log"
-	"github.com/hashicorp/terraform-exec/tfexec"
-)
-
-type destroy struct{}
-
-func (w *destroy) Name() string {
-	return "terraform.Destroy"
-}
-
-func (w *destroy) Apply(ctx context.Context, b *bundle.Bundle) diag.Diagnostics {
-	// return early if plan is empty
-	if b.Plan.IsEmpty {
-		log.Debugf(ctx, "No resources to destroy in plan. Skipping destroy.")
-		return nil
-	}
-
-	tf := b.Terraform
-	if tf == nil {
-		return diag.Errorf("terraform not initialized")
-	}
-
-	if b.Plan.Path == "" {
-		return diag.Errorf("no plan found")
-	}
-
-	// Apply terraform according to the computed destroy plan
-	err := tf.Apply(ctx, tfexec.DirOrPlan(b.Plan.Path))
-	if err != nil {
-		return diag.Errorf("terraform destroy: %v", err)
-	}
-	return nil
-}
-
-// Destroy returns a [bundle.Mutator] that runs the conceptual equivalent of
-// `terraform destroy ./plan` from the bundle's ephemeral working directory for Terraform.
-func Destroy() bundle.Mutator {
-	return &destroy{}
-}

bundle/deploy/terraform/state_push.go
@@ -2,6 +2,8 @@ package terraform
 
 import (
 	"context"
+	"errors"
+	"io/fs"
 	"os"
 	"path/filepath"
 
@@ -34,6 +36,12 @@ func (l *statePush) Apply(ctx context.Context, b *bundle.Bundle) diag.Diagnostic
 
 	// Expect the state file to live under dir.
 	local, err := os.Open(filepath.Join(dir, TerraformStateFileName))
+	if errors.Is(err, fs.ErrNotExist) {
+		// The state file can be absent if terraform apply is skipped because
+		// there are no changes to apply in the plan.
+		log.Debugf(ctx, "Local terraform state file does not exist.")
+		return nil
+	}
 	if err != nil {
 		return diag.FromErr(err)
 	}

bundle/phases/deploy.go
@@ -92,7 +92,10 @@ func Deploy() bundle.Mutator {
 	// Core mutators that CRUD resources and modify deployment state. These
 	// mutators need informed consent if they are potentially destructive.
 	deployCore := bundle.Defer(
-		terraform.Apply(),
+		bundle.Seq(
+			bundle.LogString("Deploying resources..."),
+			terraform.Apply(),
+		),
 		bundle.Seq(
 			terraform.StatePush(),
 			terraform.Load(),

bundle/phases/destroy.go
@@ -82,7 +82,7 @@ func approvalForDestroy(ctx context.Context, b *bundle.Bundle) (bool, error) {
 func Destroy() bundle.Mutator {
 	// Core destructive mutators for destroy. These require informed user consent.
 	destroyCore := bundle.Seq(
-		terraform.Destroy(),
+		terraform.Apply(),
 		terraform.StatePush(),
 		files.Delete(),
 		bundle.LogString("Destroy complete!"),