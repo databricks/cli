a240be0b5a7eed8d0746ba0f167df0dd465f06c1
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2024-08-09 17:13:31 +0200

    Run Spark JAR task test on multiple DBR versions (#1665)
    ## Changes
    
    This explores error messages on older DBRs and UC vs non-UC.
    
    ## Tests
    
    Integration tests pass.

---
+++ internal/testutil/jdk.go
@@ -0,0 +1,24 @@
+package testutil
+
+import (
+	"bytes"
+	"context"
+	"os/exec"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func RequireJDK(t *testing.T, ctx context.Context, version string) {
+	var stderr bytes.Buffer
+
+	cmd := exec.Command("javac", "-version")
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	require.NoError(t, err, "Unable to run javac -version")
+
+	// Get the first line of the output
+	line := strings.Split(stderr.String(), "\n")[0]
+	require.Contains(t, line, version, "Expected JDK version %s, got %s", version, line)
+}
