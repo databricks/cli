552f4160aa6d61f1cbcc7d98a199d4c52c6ec57c
Shreyas Goenka <shreyas.goenka@databricks.com>
2024-08-13 00:26:37 +0200

    add validation mutator to assert non empty keys

--- bundle/tests/undefined_job_test.go
+++ bundle/tests/undefined_job_test.go
@@ -1,12 +1,16 @@
 package config_tests
 
 import (
+	"context"
 	"testing"
 
+	"github.com/databricks/cli/bundle"
+	"github.com/databricks/cli/bundle/config/validate"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestUndefinedJobLoadsWithError(t *testing.T) {
-	_, diags := loadTargetWithDiags("./undefined_job", "default")
+	b := load(t, "./undefined_job")
+	diags := bundle.Apply(context.Background(), b, validate.AllResourcesHaveValues())
 	assert.ErrorContains(t, diags.Error(), "job undefined is not defined")
 }
