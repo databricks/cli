6178456e3260e9cd7ddafd32d12b22ecb86ab04e
Shreyas Goenka <shreyas.goenka@databricks.com>
2024-08-13 12:41:17 +0200

    revert bundle.Config.Mutate

--- bundle/config/validate/all_resources_have_values.go
+++ bundle/config/validate/all_resources_have_values.go
@@ -21,31 +21,27 @@ func (m *allResourcesHaveValues) Name() string {
 }
 
 func (m *allResourcesHaveValues) Apply(ctx context.Context, b *bundle.Bundle) diag.Diagnostics {
-	err := b.Config.Mutate(func(v dyn.Value) (dyn.Value, error) {
-		rv := v.Get("resources")
+	rv := b.Config.Value().Get("resources")
 
-		// Skip if there are no resources block defined, or the resources block is empty.
-		if rv.Kind() == dyn.KindInvalid || rv.Kind() == dyn.KindNil {
+	// Skip if there are no resources block defined, or the resources block is empty.
+	if rv.Kind() == dyn.KindInvalid || rv.Kind() == dyn.KindNil {
+		return nil
+	}
+
+	_, err := dyn.MapByPattern(
+		rv,
+		dyn.NewPattern(dyn.AnyKey(), dyn.AnyKey()),
+		func(p dyn.Path, v dyn.Value) (dyn.Value, error) {
+			if v.Kind() == dyn.KindInvalid || v.Kind() == dyn.KindNil {
+				// Type of the resource, stripped of the trailing 's' to make it
+				// singular.
+				rType := strings.TrimSuffix(p[0].Key(), "s")
+
+				rName := p[1].Key()
+				return v, fmt.Errorf("%s %s is not defined", rType, rName)
+			}
 			return v, nil
-		}
-
-		_, err := dyn.MapByPattern(
-			rv,
-			dyn.NewPattern(dyn.AnyKey(), dyn.AnyKey()),
-			func(p dyn.Path, v dyn.Value) (dyn.Value, error) {
-				if v.Kind() == dyn.KindInvalid || v.Kind() == dyn.KindNil {
-					// Type of the resource, stripped of the trailing 's' to make it
-					// singular.
-					rType := strings.TrimSuffix(p[0].Key(), "s")
-
-					rName := p[1].Key()
-					return v, fmt.Errorf("%s %s is not defined", rType, rName)
-				}
-				return v, nil
-			},
-		)
-		return v, err
-
-	})
+		},
+	)
 	return diag.FromErr(err)
 }
