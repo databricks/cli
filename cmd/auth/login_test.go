d9f8288c1b1b501049b850d1b70e8a79cc8b403a
Shreyas Goenka <shreyas.goenka@databricks.com>
2024-08-13 18:12:14 +0200

    add tests for account-id

--- cmd/auth/login_test.go
+++ cmd/auth/login_test.go
@@ -5,8 +5,10 @@ import (
 	"testing"
 
 	"github.com/databricks/cli/libs/auth"
+	"github.com/databricks/cli/libs/cmdio"
 	"github.com/databricks/cli/libs/env"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestSetHostDoesNotFailWithNoDatabrickscfg(t *testing.T) {
@@ -17,9 +19,9 @@ func TestSetHostDoesNotFailWithNoDatabrickscfg(t *testing.T) {
 }
 
 func TestSetHost(t *testing.T) {
-	ctx := context.Background()
 	var persistentAuth auth.PersistentAuth
 	t.Setenv("DATABRICKS_CONFIG_FILE", "./testdata/.databrickscfg")
+	ctx, _ := cmdio.SetupTest(context.Background())
 
 	// Test error when both flag and argument are provided
 	persistentAuth.Host = "val from --host"
@@ -49,4 +51,35 @@ func TestSetHost(t *testing.T) {
 	err = setHostAndAccountId(ctx, "profile-2", &persistentAuth, []string{})
 	assert.NoError(t, err)
 	assert.Equal(t, "https://www.host2.com", persistentAuth.Host)
+
+	// Test host is not set. Should prompt.
+	persistentAuth.Host = ""
+	err = setHostAndAccountId(ctx, "", &persistentAuth, []string{})
+	assert.EqualError(t, err, "the command is being run in a non-interactive environment, please specify a host using --host")
+}
+
+func TestSetAccountId(t *testing.T) {
+	var persistentAuth auth.PersistentAuth
+	t.Setenv("DATABRICKS_CONFIG_FILE", "./testdata/.databrickscfg")
+	ctx, _ := cmdio.SetupTest(context.Background())
+
+	// Test setting account-id from flag
+	persistentAuth.AccountID = "val from --account-id"
+	err := setHostAndAccountId(ctx, "account-profile", &persistentAuth, []string{})
+	assert.NoError(t, err)
+	assert.Equal(t, "https://accounts.cloud.databricks.com", persistentAuth.Host)
+	assert.Equal(t, "val from --account-id", persistentAuth.AccountID)
+
+	// Test setting account_id from profile
+	persistentAuth.AccountID = ""
+	err = setHostAndAccountId(ctx, "account-profile", &persistentAuth, []string{})
+	require.NoError(t, err)
+	assert.Equal(t, "https://accounts.cloud.databricks.com", persistentAuth.Host)
+	assert.Equal(t, "id-from-profile", persistentAuth.AccountID)
+
+	// Neither flag nor profile account-id is set, should prompt
+	persistentAuth.AccountID = ""
+	persistentAuth.Host = "https://accounts.cloud.databricks.com"
+	err = setHostAndAccountId(ctx, "", &persistentAuth, []string{})
+	assert.EqualError(t, err, "the command is being run in a non-interactive environment, please specify an account ID using --account-id")
 }
