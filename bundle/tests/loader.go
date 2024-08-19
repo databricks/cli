219f9b1f9faa5d5c4c4ad86d1a8b5d99d36c231b
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2024-08-19 16:57:31 +0200

    Share test initializer in common helper function
    These tests inadvertently were re-running mutators; the first time through
    `loadTarget` and the second time by running `phases.Initialize()` themselves.
    Some of the mutators that executed in `phases.Initializes()` are also run as
    part of `loadTarget`. This is overdue a refactor to make it unambiguous what
    runs when. Until then, this removes the duplicated execution.

--- bundle/tests/loader.go
+++ bundle/tests/loader.go
@@ -8,6 +8,10 @@ import (
 	"github.com/databricks/cli/bundle/config/mutator"
 	"github.com/databricks/cli/bundle/phases"
 	"github.com/databricks/cli/libs/diag"
+	"github.com/databricks/databricks-sdk-go/config"
+	"github.com/databricks/databricks-sdk-go/experimental/mocks"
+	"github.com/databricks/databricks-sdk-go/service/iam"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
@@ -41,5 +45,30 @@ func loadTargetWithDiags(path, env string) (*bundle.Bundle, diag.Diagnostics) {
 		mutator.MergeJobTasks(),
 		mutator.MergePipelineClusters(),
 	))
+	return b, diags
+}
+
+func configureMock(t *testing.T, b *bundle.Bundle) {
+	// Configure mock workspace client
+	m := mocks.NewMockWorkspaceClient(t)
+	m.WorkspaceClient.Config = &config.Config{
+		Host: "https://mock.databricks.workspace.com",
+	}
+	m.GetMockCurrentUserAPI().EXPECT().Me(mock.Anything).Return(&iam.User{
+		UserName: "user@domain.com",
+	}, nil)
+	b.SetWorkpaceClient(m.WorkspaceClient)
+}
+
+func initializeTarget(t *testing.T, path, env string) (*bundle.Bundle, diag.Diagnostics) {
+	b := load(t, path)
+	configureMock(t, b)
+
+	ctx := context.Background()
+	diags := bundle.Apply(ctx, b, bundle.Seq(
+		mutator.SelectTarget(env),
+		phases.Initialize(),
+	))
+
 	return b, diags
 }
