f18c453c5984d04c8a884556d5e7cacf25bbad7b
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2024-08-21 16:10:00 +0200

    Revert "Use sync root path in metadata computation"
    This reverts commit 7ea37a7ad8d8dcd3efb30acd7ee6616c9f27ccfa.

--- bundle/deploy/metadata/compute_test.go
+++ bundle/deploy/metadata/compute_test.go
@@ -16,8 +16,6 @@ import (
 
 func TestComputeMetadataMutator(t *testing.T) {
 	b := &bundle.Bundle{
-		RootPath:     "parent/my_bundle",
-		SyncRootPath: "parent",
 		Config: config.Root{
 			Workspace: config.Workspace{
 				RootPath:     "/Users/shreyas.goenka@databricks.com",
@@ -57,9 +55,9 @@ func TestComputeMetadataMutator(t *testing.T) {
 		},
 	}
 
-	bundletest.SetLocation(b, "resources.jobs.my-job-1", "parent/my_bundle/a/b/c")
-	bundletest.SetLocation(b, "resources.jobs.my-job-2", "parent/my_bundle/d/e/f")
-	bundletest.SetLocation(b, "resources.pipelines.my-pipeline", "parent/my_bundle/abc")
+	bundletest.SetLocation(b, "resources.jobs.my-job-1", "a/b/c")
+	bundletest.SetLocation(b, "resources.jobs.my-job-2", "d/e/f")
+	bundletest.SetLocation(b, "resources.pipelines.my-pipeline", "abc")
 
 	expectedMetadata := metadata.Metadata{
 		Version: metadata.Version,
@@ -81,11 +79,11 @@ func TestComputeMetadataMutator(t *testing.T) {
 			Resources: metadata.Resources{
 				Jobs: map[string]*metadata.Job{
 					"my-job-1": {
-						RelativePath: "my_bundle/a/b/c",
+						RelativePath: "a/b/c",
 						ID:           "1111",
 					},
 					"my-job-2": {
-						RelativePath: "my_bundle/d/e/f",
+						RelativePath: "d/e/f",
 						ID:           "2222",
 					},
 				},
