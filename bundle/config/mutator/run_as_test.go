7c7abeff815c7a46f39c5ac68e88723008512a63
Shreyas Goenka <shreyas.goenka@databricks.com>
2024-09-09 14:41:42 +0200

    run as support

--- bundle/config/mutator/run_as_test.go
+++ bundle/config/mutator/run_as_test.go
@@ -40,6 +40,7 @@ func allResourceTypes(t *testing.T) []string {
 		"quality_monitors",
 		"registered_models",
 		"schemas",
+		"volumes",
 	},
 		resourceTypes,
 	)
@@ -138,6 +139,7 @@ func TestRunAsErrorForUnsupportedResources(t *testing.T) {
 		"registered_models",
 		"experiments",
 		"schemas",
+		"volumes",
 	}
 
 	base := config.Root{
