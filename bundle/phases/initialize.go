3b33d8ce650cedb070facf48717304fa9cf1529f
Lennart Kats <lennart.kats@databricks.com>
2024-09-09 09:40:03 +0200

    Add comment

--- bundle/phases/initialize.go
+++ bundle/phases/initialize.go
@@ -55,6 +55,7 @@ func Initialize() bundle.Mutator {
 				"workspace",
 				"variables",
 			),
+			// Provide permission config errors & warnings after initializing all variables
 			permissions.PermissionDiagnostics(),
 			mutator.SetRunAs(),
 			mutator.OverrideCompute(),
