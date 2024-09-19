fd3dfb59614aee77bfaed4a4cf9dbdca57f43c1b
Andrew Nester <andrew.nester@databricks.com>
2024-09-19 13:09:45 +0200

    remove plugin framework files

--- bundle/internal/tf/schema/data_source_cluster_pluginframework.go
+++
@@ -1,8 +0,0 @@
-// Generated from Databricks Terraform provider schema. DO NOT EDIT.
-
-package schema
-
-type DataSourceClusterPluginframework struct {
-	ClusterId   string `json:"cluster_id,omitempty"`
-	ClusterName string `json:"cluster_name,omitempty"`
-}
