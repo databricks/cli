e885794722793a53ae2cd05ee87659cde702a8fb
Lennart Kats (databricks) <lennart.kats@databricks.com>
2024-10-10 13:18:23 +0200

    Show actionable errors for collaborative deployment scenarios (#1386)
    ## Changes
    
    This adds diagnostics for collaborative (production) deployment
    scenarios, including:
    
    - Bob deploys a bundle that is normally deployed by Alice, but this
    fails because Bob can't write to `/Users/Alice/.bundle`.
    - Charlie deploys a bundle that is normally deployed by Alice, but this
    fails because he can't create a new pipeline where Alice would be the
    owner.
    - Alice deploys a bundle where she didn't list herself as one of the
    CAN_MANAGE users in permissions. That can work, but is probably a
    mistake.
    
    ## Tests
    
    Unit tests, manual testing.

--- bundle/config/mutator/run_as_test.go
+++ bundle/config/mutator/run_as_test.go
@@ -188,11 +188,8 @@ func TestRunAsErrorForUnsupportedResources(t *testing.T) {
 			Config: *r,
 		}
 		diags := bundle.Apply(context.Background(), b, SetRunAs())
-		assert.Equal(t, diags.Error().Error(), errUnsupportedResourceTypeForRunAs{
-			resourceType:     rt,
-			resourceLocation: dyn.Location{},
-			currentUser:      "alice",
-			runAsUser:        "bob",
-		}.Error(), "expected run_as with a different identity than the current deployment user to not supported for resources of type: %s", rt)
+		assert.Contains(t, diags.Error().Error(), "do not support a setting a run_as user that is different from the owner.\n"+
+			"Current identity: alice. Run as identity: bob.\n"+
+			"See https://docs.databricks.com/dev-tools/bundles/run-as.html to learn more about the run_as property.", rt)
 	}
 }
