f919e94bce5d23a39c36fe79b9fc7d2e36a47bee
Shreyas Goenka <shreyas.goenka@databricks.com>
2024-10-14 15:05:33 +0200

    typo fix

--- bundle/config/resources/volume.go
+++ bundle/config/resources/volume.go
@@ -6,10 +6,10 @@ import (
 )
 
 type Volume struct {
-	// List of grants to apply on this schema.
+	// List of grants to apply on this volume.
 	Grants []Grant `json:"grants,omitempty"`
 
-	// Full name of the schema (catalog_name.schema_name.volume_name). This value is read from
+	// Full name of the volume (catalog_name.schema_name.volume_name). This value is read from
 	// the terraform state after deployment succeeds.
 	ID string `json:"id,omitempty" bundle:"readonly"`
 
