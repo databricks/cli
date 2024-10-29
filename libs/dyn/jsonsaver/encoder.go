bfb967628a5eab834ba708677061718b9f3f7498
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2024-10-29 12:36:38 +0100

    Add comment why we disable escaping

--- libs/dyn/jsonsaver/encoder.go
+++ libs/dyn/jsonsaver/encoder.go
@@ -15,6 +15,12 @@ type encoder struct {
 func newEncoder() encoder {
 	var buf bytes.Buffer
 	enc := json.NewEncoder(&buf)
+
+	// By default, json.Encoder escapes HTML characters, converting symbols like '<' to '\u003c'.
+	// This behavior helps prevent XSS attacks when JSON is embedded within HTML.
+	// However, we disable this feature since we're not dealing with HTML context.
+	// Keeping the escapes enabled would result in unnecessary differences when processing JSON payloads
+	// that already contain escaped characters.
 	enc.SetEscapeHTML(false)
 	return encoder{enc, &buf}
 }
