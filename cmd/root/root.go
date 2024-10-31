a7e54a22d57ff09178a736cec2ce511888e41f57
Shreyas Goenka <shreyas.goenka@databricks.com>
2024-10-31 14:48:30 +0100

    address comments

--- cmd/root/root.go
+++ cmd/root/root.go
@@ -75,8 +75,8 @@ func New(ctx context.Context) *cobra.Command {
 
 		// Configure our user agent with the command that's about to be executed.
 		ctx = withCommandInUserAgent(ctx, cmd)
-		ctx = withUpstreamInUserAgent(ctx)
 		ctx = withCommandExecIdInUserAgent(ctx)
+		ctx = withUpstreamInUserAgent(ctx)
 		cmd.SetContext(ctx)
 		return nil
 	}
--- cmd/root/user_agent_command_exec_id.go
+++ cmd/root/user_agent_command_exec_id.go
@@ -8,9 +8,7 @@ import (
 )
 
 func withCommandExecIdInUserAgent(ctx context.Context) context.Context {
-	// A UUID that'll will allow use to correlate multiple API requests made by
-	// the same command invocation.
-	// When we add telemetry to the CLI, this exec ID will allow allow us to
-	// correlate logs in HTTP access logs with logs in Frontend logs.
+	// A UUID that will allow us to correlate multiple API requests made by
+	// the same CLI invocation.
 	return useragent.InContext(ctx, "cmd-exec-id", uuid.New().String())
 }
