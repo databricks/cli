38bce2681b1dd0ab04657e224d8100b5857f59cd
Andrew Nester <andrew.nester@databricks.com>
2024-11-29 15:54:42 +0100

    Added support for bundle generate and bind for Apps

--- bundle/config/resources.go
+++ bundle/config/resources.go
@@ -97,9 +97,16 @@ func (r *Resources) FindResourceByConfigKey(key string) (ConfigResource, error) 
 			found = append(found, r.Jobs[k])
 		}
 	}
+
 	for k := range r.Pipelines {
 		if k == key {
 			found = append(found, r.Pipelines[k])
+		}
+	}
+
+	for k := range r.Apps {
+		if k == key {
+			found = append(found, r.Apps[k])
 		}
 	}
 
