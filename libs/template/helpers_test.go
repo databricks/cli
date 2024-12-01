f7c585a36df86e494e4a514e4e7200f4d64c8a38
Shreyas Goenka <shreyas.goenka@databricks.com>
2024-12-01 23:09:55 +0100

    Add the bundle_uuid helper function

--- libs/template/helpers_test.go
+++ libs/template/helpers_test.go
@@ -32,6 +32,22 @@ func TestTemplatePrintStringWithoutProcessing(t *testing.T) {
 	assert.Equal(t, `{{ fail "abc" }}`, cleanContent)
 }
 
+func TestTemplateBundleUuidFunction(t *testing.T) {
+	ctx := context.Background()
+
+	ctx = root.SetWorkspaceClient(ctx, nil)
+	helpers := loadHelpers(ctx)
+	r, err := newRenderer(ctx, nil, helpers, os.DirFS("."), "./testdata/bundle-uuid/template", "./testdata/bundle-uuid/library")
+	require.NoError(t, err)
+
+	err = r.walk()
+	assert.NoError(t, err)
+
+	assert.Len(t, r.files, 1)
+	cleanContent := strings.Trim(string(r.files[0].(*inMemoryFile).content), "\n\r")
+	assert.Equal(t, strings.Repeat(bundleUuid, 5), cleanContent)
+}
+
 func TestTemplateRegexpCompileFunction(t *testing.T) {
 	ctx := context.Background()
 
