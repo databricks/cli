39806c90e1ee5133cb0f11aae6bc0ccb0b69b39a
Denis Bilenko <denis.bilenko@databricks.com>
2024-12-04 09:51:37 +0100

    Improve git config section validation

--- bundle/config/mutator/load_git_details.go
+++ bundle/config/mutator/load_git_details.go
@@ -2,6 +2,7 @@ package mutator
 
 import (
 	"context"
+	"fmt"
 	"path/filepath"
 
 	"github.com/databricks/cli/bundle"
@@ -27,29 +28,55 @@ func (m *loadGitDetails) Apply(ctx context.Context, b *bundle.Bundle) diag.Diagn
 	}
 
 	b.WorktreeRoot = info.GuessedWorktreeRoot
+	config := &b.Config.Bundle.Git
 
-	b.Config.Bundle.Git.ActualBranch = info.CurrentBranch
-	if b.Config.Bundle.Git.Branch == "" {
-		// Only load branch if there's no user defined value
-		b.Config.Bundle.Git.Inferred = true
-		b.Config.Bundle.Git.Branch = info.CurrentBranch
+	config.ActualBranch = info.CurrentBranch
+	if config.Branch == "" && info.CurrentBranch != "" {
+		config.Inferred = true
 	}
 
-	// load commit hash if undefined
-	if b.Config.Bundle.Git.Commit == "" {
-		b.Config.Bundle.Git.Commit = info.LatestCommit
-	}
-
-	// load origin url if undefined
-	if b.Config.Bundle.Git.OriginURL == "" {
-		b.Config.Bundle.Git.OriginURL = info.OriginURL
-	}
+	diags = checkMatch(diags, "Git branch", info.CurrentBranch, &config.Branch, b.Config.Bundle.Force)
+	diags = checkMatch(diags, "Git commit", info.LatestCommit, &config.Commit, b.Config.Bundle.Force)
+	diags = checkMatch(diags, "Git originURL", info.OriginURL, &config.OriginURL, true)
 
 	relBundlePath, err := filepath.Rel(b.WorktreeRoot.Native(), b.BundleRoot.Native())
 	if err != nil {
 		diags = append(diags, diag.FromErr(err)...)
 	}
 
-	b.Config.Bundle.Git.BundleRootPath = filepath.ToSlash(relBundlePath)
+	config.BundleRootPath = filepath.ToSlash(relBundlePath)
+	return diags
+}
+
+func checkMatch(diags []diag.Diagnostic, field, fetchedValue string, configValue *string, allowedToMismatch bool) []diag.Diagnostic {
+	if fetchedValue == "" {
+		return diags
+	}
+
+	// The value from config takes precedence; however, we always warn if configValue and fetchedValue disagree.
+	// In case of branch and commit and absence of --force we raise severity to Error
+
+	if *configValue == "" {
+		*configValue = fetchedValue
+		return diags
+	}
+
+	if *configValue != fetchedValue {
+		tmpl := "not on the right %s:\n  expected according to configuration: %s\n  actual: %s%s"
+		extra := ""
+		var severity diag.Severity
+		if allowedToMismatch {
+			severity = diag.Warning
+		} else {
+			severity = diag.Error
+			extra = "\nuse --force to override"
+		}
+
+		return append(diags, diag.Diagnostic{
+			Severity: severity,
+			Summary:  fmt.Sprintf(tmpl, field, *configValue, fetchedValue, extra),
+		})
+	}
+
 	return diags
 }
