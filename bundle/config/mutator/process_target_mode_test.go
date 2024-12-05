ddc012c324418dc95952f0a57770d92933658b63
Andrew Nester <andrew.nester@databricks.com>
2024-12-05 15:40:34 +0100

    addressed feedback

--- bundle/config/mutator/process_target_mode_test.go
+++ bundle/config/mutator/process_target_mode_test.go
@@ -426,6 +426,12 @@ func TestAllNonUcResourcesAreRenamed(t *testing.T) {
 			for _, key := range field.MapKeys() {
 				resource := field.MapIndex(key)
 				nameField := resource.Elem().FieldByName("Name")
+
+				// Skip apps, as they are not renamed
+				if resourceType == "Apps" {
+					continue
+				}
+
 				if !nameField.IsValid() || nameField.Kind() != reflect.String {
 					continue
 				}
