647b09e6e299cf4698b2aab67f81a4b14eed4c42
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2024-12-05 12:09:45 +0100

    Upgrade TF provider to 1.59.0 (#1960)
    ## Changes
    
    Notable changes:
    * Fixes dashboard deployment if it was trashed out-of-band.
    * Removes client-side validation for single-node cluster configuration
    (also see #1546).
    
    Beware: for the same reason as in #1900, this excludes the changes for
    the quality monitor resource.
    
    ## Tests
    
    Integration tests pass.

--- bundle/internal/tf/schema/data_source_aws_bucket_policy.go
+++ bundle/internal/tf/schema/data_source_aws_bucket_policy.go
@@ -3,6 +3,7 @@
 package schema
 
 type DataSourceAwsBucketPolicy struct {
+	AwsPartition          string `json:"aws_partition,omitempty"`
 	Bucket                string `json:"bucket"`
 	DatabricksAccountId   string `json:"databricks_account_id,omitempty"`
 	DatabricksE2AccountId string `json:"databricks_e2_account_id,omitempty"`
---
+++ bundle/internal/tf/schema/data_source_mws_network_connectivity_configs.go
@@ -0,0 +1,9 @@
+// Generated from Databricks Terraform provider schema. DO NOT EDIT.
+
+package schema
+
+type DataSourceMwsNetworkConnectivityConfigs struct {
+	Id     string   `json:"id,omitempty"`
+	Names  []string `json:"names,omitempty"`
+	Region string   `json:"region,omitempty"`
+}
