9a5503755a4adf8cd70672900985bd246ea83f18
Ilya Kuznetsov <ilya.kuznetsov@databricks.com>
2024-12-10 16:13:51 +0100

    feat: Format markdown links to absolute

--- bundle/internal/schema/annotations.go
+++ bundle/internal/schema/annotations.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"os"
 	"reflect"
+	"regexp"
 	"strings"
 
 	yaml3 "gopkg.in/yaml.v3"
@@ -141,7 +143,7 @@ func assingAnnotation(s *jsonschema.Schema, a annotation) {
 	if a.Default != nil {
 		s.Default = a.Default
 	}
-	s.MarkdownDescription = a.MarkdownDescription
+	s.MarkdownDescription = convertLinksToAbsoluteUrl(a.MarkdownDescription)
 	s.Title = a.Title
 	s.Enum = a.Enum
 }
@@ -159,4 +161,47 @@ func saveYamlWithStyle(outputPath string, input dyn.Value) error {
 		return err
 	}
 	return nil
+}
+
+func convertLinksToAbsoluteUrl(s string) string {
+	if s == "" {
+		return s
+	}
+	base := "https://docs.databricks.com"
+	referencePage := "/dev-tools/bundles/reference.html"
+
+	// Regular expression to match Markdown-style links
+	re := regexp.MustCompile(`\[_\]\(([^)]+)\)`)
+	result := re.ReplaceAllStringFunc(s, func(match string) string {
+		// Extract the URL inside parentheses
+		matches := re.FindStringSubmatch(match)
+		if len(matches) < 2 {
+			return match // Return original if no match found
+		}
+		link := matches[1]
+
+		var text, absoluteURL string
+
+		if strings.HasPrefix(link, "#") {
+			text = strings.TrimPrefix(link, "#")
+			absoluteURL = fmt.Sprintf("%s%s%s", base, referencePage, link)
+		} else if strings.HasPrefix(link, "/") {
+			// Handle relative paths like /dev-tools/bundles/resources.html#dashboard
+			if strings.Contains(link, "#") {
+				parts := strings.Split(link, "#")
+				text = parts[1]
+				absoluteURL = fmt.Sprintf("%s%s", base, link)
+			} else {
+				text = "link"
+				absoluteURL = fmt.Sprintf("%s%s", base, link)
+			}
+			absoluteURL = strings.ReplaceAll(absoluteURL, ".md", ".html")
+		} else {
+			return match
+		}
+
+		return fmt.Sprintf("[%s](%s)", text, absoluteURL)
+	})
+
+	return result
 }
