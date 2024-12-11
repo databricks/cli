4236e7122f8d2fe66f5a95e5da49af8b8bdf2038
Denis Bilenko <denis.bilenko@databricks.com>
2024-12-11 09:44:22 +0100

    Switch to `folders.FindDirWithLeaf` (#1963)
    ## Changes
    Remove two duplicate implementations of the same logic, switch
    everywhere to folders.FindDirWithLeaf.
    
    Add Abs() call to FindDirWithLeaf, it cannot really work on relative
    paths.
    
    ## Tests
    Existing tests.

--- libs/folders/folders.go
+++ libs/folders/folders.go
@@ -9,6 +9,10 @@ import (
 // FindDirWithLeaf returns the first directory that holds `leaf`,
 // traversing up to the root of the filesystem, starting at `dir`.
 func FindDirWithLeaf(dir string, leaf string) (string, error) {
+	dir, err := filepath.Abs(dir)
+	if err != nil {
+		return "", err
+	}
 	for {
 		_, err := os.Stat(filepath.Join(dir, leaf))
 
