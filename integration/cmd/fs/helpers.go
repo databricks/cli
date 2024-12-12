2fd75ec0d3f271ce235072f621976d12a3787e47
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2024-12-12 22:08:56 +0100

    Remove unused functions

--- integration/cmd/fs/helpers.go
+++ integration/cmd/fs/helpers.go
@@ -3,8 +3,6 @@
 package fs_integration
 
 import (
-	"errors"
-	"net/http"
 	"os"
 	"path"
 	"path/filepath"
@@ -13,7 +11,6 @@ import (
 	"github.com/databricks/cli/internal/testutil"
 
 	"github.com/databricks/cli/libs/filer"
-	"github.com/databricks/databricks-sdk-go/apierr"
 	"github.com/stretchr/testify/require"
 )
 
@@ -25,32 +22,6 @@ func setupLocalFiler(t testutil.TestingT) (filer.Filer, string) {
 	require.NoError(t, err)
 
 	return f, path.Join(filepath.ToSlash(tmp))
-}
-
-func setupWsfsFiler(t testutil.TestingT) (filer.Filer, string) {
-	ctx, wt := acc.WorkspaceTest(t)
-
-	tmpdir := acc.TemporaryWorkspaceDir(wt)
-	f, err := filer.NewWorkspaceFilesClient(wt.W, tmpdir)
-	require.NoError(t, err)
-
-	// Check if we can use this API here, skip test if we cannot.
-	_, err = f.Read(ctx, "we_use_this_call_to_test_if_this_api_is_enabled")
-	var aerr *apierr.APIError
-	if errors.As(err, &aerr) && aerr.StatusCode == http.StatusBadRequest {
-		t.Skip(aerr.Message)
-	}
-
-	return f, tmpdir
-}
-
-func setupWsfsExtensionsFiler(t testutil.TestingT) (filer.Filer, string) {
-	_, wt := acc.WorkspaceTest(t)
-
-	tmpdir := acc.TemporaryWorkspaceDir(wt)
-	f, err := filer.NewWorkspaceFilesExtensionsClient(wt.W, tmpdir)
-	require.NoError(t, err)
-	return f, tmpdir
 }
 
 func setupDbfsFiler(t testutil.TestingT) (filer.Filer, string) {
