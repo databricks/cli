d741259be6093a1394162cf29229618eba7b0632
Andrew Nester <andrew.nester@databricks.com>
2024-12-13 14:48:54 +0100

    process attribute nested type correctly

--- bundle/internal/tf/schema/data_source_notification_destinations.go
+++ bundle/internal/tf/schema/data_source_notification_destinations.go
@@ -2,8 +2,14 @@
 
 package schema
 
+type DataSourceNotificationDestinationsNotificationDestinations struct {
+	DestinationType string `json:"destination_type,omitempty"`
+	DisplayName     string `json:"display_name,omitempty"`
+	Id              string `json:"id,omitempty"`
+}
+
 type DataSourceNotificationDestinations struct {
-	DisplayNameContains      string `json:"display_name_contains,omitempty"`
-	NotificationDestinations any    `json:"notification_destinations,omitempty"`
-	Type                     string `json:"type,omitempty"`
+	DisplayNameContains      string                                                       `json:"display_name_contains,omitempty"`
+	NotificationDestinations []DataSourceNotificationDestinationsNotificationDestinations `json:"notification_destinations,omitempty"`
+	Type                     string                                                       `json:"type,omitempty"`
 }
--- bundle/internal/tf/schema/data_source_registered_model.go
+++ bundle/internal/tf/schema/data_source_registered_model.go
@@ -2,9 +2,31 @@
 
 package schema
 
+type DataSourceRegisteredModelModelInfoAliases struct {
+	AliasName  string `json:"alias_name,omitempty"`
+	VersionNum int    `json:"version_num,omitempty"`
+}
+
+type DataSourceRegisteredModelModelInfo struct {
+	Aliases         []DataSourceRegisteredModelModelInfoAliases `json:"aliases,omitempty"`
+	BrowseOnly      bool                                        `json:"browse_only,omitempty"`
+	CatalogName     string                                      `json:"catalog_name,omitempty"`
+	Comment         string                                      `json:"comment,omitempty"`
+	CreatedAt       int                                         `json:"created_at,omitempty"`
+	CreatedBy       string                                      `json:"created_by,omitempty"`
+	FullName        string                                      `json:"full_name,omitempty"`
+	MetastoreId     string                                      `json:"metastore_id,omitempty"`
+	Name            string                                      `json:"name,omitempty"`
+	Owner           string                                      `json:"owner,omitempty"`
+	SchemaName      string                                      `json:"schema_name,omitempty"`
+	StorageLocation string                                      `json:"storage_location,omitempty"`
+	UpdatedAt       int                                         `json:"updated_at,omitempty"`
+	UpdatedBy       string                                      `json:"updated_by,omitempty"`
+}
+
 type DataSourceRegisteredModel struct {
-	FullName       string `json:"full_name"`
-	IncludeAliases bool   `json:"include_aliases,omitempty"`
-	IncludeBrowse  bool   `json:"include_browse,omitempty"`
-	ModelInfo      any    `json:"model_info,omitempty"`
+	FullName       string                               `json:"full_name"`
+	IncludeAliases bool                                 `json:"include_aliases,omitempty"`
+	IncludeBrowse  bool                                 `json:"include_browse,omitempty"`
+	ModelInfo      []DataSourceRegisteredModelModelInfo `json:"model_info,omitempty"`
 }
