d7500537c739c2496dd649e2c817c431b982322e
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2024-12-13 09:28:28 +0100

    Skip auth describe failure test

--- integration/cmd/auth/describe_test.go
+++ integration/cmd/auth/describe_test.go
@@ -37,6 +37,8 @@ func TestAuthDescribeSuccess(t *testing.T) {
 func TestAuthDescribeFailure(t *testing.T) {
 	t.Log(testutil.GetEnvOrSkipTest(t, "CLOUD_ENV"))
 
+	t.Skipf("Skipping because of https://github.com/databricks/cli/issues/2010")
+
 	stdout, _ := testcli.RequireSuccessfulRun(t, "auth", "describe", "--profile", "nonexistent")
 	outStr := stdout.String()
 
