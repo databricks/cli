99f8413fa3995a28b59ef36cf28ec3f68206802a
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2024-12-13 12:59:56 +0100

    Add TestMain to every integration test package

---
+++ integration/cmd/alerts/main_test.go
@@ -0,0 +1,13 @@
+package alerts_integration
+
+import (
+	"testing"
+
+	"github.com/databricks/cli/integration/internal"
+)
+
+// TestMain is the entrypoint executed by the test runner.
+// See [internal.Main] for prerequisites for running integration tests.
+func TestMain(m *testing.M) {
+	internal.Main(m)
+}
---
+++ integration/enforce_convention_test.go
@@ -0,0 +1,108 @@
+package integration
+
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+	"text/template"
+
+	"golang.org/x/exp/maps"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func enumeratePackages(t *testing.T) map[string]*ast.Package {
+	pkgmap := make(map[string]*ast.Package)
+	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Skip files.
+		if !info.IsDir() {
+			return nil
+		}
+
+		// Skip the root directory and the "internal" directory.
+		if path == "." || strings.HasPrefix(path, "internal") {
+			return nil
+		}
+
+		// Parse the package
+		fset := token.NewFileSet()
+		pkgs, err := parser.ParseDir(fset, path, nil, parser.AllErrors)
+		require.NoError(t, err)
+		if len(pkgs) == 0 {
+			return nil
+		}
+
+		// Expect one package per directory.
+		vs := maps.Values(pkgs)
+		require.Len(t, vs, 1, "Directory %s contains more than one package", path)
+		pkgmap[path] = vs[0]
+		return nil
+	})
+	require.NoError(t, err)
+	return pkgmap
+}
+
+// TestEnforcePackageNames checks that all integration test package names use the "_integration" suffix.
+// We enforce this package name to avoid package name aliasing.
+func TestEnforcePackageNames(t *testing.T) {
+	pkgmap := enumeratePackages(t)
+	for _, pkg := range pkgmap {
+		assert.True(t, strings.HasSuffix(pkg.Name, "_integration"), "Package name %s does not end with _integration", pkg.Name)
+	}
+}
+
+var mainTestTemplate = template.Must(template.New("main_test").Parse(
+	`package {{.Name}}
+
+import (
+	"testing"
+
+	"github.com/databricks/cli/integration/internal"
+)
+
+// TestMain is the entrypoint executed by the test runner.
+// See [internal.Main] for prerequisites for running integration tests.
+func TestMain(m *testing.M) {
+	internal.Main(m)
+}
+`))
+
+func TestEnforceMainTest(t *testing.T) {
+	pkgmap := enumeratePackages(t)
+	for dir, pkg := range pkgmap {
+		found := false
+		for _, file := range maps.Keys(pkg.Files) {
+			if filepath.Base(file) == "main_test.go" {
+				found = true
+				break
+			}
+		}
+
+		// Expect a "main_test.go" file in each package.
+		assert.True(t, found, "Directory %s does not contain a main_test.go file", dir)
+	}
+}
+
+func TestWriteMainTest(t *testing.T) {
+	t.Skip("Uncomment to write main_test.go files")
+
+	pkgmap := enumeratePackages(t)
+	for dir, pkg := range pkgmap {
+		// Write a "main_test.go" file to the package.
+		// This file is required to run the integration tests.
+		f, err := os.Create(filepath.Join(dir, "main_test.go"))
+		require.NoError(t, err)
+		defer f.Close()
+		err = mainTestTemplate.Execute(f, pkg)
+		require.NoError(t, err)
+	}
+}
