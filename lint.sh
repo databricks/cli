b9e88d51953b2f801d72cd5a2144388af0198054
Denis Bilenko <denis.bilenko@databricks.com>
2024-12-30 14:18:34 +0100

    update comment

--- lint.sh
+++ lint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -euo pipefail
-# With golangci-lint, if there are any compliation issues, they formatters autofix won't be applied.
+# With golangci-lint, if there are any compliation issues, then formatters' autofix won't be applied.
 # https://github.com/golangci/golangci-lint/issues/5257
 # However, running goimports first alone will actually fix some of the compilation issues.
+# Fixing formatting is also reasonable thing to do.
 # For this reason, this script runs golangci-lint in two stages:
 golangci-lint run --fix --no-config --disable-all --enable gofumpt,goimports $@
 exec golangci-lint run --fix $@
