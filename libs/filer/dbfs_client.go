ee8017357e55c434bc77c2f7fefda416c5a314c0
Shreyas Goenka <shreyas.goenka@databricks.com>
2024-12-31 16:07:53 +0530

    fix size

--- libs/filer/dbfs_client.go
+++ libs/filer/dbfs_client.go
@@ -177,7 +177,7 @@ func (w *DbfsClient) streamFile(ctx context.Context, path string, overwrite bool
 // MaxUploadLimitForPutApi is the maximum size in bytes of a file that can be uploaded
 // using the /dbfs/put API. If the file is larger than this limit, the streaming
 // API (/dbfs/create and /dbfs/add-block) will be used instead.
-var MaxDbfsPutFileSize int64 = 2 * 1024 * 1024
+var MaxDbfsPutFileSize int64 = 2 * 1024 * 1024 * 1024
 
 func (w *DbfsClient) Write(ctx context.Context, name string, reader io.Reader, mode ...WriteMode) error {
 	absPath, err := w.root.Join(name)
