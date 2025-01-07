0e9c3674d9bc66fd8925a7a1903f31ef6bc19c21
Denis Bilenko <denis.bilenko@databricks.com>
2025-01-07 23:56:20 +0100

    cache acceptance tests

--- acceptance/acceptance_test.go
+++ acceptance/acceptance_test.go
@@ -1,8 +1,11 @@
 package acceptance_test
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"errors"
 	"fmt"
+	"hash"
 	"io"
 	"os"
 	"os/exec"
@@ -35,6 +38,9 @@ var Scripts = map[string]bool{
 }
 
 func TestAccept(t *testing.T) {
+	cacheDir := GetCacheLocation(t)
+	t.Logf("cacheDir: %s", cacheDir)
+
 	cwd, err := os.Getwd()
 	require.NoError(t, err)
 
@@ -44,6 +50,13 @@ func TestAccept(t *testing.T) {
 
 	// Make helper scripts available
 	t.Setenv("PATH", fmt.Sprintf("%s%c%s", filepath.Join(cwd, "bin"), os.PathListSeparator, os.Getenv("PATH")))
+
+	execPathHash := MustCalculateMD5(t, execPath)
+	testExecPath, err := os.Executable()
+	require.NoError(t, err)
+	execPathHash += MustCalculateMD5(t, testExecPath)
+	// TODO: include DATABRICKS_HOST if running against remote
+	// TODO: ensure expiration is enforced if running against remote, to capture remote issues.
 
 	server := StartServer(t)
 	AddHandlers(server)
@@ -57,10 +70,11 @@ func TestAccept(t *testing.T) {
 
 	testDirs := getTests(t)
 	require.NotEmpty(t, testDirs)
+
 	for _, dir := range testDirs {
 		t.Run(dir, func(t *testing.T) {
 			t.Parallel()
-			runTest(t, dir)
+			runTest(t, dir, execPathHash, cacheDir)
 		})
 	}
 }
@@ -85,7 +99,16 @@ func getTests(t *testing.T) []string {
 	return testDirs
 }
 
-func runTest(t *testing.T, dir string) {
+func runTest(t *testing.T, dir, initialHash, cacheDir string) {
+	scriptContents := readMergedScriptContents(t, dir)
+
+	hit, checksumFile := isCached(t, cacheDir, initialHash, scriptContents, dir)
+
+	if hit {
+		t.Logf("cache hit")
+		return
+	}
+
 	var tmpDir string
 	var err error
 	if KeepTmp {
@@ -98,7 +121,6 @@ func runTest(t *testing.T, dir string) {
 		tmpDir = t.TempDir()
 	}
 
-	scriptContents := readMergedScriptContents(t, dir)
 	testutil.WriteFile(t, filepath.Join(tmpDir, EntryPointScript), scriptContents)
 
 	inputs := make(map[string]bool, 2)
@@ -154,6 +176,56 @@ func runTest(t *testing.T, dir string) {
 			newVal := testutil.ReadFile(t, pathNew)
 			doComparison(t, filepath.Join(dir, name), filepath.Join(tmpDir, name), newVal)
 		}
+	}
+
+	writeCache(t, checksumFile)
+}
+
+func isCached(t *testing.T, cacheDir, initialHash, scriptContents, dir string) (bool, string) {
+	if cacheDir == "" {
+		return false, ""
+	}
+
+	hash := md5.New()
+	_, err := hash.Write([]byte(initialHash))
+	require.NoError(t, err)
+	_, err = hash.Write([]byte(scriptContents))
+	require.NoError(t, err)
+
+	err = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		require.NoError(t, err)
+		if !info.IsDir() {
+			AddFile(t, hash, path)
+		}
+		return nil
+	})
+	require.NoError(t, err)
+	checksum := GetChecksum(hash)
+
+	checksumFile := filepath.Join(cacheDir, checksum)
+
+	_, err = os.Stat(checksumFile)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			t.Logf("Failed to read cache: %s", err)
+		}
+		return false, checksumFile
+	}
+
+	return true, checksumFile
+}
+
+func writeCache(t *testing.T, checksumFile string) {
+	if checksumFile == "" {
+		return
+	}
+
+	currentTime := time.Now()
+	formattedTime := currentTime.Format(time.RFC3339)
+
+	err := os.WriteFile(checksumFile, []byte(formattedTime), 0o644)
+	if err != nil {
+		t.Logf("Failed to write cache %s: %s", checksumFile, err)
 	}
 }
 
@@ -303,4 +375,58 @@ func CopyDir(src, dst string, inputs, outputs map[string]bool) error {
 
 		return copyFile(path, destPath)
 	})
+}
+
+func MustCalculateMD5(t *testing.T, filePath string) string {
+	start := time.Now()
+	hash := md5.New()
+	AddFile(t, hash, filePath)
+	checksum := hash.Sum(nil)
+	elapsed := time.Since(start)
+	result := hex.EncodeToString(checksum)
+	t.Logf("Calculated md5 of %s in %s: %s", filePath, elapsed, result)
+	return result
+}
+
+func AddFile(t *testing.T, h hash.Hash, filePath string) {
+	file, err := os.Open(filePath)
+	require.NoError(t, err)
+	defer file.Close()
+	_, err = io.Copy(h, file)
+	require.NoError(t, err)
+}
+
+func GetChecksum(h hash.Hash) string {
+	checksum := h.Sum(nil)
+	return hex.EncodeToString(checksum)
+}
+
+func GetGoCache(t *testing.T) string {
+	defaultDir := os.Getenv("GOCACHE")
+	if defaultDir != "" {
+		if filepath.IsAbs(defaultDir) {
+			return defaultDir
+		} else {
+			t.Logf("GOCACHE is not absolute path: %s", defaultDir)
+		}
+	}
+
+	dir, err := os.UserCacheDir()
+	if err != nil {
+		t.Logf("UserCacheDir failed: %s", err)
+		return ""
+	}
+
+	return filepath.Join(dir, "go-build")
+}
+
+func GetCacheLocation(t *testing.T) string {
+	dir := GetGoCache(t)
+	dir = filepath.Join(dir, "ff")
+	err := os.MkdirAll(dir, 0o755)
+	if err != nil {
+		t.Logf("Failed to create cache dir %s: %s", dir, err)
+		return ""
+	}
+	return dir
 }
