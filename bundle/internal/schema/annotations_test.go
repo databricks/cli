151a6f86dde4e469e12f065e9dc4c29d2e885769
Ilya Kuznetsov <ilya.kuznetsov@databricks.com>
2025-01-10 17:51:22 +0100

    fix: Multiple links

--- bundle/internal/schema/annotations_test.go
+++ bundle/internal/schema/annotations_test.go
@@ -9,37 +9,33 @@ func TestConvertLinksToAbsoluteUrl(t *testing.T) {
 		input    string
 		expected string
 	}{
-		// {
-		// 	input:    "",
-		// 	expected: "",
-		// },
-		// {
-		// 	input:    "Some text (not a link)",
-		// 	expected: "Some text (not a link)",
-		// },
-		// {
-		// 	input:    "This is a link to [_](#section)",
-		// 	expected: "This is a link to [section](https://docs.databricks.com/dev-tools/bundles/reference.html#section)",
-		// },
-		// {
-		// 	input:    "This is a link to [_](/dev-tools/bundles/resources.html#dashboard)",
-		// 	expected: "This is a link to [dashboard](https://docs.databricks.com/dev-tools/bundles/resources.html#dashboard)",
-		// },
-		// {
-		// 	input:    "This is a link to [_](/dev-tools/bundles/resources.html)",
-		// 	expected: "This is a link to [link](https://docs.databricks.com/dev-tools/bundles/resources.html)",
-		// },
-		// {
-		// 	input:    "This is a link to [external](https://external.com)",
-		// 	expected: "This is a link to [external](https://external.com)",
-		// },
-		// {
-		// 	input:    "This is a link to [pipelines](/api/workspace/pipelines/create)",
-		// 	expected: "This is a link to [pipelines](https://docs.databricks.com/api/workspace/pipelines/create)",
-		// },
 		{
-			input:    "The registered model resource allows you to define models in \u003cUC\u003e. For information about \u003cUC\u003e [registered models](/api/workspace/registeredmodels/create), [registered models 2](/api/workspace/registeredmodels/create)",
-			expected: "The registered model resource allows you to define models in \u003cUC\u003e. For information about \u003cUC\u003e [registered models](/api/workspace/registeredmodels/create), [registered models 2](/api/workspace/registeredmodels/create)",
+			input:    "",
+			expected: "",
+		},
+		{
+			input:    "Some text (not a link)",
+			expected: "Some text (not a link)",
+		},
+		{
+			input:    "This is a link to [_](#section)",
+			expected: "This is a link to [section](https://docs.databricks.com/dev-tools/bundles/reference.html#section)",
+		},
+		{
+			input:    "This is a link to [_](/dev-tools/bundles/resources.html#dashboard)",
+			expected: "This is a link to [dashboard](https://docs.databricks.com/dev-tools/bundles/resources.html#dashboard)",
+		},
+		{
+			input:    "This is a link to [_](/dev-tools/bundles/resources.html)",
+			expected: "This is a link to [link](https://docs.databricks.com/dev-tools/bundles/resources.html)",
+		},
+		{
+			input:    "This is a link to [external](https://external.com)",
+			expected: "This is a link to [external](https://external.com)",
+		},
+		{
+			input:    "This is a link to [one](/relative), [two](/relative-2)",
+			expected: "This is a link to [one](https://docs.databricks.com/relative), [two](https://docs.databricks.com/relative-2)",
 		},
 	}
 
