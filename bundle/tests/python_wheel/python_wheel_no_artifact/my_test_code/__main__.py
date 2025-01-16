14bc890d67727e3d5a319062cdb58ca25e72762c
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2025-01-16 16:26:30 +0100

    Format Python code with ruff

--- bundle/tests/python_wheel/python_wheel_no_artifact/my_test_code/__main__.py
+++ bundle/tests/python_wheel/python_wheel_no_artifact/my_test_code/__main__.py
@@ -7,10 +7,10 @@ import sys
 
 def main():
     # This method will print the provided arguments
-    print('Hello from my func')
-    print('Got arguments:')
+    print("Hello from my func")
+    print("Got arguments:")
     print(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
