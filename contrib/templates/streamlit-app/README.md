0328020d3945fd509b605f96e4bd7b76bb3a7aae
Andrew Nester <andrew.nester.dev@gmail.com>
2025-01-17 10:56:32 +0100

    Added a template for Streamlit Databricks data app (#56)
    This template contains a bundle structure to create and manage
    Databricks App which reads data from a SQL warehouse and visualizes it.
    The app is written in Python using Streamlit framework.
    
    Install the template using the following command:
    
    ```
    databricks bundle init https://github.com/databricks/bundle-examples --template-dir contrib/templates/streamlit-app
    ```
    
    ---------
    
    Co-authored-by: Pieter Noordhuis <pieter.noordhuis@databricks.com>

---
+++ contrib/templates/streamlit-app/README.md
@@ -0,0 +1,10 @@
+# streamlit-app template
+
+This template contains a bundle structure for creating and managing a Databricks app that reads data from a SQL warehouse and visualizes it.
+The app is written in Python using the Streamlit framework.
+
+Initialize the template using the following command:
+
+```
+databricks bundle init https://github.com/databricks/bundle-examples --template-dir contrib/templates/streamlit-app
+```
---
+++ contrib/templates/streamlit-app/template/{{.project_name}}/app/app.py
@@ -0,0 +1,51 @@
+import os
+from databricks import sql
+from databricks.sdk.core import Config
+import streamlit as st
+import pandas as pd
+
+# Ensure environment variable is set correctly
+assert os.getenv(
+    "DATABRICKS_WAREHOUSE_ID"
+), "DATABRICKS_WAREHOUSE_ID must be set in app.yaml."
+
+
+def sqlQuery(query: str) -> pd.DataFrame:
+    cfg = Config()  # Pull environment variables for auth
+    with sql.connect(
+        server_hostname=cfg.host,
+        http_path=f"/sql/1.0/warehouses/{os.getenv('DATABRICKS_WAREHOUSE_ID')}",
+        credentials_provider=lambda: cfg.authenticate,
+    ) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            return cursor.fetchall_arrow().to_pandas()
+
+
+st.set_page_config(layout="wide")
+
+
+@st.cache_data(ttl=30)  # only re-query if it's been 30 seconds
+def getData():
+    # This example query depends on the nyctaxi data set in Unity Catalog, see https://docs.databricks.com/en/discover/databricks-datasets.html for details
+    return sqlQuery("select * from samples.nyctaxi.trips limit 5000")
+
+
+data = getData()
+
+st.header("Taxi fare distribution !!! :)")
+col1, col2 = st.columns([3, 1])
+with col1:
+    st.scatter_chart(
+        data=data, height=400, width=700, y="fare_amount", x="trip_distance"
+    )
+with col2:
+    st.subheader("Predict fare")
+    pickup = st.text_input("From (zipcode)", value="10003")
+    dropoff = st.text_input("To (zipcode)", value="11238")
+    d = data[
+        (data["pickup_zip"] == int(pickup)) & (data["dropoff_zip"] == int(dropoff))
+    ]
+    st.write(f"# **${d['fare_amount'].mean() if len(d) > 0 else 99:.2f}**")
+
+st.dataframe(data=data, height=600, use_container_width=True)
