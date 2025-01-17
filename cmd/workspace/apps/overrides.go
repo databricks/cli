3b304565f7a8e75c2c443d035dc4183b2170dcfa
Andrew Nester <andrew.nester@databricks.com>
2025-01-17 12:00:00 +0000

    fix

--- cmd/workspace/apps/overrides.go
+++ cmd/workspace/apps/overrides.go
@@ -8,9 +8,9 @@ import (
 
 func listOverride(listCmd *cobra.Command, listReq *apps.ListAppsRequest) {
 	listCmd.Annotations["headerTemplate"] = cmdio.Heredoc(`
-	{{header "Name"}}		{{header "Url"}}	{{header "ComputeStatus"}}	{{header "DeploymentStatus"}}`)
+	{{header "Name"}}	{{header "Url"}}	{{header "ComputeStatus"}}	{{header "DeploymentStatus"}}`)
 	listCmd.Annotations["template"] = cmdio.Heredoc(`
-	{{range .}}{{.Name | green}}		{{.Url}}	{{if .ComputeStatus}}{{if eq .ComputeStatus.State "ACTIVE"}}{{green "%s" .ComputeStatus.State }}{{else}}{{blue "%s" .ComputeStatus.State}}{{end}}{{end}}	{{if .ActiveDeployment}}{{if eq .ActiveDeployment.Status.State "SUCCEEDED"}}{{green "%s" .ActiveDeployment.Status.State }}{{else}}{{blue "%s" .ActiveDeployment.Status.State}}{{end}}{{end}}
+	{{range .}}{{.Name | green}}	{{.Url}}	{{if .ComputeStatus}}{{if eq .ComputeStatus.State "ACTIVE"}}{{green "%s" .ComputeStatus.State }}{{else}}{{blue "%s" .ComputeStatus.State}}{{end}}{{end}}	{{if .ActiveDeployment}}{{if eq .ActiveDeployment.Status.State "SUCCEEDED"}}{{green "%s" .ActiveDeployment.Status.State }}{{else}}{{blue "%s" .ActiveDeployment.Status.State}}{{end}}{{end}}
 	{{end}}`)
 }
 
