7af9b81d296ac20e0d5738691f0dab74b700250b
Denis Bilenko <denis.bilenko@databricks.com>
2025-01-19 14:52:39 +0100

    pass all env

--- acceptance/bin/callserver.py
+++ acceptance/bin/callserver.py
@@ -3,20 +3,25 @@ import sys
 import os
 import json
 import subprocess
-from urllib.parse import quote_plus
+from urllib.parse import urlencode
 
 env = {}
 for key, value in os.environ.items():
-    if "BUNDLE_VAR" in key or "DATABRICKS" in key:
-        env[key] = value
+    if len(value) > 1000:
+        sys.stderr.write(f"Dropping key={key} value len={len(value)}")
+        continue
+    env[key] = value
 
-q = [
-    "args=" + quote_plus(" ".join(sys.argv[1:])),
-    "cwd=" + quote_plus(os.getcwd()),
-    "env=" + quote_plus(json.dumps(env)),
-]
+q = {
+    "args": " ".join(sys.argv[1:]),
+    "cwd": os.getcwd(),
+    "env": json.dumps(env),
+}
 
-url = os.environ["CMD_SERVER_URL"] + "/?" + "&".join(q)
+url = os.environ["CMD_SERVER_URL"] + "/?" + urlencode(q)
+if len(url) > 100_000:
+    sys.exit("url too large")
+
 out = subprocess.run(["curl", "-s", url], stdout=subprocess.PIPE, check=True)
 result = json.loads(out.stdout)
 sys.stderr.write(result["stderr"])
