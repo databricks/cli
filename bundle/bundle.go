c0cf3f85357fc5f32c6c0fb2ebc765f0a74d9a03
Shreyas Goenka <shreyas.goenka@databricks.com>
2025-01-21 16:30:21 +0100

    remove unused parameter

--- bundle/bundle.go
+++ bundle/bundle.go
@@ -12,7 +12,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"sync"
 
 	"github.com/databricks/cli/bundle/config"
 	"github.com/databricks/cli/bundle/env"
@@ -69,9 +68,7 @@ type Bundle struct {
 	Metadata metadata.Metadata
 
 	// Store a pointer to the workspace client.
-	// It can be initialized on demand after loading the configuration.
-	clientOnce sync.Once
-	client     *databricks.WorkspaceClient
+	client *databricks.WorkspaceClient
 
 	// Files that are synced to the workspace.file_path
 	Files []fileset.File
