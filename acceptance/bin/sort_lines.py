1c31950c0e3dbd42f3faa2f9caa2803257bd1c37
Denis Bilenko <denis.bilenko@databricks.com>
2025-01-27 10:37:54 +0100

    ruff format

--- acceptance/bin/sort_lines.py
+++ acceptance/bin/sort_lines.py
@@ -2,7 +2,9 @@
 """
 Helper to sort lines in text file. Similar to 'sort' but no dependence on locale or presence of 'sort' in PATH.
 """
+
 import sys
+
 lines = sys.stdin.readlines()
 lines.sort()
 sys.stdout.write("".join(lines))
