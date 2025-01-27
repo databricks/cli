0920b85916e33f6c6e9f9ab76e6091f2f34c0e5f
Denis Bilenko <denis.bilenko@databricks.com>
2025-01-27 15:03:24 +0100

    ruff

--- acceptance/bundle/scripts/myscript.py
+++ acceptance/bundle/scripts/myscript.py
@@ -1,7 +1,7 @@
 import sys
 
 info = " ".join(sys.argv[1:])
-sys.stderr.write(f"from myscript.py {info}: hello stderr!\n" )
+sys.stderr.write(f"from myscript.py {info}: hello stderr!\n")
 sys.stdout.write(f"from myscript.py {info}: hello stdout!\n")
 
 exitcode = int(sys.argv[1])
