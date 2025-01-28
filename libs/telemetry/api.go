6423fbb4aa1b18e6ae57ce83f5edbd112eac6c76
Shreyas Goenka <shreyas.goenka@databricks.com>
2025-01-28 11:47:43 +0100

    add comments

--- libs/telemetry/api.go
+++ libs/telemetry/api.go
@@ -2,9 +2,20 @@ package telemetry
 
 // RequestBody is the request body type bindings for the /telemetry-ext API endpoint.
 type RequestBody struct {
-	UploadTime int64    `json:"uploadTime"`
-	Items      []string `json:"items"`
-	ProtoLogs  []string `json:"protoLogs"`
+	UploadTime int64 `json:"uploadTime"`
+
+	// DO NOT USE. This is the legacy field for logging in usage logs (not lumberjack).
+	// We keep this around because the API endpoint works only if this field is serialized
+	// to an empty array.
+	Items []string `json:"items"`
+
+	// JSON encoded strings containing the proto logs. Since it's represented as a
+	// string here, the values here end up being double JSON encoded in the final
+	// request body.
+	//
+	// Any logs here will be logged in our lumberjack tables as long as a corresponding
+	// protobuf is defined in universe.
+	ProtoLogs []string `json:"protoLogs"`
 }
 
 // ResponseBody is the response body type bindings for the /telemetry-ext API endpoint.
