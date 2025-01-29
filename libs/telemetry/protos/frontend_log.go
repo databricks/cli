9d2286f248c4a01bcaf1fd3ebeb9153b78db86ca
Shreyas Goenka <shreyas.goenka@databricks.com>
2025-01-29 12:30:29 +0100

    fmt

--- libs/telemetry/protos/frontend_log.go
+++ libs/telemetry/protos/frontend_log.go
@@ -16,7 +16,7 @@ type FrontendLogEntry struct {
 type DatabricksCliLog struct {
 	ExecutionContext *ExecutionContext `json:"execution_context,omitempty"`
 
-	CliTestEvent     *CliTestEvent      `json:"cli_test_event,omitempty"`
-	BundleInitEvent  *BundleInitEvent   `json:"bundle_init_event,omitempty"`
+	CliTestEvent      *CliTestEvent      `json:"cli_test_event,omitempty"`
+	BundleInitEvent   *BundleInitEvent   `json:"bundle_init_event,omitempty"`
 	BundleDeployEvent *BundleDeployEvent `json:"bundle_deploy_event,omitempty"`
 }
