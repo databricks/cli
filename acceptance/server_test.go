24ac8d8d595df531a2aa7b9faa72f8be7132f7b1
shreyas-goenka <88374338+shreyas-goenka@users.noreply.github.com>
2025-02-11 21:20:03 +0530

    Add acceptance tests for auth resolution (#2285)
    ## Changes
    
    This PR adds acceptance tests for native Databricks auth methods: basic,
    oauth, and pat.
    
    In the future we could compare this with auth credentials used by
    downstream tools like TF or the telemetry process to ensure consistent
    auth credentials are picked up and used.
    
    Note: 
    We do not add acceptance tests for other auth methods like Azure because
    they communicate with external endpoints. To test them locally, we would
    need to set up a reverse proxy server, which is out of scope for this
    change.
    
    ## Tests
    N/A

--- acceptance/server_test.go
+++ acceptance/server_test.go
@@ -166,6 +166,22 @@ func AddHandlers(server *testserver.Server) {
 	server.Handle("GET", "/api/2.1/jobs/list", func(fakeWorkspace *testserver.FakeWorkspace, r *http.Request) (any, int) {
 		return fakeWorkspace.JobsList()
 	})
+
+	server.Handle("GET", "/oidc/.well-known/oauth-authorization-server", func(fakeWorkspace *testserver.FakeWorkspace, r *http.Request) (any, int) {
+		return map[string]string{
+			"authorization_endpoint": server.URL + "oidc/v1/authorize",
+			"token_endpoint":         server.URL + "/oidc/v1/token",
+		}, http.StatusOK
+	})
+
+	server.Handle("POST", "/oidc/v1/token", func(fakeWorkspace *testserver.FakeWorkspace, r *http.Request) (any, int) {
+		return map[string]string{
+			"access_token": "oauth-token",
+			"expires_in":   "3600",
+			"scope":        "all-apis",
+			"token_type":   "Bearer",
+		}, http.StatusOK
+	})
 }
 
 func internalError(err error) (any, int) {
