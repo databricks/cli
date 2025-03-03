9c48a0b5a0a9674cebdba40ca38ea246a38791c4
Andrew Nester <andrew.nester@databricks.com>
2025-03-03 12:59:12 +0100

    added acc test

--- bundle/apps/validate_test.go
+++ bundle/apps/validate_test.go
@@ -11,47 +11,11 @@ import (
 	"github.com/databricks/cli/bundle/config/resources"
 	"github.com/databricks/cli/bundle/internal/bundletest"
 	"github.com/databricks/cli/internal/testutil"
-	"github.com/databricks/cli/libs/diag"
 	"github.com/databricks/cli/libs/dyn"
 	"github.com/databricks/cli/libs/vfs"
 	"github.com/databricks/databricks-sdk-go/service/apps"
 	"github.com/stretchr/testify/require"
 )
-
-func TestAppsValidate(t *testing.T) {
-	tmpDir := t.TempDir()
-	testutil.Touch(t, tmpDir, "app1", "app.py")
-
-	b := &bundle.Bundle{
-		BundleRootPath: tmpDir,
-		SyncRootPath:   tmpDir,
-		SyncRoot:       vfs.MustNew(tmpDir),
-		Config: config.Root{
-			Workspace: config.Workspace{
-				FilePath: "/foo/bar/",
-			},
-			Resources: config.Resources{
-				Apps: map[string]*resources.App{
-					"app1": {
-						App: &apps.App{
-							Name: "app1",
-						},
-						SourceCodePath: "./app1",
-						Config:         map[string]any{},
-					},
-				},
-			},
-		},
-	}
-
-	bundletest.SetLocation(b, ".", []dyn.Location{{File: filepath.Join(tmpDir, "databricks.yml")}})
-
-	diags := bundle.ApplySeq(context.Background(), b, mutator.TranslatePaths(), Validate())
-	require.Len(t, diags, 1)
-	require.Equal(t, diag.Warning, diags[0].Severity)
-	require.Equal(t, "App config section detected", diags[0].Summary)
-	require.Contains(t, diags[0].Detail, "remove 'config' from app resource 'app1' section and use app.yml file in the root of this app instead")
-}
 
 func TestAppsValidateSameSourcePath(t *testing.T) {
 	tmpDir := t.TempDir()
