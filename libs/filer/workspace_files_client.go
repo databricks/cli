3eb7a9a4c397452715402ddf6d43a2053949cfad
Andrew Nester <andrew.nester@databricks.com>
2025-03-03 16:52:04 +0100

    Switch to use GET workspaces-files/{name} instead of workspace/export

--- libs/filer/workspace_files_client.go
+++ libs/filer/workspace_files_client.go
@@ -222,9 +222,7 @@ func (w *WorkspaceFilesClient) Read(ctx context.Context, name string) (io.ReadCl
 
 	// This stat call serves two purposes:
 	// 1. Checks file at path exists, and throws an error if it does not
-	// 2. Allows us to error out if the path is a directory. This is needed
-	// because the /workspace/export API does not error out, and returns the directory
-	// as a DBC archive even if format "SOURCE" is specified
+	// 2. Allows us to error out if the path is a directory.
 	stat, err := w.Stat(ctx, name)
 	if err != nil {
 		return nil, err
@@ -233,9 +231,14 @@ func (w *WorkspaceFilesClient) Read(ctx context.Context, name string) (io.ReadCl
 		return nil, NotAFile{absPath}
 	}
 
-	// Export file contents. Note the /workspace/export API has a limit of 10MBs
-	// for the file size
-	return w.workspaceClient.Workspace.Download(ctx, absPath)
+	var buf bytes.Buffer
+	urlPath := "/api/2.0/workspace-files/" + url.PathEscape(strings.TrimLeft(absPath, "/"))
+	err = w.apiClient.Do(ctx, http.MethodGet, urlPath, nil, nil, nil, &buf)
+	if err != nil {
+		return nil, err
+	}
+
+	return io.NopCloser(&buf), nil
 }
 
 func (w *WorkspaceFilesClient) Delete(ctx context.Context, name string, mode ...DeleteMode) error {
