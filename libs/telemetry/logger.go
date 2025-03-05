b3a479ce735b309648bcbaecd438e134033d227f
Shreyas Goenka <shreyas.goenka@databricks.com>
2025-03-05 15:21:12 +0100

    add sync logger

--- libs/telemetry/logger.go
+++ libs/telemetry/logger.go
@@ -2,8 +2,17 @@ package telemetry
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
 
+	"github.com/databricks/cli/libs/log"
 	"github.com/databricks/cli/libs/telemetry/protos"
+	"github.com/databricks/databricks-sdk-go/apierr"
+	"github.com/databricks/databricks-sdk-go/client"
+	"github.com/databricks/databricks-sdk-go/config"
 	"github.com/google/uuid"
 )
 
@@ -47,4 +56,87 @@ func (l *logger) setExecutionContext(ec protos.ExecutionContext) {
 	for i := range l.logs {
 		l.logs[i].Entry.DatabricksCliLog.ExecutionContext = &ec
 	}
+}
+
+// TODO: Test that the max timeout here is indeed 3 seconds.
+
+func Upload(ctx context.Context, cfg *config.Config) error {
+	l := fromContext(ctx)
+	if len(l.logs) == 0 {
+		return nil
+	}
+
+	protoLogs := make([]string, len(l.logs))
+	for i, log := range l.logs {
+		b, err := json.Marshal(log)
+		if err != nil {
+			return fmt.Errorf("failed to marshal log: %s", err)
+		}
+		protoLogs[i] = string(b)
+	}
+
+	apiClient, err := client.New(cfg)
+	if err != nil {
+		return fmt.Errorf("failed to create API client: %w", err)
+	}
+
+	ctx, _ = context.WithTimeout(ctx, 3*time.Second)
+	var resp *ResponseBody
+
+	// Only try uploading logs for a maximum of 3 times.
+	for i := range 3 {
+		select {
+		case <-ctx.Done():
+			return fmt.Errorf("uploading telemetry logs timed out: %w", ctx.Err())
+		default:
+			// proceed
+		}
+
+		resp, err = attempt(ctx, apiClient, protoLogs)
+
+		// All logs were uploaded successfully.
+		if err == nil && resp.NumProtoSuccess >= int64(len(protoLogs)) {
+			return nil
+		}
+
+		// Partial success. Retry.
+		if err == nil && resp.NumProtoSuccess < int64(len(protoLogs)) {
+			log.Debugf(ctx, "Attempt %d was a partial success. Number of logs uploaded: %d out of %d\n", i+1, resp.NumProtoSuccess, len(protoLogs))
+			time.Sleep(200 * time.Millisecond)
+			continue
+		}
+
+		// We retry for all 5xx responses. We explicitly omit 503 in the predicate here
+		// because it is already automatically retried in the SDK layer.
+		// ref: https://github.com/databricks/databricks-sdk-go/blob/cdb28002afacb8b762348534a4c4040a9f19c24b/apierr/errors.go#L91
+		var apiErr *apierr.APIError
+		if errors.As(err, &apiErr) && apiErr.StatusCode >= 500 && apiErr.StatusCode != 503 {
+			log.Debugf(ctx, "Attempt %d failed due to a server side error. Retrying status code: %d\n", i+1, apiErr.StatusCode)
+			time.Sleep(200 * time.Millisecond)
+			continue
+		}
+	}
+
+	return errors.New("failed to upload telemetry logs after three attempts")
+}
+
+func attempt(ctx context.Context, apiClient *client.DatabricksClient, protoLogs []string) (*ResponseBody, error) {
+	resp := &ResponseBody{}
+	err := apiClient.Do(ctx, http.MethodPost, "/telemetry-ext", nil, nil, RequestBody{
+		UploadTime: time.Now().UnixMilli(),
+		// There is a bug in the `/telemetry-ext` API which requires us to
+		// send an empty array for the `Items` field. Otherwise the API returns
+		// a 500.
+		Items:     []string{},
+		ProtoLogs: protoLogs,
+	}, resp)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(resp.Errors) > 0 {
+		return nil, fmt.Errorf("uploading telemetry failed: %v", resp.Errors)
+	}
+
+	return resp, nil
 }
