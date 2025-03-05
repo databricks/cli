4dba35dff47cb7a0bff87358f6f5457c2dc4393a
Andrew Nester <andrew.nester@databricks.com>
2025-03-05 10:20:55 +0000

    Upgrade TF provider to 1.68.0 (#2426)
    ## Changes
    Upgrade TF provider to 1.68.0
    
    - Added support for Volumes as a destination for Cluster log conf

--- bundle/internal/tf/schema/resource_connection.go
+++ bundle/internal/tf/schema/resource_connection.go
@@ -3,13 +3,23 @@
 package schema
 
 type ResourceConnection struct {
-	Comment        string            `json:"comment,omitempty"`
-	ConnectionType string            `json:"connection_type"`
-	Id             string            `json:"id,omitempty"`
-	MetastoreId    string            `json:"metastore_id,omitempty"`
-	Name           string            `json:"name"`
-	Options        map[string]string `json:"options"`
-	Owner          string            `json:"owner,omitempty"`
-	Properties     map[string]string `json:"properties,omitempty"`
-	ReadOnly       bool              `json:"read_only,omitempty"`
+	Comment          string            `json:"comment,omitempty"`
+	ConnectionId     string            `json:"connection_id,omitempty"`
+	ConnectionType   string            `json:"connection_type,omitempty"`
+	CreatedAt        int               `json:"created_at,omitempty"`
+	CreatedBy        string            `json:"created_by,omitempty"`
+	CredentialType   string            `json:"credential_type,omitempty"`
+	FullName         string            `json:"full_name,omitempty"`
+	Id               string            `json:"id,omitempty"`
+	MetastoreId      string            `json:"metastore_id,omitempty"`
+	Name             string            `json:"name,omitempty"`
+	Options          map[string]string `json:"options,omitempty"`
+	Owner            string            `json:"owner,omitempty"`
+	Properties       map[string]string `json:"properties,omitempty"`
+	ProvisioningInfo []any             `json:"provisioning_info,omitempty"`
+	ReadOnly         bool              `json:"read_only,omitempty"`
+	SecurableType    string            `json:"securable_type,omitempty"`
+	UpdatedAt        int               `json:"updated_at,omitempty"`
+	UpdatedBy        string            `json:"updated_by,omitempty"`
+	Url              string            `json:"url,omitempty"`
 }
