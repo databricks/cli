bc4d915c6ca348be7b7d5dd09c92b3c30198438d
Shreyas Goenka <shreyas.goenka@databricks.com>
2025-03-06 19:26:12 +0100

    add unit test

---
+++ libs/command/workspace_client_test.go
@@ -0,0 +1,40 @@
+package command_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/databricks/cli/libs/command"
+	"github.com/databricks/databricks-sdk-go"
+	"github.com/databricks/databricks-sdk-go/config"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCommand_WorkspaceClient(t *testing.T) {
+	ctx := context.Background()
+	client := &databricks.WorkspaceClient{
+		Config: &config.Config{
+			Host: "https://test.com",
+		},
+	}
+
+	// Panic if WorkspaceClient is called before SetWorkspaceClient.
+	assert.Panics(t, func() {
+		command.WorkspaceClient(ctx)
+	})
+
+	ctx = command.SetWorkspaceClient(context.Background(), client)
+
+	// Multiple calls should return a pointer to the same client.
+	w := command.WorkspaceClient(ctx)
+	assert.Equal(t, w, command.WorkspaceClient(ctx))
+	assert.Equal(t, w, command.WorkspaceClient(ctx))
+
+	// The client should have the correct configuration.
+	assert.Equal(t, "https://test.com", command.WorkspaceClient(ctx).Config.Host)
+
+	// Second call should panic.
+	assert.Panics(t, func() {
+		command.SetWorkspaceClient(ctx, client)
+	})
+}
