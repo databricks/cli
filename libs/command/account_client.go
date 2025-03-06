93928e25e68a5c4dba4ead040a016318e8c1c454
Shreyas Goenka <shreyas.goenka@databricks.com>
2025-03-06 19:54:02 +0100

    add unit test

--- libs/command/account_client.go
+++ libs/command/account_client.go
@@ -6,21 +6,6 @@ import (
 	"github.com/databricks/databricks-sdk-go"
 )
 
-// func SetWorkspaceClient(ctx context.Context, w *databricks.WorkspaceClient) context.Context {
-// 	if v := ctx.Value(workspaceClientKey); v != nil {
-// 		panic("command.SetWorkspaceClient called twice on the same context.")
-// 	}
-// 	return context.WithValue(ctx, workspaceClientKey, w)
-// }
-
-// func WorkspaceClient(ctx context.Context) *databricks.WorkspaceClient {
-// 	v := ctx.Value(workspaceClientKey)
-// 	if v == nil {
-// 		panic("command.WorkspaceClient called without calling command.SetWorkspaceClient first.")
-// 	}
-// 	return v.(*databricks.WorkspaceClient)
-// }
-
 func SetAccountClient(ctx context.Context, a *databricks.AccountClient) context.Context {
 	if v := ctx.Value(accountClientKey); v != nil {
 		panic("command.SetAccountClient called twice on the same context")
