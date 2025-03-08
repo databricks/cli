5faed430aaa04aac05e0b2da89a5e0fcdacc0cd3
hari-selvarajan_data <hari.selvarajan@databricks.com>
2025-03-08 22:04:26 +0000

    added comment in test to explain the scenario

--- cmd/labs/project/installer_test.go
+++ cmd/labs/project/installer_test.go
@@ -242,6 +242,11 @@ func TestInstallerWorksForReleases(t *testing.T) {
 }
 
 func TestOfflineInstallerWorksForReleases(t *testing.T) {
+	// This cmd is useful in systems where there is internet restriction, the user should follow a set-up as follows:
+	// install a labs project on a machine which has internet
+	// zip and copy the file to the intended machine and
+	// run databricks labs install --offline=true
+	// it will look for the code in the same install directory and if present load from there.
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path == "/api/2.1/clusters/get" {
 			respondWithJSON(t, w, &compute.ClusterDetails{
