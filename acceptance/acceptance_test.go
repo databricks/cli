01e8eb45a03fc254b0a725aa5d846617568cb609
Denis Bilenko <denis.bilenko@databricks.com>
2025-03-11 17:35:18 +0100

    update

--- acceptance/acceptance_test.go
+++ acceptance/acceptance_test.go
@@ -3,6 +3,7 @@ package acceptance_test
 import (
 	"bufio"
 	"context"
+	"encoding/base32"
 	"encoding/json"
 	"errors"
 	"flag"
@@ -258,9 +259,10 @@ func runTest(t *testing.T, dir, coverDir string, repls testdiff.ReplacementsCont
 		}
 	}
 
-	// XXX rename away from uuid1 since it's not uuid due to replacements
-	uuid1 := strings.ReplaceAll(uuid.NewString(), "-", "_")
-	repls.Set(uuid1, "[UUID1]")
+	id := uuid.New()
+	uniqueName := strings.Trim(base32.StdEncoding.EncodeToString(id[:]), "=")
+	require.Greater(t, len(uniqueName), 8, uniqueName)
+	repls.Set(uniqueName, "[UNIQUE_NAME]")
 
 	var tmpDir string
 	var err error
@@ -287,7 +289,7 @@ func runTest(t *testing.T, dir, coverDir string, repls testdiff.ReplacementsCont
 	args := []string{"bash", "-euo", "pipefail", EntryPointScript}
 	cmd := exec.Command(args[0], args[1:]...)
 	cmd.Env = os.Environ()
-	cmd.Env = append(cmd.Env, "UUID1="+uuid1)
+	cmd.Env = append(cmd.Env, "UNIQUE_NAME="+uniqueName)
 
 	var workspaceClient *databricks.WorkspaceClient
 	var user iam.User
