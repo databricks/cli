38a9da0dedee2d3a09a2b70385dc0a7900ae569c
Andrew Nester <andrew.nester@databricks.com>
2025-03-12 16:40:15 +0100

    Added databricks apps run-local command to run Databricks apps locally

---
+++ cmd/workspace/apps/run-local.go
@@ -0,0 +1,165 @@
+package apps
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/databricks/cli/cmd/root"
+	"github.com/databricks/cli/libs/appproxy"
+	"github.com/databricks/cli/libs/apps"
+	"github.com/databricks/cli/libs/auth"
+	"github.com/databricks/cli/libs/cmdctx"
+	"github.com/databricks/cli/libs/cmdio"
+	"github.com/spf13/cobra"
+)
+
+var (
+	port               int
+	debug              bool
+	prepareEnvironment bool
+	entryPoint         string
+	customEnv          []string
+)
+
+func newRunLocal() *cobra.Command {
+	cmd := &cobra.Command{}
+
+	cmd.Use = "run-local"
+	cmd.Short = `Run an app locally`
+	cmd.Long = `Run an app locally.
+
+	  This command starts an app locally.`
+
+	cmd.Flags().IntVar(&port, "port", 8001, "Port on which to run the app")
+	cmd.Flags().BoolVar(&debug, "debug", false, "Enable debug mode")
+	cmd.Flags().BoolVar(&prepareEnvironment, "prepare-environment", false, "Prepares the environment for running the app. Requires 'uv' to be installed.")
+	cmd.Flags().StringSliceVar(&customEnv, "env", nil, "Set environment variables")
+	cmd.Flags().StringVar(&entryPoint, "entry-point", "", "Specify the custom entry point with configuration (.yml file) for the app")
+
+	cmd.PreRunE = root.MustWorkspaceClient
+	cmd.RunE = func(cmd *cobra.Command, args []string) (err error) {
+		ctx := cmd.Context()
+		w := cmdctx.WorkspaceClient(ctx)
+		workspaceId, err := w.CurrentWorkspaceID(ctx)
+		if err != nil {
+			return err
+		}
+
+		cwd, err := os.Getwd()
+		if err != nil {
+			return err
+		}
+
+		config := apps.NewConfig(w.Config.Host, workspaceId, cwd)
+		if entryPoint != "" {
+			config.AppSpecFiles = []string{entryPoint}
+		}
+		spec, err := apps.ReadAppSpecFile(config)
+		if err != nil {
+			return err
+		}
+
+		env := auth.ProcessEnv(cmdctx.ConfigUsed(cmd.Context()))
+		profileFlag := cmd.Flag("profile")
+		if profileFlag != nil {
+			env = append(env, "DATABRICKS_CONFIG_PROFILE="+profileFlag.Value.String())
+		}
+
+		for _, envVar := range spec.EnvVars {
+			if envVar.Value != nil {
+				env = append(env, envVar.Name+"="+*envVar.Value)
+			}
+
+			if envVar.ValueFrom != nil {
+				found := false
+				for _, e := range customEnv {
+					if strings.HasPrefix(e, envVar.Name+"=") {
+						found = true
+						break
+					}
+				}
+				if !found {
+					return fmt.Errorf("env var %s needs to be set", envVar.Name)
+				}
+			}
+		}
+
+		app := apps.NewApp(config, spec)
+		if prepareEnvironment {
+			err := app.PrepareEnvironment()
+			if err != nil {
+				return err
+			}
+		}
+
+		specCommand, err := app.GetCommand(debug)
+		if err != nil {
+			return err
+		}
+
+		appCmd := exec.Command(specCommand[0], specCommand[1:]...)
+		appCmd.Stdin = cmd.InOrStdin()
+		appCmd.Stdout = cmd.OutOrStdout()
+		appCmd.Stderr = cmd.ErrOrStderr()
+
+		appEnvs := apps.GetBaseEnvVars(config)
+		for _, envVar := range appEnvs {
+			env = append(env, envVar.String())
+		}
+		env = append(env, customEnv...)
+
+		appCmd.Env = env
+		appCmd.Dir = config.AppPath
+
+		err = appCmd.Start()
+		if err != nil {
+			return err
+		}
+
+		proxy, err := appproxy.New(ctx, config.AppURL)
+		if err != nil {
+			return err
+		}
+
+		me, err := w.CurrentUser.Me(ctx)
+		if err != nil {
+			return err
+		}
+
+		for key, value := range apps.GetXHeaders(me) {
+			proxy.InjectHeader(key, value)
+		}
+
+		proxyAddr := fmt.Sprintf("localhost:%d", port)
+		go func() {
+			cmdio.LogString(ctx, "To access your app go to http://"+proxyAddr)
+			err := proxy.ListenAndServe(proxyAddr)
+			if err != nil {
+				cmd.PrintErrln(err)
+			}
+		}()
+
+		if debug {
+			cmdio.LogString(ctx, "To debug your app, attach a debugger to port "+apps.DEBUG_PORT)
+		}
+
+		err = appCmd.Wait()
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+
+	cmd.ValidArgsFunction = cobra.NoFileCompletions
+
+	return cmd
+}
+
+func init() {
+	cmdOverrides = append(cmdOverrides, func(cmd *cobra.Command) {
+		cmd.AddCommand(newRunLocal())
+	})
+}
