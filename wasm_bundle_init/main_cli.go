cd742d3a2b42a34f6e219ac1323daef0346f3fca
Denis Bilenko <denis.bilenko@databricks.com>
2025-03-13 14:17:12 +0100

    refactor: simplify template handling and output in main_cli.go

--- wasm_bundle_init/main_cli.go
+++ wasm_bundle_init/main_cli.go
@@ -6,29 +6,19 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-
-	"github.com/databricks/cli/libs/template"
 )
 
 func main() {
 	// Use command line arg for template name if provided, otherwise default
-	templateName := "default-python"
-	if len(os.Args) > 1 {
-		templateName = os.Args[1]
-	}
-	
-	tmpl := template.GetTemplate(templateName)
-	// Output template info as indented JSON
-	tmplJSON, _ := json.MarshalIndent(tmpl, "", "  ")
-	fmt.Printf("Template: %s\n%s\n", templateName, tmplJSON)
+	templateName := os.Args[1]
+	paramsString := os.Args[2]
 
-	// Create sample parameters
-	params := map[string]string{"param1": "value1"}
-	
-	// Render the template with the given name and parameters
+	var params map[string]string
+	// AI TODO: parse paramsString json into params
+
 	out := Render(templateName, params)
-	
+
 	// Output rendered result as indented JSON
 	outJSON, _ := json.MarshalIndent(out, "", "  ")
-	fmt.Printf("Rendered output:\n%s\n", outJSON)
+	fmt.Printf("%s\n", outJSON)
 }
