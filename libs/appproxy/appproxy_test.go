3fe4e12f7a49612e45db70d8748cc8226c9fc4ef
Andrew Nester <andrew.nester@databricks.com>
2025-03-20 13:03:16 +0100

    use 0 port

--- libs/appproxy/appproxy_test.go
+++ libs/appproxy/appproxy_test.go
@@ -16,16 +16,11 @@ import (
 )
 
 const (
-	PROXY_PORT = 8081
+	PROXY_PORT = 0
 )
 
-var (
-	PROXY_ADDR = fmt.Sprintf("localhost:%d", PROXY_PORT)
-	PROXY_URL  = "http://" + PROXY_ADDR
-)
-
-func sendTestRequest(t *testing.T, path string) (int, []byte) {
-	req, err := http.NewRequest("GET", PROXY_URL+path, bytes.NewBufferString("{'test': 'value'}"))
+func sendTestRequest(t *testing.T, url, path string) (int, []byte) {
+	req, err := http.NewRequest("GET", url+path, bytes.NewBufferString("{'test': 'value'}"))
 	require.NoError(t, err)
 	req.Header.Set("Content-Type", "application/json")
 
@@ -39,18 +34,18 @@ func sendTestRequest(t *testing.T, path string) (int, []byte) {
 	return resp.StatusCode, body
 }
 
-func startProxy(t *testing.T, serverAddr string) *Proxy {
+func startProxy(t *testing.T, serverAddr string) (*Proxy, string) {
 	proxy, err := New(context.Background(), "http://"+serverAddr)
 	require.NoError(t, err)
 
-	ln, err := proxy.listen(PROXY_ADDR)
+	ln, err := proxy.listen(fmt.Sprintf("localhost:%d", PROXY_PORT))
 	require.NoError(t, err)
 
 	go func() {
 		_ = proxy.serve(ln)
 	}()
 
-	return proxy
+	return proxy, ln.Addr().String()
 }
 
 func TestProxyStart(t *testing.T) {
@@ -65,7 +60,8 @@ func TestProxyStart(t *testing.T) {
 	}()
 
 	serverAddr := server.Listener.Addr().String()
-	proxy := startProxy(t, serverAddr)
+	proxy, addr := startProxy(t, serverAddr)
+	hostUrl := "http://" + addr
 	defer func() {
 		err := proxy.Stop()
 		require.NoError(t, err)
@@ -75,12 +71,12 @@ func TestProxyStart(t *testing.T) {
 	proxy.InjectHeader("X-Test-Header2", "test2")
 
 	// Test the proxy by making a request to it
-	code, body := sendTestRequest(t, "/")
+	code, body := sendTestRequest(t, hostUrl, "/")
 	require.Equal(t, http.StatusOK, code)
 	require.Equal(t, "{'test': 'value'}", string(body))
 
 	// Send a request to the path that returns 404
-	code, body = sendTestRequest(t, "/404")
+	code, body = sendTestRequest(t, hostUrl, "/404")
 
 	require.Equal(t, http.StatusNotFound, code)
 	require.Contains(t, string(body), "Not Found")
@@ -88,7 +84,7 @@ func TestProxyStart(t *testing.T) {
 	// Close the test server
 	server.Close()
 
-	code, body = sendTestRequest(t, "/")
+	code, body = sendTestRequest(t, hostUrl, "/")
 	require.Equal(t, http.StatusInternalServerError, code)
 	require.Contains(t, string(body), fmt.Sprintf("Error forwarding request: Get \"http://%s/\": dial tcp %s", serverAddr, serverAddr))
 }
@@ -100,13 +96,13 @@ func TestProxyHandleWebSocket(t *testing.T) {
 		server.Start()
 	}()
 
-	proxy := startProxy(t, server.Addr())
+	proxy, addr := startProxy(t, server.Addr())
 	defer func() {
 		err := proxy.Stop()
 		require.NoError(t, err)
 	}()
 
-	conn, resp, err := websocket.DefaultDialer.Dial("ws://"+PROXY_ADDR, nil)
+	conn, resp, err := websocket.DefaultDialer.Dial("ws://"+addr, nil)
 	require.NoError(t, err)
 	defer resp.Body.Close()
 	defer conn.Close()
