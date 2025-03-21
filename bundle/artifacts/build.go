databricks/cli

commit 3985c433d805f5706b3dd1e6d9481b4f494e34af

Move artifacts validation to initialize phase; simplify (#2526)
## Changes
Moved processing & validating of artifacts section into initialize phase
so that it runs as part of 'bundle validate' and results can be
inspected in 'bundle validate -o json'.
The following things where moved from build phase to initialize:
- Creating python_artifact when artifacts is nil.
- Adding default 'build' field for whl artifacts.
- Expanding globs for cases where 'build' is absent.
- Cleaning up 'dist' and 'build' folders.

I've also removed a bunch of abstractions and put everything into two
(small) mutators
- artifacts.Validate() to do all the validations and processing
- artifacts.Build() to do the build and related post-processing.

## Why
- Having this things in validate allows users to learn early if
something is wrong with the configuration.
- Putting everything into 2 mutators allows to understand the whole flow
around artifacts. I need that for expanding it with dynamic_version
feature #2520.

## Tests
Existing tests.
Converted expand_globs_test.go into acceptance test that runs "bundle
validate".

acceptance/bundle/artifacts/globs_invalid/test.toml
@@ -1,0 +1,1 @@
+RecordRequests = false

bundle/artifacts/build.go
@@ -3,55 +3,85 @@ package artifacts
 import (
 	"context"
 	"fmt"
+	"path/filepath"
 
 	"github.com/databricks/cli/bundle"
+	"github.com/databricks/cli/bundle/config"
+	"github.com/databricks/cli/libs/cmdio"
 	"github.com/databricks/cli/libs/diag"
+	"github.com/databricks/cli/libs/exec"
+	"github.com/databricks/cli/libs/log"
+	"github.com/databricks/cli/libs/python"
 )
 
-func BuildAll() bundle.Mutator {
-	return &all{
-		name: "Build",
-		fn:   buildArtifactByName,
-	}
+func Build() bundle.Mutator {
+	return &build{}
 }
 
-type build struct {
-	name string
-}
-
-func buildArtifactByName(name string) (bundle.Mutator, error) {
-	return &build{name}, nil
-}
+type build struct{}
 
 func (m *build) Name() string {
-	return fmt.Sprintf("artifacts.Build(%s)", m.name)
+	return "artifacts.Build"
 }
 
 func (m *build) Apply(ctx context.Context, b *bundle.Bundle) diag.Diagnostics {
-	artifact, ok := b.Config.Artifacts[m.name]
-	if !ok {
-		return diag.Errorf("artifact doesn't exist: %s", m.name)
-	}
+	var diags diag.Diagnostics
 
-	var mutators []bundle.Mutator
+	for _, artifactName := range sortedKeys(b.Config.Artifacts) {
+		a := b.Config.Artifacts[artifactName]
 
-	// Skip building if build command is not specified or infered
-	if artifact.BuildCommand == "" {
-		// If no build command was specified or infered and there is no
-		// artifact output files specified, artifact is misconfigured
-		if len(artifact.Files) == 0 {
-			return diag.Errorf("misconfigured artifact: please specify 'build' or 'files' property")
+		if a.BuildCommand == "" {
+			continue
 		}
 
-		// We can skip calling build mutator if there is no build command
-		// But we still need to expand glob references in files source path.
-	} else {
-		mutators = append(mutators, getBuildMutator(artifact.Type, m.name))
+		cmdio.LogString(ctx, fmt.Sprintf("Building %s...", artifactName))
+
+		var executor *exec.Executor
+		var err error
+		if a.Executable != "" {
+			executor, err = exec.NewCommandExecutorWithExecutable(a.Path, a.Executable)
+		} else {
+			executor, err = exec.NewCommandExecutor(a.Path)
+			a.Executable = executor.ShellType()
+		}
+
+		if err != nil {
+			diags = diags.Extend(diag.FromErr(err))
+			if diags.HasError() {
+				break
+			}
+		}
+
+		out, err := executor.Exec(ctx, a.BuildCommand)
+		if err != nil {
+			return diag.Errorf("build failed %s, error: %v, output: %s", artifactName, err, out)
+		}
+
+		if a.Type == "whl" {
+			dir := a.Path
+			distPath := filepath.Join(a.Path, "dist")
+			wheels := python.FindFilesWithSuffixInPath(distPath, ".whl")
+			if len(wheels) == 0 {
+				return diag.Errorf("cannot find built wheel in %s for package %s", dir, artifactName)
+			}
+			for _, wheel := range wheels {
+				a.Files = append(a.Files, config.ArtifactFile{
+					Source: wheel,
+				})
+			}
+			log.Infof(ctx, "%s: Build succeeded, found %s", artifactName, wheels)
+		} else {
+			log.Infof(ctx, "%s: Build succeeded", artifactName)
+		}
+
+		// We need to expand glob reference after build mutator is applied because
+		// if we do it before, any files that are generated by build command will
+		// not be included into artifact.Files and thus will not be uploaded.
+		diags = diags.Extend(expandGlobs(ctx, b, artifactName))
+		if diags.HasError() {
+			break
+		}
 	}
 
-	// We need to expand glob reference after build mutator is applied because
-	// if we do it before, any files that are generated by build command will
-	// not be included into artifact.Files and thus will not be uploaded.
-	mutators = append(mutators, &expandGlobs{name: m.name})
-	return bundle.ApplySeq(ctx, b, mutators...)
+	return diags
 }

bundle/artifacts/expand_globs.go
@@ -10,14 +10,6 @@ import (
 	"github.com/databricks/cli/libs/dyn"
 )
 
-type expandGlobs struct {
-	name string
-}
-
-func (m *expandGlobs) Name() string {
-	return fmt.Sprintf("artifacts.ExpandGlobs(%s)", m.name)
-}
-
 func createGlobError(v dyn.Value, p dyn.Path, message string) diag.Diagnostic {
 	// The pattern contained in v is an absolute path.
 	// Make it relative to the value's location to make it more readable.
@@ -37,12 +29,12 @@ func createGlobError(v dyn.Value, p dyn.Path, message string) diag.Diagnostic {
 	}
 }
 
-func (m *expandGlobs) Apply(ctx context.Context, b *bundle.Bundle) diag.Diagnostics {
+func expandGlobs(ctx context.Context, b *bundle.Bundle, name string) diag.Diagnostics {
 	// Base path for this mutator.
 	// This path is set with the list of expanded globs when done.
 	base := dyn.NewPath(
 		dyn.Key("artifacts"),
-		dyn.Key(m.name),
+		dyn.Key(name),
 		dyn.Key("files"),
 	)
 

bundle/artifacts/prepare.go
@@ -2,57 +2,147 @@ package artifacts
 
 import (
 	"context"
-	"fmt"
+	"os"
 	"path/filepath"
+	"sort"
 
 	"github.com/databricks/cli/bundle"
+	"github.com/databricks/cli/bundle/config"
+	"github.com/databricks/cli/bundle/libraries"
 	"github.com/databricks/cli/libs/diag"
+	"github.com/databricks/cli/libs/log"
+	"github.com/databricks/cli/libs/python"
 )
 
-func PrepareAll() bundle.Mutator {
-	return &all{
-		name: "Prepare",
-		fn:   prepareArtifactByName,
-	}
+func Prepare() bundle.Mutator {
+	return &prepare{}
 }
 
-type prepare struct {
-	name string
-}
-
-func prepareArtifactByName(name string) (bundle.Mutator, error) {
-	return &prepare{name}, nil
-}
+type prepare struct{}
 
 func (m *prepare) Name() string {
-	return fmt.Sprintf("artifacts.Prepare(%s)", m.name)
+	return "artifacts.Prepare"
 }
 
 func (m *prepare) Apply(ctx context.Context, b *bundle.Bundle) diag.Diagnostics {
-	artifact, ok := b.Config.Artifacts[m.name]
-	if !ok {
-		return diag.Errorf("artifact doesn't exist: %s", m.name)
+	var diags diag.Diagnostics
+
+	err := InsertPythonArtifact(ctx, b)
+	if err != nil {
+		return diag.FromErr(err)
 	}
 
-	l := b.Config.GetLocation("artifacts." + m.name)
-	dirPath := filepath.Dir(l.File)
+	removeFolders := make(map[string]bool, len(b.Config.Artifacts))
+	cleanupWheelFolders := make(map[string]bool, len(b.Config.Artifacts))
 
-	// Check if source paths are absolute, if not, make them absolute
-	for k := range artifact.Files {
-		f := &artifact.Files[k]
-		if !filepath.IsAbs(f.Source) {
-			f.Source = filepath.Join(dirPath, f.Source)
+	for _, artifactName := range sortedKeys(b.Config.Artifacts) {
+		artifact := b.Config.Artifacts[artifactName]
+
+		if artifact.Type == "whl" {
+			if artifact.BuildCommand == "" && len(artifact.Files) == 0 {
+				artifact.BuildCommand = python.GetExecutable() + " setup.py bdist_wheel"
+			}
+		}
+
+		l := b.Config.GetLocation("artifacts." + artifactName)
+		dirPath := filepath.Dir(l.File)
+
+		// Check if source paths are absolute, if not, make them absolute
+		for k := range artifact.Files {
+			f := &artifact.Files[k]
+			if !filepath.IsAbs(f.Source) {
+				f.Source = filepath.Join(dirPath, f.Source)
+			}
+		}
+
+		if artifact.Path == "" {
+			artifact.Path = b.BundleRootPath
+		}
+
+		if !filepath.IsAbs(artifact.Path) {
+			artifact.Path = filepath.Join(dirPath, artifact.Path)
+		}
+
+		if artifact.Type == "whl" && artifact.BuildCommand != "" {
+			dir := artifact.Path
+			removeFolders[filepath.Join(dir, "dist")] = true
+			cleanupWheelFolders[dir] = true
+		}
+
+		if artifact.BuildCommand == "" && len(artifact.Files) == 0 {
+			diags = diags.Extend(diag.Errorf("misconfigured artifact: please specify 'build' or 'files' property"))
+		}
+
+		if len(artifact.Files) > 0 && artifact.BuildCommand == "" {
+			diags = diags.Extend(expandGlobs(ctx, b, artifactName))
+		}
+
+		if diags.HasError() {
+			break
 		}
 	}
 
-	// If artifact path is not provided, use bundle root dir
-	if artifact.Path == "" {
-		artifact.Path = b.BundleRootPath
+	if diags.HasError() {
+		return diags
 	}
 
-	if !filepath.IsAbs(artifact.Path) {
-		artifact.Path = filepath.Join(dirPath, artifact.Path)
+	for _, dir := range sortedKeys(removeFolders) {
+		err := os.RemoveAll(dir)
+		if err != nil {
+			log.Infof(ctx, "Failed to remove %s: %s", dir, err)
+		}
 	}
 
-	return bundle.Apply(ctx, b, getPrepareMutator(artifact.Type, m.name))
+	for _, dir := range sortedKeys(cleanupWheelFolders) {
+		log.Infof(ctx, "Cleaning up Python build artifacts in %s", dir)
+		python.CleanupWheelFolder(dir)
+	}
+
+	return diags
+}
+
+func InsertPythonArtifact(ctx context.Context, b *bundle.Bundle) error {
+	if b.Config.Artifacts != nil {
+		log.Debugf(ctx, "artifacts block is defined, skipping auto-detecting")
+		return nil
+	}
+
+	tasks := libraries.FindTasksWithLocalLibraries(b)
+	if len(tasks) == 0 {
+		log.Infof(ctx, "No local tasks in databricks.yml config, skipping auto detect")
+		return nil
+	}
+
+	// checking if there is setup.py in the bundle root
+	setupPy := filepath.Join(b.BundleRootPath, "setup.py")
+	_, err := os.Stat(setupPy)
+	if err != nil {
+		log.Infof(ctx, "No Python wheel project found at bundle root folder")
+		return nil
+	}
+
+	log.Infof(ctx, "Found Python wheel project at %s", b.BundleRootPath)
+
+	pkgPath, err := filepath.Abs(b.BundleRootPath)
+	if err != nil {
+		return err
+	}
+
+	b.Config.Artifacts = make(map[string]*config.Artifact)
+	b.Config.Artifacts["python_artifact"] = &config.Artifact{
+		Path: pkgPath,
+		Type: config.ArtifactPythonWheel,
+		// BuildCommand will be auto set by later code
+	}
+
+	return nil
+}
+
+func sortedKeys[T any](m map[string]T) []string {
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return keys
 }

acceptance/bundle/artifacts/build_and_files/databricks.yml
@@ -1,0 +1,9 @@
+bundle:
+  name: build_and_files
+
+artifacts:
+  custom:
+    build: touch built.txt
+    files:
+      # this file does not exist yet, will be generated by build command
+      - source: built.txt

acceptance/bundle/artifacts/build_and_files/script
@@ -1,0 +1,1 @@
+$CLI bundle validate -o json | jq .artifacts

acceptance/bundle/artifacts/build_and_files/test.toml
@@ -1,0 +1,5 @@
+RecordRequests = false
+
+[[Repls]]
+Old = '\\\\'
+New = '/'

acceptance/bundle/artifacts/globs_in_files/databricks.yml
@@ -1,0 +1,8 @@
+bundle:
+  name: expand_globs
+
+artifacts:
+  test:
+    files:
+      - source: a*.txt
+      - source: subdir/*.txt

commit 4b685be272b08f1b3b596ae65db0efc95767b631

Fix a bug in artifacts where glob was applied on non-abs path (#2540)
## Changes
Bug introduced in #2526.

When calling expandGlobs as a function without mutator entry/exit copy,
we did not copy typed structure into dynamic structure. Thus expandGlobs
saw an old version of config, without absolute paths introduced by code
before it. Since it calls filepath.Glob which depends on CWD, it stopped
doing the matching.

## Tests
New regression test to catch this issues.

acceptance/bundle/artifacts/glob_exact_whl/databricks.yml
@@ -1,0 +1,21 @@
+bundle:
+  name: python-wheel
+
+artifacts:
+    my_prebuilt_whl:
+      type: whl
+      files:
+        - source: prebuilt/other_test_code-0.0.1-py3-none-any.whl
+
+resources:
+  jobs:
+    test_job:
+      name: "[${bundle.target}] My Wheel Job"
+      tasks:
+        - task_key: TestTask
+          existing_cluster_id: "0717-132531-5opeqon1"
+          python_wheel_task:
+            package_name: "my_test_code"
+            entry_point: "run"
+          libraries:
+          - whl: prebuilt/other_test_code-0.0.1-py3-none-any.whl

acceptance/bundle/artifacts/glob_exact_whl/script
@@ -1,0 +1,5 @@
+trap "rm -r prebuilt" EXIT
+mkdir prebuilt
+cp -r $TESTDIR/../whl_prebuilt_multiple/dist/lib/other_test_code-0.0.1-py3-none-any.whl prebuilt
+trace $CLI bundle validate -o json | jq .artifacts
+trace $CLI bundle deploy

acceptance/bundle/artifacts/glob_exact_whl/test.toml
@@ -1,9 +1,6 @@
+Badness = 'Uploading the same wheel twice'
 RecordRequests = false
 
 [[Repls]]
 Old = '\\\\'
-New = '/'
-
-[[Repls]]
-Old = '\\'
 New = '/'

acceptance/bundle/artifacts/globs_in_files_in_include/databricks.yml
@@ -1,0 +1,5 @@
+bundle:
+  name: expand_globs
+
+include:
+  - resources/artifacts.yml

acceptance/bundle/artifacts/globs_in_files_in_include/resources/artifacts.yml
@@ -1,0 +1,5 @@
+artifacts:
+  test:
+    files:
+      - source: a*.txt
+      - source: subdir/*.txt

acceptance/bundle/artifacts/globs_in_files_in_include/script
@@ -1,0 +1,12 @@
+touch resources/a1.txt
+touch a2.txt
+mkdir -p resources/subdir
+touch resources/subdir/hello.txt
+
+errcode trace $CLI bundle validate -o json | jq .artifacts
+errcode trace withdir resources $CLI bundle validate -o json | jq .artifacts
+
+mkdir other_dir
+errcode trace withdir other_dir $CLI bundle validate -o json | jq .artifacts
+
+rm -fr resources/a1.txt a2.txt resources/subdir

acceptance/bundle/artifacts/globs_in_files_in_include/test.toml
@@ -1,0 +1,6 @@
+Badness = "Glob expansion is performed within cwd, not relative to where path is defined"
+RecordRequests = false
+
+[[Repls]]
+Old = '\\\\'
+New = '/'

acceptance/bundle/artifacts/globs_invalid/test.toml
@@ -1,1 +1,9 @@
 RecordRequests = false
+
+[[Repls]]
+Old = '\\\\'
+New = '/'
+
+[[Repls]]
+Old = '\\'
+New = '/'

bundle/artifacts/build.go
@@ -77,7 +77,7 @@ func (m *build) Apply(ctx context.Context, b *bundle.Bundle) diag.Diagnostics {
 		// We need to expand glob reference after build mutator is applied because
 		// if we do it before, any files that are generated by build command will
 		// not be included into artifact.Files and thus will not be uploaded.
-		diags = diags.Extend(expandGlobs(ctx, b, artifactName))
+		diags = diags.Extend(bundle.Apply(ctx, b, expandGlobs{name: artifactName}))
 		if diags.HasError() {
 			break
 		}

bundle/artifacts/expand_globs.go
@@ -29,12 +29,20 @@ func createGlobError(v dyn.Value, p dyn.Path, message string) diag.Diagnostic {
 	}
 }
 
-func expandGlobs(ctx context.Context, b *bundle.Bundle, name string) diag.Diagnostics {
+type expandGlobs struct {
+	name string
+}
+
+func (e expandGlobs) Name() string {
+	return "expandGlobs"
+}
+
+func (e expandGlobs) Apply(ctx context.Context, b *bundle.Bundle) diag.Diagnostics {
 	// Base path for this mutator.
 	// This path is set with the list of expanded globs when done.
 	base := dyn.NewPath(
 		dyn.Key("artifacts"),
-		dyn.Key(name),
+		dyn.Key(e.name),
 		dyn.Key("files"),
 	)
 

bundle/artifacts/prepare.go
@@ -74,7 +74,7 @@ func (m *prepare) Apply(ctx context.Context, b *bundle.Bundle) diag.Diagnostics 
 		}
 
 		if len(artifact.Files) > 0 && artifact.BuildCommand == "" {
-			diags = diags.Extend(expandGlobs(ctx, b, artifactName))
+			diags = diags.Extend(bundle.Apply(ctx, b, expandGlobs{name: artifactName}))
 		}
 
 		if diags.HasError() {