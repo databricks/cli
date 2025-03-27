e046b3ce012b66d0783cccc6d8cb2ae396932f27
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2025-03-27 11:52:56 +0100

    fix yaml indentation, add a link to go wiki

--- libs/gorules/rule_empty_slices.go
+++ libs/gorules/rule_empty_slices.go
@@ -3,6 +3,7 @@ package gorules
 import "github.com/quasilyte/go-ruleguard/dsl"
 
 // DeclaringEmptySlices detects empty slice declarations and suggests using nil slices.
+// See: https://go.dev/wiki/CodeReviewComments#declaring-empty-slices
 func DeclaringEmptySlices(m dsl.Matcher) {
 	m.Match(`$x := []$t{}`).
 		Report(`Consider using 'var $x []$t' to declare a nil slice instead of an empty slice literal`).
