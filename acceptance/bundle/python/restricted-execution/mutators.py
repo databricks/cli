26e0f40b6861e1ec205c8fbefe8b799c0627b850
Ilya Kuznetsov <ilya.kuznetsov@databricks.com>
2025-04-01 16:16:17 +0200

    Tests simplification

--- acceptance/bundle/python/restricted-execution/mutators.py
+++ acceptance/bundle/python/restricted-execution/mutators.py
@@ -1,16 +1,14 @@
 from dataclasses import replace
-from databricks.bundles.jobs import Job, Task
+from databricks.bundles.jobs import Job
 from databricks.bundles.core import job_mutator, Bundle
 import os
 
 
 @job_mutator
 def read_envs(bundle: Bundle, job: Job) -> Job:
+    # Reading from envs to ensure that the envs are accessible for this mutator
+    # (which this test case is verifying that they are not)
     value = os.getenv("SOME_ENV_VAR", "default")
     with open("envs.txt", "w") as file:
         file.write(value)
-
-    tasks = bundle.resolve_variable(job.tasks)
-    task_2 = Task(task_key="task_2")
-
-    return replace(job, tasks=[*tasks, task_2])
+    return job
