f183b9ba5cbdb5b071cb17257320153733a908b0
Ilya Kuznetsov <ilya.kuznetsov@databricks.com>
2025-04-02 11:49:19 +0200

    Fixes incorrectly removed script file

--- acceptance/bundle/scripts/restricted-execution/myscript.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-info = " ".join(sys.argv[1:])
-env = os.getenv("SOME_ENV_VAR", "default")
-sys.stdout.write(f"from myscript.py {info}: env: {env}\n")
-
-exitcode = int(sys.argv[1])
-sys.exit(exitcode)
