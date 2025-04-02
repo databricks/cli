6fe8acb4da738241439b8ee1c68796849eb16914
shreyas-goenka <88374338+shreyas-goenka@users.noreply.github.com>
2025-04-02 11:23:01 +0200

    Track number of targets defined in deploy telemetry (#2595)
    ## Why
    Tracking this gives us insight into how many copies of their bundles to
    customers deploy.
    
    ## Tests
    Acceptance tests.

--- bundle/phases/deploy.go
+++ bundle/phases/deploy.go
@@ -352,6 +352,7 @@ func logTelemetry(ctx context.Context, b *bundle.Bundle) {
 			Experimental: &protos.BundleDeployExperimental{
 				BundleMode:             mode,
 				ConfigurationFileCount: b.Metrics.ConfigurationFileCount,
+				TargetCount:            b.Metrics.TargetCount,
 			},
 		},
 	})
