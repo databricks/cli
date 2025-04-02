13000739c75a33811dc70ddedb5de03aab558e61
Gleb Kanterov <kanterov@users.noreply.github.com>
2025-04-02 09:39:15 +0200

    Simplify tests for path mutators (#2610)
    ## Changes
    Simplify tests for path mutators

--- bundle/config/mutator/paths/app_paths_visitor_test.go
+++ bundle/config/mutator/paths/app_paths_visitor_test.go
@@ -7,7 +7,6 @@ import (
 	"github.com/databricks/cli/bundle/config/resources"
 	"github.com/databricks/cli/libs/dyn"
 	assert "github.com/databricks/cli/libs/dyn/dynassert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestAppPathsVisitor(t *testing.T) {
@@ -21,22 +20,10 @@ func TestAppPathsVisitor(t *testing.T) {
 		},
 	}
 
-	actual := visitAppPaths(t, root)
+	actual := collectVisitedPaths(t, root, VisitAppPaths)
 	expected := []dyn.Path{
 		dyn.MustPathFromString("resources.apps.app0.source_code_path"),
 	}
 
 	assert.ElementsMatch(t, expected, actual)
-}
-
-func visitAppPaths(t *testing.T, root config.Root) []dyn.Path {
-	var actual []dyn.Path
-	err := root.Mutate(func(value dyn.Value) (dyn.Value, error) {
-		return VisitAppPaths(value, func(p dyn.Path, mode TranslateMode, v dyn.Value) (dyn.Value, error) {
-			actual = append(actual, p)
-			return v, nil
-		})
-	})
-	require.NoError(t, err)
-	return actual
 }
