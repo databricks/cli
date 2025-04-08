dfc8de5f0e8bbc289f19707580479d9f516fe23c
Gleb Kanterov <gleb.kanterov@databricks.com>
2025-04-08 09:43:00 +0200

    Address CR comments

--- acceptance/bundle/python/pipelines-support/resources.py
+++ acceptance/bundle/python/pipelines-support/resources.py
@@ -4,6 +4,6 @@ from databricks.bundles.core import Resources
 def load_resources() -> Resources:
     resources = Resources()
 
-    resources.add_pipeline("my_pipeline_1", {"name": "My Pipeline 1"})
+    resources.add_pipeline("my_pipeline_2", {"name": "My Pipeline 2"})
 
     return resources
--- experimental/python/databricks/bundles/pipelines/_models/pipeline.py
+++ experimental/python/databricks/bundles/pipelines/_models/pipeline.py
@@ -1,14 +1,14 @@
-from dataclasses import dataclass
-from typing import TYPE_CHECKING, TypedDict
+from dataclasses import dataclass, field
+from typing import TYPE_CHECKING, Any, TypedDict
 
-from databricks.bundles.core import Resource, VariableOrOptional
+from databricks.bundles.core import Resource, VariableOrList, VariableOrOptional
 from databricks.bundles.core._transform import _transform
 from databricks.bundles.core._transform_to_json import _transform_to_json_value
 
 if TYPE_CHECKING:
     from typing_extensions import Self
 
-# TODO generate this class and its dependencies
+# TODO generate Pipeline class from jsonschema
 
 
 @dataclass(kw_only=True)
@@ -16,9 +16,9 @@ class Pipeline(Resource):
     """"""
 
     name: VariableOrOptional[str]
-    """
-    TODO
-    """
+
+    # permission field is always present after normalization, add stub not to error on unknown property
+    permissions: VariableOrList[Any] = field(default_factory=list)
 
     @classmethod
     def from_dict(cls, value: "PipelineDict") -> "Self":
