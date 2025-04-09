eaf2e7bc72cd6f67df8f33a2f98633b7d85b7e61
Denis Bilenko <denis.bilenko@databricks.com>
2025-04-09 15:52:29 +0200

    Add utils module with SortedKeys helper (#2689)
    ## Changes
    - Move helper sortedKeys from artifacts package to libs/utils
    - Use it where it's useful.
    
    ## Tests
    Existing tests.

--- bundle/artifacts/build.go
+++ bundle/artifacts/build.go
@@ -15,6 +15,7 @@ import (
 	"github.com/databricks/cli/libs/log"
 	"github.com/databricks/cli/libs/patchwheel"
 	"github.com/databricks/cli/libs/python"
+	"github.com/databricks/cli/libs/utils"
 )
 
 func Build() bundle.Mutator {
@@ -38,7 +39,7 @@ func (m *build) Apply(ctx context.Context, b *bundle.Bundle) diag.Diagnostics {
 		})
 	}
 
-	for _, artifactName := range sortedKeys(b.Config.Artifacts) {
+	for _, artifactName := range utils.SortedKeys(b.Config.Artifacts) {
 		a := b.Config.Artifacts[artifactName]
 
 		if a.BuildCommand != "" {
