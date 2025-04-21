37c5fd4faa6b3c71e4886e565162369f164da7c3
Shreyas Goenka <shreyas.goenka@databricks.com>
2025-04-21 19:36:07 +0200

    comments

--- libs/exec/execv.go
+++ libs/exec/execv.go
@@ -6,16 +6,22 @@ import "io"
 // that users can read to figure out the original CWD. I'll do that when
 // adding support for the scripts section.
 type ExecvOptions struct {
+	// Args is the name of the command to run and its arguments.
+	// Eg: ["echo", "hello"] for "echo hello"
 	Args []string
-	Env  []string
-	Dir  string
+
+	// Env is set the environment variables to set in the child process.
+	Env []string
+
+	// Dir is the working directory of the child process.
+	Dir string
 
 	// Stdin, Stderr and Stdout are only used for Windows where execv is not supported.
 	// For Unix, these are automatically inherited during the exec
 	// system call.
 	Stdin  io.Reader
-	Stderr io.Writer
 	Stdout io.Writer
+	Stderr io.Writer
 }
 
 func Execv(opts ExecvOptions) error {
