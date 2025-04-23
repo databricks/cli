d4a5fc39e87f7ec19eacdf037e94d93fbc49b9b5
Denis Bilenko <denis.bilenko@databricks.com>
2025-04-23 11:19:50 +0200

    feat: add Move struct and method to processor for field relocation

--- bundle/terranova/processor.go
+++ bundle/terranova/processor.go
@@ -5,18 +5,46 @@ import (
 	"github.com/databricks/cli/libs/dyn/merge"
 )
 
+type Move struct {
+	Fields []string
+	Target string
+}
+
+// Example:
+// Input: {"job_id": 123, "field1": "hello", "field2": "world"}, Fields: ["field1", "field2"], Target: "data", Result: {"job_id": 123, "data": {"field1": "hello", "field2": "world"}}
+func (p *Move) ApplyMove(v dyn.Value) (dyn.Value, error) {
+	// AI TODO: implement this function; add test cases in processor_test.go
+
+}
+
 type Processor struct {
 	Select []string
 	Drop   []string
+	Moves  []Move
 	// TODO: custom func (Value) Value for complex cases
 }
 
 func (p *Processor) ApplyProcessor(v dyn.Value) (dyn.Value, error) {
 	if p.Select != nil {
-		return merge.Select(v, p.Select)
+		v, err := merge.Select(v, p.Select)
+		if err != nil {
+			return v, err
+		}
 	}
+
 	if p.Drop != nil {
-		return merge.AntiSelect(v, p.Drop)
+		v, err := merge.AntiSelect(v, p.Drop)
+		if err != nil {
+			return v, err
+		}
 	}
+
+	for _, move := range p.Moves {
+		v, err := move.ApplyMove(v)
+		if err != nil {
+			return v, err
+		}
+	}
+
 	return v, nil
 }
