f974136e39661c18273f1b252399d596dc850339
Gleb Kanterov <gleb.kanterov@databricks.com>
2025-04-24 14:59:18 +0200

    Add comment

--- experimental/python/databricks/bundles/core/_load.py
+++ experimental/python/databricks/bundles/core/_load.py
@@ -161,6 +161,7 @@ def _parse_locations(module: ModuleType) -> dict[str, Location]:
 
             locations[var_name] = Location(
                 line=stmt.lineno,
+                # do conversion: col_offset is 0-based, and column is 1-based
                 column=stmt.col_offset + 1,
                 file=file,
             )
