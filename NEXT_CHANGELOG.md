47948b76f81e1bec44b825db26da0d90f8dd1651
shreyas-goenka <88374338+shreyas-goenka@users.noreply.github.com>
2025-04-24 23:22:45 +0200

    Add inline script execution support to `bundle run` (#2413)
    ## Changes
    This PR adds the ability to execute scripts inline using the `databricks
    bundle run` command. This allows users to execute commands with the same
    authentication credentials as the bundle is configured to use..
    
    Note: The auth credentials are passed to child processes via environment
    variables.
    
    ## Why
    Configuring downstream tools like DBConnect to use the same
    authentication credentials as your bundle can be challenging. Having
    this command allows users to directly run `databricks bundle run -t foo
    -- uv run pytest` (or equivalent) to directly run integration tests
    against the configured target without having to worry about how to pass
    authentication credentials to DBConnect.
    
    ## Tests
    Acceptance tests.

--- NEXT_CHANGELOG.md
+++ NEXT_CHANGELOG.md
@@ -3,6 +3,7 @@
 ## Release v0.250.0
 
 ### Notable Changes
+* Added inline script execution support to bundle run. You can now run scripts in the same authentication context as a DAB using the databricks bundle run command. ([#2413](https://github.com/databricks/cli/pull/2413))
 
 ### Dependency updates
 
---
+++ libs/exec/execv_unix.go
@@ -0,0 +1,25 @@
+//go:build linux || darwin
+
+package exec
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func execv(opts ExecvOptions) error {
+	err := os.Chdir(opts.Dir)
+	if err != nil {
+		return fmt.Errorf("changing directory to %s failed: %w", opts.Dir, err)
+	}
+
+	// execve syscall does not perform PATH lookup. Thus we need to query path
+	// before making the exec syscall.
+	path, err := exec.LookPath(opts.Args[0])
+	if err != nil {
+		return fmt.Errorf("looking up %q failed: %w", opts.Args[0], err)
+	}
+	return syscall.Exec(path, opts.Args, opts.Env)
+}
