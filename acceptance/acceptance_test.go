12ed68365b303b21f404fa323eeb48269f397924
Shreyas Goenka <shreyas.goenka@databricks.com>
2025-04-24 14:32:17 +0200

    -

--- acceptance/acceptance_test.go
+++ acceptance/acceptance_test.go
@@ -11,7 +11,6 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
-	"path"
 	"path/filepath"
 	"regexp"
 	"runtime"
@@ -78,50 +77,49 @@ var Ignored = map[string]bool{
 	ReplsFile: true,
 }
 
-// Detects if test is run from "debug test" feature in VS Code.
-func IsInDebug() bool {
-	ex, _ := os.Executable()
-	return strings.HasPrefix(path.Base(ex), "__debug_bin")
-}
+// // Detects if test is run from "debug test" feature in VS Code.
+// func IsInDebug() bool {
+// 	ex, _ := os.Executable()
+// 	return strings.HasPrefix(path.Base(ex), "__debug_bin")
+// }
 
-// Loads debug environment from ~/.databricks/debug-env.json.
-func loadDebugEnvIfRunFromIDE(t testutil.TestingT, key string) {
-	if !IsInDebug() {
-		return
-	}
-	home, err := os.UserHomeDir()
-	if err != nil {
-		t.Fatalf("cannot find user home: %s", err)
-	}
-	raw, err := os.ReadFile(filepath.Join(home, ".databricks/debug-env.json"))
-	if err != nil {
-		t.Fatalf("cannot load ~/.databricks/debug-env.json: %s", err)
-	}
-	var conf map[string]map[string]string
-	err = json.Unmarshal(raw, &conf)
-	if err != nil {
-		t.Fatalf("cannot parse ~/.databricks/debug-env.json: %s", err)
-	}
-	vars, ok := conf[key]
-	if !ok {
-		t.Fatalf("~/.databricks/debug-env.json#%s not configured", key)
-	}
-	for k, v := range vars {
-		os.Setenv(k, v)
-	}
-}
+// // Loads debug environment from ~/.databricks/debug-env.json.
+// func loadDebugEnvIfRunFromIDE(t testutil.TestingT, key string) {
+// 	if !IsInDebug() {
+// 		return
+// 	}
+// 	home, err := os.UserHomeDir()
+// 	if err != nil {
+// 		t.Fatalf("cannot find user home: %s", err)
+// 	}
+// 	raw, err := os.ReadFile(filepath.Join(home, ".databricks/debug-env.json"))
+// 	if err != nil {
+// 		t.Fatalf("cannot load ~/.databricks/debug-env.json: %s", err)
+// 	}
+// 	var conf map[string]map[string]string
+// 	err = json.Unmarshal(raw, &conf)
+// 	if err != nil {
+// 		t.Fatalf("cannot parse ~/.databricks/debug-env.json: %s", err)
+// 	}
+// 	vars, ok := conf[key]
+// 	if !ok {
+// 		t.Fatalf("~/.databricks/debug-env.json#%s not configured", key)
+// 	}
+// 	for k, v := range vars {
+// 		os.Setenv(k, v)
+// 	}
+// }
 
 func TestAccept(t *testing.T) {
 	// TODO: Clean this up.
 	// loadDebugEnvIfRunFromIDE(t, "workspace")
-	testAccept(t, InprocessMode, "")
+	testAccept(t, InprocessMode, "selftest/record_cloud")
 }
 
 func TestInprocessMode(t *testing.T) {
 	if InprocessMode && !Forcerun {
 		t.Skip("Already tested by TestAccept")
 	}
-	loadDebugEnvIfRunFromIDE(t, "CLOUD_ENV")
 	require.Equal(t, 1, testAccept(t, true, "selftest/record_cloud"))
 	require.Equal(t, 1, testAccept(t, true, "selftest/server"))
 }
