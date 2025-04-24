c6da8f0b002ac6346c10fe7e64252d3210292291
Andrew Nester <andrew.nester@databricks.com>
2025-04-24 12:54:05 +0200

    fix

--- bundle/config/validate/validate_volume_path.go
+++ bundle/config/validate/validate_volume_path.go
@@ -20,48 +20,33 @@ func (m *validateVolumePath) Name() string {
 
 func (m *validateVolumePath) Apply(ctx context.Context, b *bundle.Bundle) diag.Diagnostics {
 	var diags diag.Diagnostics
-
-	if b.Config.Workspace.RootPath != "" && strings.HasPrefix(b.Config.Workspace.RootPath, "/Volumes/") {
-		diags = diags.Append(diag.Diagnostic{
-			Severity:  diag.Error,
-			Summary:   fmt.Sprintf("workspace.root_path %s starts with /Volumes. /Volumes can only be used with workspace.artifact_path.", b.Config.Workspace.RootPath),
-			Detail:    "For more information, see https://docs.databricks.com/aws/en/dev-tools/bundles/settings#workspace",
-			Locations: b.Config.GetLocations("workspace.root_path"),
-			Paths:     []dyn.Path{dyn.MustPathFromString("workspace.root_path")},
-		})
-
-		// We return early here because we don't want to check the other paths if the root path is invalid
-		return diags
+	// Define paths to check and their corresponding config field names
+	pathChecks := []struct {
+		path       string
+		configName string
+	}{
+		{b.Config.Workspace.RootPath, "workspace.root_path"},
+		{b.Config.Workspace.FilePath, "workspace.file_path"},
+		{b.Config.Workspace.StatePath, "workspace.state_path"},
+		{b.Config.Workspace.ResourcePath, "workspace.resource_path"},
 	}
 
-	if b.Config.Workspace.FilePath != "" && strings.HasPrefix(b.Config.Workspace.FilePath, "/Volumes/") {
-		diags = diags.Append(diag.Diagnostic{
-			Severity:  diag.Error,
-			Summary:   fmt.Sprintf("workspace.file_path %s starts with /Volumes. /Volumes can only be used with workspace.artifact_path.", b.Config.Workspace.FilePath),
-			Detail:    "For more information, see https://docs.databricks.com/aws/en/dev-tools/bundles/settings#workspace",
-			Locations: b.Config.GetLocations("workspace.file_path"),
-			Paths:     []dyn.Path{dyn.MustPathFromString("workspace.file_path")},
-		})
-	}
+	// Check each path
+	for _, check := range pathChecks {
+		if check.path != "" && strings.HasPrefix(check.path, "/Volumes/") {
+			diags = diags.Append(diag.Diagnostic{
+				Severity:  diag.Error,
+				Summary:   fmt.Sprintf("%s %s starts with /Volumes. /Volumes can only be used with workspace.artifact_path.", check.configName, check.path),
+				Detail:    "For more information, see https://docs.databricks.com/aws/en/dev-tools/bundles/settings#workspace",
+				Locations: b.Config.GetLocations(check.configName),
+				Paths:     []dyn.Path{dyn.MustPathFromString(check.configName)},
+			})
 
-	if b.Config.Workspace.StatePath != "" && strings.HasPrefix(b.Config.Workspace.StatePath, "/Volumes/") {
-		diags = diags.Append(diag.Diagnostic{
-			Severity:  diag.Error,
-			Summary:   fmt.Sprintf("workspace.state_path %s starts with /Volumes. /Volumes can only be used with workspace.artifact_path.", b.Config.Workspace.StatePath),
-			Detail:    "For more information, see https://docs.databricks.com/aws/en/dev-tools/bundles/settings#workspace",
-			Locations: b.Config.GetLocations("workspace.state_path"),
-			Paths:     []dyn.Path{dyn.MustPathFromString("workspace.state_path")},
-		})
-	}
-
-	if b.Config.Workspace.ResourcePath != "" && strings.HasPrefix(b.Config.Workspace.ResourcePath, "/Volumes/") {
-		diags = diags.Append(diag.Diagnostic{
-			Severity:  diag.Error,
-			Summary:   fmt.Sprintf("workspace.resource_path %s starts with /Volumes. /Volumes can only be used with workspace.artifact_path.", b.Config.Workspace.ResourcePath),
-			Detail:    "For more information, see https://docs.databricks.com/aws/en/dev-tools/bundles/settings#workspace",
-			Locations: b.Config.GetLocations("workspace.resource_path"),
-			Paths:     []dyn.Path{dyn.MustPathFromString("workspace.resource_path")},
-		})
+			// Return early for root path validation
+			if check.configName == "workspace.root_path" {
+				return diags
+			}
+		}
 	}
 
 	return diags
