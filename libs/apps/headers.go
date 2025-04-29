63af0bd2db28129b565cd35073fb88ddfae845d0
Andrew Nester <andrew.nester@databricks.com>
2025-04-29 13:51:17 +0200

    fix

--- libs/apps/headers.go
+++ libs/apps/headers.go
@@ -10,8 +10,15 @@ func GetXHeaders(user *iam.User) map[string]string {
 		"X-Forwarded-Host":               "localhost",
 		"X-Forwarded-Preferred-Username": user.DisplayName,
 		"X-Forwarded-User":               user.UserName,
-		"X-Forwarded-Email":              user.UserName, // UserName is the email
+		"X-Forwarded-Email":              getEmail(user),
 		"X-Real-Ip":                      "127.0.0.1",
 		"X-Request-Id":                   uuid.New().String(),
 	}
+}
+
+func getEmail(user *iam.User) string {
+	if len(user.Emails) > 0 {
+		return user.Emails[0].Value
+	}
+	return user.UserName
 }
