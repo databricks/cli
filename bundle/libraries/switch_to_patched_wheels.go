6fb85e74aca4e78be2aa980f863bec66a1cbe1c5
Denis Bilenko <denis.bilenko@databricks.com>
2025-05-01 16:19:35 +0200

    Fix dynamic_version when sync root != bundle root

--- bundle/libraries/switch_to_patched_wheels.go
+++ bundle/libraries/switch_to_patched_wheels.go
@@ -8,12 +8,17 @@ import (
 	"github.com/databricks/cli/bundle/config"
 	"github.com/databricks/cli/libs/diag"
 	"github.com/databricks/cli/libs/log"
+	"github.com/databricks/cli/libs/utils"
 )
 
 type switchToPatchedWheels struct{}
 
 func (c switchToPatchedWheels) Apply(ctx context.Context, b *bundle.Bundle) diag.Diagnostics {
-	replacements := getReplacements(ctx, b.Config.Artifacts, b.BundleRoot.Native())
+	replacements := getReplacements(ctx, b.Config.Artifacts, b.SyncRoot.Native())
+
+	if len(replacements) == 0 {
+		return nil
+	}
 
 	for jobName, jobRef := range b.Config.Resources.Jobs {
 		if jobRef == nil {
@@ -27,13 +32,14 @@ func (c switchToPatchedWheels) Apply(ctx context.Context, b *bundle.Bundle) diag
 		}
 
 		for taskInd, task := range job.Tasks {
-
 			// Update resources.jobs.*.task[*].libraries[*].whl
 			for libInd, lib := range task.Libraries {
 				repl := replacements[lib.Whl]
 				if repl != "" {
 					log.Debugf(ctx, "Updating resources.jobs.%s.task[%d].libraries[%d].whl from %s to %s", jobName, taskInd, libInd, lib.Whl, repl)
 					job.Tasks[taskInd].Libraries[libInd].Whl = repl
+				} else {
+					log.Debugf(ctx, "Not updating resources.jobs.%s.task[%d].libraries[%d].whl from %s. Available replacements: %v", jobName, taskInd, libInd, lib.Whl, utils.SortedKeys(replacements))
 				}
 			}
 
@@ -46,6 +52,8 @@ func (c switchToPatchedWheels) Apply(ctx context.Context, b *bundle.Bundle) diag
 					if repl != "" {
 						log.Debugf(ctx, "Updating resources.jobs.%s.task[%d].for_each_task.task.libraries[%d].whl from %s to %s", jobName, taskInd, libInd, lib.Whl, repl)
 						foreachptr.Task.Libraries[libInd].Whl = repl
+					} else {
+						log.Debugf(ctx, "Not updating resources.jobs.%s.task[%d].for_each_task.task.libraries[%d].whl from %s. Available replacements: %v", jobName, taskInd, libInd, lib.Whl, repl, utils.SortedKeys(replacements))
 					}
 				}
 			}
@@ -62,6 +70,8 @@ func (c switchToPatchedWheels) Apply(ctx context.Context, b *bundle.Bundle) diag
 				if repl != "" {
 					log.Debugf(ctx, "Updating resources.jobs.%s.environments[%d].spec.dependencies[%d] from %s to %s", jobName, envInd, depInd, dep, repl)
 					specptr.Dependencies[depInd] = repl
+				} else {
+					log.Debugf(ctx, "Not updating resources.jobs.%s.environments[%d].spec.dependencies[%d] from %s. Available replacements: %v", jobName, envInd, depInd, dep, repl, utils.SortedKeys(replacements))
 				}
 			}
 		}
@@ -70,19 +80,19 @@ func (c switchToPatchedWheels) Apply(ctx context.Context, b *bundle.Bundle) diag
 	return nil
 }
 
-func getReplacements(ctx context.Context, artifacts config.Artifacts, bundleRoot string) map[string]string {
+func getReplacements(ctx context.Context, artifacts config.Artifacts, root string) map[string]string {
 	result := make(map[string]string)
 	for _, artifact := range artifacts {
 		for _, file := range artifact.Files {
 			if file.Patched != "" {
-				source, err := filepath.Rel(bundleRoot, file.Source)
+				source, err := filepath.Rel(root, file.Source)
 				if err != nil {
-					log.Debugf(ctx, "Failed to get relative path (%s, %s): %s", bundleRoot, file.Source, err)
+					log.Debugf(ctx, "Failed to get relative path (%s, %s): %s", root, file.Source, err)
 					continue
 				}
-				patched, err := filepath.Rel(bundleRoot, file.Patched)
+				patched, err := filepath.Rel(root, file.Patched)
 				if err != nil {
-					log.Debugf(ctx, "Failed to get relative path (%s, %s): %s", bundleRoot, file.Patched, err)
+					log.Debugf(ctx, "Failed to get relative path (%s, %s): %s", root, file.Patched, err)
 					continue
 				}
 				result[source] = patched
