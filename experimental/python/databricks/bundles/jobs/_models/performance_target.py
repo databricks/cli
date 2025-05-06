a0dd6bc34dbcd4b60facf3bd2beff996e73d6c6c
Andrew Nester <andrew.nester.dev@gmail.com>
2025-05-06 12:39:16 +0100

    Regenerated Python code after Go SDK upgrade (#2817)
    ## Changes
    Regenerated Python code after Go SDK upgrade
    
    ## Why
    It should be part of Go SDK upgrade but was not done, also added it to
    the generate step

--- experimental/python/databricks/bundles/jobs/_models/performance_target.py
+++ experimental/python/databricks/bundles/jobs/_models/performance_target.py
@@ -4,8 +4,6 @@ from typing import Literal
 
 class PerformanceTarget(Enum):
     """
-    :meta private: [EXPERIMENTAL]
-
     PerformanceTarget defines how performant (lower latency) or cost efficient the execution of run on serverless compute should be.
     The performance mode on the job or pipeline should map to a performance setting that is passed to Cluster Manager
     (see cluster-common PerformanceTarget).
