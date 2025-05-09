197545cbc14f7355aaeeb95069e88fca9badb603
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2025-05-09 15:42:31 +0200

    add secret_scopes to the allow list for run_as

--- bundle/config/mutator/resourcemutator/run_as.go
+++ bundle/config/mutator/resourcemutator/run_as.go
@@ -129,16 +129,6 @@ func validateRunAs(b *bundle.Bundle) diag.Diagnostics {
 		))
 	}
 
-	// Secret scopes do not support run_as in the API.
-	if len(b.Config.Resources.SecretScopes) > 0 {
-		diags = diags.Extend(reportRunAsNotSupported(
-			"secret_scopes",
-			b.Config.GetLocation("resources.secret_scopes"),
-			b.Config.Workspace.CurrentUser.UserName,
-			identity,
-		))
-	}
-
 	return diags
 }
 
