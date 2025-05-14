a58a397ad44e643fa61dbd566d5fb0703a12d780
Shreyas Goenka <shreyas.goenka@databricks.com>
2025-05-14 11:36:47 +0200

    address comments

--- libs/exec/shell_execv.go
+++ libs/exec/shell_execv.go
@@ -28,5 +28,15 @@ func ShellExecv(content, dir string, env []string) error {
 		return err
 	}
 
+	// Note: To execute the content of the script, we write a temporary file and execute that
+	// using a shell. Since execv does not return control to the CLI process, we need to
+	// clean up the temporary file here.
+	// This is fine for Unix and Mac OS systems because files in /tmp are automatically
+	// cleaned up.
+	// ref1: https://serverfault.com/questions/377348/when-does-tmp-get-cleared
+	// ref2: https://superuser.com/questions/187071/in-macos-how-often-is-tmp-deleted
+	//
+	// For windows the temp files are not automatically cleaned up. Automatic cleanup
+	// is a opt-in behavior.
 	return Execv(newOpts)
 }
