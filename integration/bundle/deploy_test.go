b13c92a4bc0be26ba6ceba627da1273c278a8b13
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2025-05-14 17:43:49 +0200

    remove original TestBundleDeployUcSchema and TestBundleDeployUcSchemaFailsWithoutAutoApprove integration tests

--- integration/bundle/deploy_test.go
+++ integration/bundle/deploy_test.go
@@ -1,9 +1,7 @@
 package bundle_test
 
 import (
-	"context"
 	"fmt"
-	"io"
 	"os"
 	"path/filepath"
 	"strings"
@@ -14,115 +12,11 @@ import (
 	"github.com/databricks/cli/internal/testcli"
 	"github.com/databricks/cli/internal/testutil"
 	"github.com/databricks/cli/libs/env"
-	"github.com/databricks/databricks-sdk-go"
-	"github.com/databricks/databricks-sdk-go/apierr"
 	"github.com/databricks/databricks-sdk-go/service/catalog"
-	"github.com/databricks/databricks-sdk-go/service/files"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
-func setupUcSchemaBundle(t *testing.T, ctx context.Context, w *databricks.WorkspaceClient, uniqueId string) string {
-	bundleRoot := initTestTemplate(t, ctx, "uc_schema", map[string]any{
-		"unique_id": uniqueId,
-	})
-
-	deployBundle(t, ctx, bundleRoot)
-
-	t.Cleanup(func() {
-		destroyBundle(t, ctx, bundleRoot)
-	})
-
-	// Assert the schema is created
-	catalogName := "main"
-	schemaName := "test-schema-" + uniqueId
-	schema, err := w.Schemas.GetByFullName(ctx, strings.Join([]string{catalogName, schemaName}, "."))
-	require.NoError(t, err)
-	require.Equal(t, strings.Join([]string{catalogName, schemaName}, "."), schema.FullName)
-	require.Equal(t, "This schema was created from DABs", schema.Comment)
-
-	// Assert the pipeline is created
-	pipelineName := "test-pipeline-" + uniqueId
-	pipeline, err := w.Pipelines.GetByName(ctx, pipelineName)
-	require.NoError(t, err)
-	require.Equal(t, pipelineName, pipeline.Name)
-	id := pipeline.PipelineId
-
-	// Assert the pipeline uses the schema
-	i, err := w.Pipelines.GetByPipelineId(ctx, id)
-	require.NoError(t, err)
-	require.Equal(t, catalogName, i.Spec.Catalog)
-	require.Equal(t, strings.Join([]string{catalogName, schemaName}, "."), i.Spec.Target)
-
-	// Create a volume in the schema, and add a file to it. This ensures that the
-	// schema has some data in it and deletion will fail unless the generated
-	// terraform configuration has force_destroy set to true.
-	volumeName := "test-volume-" + uniqueId
-	volume, err := w.Volumes.Create(ctx, catalog.CreateVolumeRequestContent{
-		CatalogName: catalogName,
-		SchemaName:  schemaName,
-		Name:        volumeName,
-		VolumeType:  catalog.VolumeTypeManaged,
-	})
-	require.NoError(t, err)
-	require.Equal(t, volume.Name, volumeName)
-
-	fileName := "test-file-" + uniqueId
-	err = w.Files.Upload(ctx, files.UploadRequest{
-		Contents: io.NopCloser(strings.NewReader("Hello, world!")),
-		FilePath: fmt.Sprintf("/Volumes/%s/%s/%s/%s", catalogName, schemaName, volumeName, fileName),
-	})
-	require.NoError(t, err)
-
-	return bundleRoot
-}
-
-func TestBundleDeployUcSchema(t *testing.T) {
-	ctx, wt := acc.UcWorkspaceTest(t)
-	w := wt.W
-
-	uniqueId := uuid.New().String()
-	schemaName := "test-schema-" + uniqueId
-	catalogName := "main"
-
-	bundleRoot := setupUcSchemaBundle(t, ctx, w, uniqueId)
-
-	// Remove the UC schema from the resource configuration.
-	err := os.Remove(filepath.Join(bundleRoot, "schema.yml"))
-	require.NoError(t, err)
-
-	// Redeploy the bundle
-	deployBundle(t, ctx, bundleRoot)
-
-	// Assert the schema is deleted
-	_, err = w.Schemas.GetByFullName(ctx, strings.Join([]string{catalogName, schemaName}, "."))
-	apiErr := &apierr.APIError{}
-	assert.ErrorAs(t, err, &apiErr)
-	assert.Equal(t, "SCHEMA_DOES_NOT_EXIST", apiErr.ErrorCode)
-}
-
-func TestBundleDeployUcSchemaFailsWithoutAutoApprove(t *testing.T) {
-	ctx, wt := acc.UcWorkspaceTest(t)
-	w := wt.W
-
-	uniqueId := uuid.New().String()
-	bundleRoot := setupUcSchemaBundle(t, ctx, w, uniqueId)
-
-	// Remove the UC schema from the resource configuration.
-	err := os.Remove(filepath.Join(bundleRoot, "schema.yml"))
-	require.NoError(t, err)
-
-	// Redeploy the bundle
-	ctx = env.Set(ctx, "BUNDLE_ROOT", bundleRoot)
-	ctx = env.Set(ctx, "TERM", "dumb")
-	c := testcli.NewRunner(t, ctx, "bundle", "deploy", "--force-lock")
-	stdout, stderr, err := c.Run()
-
-	assert.EqualError(t, err, root.ErrAlreadyPrinted.Error())
-	assert.Contains(t, stderr.String(), "The following UC schemas will be deleted or recreated. Any underlying data may be lost:\n  delete schema bar")
-	assert.Contains(t, stdout.String(), "the deployment requires destructive actions, but current console does not support prompting. Please specify --auto-approve if you would like to skip prompts and proceed")
-}
 
 func TestBundlePipelineDeleteWithoutAutoApprove(t *testing.T) {
 	ctx, wt := acc.WorkspaceTest(t)
