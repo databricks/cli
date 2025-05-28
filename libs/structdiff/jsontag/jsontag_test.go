c6716dcffb67360566f2fbeacef6111a8c67e71b
Denis Bilenko <denis.bilenko@databricks.com>
2025-05-28 12:03:25 +0200

    lint fix

--- libs/structdiff/jsontag/jsontag_test.go
+++ libs/structdiff/jsontag/jsontag_test.go
@@ -59,7 +59,7 @@ var benchTags = []JSONTag{
 }
 
 func BenchmarkJSONTagName(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for range b.N {
 		for _, tag := range benchTags {
 			tag.Name()
 		}
@@ -67,7 +67,7 @@ func BenchmarkJSONTagName(b *testing.B) {
 }
 
 func BenchmarkJSONTagOmitEmpty(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for range b.N {
 		for _, tag := range benchTags {
 			tag.OmitEmpty()
 		}
