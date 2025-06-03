c3adff2a4e48412a3a132ae625339d86d5743384
Shreyas Goenka <shreyas.goenka@databricks.com>
2025-06-03 11:13:48 +0200

    Add bundletag package

---
+++ libs/structdiff/bundletag/bundletag_test.go
@@ -0,0 +1,43 @@
+package bundletag
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBundleTagMethods(t *testing.T) {
+	tests := []struct {
+		tag          string
+		isReadOnly   bool
+		isInternal   bool
+		isDeprecated bool
+	}{
+		// only one annotation.
+		{"readonly", true, false, false},
+		{"internal", false, true, false},
+		{"deprecated", false, false, true},
+
+		// multiple annotations.
+		{"readonly,internal", true, true, false},
+		{"readonly,deprecated", true, false, true},
+		{"internal,deprecated", false, true, true},
+		{"readonly,internal,deprecated", true, true, true},
+
+		// unknown annotations are ignored.
+		{"something", false, false, false},
+		{"-", false, false, false},
+		{"name,string", false, false, false},
+		{"weird,whatever,readonly,foo", true, false, false},
+	}
+
+	for _, test := range tests {
+		t.Run(test.tag, func(t *testing.T) {
+			tag := BundleTag(test.tag)
+
+			assert.Equal(t, test.isReadOnly, tag.ReadOnly())
+			assert.Equal(t, test.isInternal, tag.Internal())
+			assert.Equal(t, test.isDeprecated, tag.Deprecated())
+		})
+	}
+}
