b60579b394d64ee70f89f266c2ef61fb12d46107
Andrew Nester <andrew.nester.dev@gmail.com>
2025-06-03 14:05:44 +0100

    Added `experimental.skip_artifact_cleanup` flag (#2980)
    ## Changes
    Added `experimental.skip_artifact_cleanup` flag. When set to `true`
    .internal folder in Databricks workspace which contains built artifacts
    such as wheels won't be cleaned up on deploy
    
    Fixes #2969 #2958
    
    ## Why
    It can be useful to keep old built wheels around for cases when they are
    running jobs using the older version of wheels to prevent new
    deployments from breaking these jobs.
    
    ## Tests
    Added acceptance tests
    
    <!-- If your PR needs to be included in the release notes for next
    release,
    add a separate entry in NEXT_CHANGELOG.md as part of your PR. -->

--- NEXT_CHANGELOG.md
+++ NEXT_CHANGELOG.md
@@ -9,6 +9,7 @@
 ### CLI
 
 ### Bundles
+* Added `experimental.skip_artifact_cleanup` flag ([#2980](https://github.com/databricks/cli/pull/2980))
 * Add an experimental project template for Lakeflow Declarative Pipelines ([#2959](https://github.com/databricks/cli/pull/2959))
 
 ### API Changes
--- bundle/artifacts/upload.go
+++ bundle/artifacts/upload.go
@@ -26,13 +26,19 @@ func (m *cleanUp) Apply(ctx context.Context, b *bundle.Bundle) diag.Diagnostics 
 		return diags
 	}
 
-	// We intentionally ignore the error because it is not critical to the deployment
-	err := client.Delete(ctx, libraries.InternalDirName, filer.DeleteRecursively)
-	if err != nil {
-		log.Debugf(ctx, "failed to delete %s: %v", uploadPath, err)
+	skipArtifactsCleanup := b.Config.Experimental != nil && b.Config.Experimental.SkipArtifactCleanup
+	b.Metrics.AddBoolValue("skip_artifact_cleanup", skipArtifactsCleanup)
+	if skipArtifactsCleanup {
+		log.Info(ctx, "Skip cleaning up artifacts folder")
+	} else {
+		// We intentionally ignore the error because it is not critical to the deployment
+		err := client.Delete(ctx, libraries.InternalDirName, filer.DeleteRecursively)
+		if err != nil {
+			log.Debugf(ctx, "failed to delete %s: %v", uploadPath, err)
+		}
 	}
 
-	err = client.Mkdir(ctx, libraries.InternalDirName)
+	err := client.Mkdir(ctx, libraries.InternalDirName)
 	if err != nil {
 		return diag.Errorf("unable to create directory for %s: %v", uploadPath, err)
 	}
