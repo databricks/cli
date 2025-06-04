1d5819b22aea7acc8ba3b0e57ec636a0a7be9040
Denis Bilenko <denis.bilenko@databricks.com>
2025-06-04 09:59:46 +0200

    clean up and extend tests

--- libs/patchwheel/filter_test.go
+++ libs/patchwheel/filter_test.go
@@ -9,39 +9,26 @@ import (
 
 func TestFilterLatestWheels(t *testing.T) {
 	paths := []string{
+		"project_name_bvs7tide6bhhpjy4dmcsb2qg44-0.0.1+20250604.74809-py3-none-any.whl",
+		"not-a-wheel.txt",
 		"mypkg-0.1.0-py3-none-any.whl",
 		"mypkg-0.2.0-py3-none-any.whl",
 		"other-1.0.0-py3-none-any.whl",
 		"other-0.9.0-py3-none-any.whl",
+		"project_name_bvs7tide6bhhpjy4dmcsb2qg44-0.0.1+20250604.74804-py3-none-any.whl",
+		"not-a-wheel.whl",
+
+		"hello-1.2.3-py3-none-any.whl",
+		"hello-1.2.3+1741091696780123321-py3-none-any.whl",
 	}
 
 	filtered := FilterLatestWheels(context.Background(), paths)
 	require.ElementsMatch(t, []string{
+		"project_name_bvs7tide6bhhpjy4dmcsb2qg44-0.0.1+20250604.74809-py3-none-any.whl",
+		"not-a-wheel.txt",
 		"mypkg-0.2.0-py3-none-any.whl",
 		"other-1.0.0-py3-none-any.whl",
-	}, filtered)
-}
-
-func TestFilterLatestWheelsWithTimestamp(t *testing.T) {
-	// Second package has timestamp suffix which should win over plain version.
-	paths := []string{
-		"mypkg-1.2.3-py3-none-any.whl",
-		"mypkg-1.2.3+1741091696780123321-py3-none-any.whl",
-	}
-	filtered := FilterLatestWheels(context.Background(), paths)
-	require.Equal(t, []string{"mypkg-1.2.3+1741091696780123321-py3-none-any.whl"}, filtered)
-}
-
-func TestFilterLatestWheelsKeepsUnparsable(t *testing.T) {
-	paths := []string{
-		"not-a-wheel.txt",
-		"mypkg-0.1.0-py3-none-any.whl",
-		"mypkg-0.2.0-py3-none-any.whl",
-	}
-
-	filtered := FilterLatestWheels(context.Background(), paths)
-	require.Equal(t, []string{
-		"not-a-wheel.txt",
-		"mypkg-0.2.0-py3-none-any.whl",
+		"not-a-wheel.whl",
+		"hello-1.2.3+1741091696780123321-py3-none-any.whl",
 	}, filtered)
 }
