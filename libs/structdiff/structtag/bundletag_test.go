aa0308a723beb18bc327d944c5fecc9edc877bcc
Shreyas Goenka <shreyas.goenka@databricks.com>
2025-06-05 13:11:17 +0200

    remove deprecated bundle tag

--- libs/structdiff/structtag/bundletag_test.go
+++ libs/structdiff/structtag/bundletag_test.go
@@ -8,21 +8,16 @@ import (
 
 func TestBundleTagMethods(t *testing.T) {
 	tests := []struct {
-		tag          string
-		isReadOnly   bool
-		isInternal   bool
-		isDeprecated bool
+		tag        string
+		isReadOnly bool
+		isInternal bool
 	}{
 		// only one annotation.
 		{tag: "readonly", isReadOnly: true},
 		{tag: "internal", isInternal: true},
-		{tag: "deprecated", isDeprecated: true},
 
 		// multiple annotations.
 		{tag: "readonly,internal", isReadOnly: true, isInternal: true},
-		{tag: "readonly,deprecated", isReadOnly: true, isDeprecated: true},
-		{tag: "internal,deprecated", isInternal: true, isDeprecated: true},
-		{tag: "readonly,internal,deprecated", isReadOnly: true, isInternal: true, isDeprecated: true},
 
 		// unknown annotations are ignored.
 		{tag: "something"},
@@ -37,7 +32,6 @@ func TestBundleTagMethods(t *testing.T) {
 
 			assert.Equal(t, test.isReadOnly, tag.ReadOnly())
 			assert.Equal(t, test.isInternal, tag.Internal())
-			assert.Equal(t, test.isDeprecated, tag.Deprecated())
 		})
 	}
 }
