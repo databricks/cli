3289f90af551be5cc3357518b9ca982017d50ce6
shreyas-goenka <88374338+shreyas-goenka@users.noreply.github.com>
2025-06-10 15:24:27 +0200

    Fix dashboard generation for already imported dashboard (#3016)
    ## Changes
    We started inlining the dashboard definition in
    https://github.com/databricks/cli/pull/2738 by reading `file_path` and
    setting it in `serialized_dashboard`.
    
    This broke the `bundle generate` CUJ for updating an already imported
    dashboard when working with DABs. This PR fixes that by keeping the
    `file_path` around in the configuration tree as metadata and only
    dropping it when we generate the TF configuration.
    
    This PR also adds regression tests to ensure this keeps working in the
    future.
    
    ## Why
    Fixes
    https://github.com/databricks/cli/issues/2998#issuecomment-2950723565.
    
    ## Tests
    Acceptance tests.

---
+++ libs/testserver/dashboards.go
@@ -0,0 +1,67 @@
+package testserver
+
+import (
+	"encoding/json"
+
+	"github.com/databricks/databricks-sdk-go/service/dashboards"
+	"github.com/google/uuid"
+)
+
+func (s *FakeWorkspace) DashboardCreate(req Request) Response {
+	defer s.LockUnlock()()
+
+	var dashboard dashboards.Dashboard
+	if err := json.Unmarshal(req.Body, &dashboard); err != nil {
+		return Response{
+			StatusCode: 400,
+		}
+	}
+
+	dashboard.DashboardId = uuid.New().String()
+	s.Dashboards[dashboard.DashboardId] = dashboard
+
+	return Response{
+		Body: dashboards.Dashboard{
+			DashboardId: dashboard.DashboardId,
+			Etag:        uuid.New().String(),
+		},
+	}
+}
+
+func (s *FakeWorkspace) DashboardUpdate(req Request) Response {
+	defer s.LockUnlock()()
+
+	var dashboard dashboards.Dashboard
+	if err := json.Unmarshal(req.Body, &dashboard); err != nil {
+		return Response{
+			StatusCode: 400,
+		}
+	}
+
+	// Update the etag for the dashboard.
+	dashboard.Etag = uuid.New().String()
+
+	dashboardId := req.Vars["dashboard_id"]
+	s.Dashboards[dashboardId] = dashboard
+
+	return Response{
+		Body: dashboard,
+	}
+}
+
+func (s *FakeWorkspace) DashboardPublish(req Request) Response {
+	defer s.LockUnlock()()
+
+	var dashboard dashboards.Dashboard
+	if err := json.Unmarshal(req.Body, &dashboard); err != nil {
+		return Response{
+			StatusCode: 400,
+		}
+	}
+
+	return Response{
+		Body: dashboards.PublishedDashboard{
+			WarehouseId: dashboard.WarehouseId,
+		},
+	}
+}
