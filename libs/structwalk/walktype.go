a23be2d04ddd0f23796fb7c395edbf073721ec6b
shreyas-goenka <88374338+shreyas-goenka@users.noreply.github.com>
2025-06-10 11:04:04 +0200

    Rename jsontag to structtag; support bundle tags (#2976)
    ## Why
    We use bundle tags in our structs to table internal / readonly or
    deprecated fields. This is a general package that allows us to inspect
    those tags during a `structwalk.Walk`. Support for `structwalk.Walk`
    will be added in a followup PR.
    
    JSON tags can also be accessed via the same package.
    
    ## Tests
    Unit test.

--- libs/structwalk/walktype.go
+++ libs/structwalk/walktype.go
@@ -4,8 +4,8 @@ import (
 	"errors"
 	"reflect"
 
-	"github.com/databricks/cli/libs/structdiff/jsontag"
 	"github.com/databricks/cli/libs/structdiff/structpath"
+	"github.com/databricks/cli/libs/structdiff/structtag"
 )
 
 // VisitTypeFunc is invoked for fields encountered while walking typ. This includes both leaf nodes as well as any
@@ -111,7 +111,7 @@ func walkTypeStruct(path *structpath.PathNode, st reflect.Type, visit VisitTypeF
 		if tag == "-" {
 			continue // skip fields without json name
 		}
-		jsonTag := jsontag.JSONTag(tag)
+		jsonTag := structtag.JSONTag(tag)
 		if jsonTag.Name() == "-" {
 			continue
 		}
