7ac6474af6047ce468fa8454685b7346e1126346
Andrew Nester <andrew.nester@databricks.com>
2025-06-10 16:52:05 +0200

    fixed walktype test

--- libs/structwalk/walktype_test.go
+++ libs/structwalk/walktype_test.go
@@ -117,7 +117,7 @@ func TestTypeJobSettings(t *testing.T) {
 func TestTypeRoot(t *testing.T) {
 	testStruct(t,
 		reflect.TypeOf(config.Root{}),
-		3400, 3500, // 3487 at this time
+		3500, 3600, // 3516 at this time
 		map[string]any{
 			".bundle.target":                 "",
 			`.variables[*].lookup.dashboard`: "",
