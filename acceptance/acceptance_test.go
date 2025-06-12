b3db475aa265b3272bfe8943ef1deb9fc4bff09b
Shreyas Goenka <shreyas.goenka@databricks.com>
2025-06-12 14:44:32 +0200

    deterministic output

--- acceptance/acceptance_test.go
+++ acceptance/acceptance_test.go
@@ -99,7 +99,7 @@ var Ignored = map[string]bool{
 }
 
 func TestAccept(t *testing.T) {
-	testAccept(t, true, "bundle/validate/required")
+	testAccept(t, InprocessMode, "")
 }
 
 func TestInprocessMode(t *testing.T) {
