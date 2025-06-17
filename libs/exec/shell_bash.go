databricks/cli

commit b98ab5a745b579204c99e1d533b9e1dc44da2cf6

Inline scripts executed via `bash` or `sh` (#2896)
## Changes
This PR inlines the script content using the `-c` flag in bash and sh
instead of passing them in via `-e <file>`.

## Why
Based on discussion in
https://github.com/databricks/cli/pull/2862#discussion_r2086894158.
Getting rid of the temp file means we no longer have to clean up the
temporary file created.

## Tests
Tests that were added in: https://github.com/databricks/cli/pull/2884

libs/exec/shell_bash.go
@@ -11,15 +11,9 @@ type bashShell struct {
 }
 
 func (s bashShell) prepare(command string) (*execContext, error) {
-	filename, err := createTempScript(command, ".sh")
-	if err != nil {
-		return nil, err
-	}
-
 	return &execContext{
 		executable: s.executable,
-		args:       []string{"-e", filename},
-		scriptFile: filename,
+		args:       []string{"-c", command},
 	}, nil
 }
 

libs/exec/shell_sh.go
@@ -10,15 +10,9 @@ type shShell struct {
 }
 
 func (s shShell) prepare(command string) (*execContext, error) {
-	filename, err := createTempScript(command, ".sh")
-	if err != nil {
-		return nil, err
-	}
-
 	return &execContext{
 		executable: s.executable,
-		args:       []string{"-e", filename},
-		scriptFile: filename,
+		args:       []string{"-c", command},
 	}, nil
 }
 

libs/exec/shell_execv_test.go
@@ -4,21 +4,20 @@ import (
 	"os/exec"
 	"testing"
 
-	"github.com/databricks/cli/internal/testutil"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestShellExecvOpts(t *testing.T) {
-	newOpts, err := shellExecvOpts("echo hello", "/a/b/c", []string{"key1=value1", "key2=value2"})
+	opts, err := shellExecvOpts("echo hello", "/a/b/c", []string{"key1=value1", "key2=value2"})
 	require.NoError(t, err)
 
-	assert.Equal(t, []string{"key1=value1", "key2=value2"}, newOpts.Env)
-	assert.Equal(t, "/a/b/c", newOpts.Dir)
+	assert.Equal(t, []string{"key1=value1", "key2=value2"}, opts.Env)
+	assert.Equal(t, "/a/b/c", opts.Dir)
 
 	bashPath, err := exec.LookPath("bash")
 	require.NoError(t, err)
-	assert.Equal(t, bashPath, newOpts.Args[0])
-	assert.Equal(t, "-e", newOpts.Args[1])
-	assert.Equal(t, "echo hello", testutil.ReadFile(t, newOpts.Args[2]))
+	assert.Equal(t, bashPath, opts.Args[0])
+	assert.Equal(t, "-c", opts.Args[1])
+	assert.Equal(t, "echo hello", opts.Args[2])
 }

libs/exec/exec.go
@@ -42,7 +42,7 @@ type command struct {
 
 func (c *command) Wait() error {
 	// After the command has finished (cmd.Wait call), remove the temporary script file
-	defer os.Remove(c.execContext.scriptFile)
+	defer c.execContext.cleanup()
 
 	err := c.cmd.Wait()
 	if err != nil {
@@ -140,7 +140,7 @@ func (e *Executor) Exec(ctx context.Context, command string) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	defer os.Remove(ec.scriptFile)
+	defer ec.cleanup()
 	return cmd.CombinedOutput()
 }
 

libs/exec/exec_test.go
@@ -117,7 +117,11 @@ func TestExecutorNoShellFound(t *testing.T) {
 }
 
 func TestExecutorCleanupsTempFiles(t *testing.T) {
-	executor, err := NewCommandExecutor(".")
+	if runtime.GOOS != "windows" {
+		t.Skipf("cmd.exe is not available on non-Windows systems")
+	}
+
+	executor, err := NewCommandExecutorWithExecutable(".", CmdExecutable)
 	assert.NoError(t, err)
 
 	cmd, ec, err := executor.prepareCommand(context.Background(), "echo 'Hello'")
@@ -126,7 +130,8 @@ func TestExecutorCleanupsTempFiles(t *testing.T) {
 	command, err := executor.start(cmd, ec)
 	assert.NoError(t, err)
 
-	fileName := ec.args[1]
+	fileName := ec.scriptFile
+	assert.NotEmpty(t, fileName)
 	assert.FileExists(t, fileName)
 
 	err = command.Wait()

libs/exec/shell.go
@@ -14,7 +14,17 @@ type shell interface {
 type execContext struct {
 	executable string
 	args       []string
+
+	// scriptFile is the file that contains the command to be executed.
+	// We only use it for the cmd.exe shell since cmd.exe does not support
+	// inlining scripts.
 	scriptFile string
+}
+
+func (e *execContext) cleanup() {
+	if e.scriptFile != "" {
+		os.Remove(e.scriptFile)
+	}
 }
 
 func findShell() (shell, error) {

commit 86012df6583a70c8a9b1fdaf5d779543ad0c962c

Fix: Pass -e to sh and bash during command execution (#3065)
## Changes
This PR fixes a regression introduced in
https://github.com/databricks/cli/pull/2896 where we stopped passing the
`-e` flag to the shell, which makes both sh and bash bail out at the
first command with a non-zero exit code.

## Why
Adding back `-e` keeps the behaviour consistent with the one before
https://github.com/databricks/cli/pull/2896 was introduced.

## Tests
Newly added regression tests

.wsignore
@@ -17,6 +17,10 @@ experimental/python/docs/images/databricks-logo.svg
 # In order to prevent that difference, hello.txt does not have a trailing newline.
 acceptance/selftest/record_cloud/volume-io/hello.txt
 
+# error output when artifact build fails contains trailing whitespace:
+acceptance/bundle/artifacts/shell/err-bash/output.txt
+acceptance/bundle/artifacts/shell/err-sh/output.txt
+
 # "bundle init" has trailing whitespace:
 acceptance/bundle/templates-machinery/helpers-error/output.txt
 

acceptance/bundle/artifacts/shell/err-bash/databricks.yml
@@ -1,0 +1,8 @@
+artifacts:
+  my_artifact:
+    executable: bash
+    build: |-
+      echo "hello" > out.shell.txt
+      echo "world" >> out.shell.txt
+      false
+      echo "this should not be printed since bash is run with -e" >> out.shell.txt

acceptance/bundle/artifacts/shell/err-bash/script
@@ -1,0 +1,1 @@
+trace $CLI bundle deploy

acceptance/bundle/artifacts/shell/err-sh/databricks.yml
@@ -1,0 +1,8 @@
+artifacts:
+  my_artifact:
+    executable: sh
+    build: |-
+      echo "hello" > out.shell.txt
+      echo "world" >> out.shell.txt
+      false
+      echo "this should not be printed since sh is run with -e" >> out.shell.txt

acceptance/bundle/artifacts/shell/err-sh/script
@@ -1,0 +1,1 @@
+trace $CLI bundle deploy

libs/exec/shell_bash.go
@@ -13,7 +13,7 @@ type bashShell struct {
 func (s bashShell) prepare(command string) (*execContext, error) {
 	return &execContext{
 		executable: s.executable,
-		args:       []string{"-c", command},
+		args:       []string{"-ec", command},
 	}, nil
 }
 

libs/exec/shell_sh.go
@@ -12,7 +12,7 @@ type shShell struct {
 func (s shShell) prepare(command string) (*execContext, error) {
 	return &execContext{
 		executable: s.executable,
-		args:       []string{"-c", command},
+		args:       []string{"-ec", command},
 	}, nil
 }
 

libs/exec/shell_execv_test.go
@@ -18,6 +18,6 @@ func TestShellExecvOpts(t *testing.T) {
 	bashPath, err := exec.LookPath("bash")
 	require.NoError(t, err)
 	assert.Equal(t, bashPath, opts.Args[0])
-	assert.Equal(t, "-c", opts.Args[1])
+	assert.Equal(t, "-ec", opts.Args[1])
 	assert.Equal(t, "echo hello", opts.Args[2])
 }