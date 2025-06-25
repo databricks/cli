be73133024f330f91fb3396bc6525b48eef8d666
Alyssa Gorbaneva <alyssa.gorbaneva@databricks.com>
2025-06-25 12:58:52 -0700

    windows test fix

--- cmd/pipelines/init.go
+++ cmd/pipelines/init.go
@@ -7,37 +7,38 @@ import (
 )
 
 func initCommand() *cobra.Command {
-	var outputDir string
-	var configFile string
 	cmd := &cobra.Command{
 		Use:     "init",
 		Short:   "Initialize a new pipelines project",
 		PreRunE: root.MustWorkspaceClient,
 		Args:    cobra.NoArgs,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			ctx := cmd.Context()
-
-			r := template.Resolver{
-				TemplatePathOrUrl: "pipelines",
-				ConfigFile:        configFile,
-				OutputDir:         outputDir,
-			}
-
-			tmpl, err := r.Resolve(ctx)
-			if err != nil {
-				return err
-			}
-			defer tmpl.Reader.Cleanup(ctx)
-
-			err = tmpl.Writer.Materialize(ctx, tmpl.Reader)
-			if err != nil {
-				return err
-			}
-			tmpl.Writer.LogTelemetry(ctx)
-			return nil
-		},
 	}
-	cmd.Flags().StringVar(&outputDir, "output-dir", "", "Directory to write the initialized template to")
-	cmd.Flags().StringVar(&configFile, "config-file", "", "JSON file containing key value pairs of input parameters required for template initialization")
+	var configFile string
+	var outputDir string
+	cmd.Flags().StringVar(&configFile, "config-file", "", "JSON file containing key value pairs of input parameters required for template initialization.")
+	cmd.Flags().StringVar(&outputDir, "output-dir", "", "Directory to write the initialized template to.")
+
+	cmd.RunE = func(cmd *cobra.Command, args []string) error {
+		ctx := cmd.Context()
+
+		r := template.Resolver{
+			TemplatePathOrUrl: "pipelines",
+			ConfigFile:        configFile,
+			OutputDir:         outputDir,
+		}
+
+		tmpl, err := r.Resolve(ctx)
+		if err != nil {
+			return err
+		}
+		defer tmpl.Reader.Cleanup(ctx)
+
+		err = tmpl.Writer.Materialize(ctx, tmpl.Reader)
+		if err != nil {
+			return err
+		}
+		tmpl.Writer.LogTelemetry(ctx)
+		return nil
+	}
 	return cmd
 }
