databricks/cli

commit 14aeb2a2ec3c40d4d39a22a83a14464cec4d5278

Fix "bundle summary" to not lose warning messages (#3123)
## Changes
- Merge rendering logic of "bundle validate" and "bundle summary" into
one function.
- Now "bundle summary" logs all warning diagnostics to stderr like
"bundle validate" does.

## Why
Not rendering warnings is a bug IMO. These are not just validation
issues but could be real issues that are only related to summary and
don't show up in "bundle validate".

This is not the first time "bundle summary" differs from "bundle
validate" (https://github.com/databricks/cli/pull/2990) so in this PR I
go beyond fixing and merge two code paths.

cmd/bundle/validate.go
@@ -12,6 +12,7 @@ import (
 	"github.com/databricks/cli/bundle/render"
 	"github.com/databricks/cli/cmd/bundle/utils"
 	"github.com/databricks/cli/cmd/root"
+	"github.com/databricks/cli/libs/diag"
 	"github.com/databricks/cli/libs/flags"
 	"github.com/spf13/cobra"
 )
@@ -63,41 +64,60 @@ func newValidateCommand() *cobra.Command {
 			diags = diags.Extend(bundle.Apply(ctx, b, mutator.PopulateLocations()))
 		}
 
-		switch root.OutputType(cmd) {
-		case flags.OutputText:
-			renderOpts := render.RenderOptions{RenderSummaryTable: true}
-			err := render.RenderDiagnostics(cmd.OutOrStdout(), b, diags, renderOpts)
-			if err != nil {
-				return fmt.Errorf("failed to render output: %w", err)
-			}
-
-			if diags.HasError() {
-				return root.ErrAlreadyPrinted
-			}
-
-			return nil
-		case flags.OutputJSON:
-			renderOpts := render.RenderOptions{RenderSummaryTable: false}
-			err1 := render.RenderDiagnostics(cmd.ErrOrStderr(), b, diags, renderOpts)
-			err2 := renderJsonOutput(cmd, b)
-
-			if err2 != nil {
-				return err2
-			}
-
-			if err1 != nil {
-				return err1
-			}
-
-			if diags.HasError() {
-				return root.ErrAlreadyPrinted
-			}
-
-			return nil
-		default:
-			return fmt.Errorf("unknown output type %s", root.OutputType(cmd))
-		}
+		return renderBundle(cmd, b, diags, false)
 	}
 
 	return cmd
+}
+
+// This function is used to render results both for "bundle validate" and "bundle summary".
+// In JSON mode, there is no difference in rendering between these two (but there is a difference in how we prepare the bundle).
+// In non-JSON mode both "bundle validate" and "bundle summary" will print diagnostics to stderr but "bundle validate"
+// will also print "summary" message via RenderSummaryTable option.
+func renderBundle(cmd *cobra.Command, b *bundle.Bundle, diags diag.Diagnostics, withBundleSummary bool) error {
+	ctx := cmd.Context()
+	switch root.OutputType(cmd) {
+	case flags.OutputText:
+		// Confusingly RenderSummaryTable relates to "Validation OK" and related messages, it has nothing
+		// to do with "bundle summary" command and we don't want to show it in bundle summary command.
+		renderOpts := render.RenderOptions{RenderSummaryTable: !withBundleSummary}
+		err1 := render.RenderDiagnostics(cmd.OutOrStdout(), b, diags, renderOpts)
+		if b != nil && withBundleSummary {
+			// Now RenderSummary actually related to "bundle summary"
+			err2 := render.RenderSummary(ctx, cmd.OutOrStdout(), b)
+			if err2 != nil {
+				return err2
+			}
+		}
+
+		if err1 != nil {
+			return err1
+		}
+
+		if diags.HasError() {
+			return root.ErrAlreadyPrinted
+		}
+
+		return nil
+	case flags.OutputJSON:
+		renderOpts := render.RenderOptions{RenderSummaryTable: false}
+		err1 := render.RenderDiagnostics(cmd.ErrOrStderr(), b, diags, renderOpts)
+		err2 := renderJsonOutput(cmd, b)
+
+		if err2 != nil {
+			return err2
+		}
+
+		if err1 != nil {
+			return err1
+		}
+
+		if diags.HasError() {
+			return root.ErrAlreadyPrinted
+		}
+
+		return nil
+	default:
+		return fmt.Errorf("unknown output type %s", root.OutputType(cmd))
+	}
 }

acceptance/bundle/variables/host/test.toml
@@ -1,0 +1,3 @@
+[[Repls]]
+Old = '\\'
+New = '/'

acceptance/bundle/deploy/mlops-stacks/script
@@ -31,6 +31,6 @@ trace $CLI bundle deploy
 
 trace $CLI bundle summary -o json | jq -r '{experiment_id: .resources.experiments.experiment.id, model_id: .resources.models.model.id, inference_job_id: .resources.jobs.batch_inference_job.id, training_job_id: .resources.jobs.model_training_job.id}'
 
-title "Assert the batch inference job actually exists"
+title "Assert the batch inference job actually exists\n"
 JOB_ID=$($CLI bundle summary -o json | jq -r '.resources.jobs.batch_inference_job.id')
 $CLI jobs get "${JOB_ID}" | jq '{name: .settings.name}'

acceptance/bundle/variables/host/script
@@ -1,2 +1,3 @@
 trace errcode $CLI bundle validate -o json
 trace errcode $CLI bundle validate
+trace errcode $CLI bundle summary

bundle/render/render_text_output.go
@@ -187,6 +187,9 @@ func RenderDiagnostics(out io.Writer, b *bundle.Bundle, diags diag.Diagnostics, 
 }
 
 func RenderSummary(ctx context.Context, out io.Writer, b *bundle.Bundle) error {
+	if b == nil {
+		panic("internal error: b must not be nil")
+	}
 	if err := renderSummaryHeaderTemplate(out, b); err != nil {
 		return err
 	}

cmd/bundle/summary.go
@@ -1,9 +1,7 @@
 package bundle
 
 import (
-	"encoding/json"
 	"errors"
-	"fmt"
 	"os"
 	"path/filepath"
 
@@ -11,11 +9,10 @@ import (
 	"github.com/databricks/cli/bundle/config/mutator"
 	"github.com/databricks/cli/bundle/deploy/terraform"
 	"github.com/databricks/cli/bundle/phases"
-	"github.com/databricks/cli/bundle/render"
 	"github.com/databricks/cli/bundle/statemgmt"
 	"github.com/databricks/cli/cmd/bundle/utils"
 	"github.com/databricks/cli/cmd/root"
-	"github.com/databricks/cli/libs/flags"
+	"github.com/databricks/cli/libs/diag"
 	"github.com/spf13/cobra"
 )
 
@@ -33,66 +30,57 @@ func newSummaryCommand() *cobra.Command {
 	cmd.Flags().MarkHidden("include-locations")
 
 	cmd.RunE = func(cmd *cobra.Command, args []string) error {
-		ctx := cmd.Context()
-		b, diags := utils.ConfigureBundleWithVariables(cmd)
-		if err := diags.Error(); err != nil {
-			return diags.Error()
-		}
-
-		diags = phases.Initialize(ctx, b)
-		if err := diags.Error(); err != nil {
-			return err
-		}
-
-		cacheDir, err := terraform.Dir(ctx, b)
-		if err != nil {
-			return err
-		}
-		_, stateFileErr := os.Stat(filepath.Join(cacheDir, b.StateFilename()))
-		_, configFileErr := os.Stat(filepath.Join(cacheDir, terraform.TerraformConfigFileName))
-		noCache := errors.Is(stateFileErr, os.ErrNotExist) || errors.Is(configFileErr, os.ErrNotExist)
-
-		if forcePull || noCache {
-			diags = bundle.ApplySeq(ctx, b,
-				statemgmt.StatePull(),
-				terraform.Interpolate(),
-				terraform.Write(),
-			)
-			if err := diags.Error(); err != nil {
-				return err
-			}
-		}
-
-		diags = bundle.ApplySeq(ctx, b,
-			terraform.Load(),
-			mutator.InitializeURLs(),
-		)
-
-		// Include location information in the output if the flag is set.
-		if includeLocations {
-			diags = diags.Extend(bundle.Apply(ctx, b, mutator.PopulateLocations()))
-		}
-
-		if err := diags.Error(); err != nil {
-			return err
-		}
-
-		switch root.OutputType(cmd) {
-		case flags.OutputText:
-			return render.RenderSummary(ctx, cmd.OutOrStdout(), b)
-		case flags.OutputJSON:
-			buf, err := json.MarshalIndent(b.Config.Value().AsAny(), "", "  ")
-			if err != nil {
-				return err
-			}
-			_, _ = cmd.OutOrStdout().Write(buf)
-			_, _ = cmd.OutOrStdout().Write([]byte{'\n'})
-		default:
-			return fmt.Errorf("unknown output type %s", root.OutputType(cmd))
-		}
-
-		return nil
+		b, diags := prepareBundleForSummary(cmd, forcePull, includeLocations)
+		return renderBundle(cmd, b, diags, true)
 	}
 
 	return cmd
+}
+
+func prepareBundleForSummary(cmd *cobra.Command, forcePull, includeLocations bool) (*bundle.Bundle, diag.Diagnostics) {
+	ctx := cmd.Context()
+	b, diags := utils.ConfigureBundleWithVariables(cmd)
+	if err := diags.Error(); err != nil {
+		return nil, diags
+	}
+
+	diags = diags.Extend(phases.Initialize(ctx, b))
+	if err := diags.Error(); err != nil {
+		return nil, diags
+	}
+
+	cacheDir, err := terraform.Dir(ctx, b)
+	if err != nil {
+		return nil, diags
+	}
+	_, stateFileErr := os.Stat(filepath.Join(cacheDir, b.StateFilename()))
+	_, configFileErr := os.Stat(filepath.Join(cacheDir, terraform.TerraformConfigFileName))
+	noCache := errors.Is(stateFileErr, os.ErrNotExist) || errors.Is(configFileErr, os.ErrNotExist)
+
+	if forcePull || noCache {
+		diags = diags.Extend(bundle.ApplySeq(ctx, b,
+			statemgmt.StatePull(),
+			terraform.Interpolate(),
+			terraform.Write(),
+		))
+		if err := diags.Error(); err != nil {
+			return nil, diags
+		}
+	}
+
+	diags = diags.Extend(bundle.ApplySeq(ctx, b,
+		terraform.Load(),
+		mutator.InitializeURLs(),
+	))
+
+	// Include location information in the output if the flag is set.
+	if includeLocations {
+		diags = diags.Extend(bundle.Apply(ctx, b, mutator.PopulateLocations()))
+	}
+
+	if err := diags.Error(); err != nil {
+		return nil, diags
+	}
+
+	return b, diags
 }

commit 6d67410c2d81b763a7671e81a36e78e92f7e0f26

Fix crash in "bundle summary" (#3141)
## Changes
Fix crash in "bundle summary" when there was an error. Crash introduced
in https://github.com/databricks/cli/pull/3123 (not released yet).

## Tests
New acceptance test added in https://github.com/databricks/cli/pull/3138

acceptance/bundle/summary/missing-libraries-file-path/test.toml
@@ -1,9 +1,0 @@
-Badness = "Unexpected panic"
-
-[[Repls]]
-Old = '0x[0-9a-f]{1,}'
-New = 'HEX'
-
-[[Repls]]
-Old = '\n\s+.*\n'
-New = "\n"

cmd/bundle/validate.go
@@ -18,6 +18,9 @@ import (
 )
 
 func renderJsonOutput(cmd *cobra.Command, b *bundle.Bundle) error {
+	if b == nil {
+		return nil
+	}
 	buf, err := json.MarshalIndent(b.Config.Value().AsAny(), "", "  ")
 	if err != nil {
 		return err