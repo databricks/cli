58ce38717b773d35a23656b5997de212a5edd7d2
Andrew Nester <andrew.nester@databricks.com>
2025-07-01 11:59:19 +0200

    Added support for pipeline environment field

--- bundle/config/mutator/paths/pipeline_paths_visitor.go
+++ bundle/config/mutator/paths/pipeline_paths_visitor.go
@@ -1,12 +1,14 @@
 package paths
 
 import (
+	"github.com/databricks/cli/bundle/libraries"
 	"github.com/databricks/cli/libs/dyn"
 )
 
 type pipelineRewritePattern struct {
-	pattern dyn.Pattern
-	mode    TranslateMode
+	pattern     dyn.Pattern
+	mode        TranslateMode
+	skipRewrite func(string) bool
 }
 
 func pipelineRewritePatterns() []pipelineRewritePattern {
@@ -17,25 +19,49 @@ func pipelineRewritePatterns() []pipelineRewritePattern {
 		dyn.AnyKey(),
 	)
 
-	// Compile list of configuration paths to rewrite.
-	return []pipelineRewritePattern{
+	pipelineEnvironmentsPatterns := []pipelineRewritePattern{
 		{
-			pattern: base.Append(dyn.Key("libraries"), dyn.AnyIndex(), dyn.Key("notebook"), dyn.Key("path")),
-			mode:    TranslateModeNotebook,
-		},
-		{
-			pattern: base.Append(dyn.Key("libraries"), dyn.AnyIndex(), dyn.Key("file"), dyn.Key("path")),
-			mode:    TranslateModeFile,
-		},
-		{
-			pattern: base.Append(dyn.Key("libraries"), dyn.AnyIndex(), dyn.Key("glob"), dyn.Key("include")),
-			mode:    TranslateModeGlob,
-		},
-		{
-			pattern: base.Append(dyn.Key("root_path")),
-			mode:    TranslateModeDirectory,
+			dyn.NewPattern(
+				dyn.Key("resources"),
+				dyn.Key("pipelines"),
+				dyn.AnyKey(),
+				dyn.Key("environment"),
+				dyn.Key("dependencies"),
+				dyn.AnyIndex(),
+			),
+			TranslateModeLocalRelativeWithPrefix,
+			func(s string) bool {
+				return !libraries.IsLibraryLocal(s)
+			},
 		},
 	}
+
+	// Compile list of configuration paths to rewrite.
+	allPatterns := []pipelineRewritePattern{
+		{
+			pattern:     base.Append(dyn.Key("libraries"), dyn.AnyIndex(), dyn.Key("notebook"), dyn.Key("path")),
+			mode:        TranslateModeNotebook,
+			skipRewrite: noSkipRewrite,
+		},
+		{
+			pattern:     base.Append(dyn.Key("libraries"), dyn.AnyIndex(), dyn.Key("file"), dyn.Key("path")),
+			mode:        TranslateModeFile,
+			skipRewrite: noSkipRewrite,
+		},
+		{
+			pattern:     base.Append(dyn.Key("libraries"), dyn.AnyIndex(), dyn.Key("glob"), dyn.Key("include")),
+			mode:        TranslateModeGlob,
+			skipRewrite: noSkipRewrite,
+		},
+		{
+			pattern:     base.Append(dyn.Key("root_path")),
+			mode:        TranslateModeDirectory,
+			skipRewrite: noSkipRewrite,
+		},
+	}
+
+	allPatterns = append(allPatterns, pipelineEnvironmentsPatterns...)
+	return allPatterns
 }
 
 func VisitPipelinePaths(value dyn.Value, fn VisitFunc) (dyn.Value, error) {
@@ -44,6 +70,10 @@ func VisitPipelinePaths(value dyn.Value, fn VisitFunc) (dyn.Value, error) {
 
 	for _, rewritePattern := range pipelineRewritePatterns() {
 		newValue, err = dyn.MapByPattern(newValue, rewritePattern.pattern, func(p dyn.Path, v dyn.Value) (dyn.Value, error) {
+			if rewritePattern.skipRewrite(v.MustString()) {
+				return v, nil
+			}
+
 			return fn(p, rewritePattern.mode, v)
 		})
 		if err != nil {
