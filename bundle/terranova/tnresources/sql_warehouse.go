db9efbfa5aa79d5b8d8f223b1f2741bf60f87768
Andrew Nester <andrew.nester@databricks.com>
2025-07-02 12:55:06 +0200

    added support for direct deployment

---
+++ bundle/terranova/tnresources/sql_warehouse.go
@@ -0,0 +1,94 @@
+package tnresources
+
+import (
+	"context"
+
+	"github.com/databricks/cli/bundle/config/resources"
+	"github.com/databricks/cli/bundle/deployplan"
+	"github.com/databricks/cli/libs/structdiff"
+	"github.com/databricks/databricks-sdk-go"
+	"github.com/databricks/databricks-sdk-go/service/sql"
+)
+
+type ResourceSqlWarehouse struct {
+	client       *databricks.WorkspaceClient
+	config       sql.CreateWarehouseRequest
+	createWaiter *sql.WaitGetWarehouseRunning[sql.CreateWarehouseResponse]
+	updateWaiter *sql.WaitGetWarehouseRunning[struct{}]
+}
+
+func NewResourceSqlWarehouse(client *databricks.WorkspaceClient, resource *resources.SqlWarehouse) (*ResourceSqlWarehouse, error) {
+	return &ResourceSqlWarehouse{
+		client: client,
+		config: resource.CreateWarehouseRequest,
+	}, nil
+}
+
+func (r *ResourceSqlWarehouse) Config() any {
+	return r.config
+}
+
+func (r *ResourceSqlWarehouse) DoCreate(ctx context.Context) (string, error) {
+	waiter, err := r.client.Warehouses.Create(ctx, r.config)
+	if err != nil {
+		return "", SDKError{Method: "Warehouses.Create", Err: err}
+	}
+
+	r.createWaiter = waiter
+	return waiter.Id, nil
+}
+
+func (r *ResourceSqlWarehouse) DoUpdate(ctx context.Context, oldID string) (string, error) {
+	request := sql.EditWarehouseRequest{}
+	err := copyViaJSON(&request, r.config)
+	if err != nil {
+		return "", err
+	}
+
+	waiter, err := r.client.Warehouses.Edit(ctx, request)
+	if err != nil {
+		return "", SDKError{Method: "Warehouses.Edit", Err: err}
+	}
+	r.updateWaiter = waiter
+	return waiter.Id, nil
+}
+
+func (r *ResourceSqlWarehouse) WaitAfterCreate(ctx context.Context) error {
+	if r.createWaiter == nil {
+		return nil
+	}
+
+	_, err := r.createWaiter.Get()
+	if err != nil {
+		return err
+	}
+
+	r.createWaiter = nil
+	return nil
+}
+
+func (r *ResourceSqlWarehouse) WaitAfterUpdate(ctx context.Context) error {
+	if r.updateWaiter == nil {
+		return nil
+	}
+
+	_, err := r.updateWaiter.Get()
+	if err != nil {
+		return err
+	}
+
+	r.updateWaiter = nil
+	return nil
+}
+
+func (r *ResourceSqlWarehouse) ClassifyChanges(changes []structdiff.Change) deployplan.ActionType {
+	return deployplan.ActionTypeUpdate
+}
+
+func DeleteSqlWarehouse(ctx context.Context, client *databricks.WorkspaceClient, oldID string) error {
+	err := client.Warehouses.DeleteById(ctx, oldID)
+	if err != nil {
+		return SDKError{Method: "Warehouses.DeleteById", Err: err}
+	}
+	return nil
+}
