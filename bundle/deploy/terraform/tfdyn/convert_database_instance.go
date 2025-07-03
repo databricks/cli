b3f11b04961cbdea8f55f0d962a087bb86fbae87
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2025-07-03 18:15:32 +0200

    initial struct and converter for DatabaseInstance resource

---
+++ bundle/deploy/terraform/tfdyn/convert_database_instance.go
@@ -0,0 +1,27 @@
+package tfdyn
+
+import (
+	"context"
+	"github.com/databricks/cli/bundle/internal/tf/schema"
+	"github.com/databricks/cli/libs/dyn"
+	"github.com/databricks/cli/libs/dyn/convert"
+	"github.com/databricks/cli/libs/log"
+	"github.com/databricks/databricks-sdk-go/service/database"
+)
+
+type databaseInstanceConverter struct{}
+
+func (d databaseInstanceConverter) Convert(ctx context.Context, key string, vin dyn.Value, out *schema.Resources) error {
+	// Normalize the output value to the target schema.
+	vout, diags := convert.Normalize(database.DatabaseInstance{}, vin)
+	for _, diag := range diags {
+		log.Debugf(ctx, "database instance normalization diagnostic: %s", diag.Summary)
+	}
+	out.DatabaseInstance[key] = vout.AsAny()
+
+	return nil
+}
+
+func init() {
+	registerConverter("database_instances", databaseInstanceConverter{})
+}
