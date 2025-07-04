1f1d97f8ac7cbcbc405317daf0fbf2fabdc9777a
Denis Bilenko <denis.bilenko@databricks.com>
2025-07-04 10:24:25 +0200

    add missing context init in cmd/bundle/generate/pipeline

--- cmd/bundle/generate/pipeline.go
+++ cmd/bundle/generate/pipeline.go
@@ -12,6 +12,7 @@ import (
 	"github.com/databricks/cli/libs/cmdio"
 	"github.com/databricks/cli/libs/dyn"
 	"github.com/databricks/cli/libs/dyn/yamlsaver"
+	"github.com/databricks/cli/libs/logdiag"
 	"github.com/databricks/cli/libs/textutil"
 	"github.com/databricks/databricks-sdk-go/service/pipelines"
 	"github.com/spf13/cobra"
@@ -37,7 +38,9 @@ func NewGeneratePipelineCommand() *cobra.Command {
 	cmd.Flags().BoolVarP(&force, "force", "f", false, `Force overwrite existing files in the output directory`)
 
 	cmd.RunE = func(cmd *cobra.Command, args []string) error {
-		ctx := cmd.Context()
+		ctx := logdiag.InitContext(cmd.Context())
+		cmd.SetContext(ctx)
+
 		b := root.MustConfigureBundle(cmd)
 		if b == nil {
 			return root.ErrAlreadyPrinted
