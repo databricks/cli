45395984963618715c29ce3f22414e7d7783231c
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2025-07-09 16:17:07 +0200

    do not use default port if it is provided in extra args

--- acceptance/cmd/psql/echo-arguments.sh
+++ acceptance/cmd/psql/echo-arguments.sh
@@ -4,4 +4,6 @@
 # The test script renames this script to "psql" in order to capture the arguments that the CLI passes to psql command
 #
 echo "echo-arguments.sh was called with the following arguments: $@"
+echo "PGPASSWORD=${PGPASSWORD}"
+echo "PGSSLMODE=${PGSSLMODE}"
 exit 0
