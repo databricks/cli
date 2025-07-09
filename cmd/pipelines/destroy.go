541ab8c44abc619196292425d0e2d7ce0890228f
Alyssa Gorbaneva <alyssa.gorbaneva@databricks.com>
2025-07-09 09:45:33 -0700

    Update destroy.go

--- cmd/pipelines/destroy.go
+++ cmd/pipelines/destroy.go
@@ -1,5 +1,5 @@
 // Copied from cmd/bundle/destroy.go and adapted for pipelines use.
-// Consider if changes made there should be made to the pipelines counterpart as well.
+// Consider if changes made here should be made to the bundle counterpart as well.
 package pipelines
 
 import (
