9cc5d77fce72b6486096899285143aa42a3409d1
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2025-07-10 17:33:47 +0200

    revert bat script changes

--- acceptance/cmd/psql/echo-arguments.bat
+++ acceptance/cmd/psql/echo-arguments.bat
@@ -1,1 +1,6 @@
-echo "this is a test"
+@echo off
+
+REM This script runs psql command bypassing all its arguments to it and exits
+REM Test script renames this file to psql.bat in order to capture the arguments that the CLI passes to psql command on Windows
+
+bash "./psql" %*
