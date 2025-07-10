c161d8e56491272ba8a73b8e1f2153437e7edd64
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2025-07-10 16:05:43 +0200

    call ./psql in local folder instead of doing PATH lookup again

--- acceptance/cmd/psql/echo-arguments.bat
+++ acceptance/cmd/psql/echo-arguments.bat
@@ -3,4 +3,4 @@
 REM This script runs psql command bypassing all its arguments to it and exits
 REM Test script renames this file to psql.bat in order to capture the arguments that the CLI passes to psql command on Windows
 
-bash "psql" %*
+bash "./psql" %*
