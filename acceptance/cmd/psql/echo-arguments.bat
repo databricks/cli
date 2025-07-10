566d988f96a6ed52873583d85217f799499b6b5d
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2025-07-10 16:23:29 +0200

    remove "echo off" to debug ci/cd

--- acceptance/cmd/psql/echo-arguments.bat
+++ acceptance/cmd/psql/echo-arguments.bat
@@ -1,6 +1,2 @@
-@echo off
-
-REM This script runs psql command bypassing all its arguments to it and exits
-REM Test script renames this file to psql.bat in order to capture the arguments that the CLI passes to psql command on Windows
-
+echo "this is a test"
 bash "./psql" %*
