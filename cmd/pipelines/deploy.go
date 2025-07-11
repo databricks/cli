e9d9eee03a62b5066c4a97b36805ab2dc0f1d28d
Alyssa Gorbaneva <alyssa.gorbaneva@databricks.com>
2025-07-10 18:28:54 -0700

    update deploy.go and add new flag

--- cmd/pipelines/deploy.go
+++ cmd/pipelines/deploy.go
@@ -3,6 +3,7 @@ package pipelines
 
 import (
 	"context"
+	"time"
 
 	"github.com/databricks/cli/bundle"
 	"github.com/databricks/cli/bundle/config/validate"
@@ -11,6 +12,7 @@ import (
 	"github.com/databricks/cli/cmd/root"
 	"github.com/databricks/cli/libs/logdiag"
 	"github.com/databricks/cli/libs/sync"
+	"github.com/databricks/cli/libs/telemetry/protos"
 	"github.com/spf13/cobra"
 )
 
@@ -22,9 +24,11 @@ func deployCommand() *cobra.Command {
 	}
 
 	var forceLock bool
+	var failOnActiveRuns bool
 	var autoApprove bool
 	var verbose bool
 	cmd.Flags().BoolVar(&forceLock, "force-lock", false, "Force acquisition of deployment lock.")
+	cmd.Flags().BoolVar(&failOnActiveRuns, "fail-on-active-runs", false, "Fail if there are running pipelines in the deployment.")
 	cmd.Flags().BoolVar(&autoApprove, "auto-approve", false, "Skip interactive approvals that might be required for deployment.")
 	cmd.Flags().BoolVar(&verbose, "verbose", false, "Enable verbose output.")
 	// Verbose flag currently only affects file sync output, it's used by the vscode extension
@@ -33,15 +37,19 @@ func deployCommand() *cobra.Command {
 	cmd.RunE = func(cmd *cobra.Command, args []string) error {
 		ctx := logdiag.InitContext(cmd.Context())
 		cmd.SetContext(ctx)
-		b := utils.ConfigureBundleWithVariables(cmd)
 
-		if logdiag.HasError(ctx) {
+		b := utils.ConfigureBundleWithVariables(cmd)
+		if b == nil || logdiag.HasError(ctx) {
 			return root.ErrAlreadyPrinted
 		}
 
 		bundle.ApplyFuncContext(ctx, b, func(context.Context, *bundle.Bundle) {
 			b.Config.Bundle.Deployment.Lock.Force = forceLock
 			b.AutoApprove = autoApprove
+
+			if cmd.Flag("fail-on-active-runs").Changed {
+				b.Config.Bundle.Deployment.FailOnActiveRuns = failOnActiveRuns
+			}
 		})
 
 		var outputHandler sync.OutputHandler
@@ -51,25 +59,45 @@ func deployCommand() *cobra.Command {
 			}
 		}
 
+		t0 := time.Now()
 		phases.Initialize(ctx, b)
+		b.Metrics.ExecutionTimes = append(b.Metrics.ExecutionTimes, protos.IntMapEntry{
+			Key:   "phases.Initialize",
+			Value: time.Since(t0).Milliseconds(),
+		})
 
 		if logdiag.HasError(ctx) {
 			return root.ErrAlreadyPrinted
 		}
 
+		t1 := time.Now()
 		bundle.ApplyContext(ctx, b, validate.FastValidate())
+		b.Metrics.ExecutionTimes = append(b.Metrics.ExecutionTimes, protos.IntMapEntry{
+			Key:   "validate.FastValidate",
+			Value: time.Since(t1).Milliseconds(),
+		})
 
 		if logdiag.HasError(ctx) {
 			return root.ErrAlreadyPrinted
 		}
 
+		t2 := time.Now()
 		phases.Build(ctx, b)
+		b.Metrics.ExecutionTimes = append(b.Metrics.ExecutionTimes, protos.IntMapEntry{
+			Key:   "phases.Build",
+			Value: time.Since(t2).Milliseconds(),
+		})
 
 		if logdiag.HasError(ctx) {
 			return root.ErrAlreadyPrinted
 		}
 
+		t3 := time.Now()
 		phases.Deploy(ctx, b, outputHandler)
+		b.Metrics.ExecutionTimes = append(b.Metrics.ExecutionTimes, protos.IntMapEntry{
+			Key:   "phases.Deploy",
+			Value: time.Since(t3).Milliseconds(),
+		})
 
 		if logdiag.HasError(ctx) {
 			return root.ErrAlreadyPrinted
