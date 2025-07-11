2d8e3cc35c70a6ae9921a5860c0aa1b99d485e43
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2025-07-11 17:19:40 +0200

    set echo-arguments.sh as executable

--- acceptance/cmd/psql/echo-arguments.sh
+++ acceptance/cmd/psql/echo-arguments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-# This script prints its arguments and exits
-# The test script renames this script to "psql" in order to capture the arguments that the CLI passes to psql command
+# This script prints its arguments and exits.
+# The test script renames this script to "psql" in order to capture the arguments that the CLI passes to psql command.
 #
 echo "echo-arguments.sh was called with the following arguments: $@"
 echo "PGPASSWORD=${PGPASSWORD}"
