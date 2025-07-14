dea8c27cfbff0e091ffb6af7bac5b8b873f5dac5
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2025-07-14 16:25:58 +0200

    print $(pwd); replace .bat contents with hello world program

--- acceptance/cmd/psql/psql.bat
+++ acceptance/cmd/psql/psql.bat
@@ -1,6 +1,8 @@
-@echo off
+@echo on
+
+echo "Hello, World!"
 
 REM This script runs psql command bypassing all its arguments to it and exits
 REM Test script renames this file to psql.bat in order to capture the arguments that the CLI passes to psql command on Windows
 
-bash "./psql.sh" %*
+REM bash "./psql.sh" %*
