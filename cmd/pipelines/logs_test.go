0d7a1507f063792a622f667363589fb35edf9d46
Alyssa Gorbaneva <alyssa.gorbaneva@databricks.com>
2025-07-17 16:10:10 -0700

    renaming

--- cmd/pipelines/logs_test.go
+++ cmd/pipelines/logs_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestBuildInFilter(t *testing.T) {
+func TestBuildFieldFilter(t *testing.T) {
 	tests := []struct {
 		name     string
 		field    string
@@ -41,7 +41,7 @@ func TestBuildInFilter(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			result := buildInFilter(tt.field, tt.values)
+			result := buildFieldFilter(tt.field, tt.values)
 			assert.Equal(t, tt.expected, result)
 		})
 	}
