f7b8157dee219f641f7f5a6bbf3fc22e6d16ce2f
Denis Bilenko <denis.bilenko@databricks.com>
2025-07-30 14:55:54 +0200

    Add hidden "bundle plan" command

---
+++ cmd/bundle/plan.go
@@ -0,0 +1,91 @@
+package bundle
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/databricks/cli/bundle"
+	"github.com/databricks/cli/bundle/config/validate"
+	"github.com/databricks/cli/bundle/phases"
+	"github.com/databricks/cli/cmd/bundle/utils"
+	"github.com/databricks/cli/cmd/root"
+	"github.com/databricks/cli/libs/cmdio"
+	"github.com/databricks/cli/libs/logdiag"
+	"github.com/spf13/cobra"
+)
+
+func newPlanCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "plan",
+		Short: "Show deployment plan",
+		Args:  root.NoArgs,
+
+		// Output format may change without notice; main use case is in acceptance tests.
+		Hidden: true,
+	}
+
+	var force bool
+	var forceLock bool
+	var clusterId string
+	var autoApprove bool
+	cmd.Flags().BoolVar(&force, "force", false, "Force-override Git branch validation.")
+	cmd.Flags().StringVar(&clusterId, "compute-id", "", "Override cluster in the deployment with the given compute ID.")
+	cmd.Flags().StringVarP(&clusterId, "cluster-id", "c", "", "Override cluster in the deployment with the given cluster ID.")
+	cmd.Flags().MarkDeprecated("compute-id", "use --cluster-id instead")
+
+	cmd.RunE = func(cmd *cobra.Command, args []string) error {
+		ctx := logdiag.InitContext(cmd.Context())
+		cmd.SetContext(ctx)
+
+		b := utils.ConfigureBundleWithVariables(cmd)
+		if b == nil || logdiag.HasError(ctx) {
+			return root.ErrAlreadyPrinted
+		}
+
+		bundle.ApplyFuncContext(ctx, b, func(context.Context, *bundle.Bundle) {
+			b.Config.Bundle.Force = force
+			b.Config.Bundle.Deployment.Lock.Force = forceLock
+			b.AutoApprove = autoApprove
+
+			if cmd.Flag("compute-id").Changed {
+				b.Config.Bundle.ClusterId = clusterId
+			}
+
+			if cmd.Flag("cluster-id").Changed {
+				b.Config.Bundle.ClusterId = clusterId
+			}
+		})
+
+		phases.Initialize(ctx, b)
+
+		if logdiag.HasError(ctx) {
+			return root.ErrAlreadyPrinted
+		}
+
+		bundle.ApplyContext(ctx, b, validate.FastValidate())
+
+		if logdiag.HasError(ctx) {
+			return root.ErrAlreadyPrinted
+		}
+
+		phases.Build(ctx, b)
+
+		if logdiag.HasError(ctx) {
+			return root.ErrAlreadyPrinted
+		}
+
+		changes := phases.Diff(ctx, b)
+
+		for _, change := range changes {
+			cmdio.LogString(ctx, fmt.Sprintf("%s %s.%s", change.ActionType, change.Group, change.Name))
+		}
+
+		if logdiag.HasError(ctx) {
+			return root.ErrAlreadyPrinted
+		}
+
+		return nil
+	}
+
+	return cmd
+}
