27ebea46dee2e2dbbacdc4a82c8cf56e7eeb0b6c
Alyssa Gorbaneva <alyssa.gorbaneva@databricks.com>
2025-08-05 11:07:02 -0700

    remove cancelled

--- cmd/pipelines/run.go
+++ cmd/pipelines/run.go
@@ -175,9 +175,6 @@ Catalog & Schema: {{ .Update.Config.Catalog }}{{ if and .Update.Config.Catalog .
 {{- end }}
 `
 
-const pipelineCanceledTemplate = `Update {{ .Update.UpdateId }} for pipeline{{- if .Update.Config }} {{ .Update.Config.Name }}{{ end }}{{- if .Update.Config }} {{ .Update.Config.Id }}{{ end }} was canceled.
-`
-
 func getRefreshSelectionString(update pipelines.UpdateInfo) string {
 	if update.FullRefresh {
 		return "full-refresh-all"
@@ -283,13 +280,6 @@ func fetchAndDisplayPipelineUpdate(ctx context.Context, bundle *bundle.Bundle, r
 	if latestUpdate.State == pipelines.UpdateInfoStateCompleted {
 >>>>>>> ee0f99139 (endpoint)
 		err = displayPipelineUpdate(ctx, latestUpdate, pipelineID, events)
-		if err != nil {
-			return err
-		}
-	}
-
-	if latestUpdate.State == pipelines.UpdateInfoStateCanceled {
-		err = displayCanceled(ctx, latestUpdate, pipelineID)
 		if err != nil {
 			return err
 		}
@@ -419,9 +409,6 @@ func displayPipelineUpdate(ctx context.Context, update pipelines.UpdateInfo, pip
 	return cmdio.RenderWithTemplate(ctx, data, "", pipelineUpdateTemplate)
 }
 
-<<<<<<< HEAD
->>>>>>> a44303fd9 (removed templates)
-=======
 func displayCanceled(ctx context.Context, update pipelines.UpdateInfo, pipelineID string) error {
 	data := PipelineUpdateData{
 		PipelineId: pipelineID,
