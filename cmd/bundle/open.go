databricks/cli

commit 45821265a906bbc10b7f38f5c81580ac9e624757

Log diagnostics immediately after mutator ends (#3175)
## Changes
- New package: logdiag with
- logdiag.LogDiag(ctx, d): render and logs Diagnostic object on stderr
right away. Counts diagnostic type (error, warning, recommendation) on
the context variable.
- logdiag.HasError(ctx) — return true if there were any errors seen so
far.
- New functions bundle.ApplyContext/ApplySeqContext (to replace
bundle.Apply/ApplySeq). These functions do not return diagnostics, but
calls LogDiag on every diagnostic returned by the mutator.

bundle.Apply and bundle.ApplySeq are still available as wrappers around
Context counterparts. The main use case for these is unit tests.

Note, with this PR mutators may choose whether to use LogDiag to log
diagnostics right away or to return it as usual. This has exactly the
same effect (except if you return it it is delayed a bit). For new code,
LogDiag should be preferred, eventually mutator interface will be
modified not to return diagnostics. Note, even if you use LogDiag,
bundle.Apply will still return the diagnostics logged.

Other behavior changes:
- diagnostics are now always logged to stderr; previously "bundle
summary" non-json would print them to stdout.
- slashes in the filenames in diagnostics are normalized to have forward
slashes, to help with acceptance tests.
- summary command won't print recommendations (but it'll keep on
printing warnings)

## Why
Collection of diagnostics and printing them at the very end is poor user
and developer experience.

User experience:
 - Users will miss diagnostics if process crashes/panics; 
- For long running deployments, useful information is not printed right
away;
- For deploys with interactive approval, diagnostics may show issues
that should be fixed before the deployment. Since they not shown, users
cannot make informed choice.

Developer experience:
 - High accidental complexity to propagate the diagnostics properly.
- Various bugs over time where diagnostics were lost due to logic or

cmd/bundle/open.go
@@ -16,6 +16,7 @@ import (
 	"github.com/databricks/cli/cmd/bundle/utils"
 	"github.com/databricks/cli/cmd/root"
 	"github.com/databricks/cli/libs/cmdio"
+	"github.com/databricks/cli/libs/logdiag"
 	"github.com/spf13/cobra"
 	"golang.org/x/exp/maps"
 
@@ -62,15 +63,17 @@ func newOpenCommand() *cobra.Command {
 	cmd.Flags().BoolVar(&forcePull, "force-pull", false, "Skip local cache and load the state from the remote workspace")
 
 	cmd.RunE = func(cmd *cobra.Command, args []string) error {
-		ctx := cmd.Context()
-		b, diags := utils.ConfigureBundleWithVariables(cmd)
-		if err := diags.Error(); err != nil {
-			return diags.Error()
+		ctx := logdiag.InitContext(cmd.Context())
+		cmd.SetContext(ctx)
+
+		b := utils.ConfigureBundleWithVariables(cmd)
+		if b == nil || logdiag.HasError(ctx) {
+			return root.ErrAlreadyPrinted
 		}
 
-		diags = phases.Initialize(ctx, b)
-		if err := diags.Error(); err != nil {
-			return err
+		phases.Initialize(ctx, b)
+		if logdiag.HasError(ctx) {
+			return root.ErrAlreadyPrinted
 		}
 
 		arg, err := resolveOpenArgument(ctx, b, args)
@@ -87,29 +90,29 @@ func newOpenCommand() *cobra.Command {
 		noCache := errors.Is(stateFileErr, os.ErrNotExist) || errors.Is(configFileErr, os.ErrNotExist)
 
 		if forcePull || noCache {
-			diags = bundle.Apply(ctx, b, statemgmt.StatePull())
-			if err = diags.Error(); err != nil {
-				return err
+			bundle.ApplyContext(ctx, b, statemgmt.StatePull())
+			if logdiag.HasError(ctx) {
+				return root.ErrAlreadyPrinted
 			}
 
 			if !b.DirectDeployment {
-				diags = bundle.ApplySeq(ctx, b,
+				bundle.ApplySeqContext(ctx, b,
 					terraform.Interpolate(),
 					terraform.Write(),
 				)
+			}
 
-				if err = diags.Error(); err != nil {
-					return err
-				}
+			if logdiag.HasError(ctx) {
+				return root.ErrAlreadyPrinted
 			}
 		}
 
-		diags = bundle.ApplySeq(ctx, b,
+		bundle.ApplySeqContext(ctx, b,
 			statemgmt.Load(),
 			mutator.InitializeURLs(),
 		)
-		if err := diags.Error(); err != nil {
-			return err
+		if logdiag.HasError(ctx) {
+			return root.ErrAlreadyPrinted
 		}
 
 		// Locate resource to open.
@@ -129,9 +132,8 @@ func newOpenCommand() *cobra.Command {
 	}
 
 	cmd.ValidArgsFunction = func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
-		b, diags := root.MustConfigureBundle(cmd)
-		if err := diags.Error(); err != nil {
-			cobra.CompErrorln(err.Error())
+		b := root.MustConfigureBundle(cmd)
+		if logdiag.HasError(cmd.Context()) {
 			return nil, cobra.ShellCompDirectiveError
 		}
 

cmd/bundle/run.go
@@ -22,6 +22,7 @@ import (
 	"github.com/databricks/cli/libs/cmdio"
 	"github.com/databricks/cli/libs/exec"
 	"github.com/databricks/cli/libs/flags"
+	"github.com/databricks/cli/libs/logdiag"
 	"github.com/spf13/cobra"
 	"golang.org/x/exp/maps"
 )
@@ -126,10 +127,12 @@ Example usage:
 	cmd.Flags().BoolVar(&restart, "restart", false, "Restart the run if it is already running.")
 
 	cmd.RunE = func(cmd *cobra.Command, args []string) error {
-		ctx := cmd.Context()
-		b, diags := utils.ConfigureBundleWithVariables(cmd)
-		if diags.HasError() {
-			return renderDiagnostics(cmd.OutOrStdout(), b, diags)
+		ctx := logdiag.InitContext(cmd.Context())
+		cmd.SetContext(ctx)
+
+		b := utils.ConfigureBundleWithVariables(cmd)
+		if b == nil || logdiag.HasError(ctx) {
+			return root.ErrAlreadyPrinted
 		}
 
 		// If user runs the bundle run command as:
@@ -139,9 +142,9 @@ Example usage:
 			return executeInline(cmd, args, b)
 		}
 
-		diags = diags.Extend(phases.Initialize(ctx, b))
-		if diags.HasError() {
-			return renderDiagnostics(cmd.OutOrStdout(), b, diags)
+		phases.Initialize(ctx, b)
+		if logdiag.HasError(ctx) {
+			return root.ErrAlreadyPrinted
 		}
 
 		key, args, err := resolveRunArgument(ctx, b, args)
@@ -150,21 +153,21 @@ Example usage:
 		}
 
 		if !b.DirectDeployment {
-			diags = diags.Extend(bundle.ApplySeq(ctx, b,
+			bundle.ApplySeqContext(ctx, b,
 				terraform.Interpolate(),
 				terraform.Write(),
-			))
-			if diags.HasError() {
-				return renderDiagnostics(cmd.OutOrStdout(), b, diags)
+			)
+			if logdiag.HasError(ctx) {
+				return root.ErrAlreadyPrinted
 			}
 		}
 
-		diags = diags.Extend(bundle.ApplySeq(ctx, b,
+		bundle.ApplySeqContext(ctx, b,
 			statemgmt.StatePull(),
 			statemgmt.Load(statemgmt.ErrorOnEmptyState),
-		))
-		if diags.HasError() {
-			return renderDiagnostics(cmd.OutOrStdout(), b, diags)
+		)
+		if logdiag.HasError(ctx) {
+			return root.ErrAlreadyPrinted
 		}
 
 		runner, err := keyToRunner(b, key)
@@ -218,9 +221,8 @@ Example usage:
 	}
 
 	cmd.ValidArgsFunction = func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
-		b, diags := root.MustConfigureBundle(cmd)
-		if err := diags.Error(); err != nil {
-			cobra.CompErrorln(err.Error())
+		b := root.MustConfigureBundle(cmd)
+		if logdiag.HasError(cmd.Context()) {
 			return nil, cobra.ShellCompDirectiveError
 		}
 

commit 9ba21868c587265ad0d43310db52e0dd30854a5e

Fix panic in auto-completion handler for bundle run/open (#3358)
## Why
Regression introduced in https://github.com/databricks/cli/pull/3175 /
version 0.259.0.

Fixes #3338

## Tests
New acceptance tests that trigger completion handler for bundle/open.

acceptance/bundle/open/script
@@ -11,3 +11,6 @@ export PATH=.:$PATH
 
 title "open after deployment. This will fail to open browser and complain, that's ok, we only want the message"
 musterr trace $CLI bundle open foo
+
+title "test auto-completion handler"
+trace $CLI __complete bundle open ,

acceptance/bundle/run/basic/script
@@ -14,3 +14,6 @@ errcode trace $CLI bundle run foo -- arg1 arg2
 
 title "inline script"
 errcode trace $CLI bundle run -- echo "hello"
+
+title "completion handler"
+trace $CLI __complete bundle run ,

cmd/bundle/open.go
@@ -134,6 +134,9 @@ func newOpenCommand() *cobra.Command {
 	}
 
 	cmd.ValidArgsFunction = func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		ctx := logdiag.InitContext(cmd.Context())
+		cmd.SetContext(ctx)
+
 		b := root.MustConfigureBundle(cmd)
 		if logdiag.HasError(cmd.Context()) {
 			return nil, cobra.ShellCompDirectiveError

cmd/bundle/run.go
@@ -225,6 +225,9 @@ Example usage:
 	}
 
 	cmd.ValidArgsFunction = func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		ctx := logdiag.InitContext(cmd.Context())
+		cmd.SetContext(ctx)
+
 		b := root.MustConfigureBundle(cmd)
 		if logdiag.HasError(cmd.Context()) {
 			return nil, cobra.ShellCompDirectiveError