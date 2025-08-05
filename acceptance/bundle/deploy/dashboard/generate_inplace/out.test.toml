databricks/cli

commit 45821265a906bbc10b7f38f5c81580ac9e624757

Log diagnostics immediately after mutator ends (#3175)
## Changes
- New package: logdiag with
- logdiag.LogDiag(ctx, d): render and logs Diagnostic object on stderr
right away. Counts diagnostic type (error, warning, recommendation) on
the context variable.
- logdiag.HasError(ctx) — return true if there were any errors seen so
far.
- New functions bundle.ApplyContext/ApplySeqContext (to replace
bundle.Apply/ApplySeq). These functions do not return diagnostics, but
calls LogDiag on every diagnostic returned by the mutator.

bundle.Apply and bundle.ApplySeq are still available as wrappers around
Context counterparts. The main use case for these is unit tests.

Note, with this PR mutators may choose whether to use LogDiag to log
diagnostics right away or to return it as usual. This has exactly the
same effect (except if you return it it is delayed a bit). For new code,
LogDiag should be preferred, eventually mutator interface will be
modified not to return diagnostics. Note, even if you use LogDiag,
bundle.Apply will still return the diagnostics logged.

Other behavior changes:
- diagnostics are now always logged to stderr; previously "bundle
summary" non-json would print them to stdout.
- slashes in the filenames in diagnostics are normalized to have forward
slashes, to help with acceptance tests.
- summary command won't print recommendations (but it'll keep on
printing warnings)

## Why
Collection of diagnostics and printing them at the very end is poor user
and developer experience.

User experience:
 - Users will miss diagnostics if process crashes/panics; 
- For long running deployments, useful information is not printed right
away;
- For deploys with interactive approval, diagnostics may show issues
that should be fixed before the deployment. Since they not shown, users
cannot make informed choice.

Developer experience:
 - High accidental complexity to propagate the diagnostics properly.
- Various bugs over time where diagnostics were lost due to logic or

cmd/bundle/generate/dashboard.go
@@ -16,13 +16,13 @@ import (
 	"github.com/databricks/cli/bundle/deploy/terraform"
 	"github.com/databricks/cli/bundle/generate"
 	"github.com/databricks/cli/bundle/phases"
-	"github.com/databricks/cli/bundle/render"
 	"github.com/databricks/cli/bundle/resources"
 	"github.com/databricks/cli/bundle/statemgmt"
 	"github.com/databricks/cli/cmd/root"
 	"github.com/databricks/cli/libs/diag"
 	"github.com/databricks/cli/libs/dyn"
 	"github.com/databricks/cli/libs/dyn/yamlsaver"
+	"github.com/databricks/cli/libs/logdiag"
 	"github.com/databricks/cli/libs/textutil"
 	"github.com/databricks/databricks-sdk-go"
 	"github.com/databricks/databricks-sdk-go/apierr"
@@ -55,7 +55,7 @@ type dashboard struct {
 	relativeDashboardDir string
 }
 
-func (d *dashboard) resolveID(ctx context.Context, b *bundle.Bundle) (string, diag.Diagnostics) {
+func (d *dashboard) resolveID(ctx context.Context, b *bundle.Bundle) string {
 	switch {
 	case d.existingPath != "":
 		return d.resolveFromPath(ctx, b)
@@ -63,68 +63,70 @@ func (d *dashboard) resolveID(ctx context.Context, b *bundle.Bundle) (string, di
 		return d.resolveFromID(ctx, b)
 	}
 
-	return "", diag.Errorf("expected one of --dashboard-path, --dashboard-id")
+	logdiag.LogError(ctx, errors.New("expected one of --dashboard-path, --dashboard-id"))
+	return ""
 }
 
-func (d *dashboard) resolveFromPath(ctx context.Context, b *bundle.Bundle) (string, diag.Diagnostics) {
+func (d *dashboard) resolveFromPath(ctx context.Context, b *bundle.Bundle) string {
 	w := b.WorkspaceClient()
 	obj, err := w.Workspace.GetStatusByPath(ctx, d.existingPath)
 	if err != nil {
 		if apierr.IsMissing(err) {
-			return "", diag.Errorf("dashboard %q not found", path.Base(d.existingPath))
+			logdiag.LogError(ctx, fmt.Errorf("dashboard %q not found", path.Base(d.existingPath)))
+			return ""
 		}
 
 		// Emit a more descriptive error message for legacy dashboards.
 		if errors.Is(err, apierr.ErrBadRequest) && strings.HasPrefix(err.Error(), "dbsqlDashboard ") {
-			return "", diag.Diagnostics{
-				{
-					Severity: diag.Error,
-					Summary:  fmt.Sprintf("dashboard %q is a legacy dashboard", path.Base(d.existingPath)),
-					Detail: "" +
-						"Databricks Asset Bundles work exclusively with AI/BI dashboards.\n" +
-						"\n" +
-						"Instructions on how to convert a legacy dashboard to an AI/BI dashboard\n" +
-						"can be found at: https://docs.databricks.com/en/dashboards/clone-legacy-to-aibi.html.",
-				},
-			}
+			logdiag.LogDiag(ctx, diag.Diagnostic{
+				Severity: diag.Error,
+				Summary:  fmt.Sprintf("dashboard %q is a legacy dashboard", path.Base(d.existingPath)),
+				Detail: "" +
+					"Databricks Asset Bundles work exclusively with AI/BI dashboards.\n" +
+					"\n" +
+					"Instructions on how to convert a legacy dashboard to an AI/BI dashboard\n" +
+					"can be found at: https://docs.databricks.com/en/dashboards/clone-legacy-to-aibi.html.",
+			})
+			return ""
 		}
 
-		return "", diag.FromErr(err)
+		logdiag.LogError(ctx, err)
+		return ""
 	}
 
 	if obj.ObjectType != workspace.ObjectTypeDashboard {
 		found := strings.ToLower(obj.ObjectType.String())
-		return "", diag.Diagnostics{
-			{
-				Severity: diag.Error,
-				Summary:  "expected a dashboard, found a " + found,
-			},
-		}
+		logdiag.LogDiag(ctx, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "expected a dashboard, found a " + found,
+		})
+		return ""
 	}
 
 	if obj.ResourceId == "" {
-		return "", diag.Diagnostics{
-			{
-				Severity: diag.Error,
-				Summary:  "expected a non-empty dashboard resource ID",
-			},
-		}
+		logdiag.LogDiag(ctx, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "expected a non-empty dashboard resource ID",
+		})
+		return ""
 	}
 
-	return obj.ResourceId, nil
+	return obj.ResourceId
 }
 
-func (d *dashboard) resolveFromID(ctx context.Context, b *bundle.Bundle) (string, diag.Diagnostics) {
+func (d *dashboard) resolveFromID(ctx context.Context, b *bundle.Bundle) string {
 	w := b.WorkspaceClient()
 	obj, err := w.Lakeview.GetByDashboardId(ctx, d.existingID)
 	if err != nil {
 		if apierr.IsMissing(err) {
-			return "", diag.Errorf("dashboard with ID %s not found", d.existingID)
+			logdiag.LogError(ctx, fmt.Errorf("dashboard with ID %s not found", d.existingID))
+			return ""
 		}
-		return "", diag.FromErr(err)
+		logdiag.LogError(ctx, err)
+		return ""
 	}
 
-	return obj.DashboardId, nil
+	return obj.DashboardId
 }
 
 func remarshalJSON(data []byte) ([]byte, error) {
@@ -310,23 +312,22 @@ func (d *dashboard) updateDashboardForResource(ctx context.Context, b *bundle.Bu
 	}
 }
 
-func (d *dashboard) generateForExisting(ctx context.Context, b *bundle.Bundle, dashboardID string) diag.Diagnostics {
+func (d *dashboard) generateForExisting(ctx context.Context, b *bundle.Bundle, dashboardID string) {
 	w := b.WorkspaceClient()
 	dashboard, err := w.Lakeview.GetByDashboardId(ctx, dashboardID)
 	if err != nil {
-		return diag.FromErr(err)
+		logdiag.LogError(ctx, err)
+		return
 	}
 
 	key := textutil.NormalizeString(dashboard.DisplayName)
 	err = d.saveConfiguration(ctx, b, dashboard, key)
 	if err != nil {
-		return diag.FromErr(err)
+		logdiag.LogError(ctx, err)
 	}
-
-	return nil
 }
 
-func (d *dashboard) initialize(b *bundle.Bundle) diag.Diagnostics {
+func (d *dashboard) initialize(ctx context.Context, b *bundle.Bundle) {
 	// Make the paths absolute if they aren't already.
 	if !filepath.IsAbs(d.resourceDir) {
 		d.resourceDir = filepath.Join(b.BundleRootPath, d.resourceDir)
@@ -338,67 +339,63 @@ func (d *dashboard) initialize(b *bundle.Bundle) diag.Diagnostics {
 	// Make sure we know how the dashboard path is relative to the resource path.
 	rel, err := filepath.Rel(d.resourceDir, d.dashboardDir)
 	if err != nil {
-		return diag.FromErr(err)
+		logdiag.LogError(ctx, err)
+		return
 	}
 
 	d.relativeDashboardDir = filepath.ToSlash(rel)
-	return nil
 }
 
-func (d *dashboard) runForResource(ctx context.Context, b *bundle.Bundle) diag.Diagnostics {
-	diags := phases.Initialize(ctx, b)
-	if diags.HasError() {
-		return diags
+func (d *dashboard) runForResource(ctx context.Context, b *bundle.Bundle) {
+	phases.Initialize(ctx, b)
+	if logdiag.HasError(ctx) {
+		return
 	}
 
-	diags = diags.Extend(bundle.ApplySeq(ctx, b,
+	bundle.ApplySeqContext(ctx, b,
 		terraform.Interpolate(),
 		terraform.Write(),
 		statemgmt.StatePull(),
 		statemgmt.Load(),
-	))
-	if diags.HasError() {
-		return diags
+	)
+	if logdiag.HasError(ctx) {
+		return
 	}
 
-	return d.updateDashboardForResource(ctx, b)
+	d.updateDashboardForResource(ctx, b)
 }
 
-func (d *dashboard) runForExisting(ctx context.Context, b *bundle.Bundle) diag.Diagnostics {
+func (d *dashboard) runForExisting(ctx context.Context, b *bundle.Bundle) {
 	// Resolve the ID of the dashboard to generate configuration for.
-	dashboardID, diags := d.resolveID(ctx, b)
-	if diags.HasError() {
-		return diags
+	dashboardID := d.resolveID(ctx, b)
+	if logdiag.HasError(ctx) {
+		return
 	}
 
-	return d.generateForExisting(ctx, b, dashboardID)
+	d.generateForExisting(ctx, b, dashboardID)
 }
 
 func (d *dashboard) RunE(cmd *cobra.Command, args []string) error {
-	ctx := cmd.Context()
-	b, diags := root.MustConfigureBundle(cmd)
-	if diags.HasError() {
-		return diags.Error()
+	ctx := logdiag.InitContext(cmd.Context())
+	cmd.SetContext(ctx)
+
+	b := root.MustConfigureBundle(cmd)
+	if b == nil {
+		return root.ErrAlreadyPrinted
 	}
 
-	diags = d.initialize(b)
-	if diags.HasError() {
-		return diags.Error()
+	d.initialize(ctx, b)
+	if logdiag.HasError(ctx) {
+		return root.ErrAlreadyPrinted
 	}
 
 	if d.resource != "" {
-		diags = d.runForResource(ctx, b)
+		d.runForResource(ctx, b)
 	} else {
-		diags = d.runForExisting(ctx, b)
+		d.runForExisting(ctx, b)
 	}
 
-	renderOpts := render.RenderOptions{RenderSummaryTable: false}
-	err := render.RenderDiagnostics(cmd.OutOrStdout(), b, diags, renderOpts)
-	if err != nil {
-		return fmt.Errorf("failed to render output: %w", err)
-	}
-
-	if diags.HasError() {
+	if logdiag.HasError(ctx) {
 		return root.ErrAlreadyPrinted
 	}
 
@@ -412,9 +409,8 @@ func filterDashboards(ref resources.Reference) bool {
 
 // dashboardResourceCompletion executes to autocomplete the argument to the resource flag.
 func dashboardResourceCompletion(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
-	b, diags := root.MustConfigureBundle(cmd)
-	if err := diags.Error(); err != nil {
-		cobra.CompErrorln(err.Error())
+	b := root.MustConfigureBundle(cmd)
+	if logdiag.HasError(cmd.Context()) {
 		return nil, cobra.ShellCompDirectiveError
 	}
 

commit 280e218a1e365169a13c45d131b579226c53d51b

Fix error propogation in the `bundle generate dashboard` command (#3354)
## Changes
Fixes a regression introduced in
https://github.com/databricks/cli/pull/3175, where the errors in these
functions would be dropped instead of being propagated. I noticed this
regression while updating the CLI dashboard.

This also highlights the downside returning diagnostics and using
`logdiag` to log it later, outside mutators, since a linter would have
caught it otherwise.

## Tests
Added regression test. Also manually:

Before:
```
➜  cli_telemetry_dashboard git:(main) databricks bundle generate dashboard --resource cli_telemetry -p logfood        
➜  cli_telemetry_dashboard git:(main) git status                     
On branch main
Your branch is up to date with 'origin/main'.
```

After:
```
➜  cli_telemetry_dashboard git:(main) cli bundle generate dashboard --resource cli_telemetry -p logfood        
Error: src/cli_telemetry.lvdash.json already exists. Use --force to overwrite
```

Ideally, we would not use logdiag here and instead refactor these and
other functions like these to return errors. It's out of scope for me
for now.

acceptance/bundle/deploy/dashboard/generate_inplace/databricks.yml
@@ -1,0 +1,9 @@
+bundle:
+  name: dashboard-replace-$UNIQUE_NAME
+
+resources:
+  dashboards:
+    already_exists:
+      file_path: ./dashboard.lvdash.json
+      display_name: Already Exists
+      warehouse_id: $TEST_DEFAULT_WAREHOUSE_ID

acceptance/bundle/deploy/dashboard/generate_inplace/script
@@ -1,0 +1,28 @@
+envsubst < databricks.yml > databricks.yml.subst
+mv databricks.yml.subst databricks.yml
+
+# Deploy bundle once to create the dashboard.
+trace $CLI bundle deploy
+
+cleanup() {
+    trace $CLI bundle destroy --auto-approve
+}
+trap cleanup EXIT
+
+dashboard_id=$($CLI bundle summary -o json | jq .resources.dashboards.already_exists.id -r)
+
+title "Get current dashboard\n"
+$CLI lakeview get $dashboard_id | jq .serialized_dashboard
+
+title "Update dashboard\n"
+$CLI lakeview update $dashboard_id --display-name "Updated dashboard" | jq .serialized_dashboard
+
+# This should error.
+errcode trace $CLI bundle generate dashboard --resource already_exists
+
+trace cat dashboard.lvdash.json
+
+# this should successfully update the dashboard.
+trace $CLI bundle generate dashboard --resource already_exists --force
+
+trace cat dashboard.lvdash.json

acceptance/bundle/deploy/dashboard/generate_inplace/test.toml
@@ -1,0 +1,2 @@
+Cloud = true
+Local = false

cmd/bundle/generate/dashboard.go
@@ -238,17 +238,19 @@ func (d *dashboard) saveConfiguration(ctx context.Context, b *bundle.Bundle, das
 	return nil
 }
 
-func waitForChanges(ctx context.Context, w *databricks.WorkspaceClient, dashboard *dashboards.Dashboard) diag.Diagnostics {
+func waitForChanges(ctx context.Context, w *databricks.WorkspaceClient, dashboard *dashboards.Dashboard) {
 	// Compute [time.Time] for the most recent update.
 	tref, err := time.Parse(time.RFC3339, dashboard.UpdateTime)
 	if err != nil {
-		return diag.FromErr(err)
+		logdiag.LogError(ctx, err)
+		return
 	}
 
 	for {
 		obj, err := w.Workspace.GetStatusByPath(ctx, dashboard.Path)
 		if err != nil {
-			return diag.FromErr(err)
+			logdiag.LogError(ctx, err)
+			return
 		}
 
 		// Compute [time.Time] from timestamp in millis since epoch.
@@ -259,18 +261,18 @@ func waitForChanges(ctx context.Context, w *databricks.WorkspaceClient, dashboar
 
 		time.Sleep(1 * time.Second)
 	}
-
-	return nil
 }
 
-func (d *dashboard) updateDashboardForResource(ctx context.Context, b *bundle.Bundle) diag.Diagnostics {
+func (d *dashboard) updateDashboardForResource(ctx context.Context, b *bundle.Bundle) {
 	resource, ok := b.Config.Resources.Dashboards[d.resource]
 	if !ok {
-		return diag.Errorf("dashboard resource %q is not defined", d.resource)
+		logdiag.LogError(ctx, fmt.Errorf("dashboard resource %q is not defined", d.resource))
+		return
 	}
 
 	if resource.FilePath == "" {
-		return diag.Errorf("dashboard resource %q has no file path defined", d.resource)
+		logdiag.LogError(ctx, fmt.Errorf("dashboard resource %q has no file path defined", d.resource))
+		return
 	}
 
 	// Resolve the dashboard ID from the resource.
@@ -286,19 +288,21 @@ func (d *dashboard) updateDashboardForResource(ctx context.Context, b *bundle.Bu
 	for {
 		dashboard, err := w.Lakeview.GetByDashboardId(ctx, dashboardID)
 		if err != nil {
-			return diag.FromErr(err)
+			logdiag.LogError(ctx, err)
+			return
 		}
 
 		if etag != dashboard.Etag {
 			err = d.saveSerializedDashboard(ctx, b, dashboard, dashboardPath)
 			if err != nil {
-				return diag.FromErr(err)
+				logdiag.LogError(ctx, err)
+				return
 			}
 		}
 
 		// Abort if we are not watching for changes.
 		if !d.watch {
-			return nil
+			return
 		}
 
 		// Update the etag for the next iteration.

acceptance/bundle/deploy/dashboard/generate_inplace/out.test.toml
@@ -1,0 +1,5 @@
+Local = false
+Cloud = true
+
+[EnvMatrix]
+  DATABRICKS_CLI_DEPLOYMENT = ["terraform"]