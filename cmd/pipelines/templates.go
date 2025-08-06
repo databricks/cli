513d3de8f402881e1c2d43f0e5bff32e21ef0e9d
Alyssa Gorbaneva <alyssa.gorbaneva@databricks.com>
2025-08-06 16:27:15 -0700

    pipelines run: enrich output of pipelines (#3332)
    ## Changes
    On run, the corresponding run's update information is displayed.
    
    ## Why
    enrich run information.
    
    ## Tests
    doc can be found
    [here](https://docs.google.com/document/d/1JUpKLpQM1GP6NV7QtJmtEzU4IDXhl8GxrCNA9n_hvMs/edit?tab=t.0#heading=h.xeb4b9r7dqoi).
    
    Sample output:
    Update ID: dc390a6b-71e0-4274-8382-49472ac85632
    Update for pipeline [dev alyssa_gorbaneva] hi_pipeline completed
    successfully.
    Pipeline ID: ef8b547f-1e96-43c4-b50a-4c1c3a74e491
    Update start time: 2025-08-06T23:00:19Z
    Update end time: 2025-08-06T23:01:40Z.
    Pipeline configurations for this update:
    • All tables are refreshed
    • Update cause: API_CALL
    • Serverless compute
    • Channel: CURRENT
    • Development mode
    • Catalog: main
    • Schema: alyssa_gorbaneva
    
    Follows https://github.com/databricks/cli/pull/3331
    
    Follow up: https://github.com/databricks/cli/pull/3337

---
+++ cmd/pipelines/templates.go
@@ -0,0 +1,55 @@
+package pipelines
+
+const pipelineUpdateTemplate = `Update for pipeline {{- if .Update.Config }} {{ .Update.Config.Name }}{{ end }} completed successfully.
+{{- if .Update.Config }}
+Pipeline ID: {{ .Update.Config.Id }}
+{{- end }}
+{{- if and .Update.CreationTime .LastEventTime }}
+Update start time: {{ .Update.CreationTime | pretty_UTC_date_from_millis }}
+Update end time: {{ .LastEventTime }}.
+{{- end }}
+Pipeline configurations for this update:
+{{- if .Update.FullRefresh }}
+• All tables are fully refreshed
+{{- else if and (eq (len .Update.RefreshSelection) 0) (eq (len .Update.FullRefreshSelection) 0) }}
+• All tables are refreshed
+{{- else }}
+{{- if gt (len .Update.RefreshSelection) 0 }}
+• Refreshed [{{ join .Update.RefreshSelection ", " }}]
+{{- end }}
+{{- if gt (len .Update.FullRefreshSelection) 0 }}
+• Full refreshed [{{ join .Update.FullRefreshSelection ", " }}]
+{{- end }}
+{{- end }}
+{{- if .Update.Cause }}
+• Update cause: {{ .Update.Cause }}
+{{- end }}
+{{- if .Update.Config }}
+{{- if .Update.Config.Serverless }}
+• Serverless compute
+{{- else if .Update.ClusterId }}
+• Classic compute: {{ .Update.ClusterId }}
+{{- end }}
+{{- else if .Update.ClusterId }}
+• Classic compute: {{ .Update.ClusterId }}
+{{- end }}
+{{- if .Update.Config }}
+{{- if .Update.Config.Channel }}
+• Channel: {{ .Update.Config.Channel }}
+{{- end }}
+{{- if .Update.Config.Continuous }}
+• {{ if .Update.Config.Continuous }}Continuous{{ else }}Triggered{{ end }} pipeline
+{{- end }}
+{{- if .Update.Config.Development }}
+• {{ if .Update.Config.Development }}Development{{ else }}Production{{ end }} mode
+{{- end }}
+{{- if .Update.Config.Catalog }}
+• Catalog: {{ .Update.Config.Catalog }}
+{{- if .Update.Config.Schema }}
+• Schema: {{ .Update.Config.Schema }}
+{{- end }}
+{{- else if .Update.Config.Storage }}
+• Storage: {{ .Update.Config.Storage }}
+{{- end }}
+{{- end }}
+`
