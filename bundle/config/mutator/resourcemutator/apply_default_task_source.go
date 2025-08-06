241473000a39d1ff1d619c9d970ad879646bea5a
Andrew Nester <andrew.nester.dev@gmail.com>
2025-08-06 12:35:09 +0200

    Always set default value for source field for tasks if not explicitly set already (#3359)
    ## Changes
    If the job has `git_source` set, DABs will set `source: GIT` to all the
    tasks where source is not already set
    If the job has no `git_source` set, DABs will set `source: WORKSPACE` to
    all the tasks where source is not already set
    
    We only do this for a subset of task types which support source field as
    per https://docs.databricks.com/api/workspace/jobs/create
    
    ## Why
    Fixes https://github.com/databricks/cli/issues/2561
    
    ## Tests
    Added an acceptance test
    
    <!-- If your PR needs to be included in the release notes for next
    release,
    add a separate entry in NEXT_CHANGELOG.md as part of your PR. -->

---
+++ bundle/config/mutator/resourcemutator/apply_default_task_source.go
@@ -0,0 +1,90 @@
+package resourcemutator
+
+import (
+	"context"
+
+	"github.com/databricks/cli/bundle"
+	"github.com/databricks/cli/libs/diag"
+	"github.com/databricks/cli/libs/dyn"
+)
+
+type applyDefaultTaskSource struct{}
+
+func ApplyDefaultTaskSource() bundle.Mutator {
+	return &applyDefaultTaskSource{}
+}
+
+var pattern = dyn.NewPattern(
+	dyn.Key("resources"),
+	dyn.Key("jobs"),
+	dyn.AnyKey(),
+)
+
+var taskPattern = dyn.NewPattern(
+	dyn.Key("tasks"),
+	dyn.AnyIndex(),
+)
+
+var foreachTaskPattern = dyn.NewPattern(
+	dyn.Key("for_each_task"),
+	dyn.Key("task"),
+)
+
+func (a *applyDefaultTaskSource) Apply(ctx context.Context, b *bundle.Bundle) diag.Diagnostics {
+	err := b.Config.Mutate(func(v dyn.Value) (dyn.Value, error) {
+		return dyn.MapByPattern(v, pattern, func(p dyn.Path, job dyn.Value) (dyn.Value, error) {
+			defaultSource := "WORKSPACE"
+
+			// Check if the job has git_source set and set the default source to GIT if it does
+			_, err := dyn.Get(job, "git_source")
+			if err == nil {
+				defaultSource = "GIT"
+			}
+
+			// Then iterate over the tasks and set the source to the default if it's not set
+			return dyn.MapByPattern(job, taskPattern, func(p dyn.Path, task dyn.Value) (dyn.Value, error) {
+				// Then iterate over the foreach tasks and set the source to the default if it's not set
+				task, err = dyn.MapByPattern(task, foreachTaskPattern, func(p dyn.Path, foreachTask dyn.Value) (dyn.Value, error) {
+					return setSourceIfNotSet(foreachTask, defaultSource)
+				})
+				if err != nil {
+					return task, err
+				}
+
+				return setSourceIfNotSet(task, defaultSource)
+			})
+		})
+	})
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
+
+// These are the task types that support the source field
+// https://docs.databricks.com/api/workspace/jobs/create
+var supportedTypeTasks = []string{
+	"db_task",
+	"notebook_task",
+	"spark_python_task",
+}
+
+func setSourceIfNotSet(task dyn.Value, defaultSource string) (dyn.Value, error) {
+	for _, taskType := range supportedTypeTasks {
+		t, err := dyn.Get(task, taskType)
+		if err != nil {
+			continue
+		}
+
+		_, err = dyn.Get(t, "source")
+		if err != nil {
+			return dyn.Set(task, taskType+".source", dyn.V(defaultSource))
+		}
+	}
+	return task, nil
+}
+
+func (a *applyDefaultTaskSource) Name() string {
+	return "ApplyDefaultTaskSource"
+}
