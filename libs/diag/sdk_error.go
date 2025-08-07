c825251c83b50e8f40dc6acb1eec685fd403ea4e
Denis Bilenko <denis.bilenko@databricks.com>
2025-08-07 15:06:31 +0200

    Better error diagnostic for SDK errors
    Takes advantage of https://github.com/databricks/databricks-sdk-go/pull/1261

---
+++ libs/diag/sdk_error.go
@@ -0,0 +1,61 @@
+package diag
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/databricks/databricks-sdk-go/apierr"
+)
+
+func findApiErr(e error) *apierr.APIError {
+	for {
+		cast, ok := e.(*apierr.APIError)
+		if ok {
+			return cast
+		}
+
+		inner := errors.Unwrap(e)
+		if inner == nil {
+			break
+		}
+		e = inner
+	}
+	return nil
+}
+
+func FormatAPIErrorSummary(e error) string {
+	extra := ""
+	apiErr := findApiErr(e)
+	if apiErr != nil {
+		extra = strings.TrimSpace(fmt.Sprintf("%d %s", apiErr.StatusCode, apiErr.ErrorCode))
+		extra = " (" + extra + ")"
+	}
+
+	return e.Error() + extra
+}
+
+func FormatAPIErrorDetails(e error) string {
+	apiErr := findApiErr(e)
+	if apiErr == nil {
+		return ""
+	}
+	endpoint := "n/a"
+	httpStatus := ""
+	w := apiErr.ResponseWrapper
+	if w != nil {
+		resp := w.Response
+		if resp != nil {
+			httpStatus = resp.Status
+			req := resp.Request
+			if req != nil {
+				endpoint = fmt.Sprintf("%s %s", req.Method, req.URL)
+			}
+		}
+	}
+	if len(httpStatus) == 0 {
+		httpStatus = strconv.Itoa(apiErr.StatusCode)
+	}
+	return fmt.Sprintf("Endpoint: %s\nHTTP Status: %s\nAPI error_code: %s\nAPI message: %s", endpoint, httpStatus, apiErr.ErrorCode, apiErr.Message)
+}
