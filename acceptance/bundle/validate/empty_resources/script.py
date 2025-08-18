d2b25a16d96e5a5c5aa0187d12f6120bc212a552
Shreyas Goenka <shreyas.goenka@databricks.com>
2025-08-18 17:10:10 +0200

    TO REVERT IF WRONG: try shell function

--- acceptance/bundle/validate/empty_resources/script.py
+++ acceptance/bundle/validate/empty_resources/script.py
@@ -1,12 +1,6 @@
 import os
 import sys
 
-# On windows, shutil.which or os.system cannot find "envsubst.py" even though
-# it is available in the path. In order to work around this we directly
-# import the envsubst module from the bin directory.
-sys.path.append(os.path.join(os.path.dirname(__file__), "../../../bin"))
-import envsubst
-
 SECTIONS = [
     "jobs",
     "pipelines",
@@ -29,13 +23,7 @@ assert os.path.exists(TEMPLATE), TEMPLATE
 
 for section in SECTIONS:
     os.environ["SECTION"] = section
-
-    # Read the template file and substitute the variables.
-    with open(TEMPLATE, "r") as f:
-        template = f.read()
-    with open("databricks.yml", "w") as f:
-        f.write(envsubst.substitute_variables(template))
-
+    os.system(f"envsubst < {TEMPLATE} > databricks.yml")
     print(f"\n=== resources.{section}.rname ===", flush=True)
 
     ret = os.system(CLI + " bundle validate -o json | jq .resources")
