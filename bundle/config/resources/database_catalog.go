c833dc630833fca1ae42a07c0c807ba715497a26
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2025-08-19 12:53:06 +0200

    remove DatabaseCatalogPermissionLevel

--- bundle/config/resources/database_catalog.go
+++ bundle/config/resources/database_catalog.go
@@ -10,23 +10,10 @@ import (
 	"github.com/databricks/databricks-sdk-go/service/database"
 )
 
-type DatabaseCatalogPermissionLevel string
-
-// DatabaseCatalogPermission holds the permission level setting for a single principal.
-// Multiple of these can be defined on any database Catalog.
-type DatabaseCatalogPermission struct {
-	Level DatabaseCatalogPermissionLevel `json:"level"`
-
-	UserName             string `json:"user_name,omitempty"`
-	ServicePrincipalName string `json:"service_principal_name,omitempty"`
-	GroupName            string `json:"group_name,omitempty"`
-}
-
 type DatabaseCatalog struct {
-	ID             string                      `json:"id,omitempty" bundle:"readonly"`
-	URL            string                      `json:"url,omitempty" bundle:"internal"`
-	Permissions    []DatabaseCatalogPermission `json:"permissions,omitempty"`
-	ModifiedStatus ModifiedStatus              `json:"modified_status,omitempty" bundle:"internal"`
+	ID             string         `json:"id,omitempty" bundle:"readonly"`
+	URL            string         `json:"url,omitempty" bundle:"internal"`
+	ModifiedStatus ModifiedStatus `json:"modified_status,omitempty" bundle:"internal"`
 
 	database.DatabaseCatalog
 }
