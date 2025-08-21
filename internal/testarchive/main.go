afc0e0a49f1f2bc6033a269dc77f41af09f1141b
Shreyas Goenka <shreyas.goenka@databricks.com>
2025-08-21 11:47:39 +0200

    typo

--- internal/testarchive/main.go
+++ internal/testarchive/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	// Directories with the _ prefix are ignored by Go. That is important
-	// since the go installation in _bin would include go modules which would
+	// since the go installation in _bin would include stdlib go modules which would
 	// otherwise cause an error during a build of the CLI.
 	err := createArchive("_build", "_bin", "../..")
 	if err != nil {
