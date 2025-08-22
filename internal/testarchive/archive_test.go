f650be8b335e75c57ba6f823e3caafe87bc374c0
shreyas-goenka <88374338+shreyas-goenka@users.noreply.github.com>
2025-08-22 17:06:50 +0200

    Add archive build for DBR testing (#3452)
    ## Changes
    This PR adds an internal program that can build a CLI test archive that
    contains the entire repo and binaries like uv, go, and jq to run
    acceptance tests.
    
    This PR builds on top of https://github.com/databricks/cli/pull/3444
    
    ## Why
    A single deployable binary makes testing on DBR very straightforward.
    All you then need to do is decompile the binary and run tests using a
    test runner of your choice.
    
    For now, the tests will be run using the go binary, but this could be
    extended to run them using gotestsum.
    
    ## Tests
    A new unit test. This has a build tag so that we can limit its execution
    to only a DBR CI environment.

---
+++ internal/testarchive/archive_test.go
@@ -0,0 +1,36 @@
+//go:build dbr_only
+
+package main
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestArchive(t *testing.T) {
+	archiveDir := t.TempDir()
+	binDir := t.TempDir()
+	repoRoot := "../.."
+
+	err := createArchive(archiveDir, binDir, repoRoot)
+	require.NoError(t, err)
+
+	assertDir := t.TempDir()
+	err = extractTarGz(filepath.Join(archiveDir, "archive.tar.gz"), assertDir)
+	require.NoError(t, err)
+
+	// Go installation is a directory because it includes the
+	// standard library source code along with the Go binary.
+	assert.FileExists(t, filepath.Join(assertDir, "bin", "arm64", "go", "bin", "go"))
+	assert.FileExists(t, filepath.Join(assertDir, "bin", "amd64", "go", "bin", "go"))
+	assert.FileExists(t, filepath.Join(assertDir, "bin", "arm64", "uv"))
+	assert.FileExists(t, filepath.Join(assertDir, "bin", "amd64", "uv"))
+	assert.FileExists(t, filepath.Join(assertDir, "bin", "arm64", "jq"))
+	assert.FileExists(t, filepath.Join(assertDir, "bin", "amd64", "jq"))
+
+	assert.FileExists(t, filepath.Join(assertDir, "cli", "go.mod"))
+	assert.FileExists(t, filepath.Join(assertDir, "cli", "go.sum"))
+}
