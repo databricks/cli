7df2b68ab985fa33c2d2aeecd04ef722d1723bf5
Shreyas Goenka <shreyas.goenka@databricks.com>
2025-08-25 12:43:36 +0200

    pass build and bin directories as args

--- internal/testarchive/main.go
+++ internal/testarchive/main.go
@@ -6,10 +6,18 @@ import (
 )
 
 func main() {
+	if len(os.Args) != 3 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <build-dir> <bin-dir>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	buildDir := os.Args[1]
+	binDir := os.Args[2]
+
 	// Directories with the _ prefix are ignored by Go. That is important
 	// since the go installation in _bin would include stdlib go modules which would
 	// otherwise cause an error during a build of the CLI.
-	err := createArchive("_build", "_bin", "../..")
+	err := createArchive(buildDir, binDir, "../..")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error creating archive: %v\n", err)
 		os.Exit(1)
