a938206e0ed9f34eea94e23df9445d449d7cb724
Andrew Nester <andrew.nester@databricks.com>
2025-08-26 12:04:58 +0200

    moved to utils

--- bundle/terranova/tnresources/app.go
+++ bundle/terranova/tnresources/app.go
@@ -101,16 +101,3 @@ func (r *ResourceApp) waitForApp(ctx context.Context, w *databricks.WorkspaceCli
 		}
 	})
 }
-
-// This is copied from the retries package of the databricks-sdk-go. It should be made public,
-// but for now, I'm copying it here.
-func shouldRetry(err error) bool {
-	if err == nil {
-		return false
-	}
-	e := err.(*retries.Err)
-	if e == nil {
-		return false
-	}
-	return !e.Halt
-}
