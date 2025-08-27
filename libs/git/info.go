67f7d9765b7ddd4b5851649f86f102c928c34a13
Shreyas Goenka <shreyas.goenka@databricks.com>
2025-08-27 15:08:36 +0200

    Downgrade load git info failure to debug logs

--- libs/git/info.go
+++ libs/git/info.go
@@ -86,7 +86,7 @@ func fetchRepositoryInfoAPI(ctx context.Context, path string, w *databricks.Work
 		result.CurrentBranch = gi.Branch
 		result.WorktreeRoot = fixedPath
 	} else {
-		log.Warnf(ctx, "Failed to load git info from %s", apiEndpoint)
+		log.Debugf(ctx, "Failed to load git info from %s", apiEndpoint)
 	}
 
 	return result, nil
