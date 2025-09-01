ce09f955fecacad06988f6fae59979e36289299f
Ilia Babanov <ilia.babanov@databricks.com>
2025-09-01 12:50:22 +0200

    Remove unnecessary warnings

--- cmd/ssh/ssh.go
+++ cmd/ssh/ssh.go
@@ -7,10 +7,8 @@ import (
 const disclaimer = `WARNING! This is an experimental feature:
 - The product is in preview and not intended to be used in production;
 - The product may change or may never be released;
-- While we will not charge separately for this product right now, we may charge for it in the future. You will still incur charges for DBUs.
-- There's no formal support or SLAs for the preview - so please reach out to your account or other contact with any questions or feedback.
-- We may terminate the preview or your access at any time;
-- Non-public information about the preview (including the fact that there is a preview for the feature/product itself) is confidential;`
+- While we will not charge separately for this product right now, we may charge for it in the future. You will still incur charges for DBUs;
+- There's no formal support or SLAs for the preview - so please reach out to your account or other contact with any questions or feedback;`
 
 func New() *cobra.Command {
 	cmd := &cobra.Command{
