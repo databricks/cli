f222dfc4d25d1ee7f5744025988a7f05d4f8ac55
Lennart Kats <lennart.kats@databricks.com>
2025-09-01 10:30:53 +0200

    Update template

---
+++ acceptance/bundle/templates/default-python/classic/output/my_default_python/src/default_python_etl/transformations/sample_zones_jan_01_1034.py
@@ -0,0 +1,17 @@
+import dlt
+from pyspark.sql.functions import col, sum
+
+
+# This file defines a sample transformation.
+# Edit the sample below or add new transformations
+# using "+ Add" in the file browser.
+
+
+@dlt.table
+def sample_zones_jan_01_1034():
+    # Read from the "sample_trips" table, then sum all the fares
+    return (
+        spark.read.table(f"sample_trips_jan_01_1034")
+        .groupBy(col("pickup_zip"))
+        .agg(sum("fare_amount").alias("total_fare"))
+    )
