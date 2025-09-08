f8bc3b37865fd24a306b7f000ff722d81756975e
Ilia Babanov <ilia.babanov@databricks.com>
2025-09-08 14:30:04 +0200

    Add "ssh connect" command (#3471)
    ## Changes
    See the base PR for the context: 
    - https://github.com/databricks/cli/pull/3470
    
    This PR adds `databricks ssh connect` subcommand and all related
    utilities.
    
    The main logic here is in `libs/ssh/client.go` and `libs/ssh/proxy.go`
    files.
    
    See `proxy_test.go` for an overview of how client and server interact
    with each other through the proxy.
    
    Overview of the `ssh client` logic:
    - Generate local ssh keys if necessary
    (`~/.databricks/ssh-tunnel-keys/<cluster-id>`)
    - Save the public key in the secret scope
    (`<username>-<cluster-id>-ssh-tunnel-keys`)
    - Upload databricks releases (linux arm and amd) with the marching
    version to the /Workspace
    - Get /Workspace/metadata.json file with the server port info
    - If the metadata is not there, execute `ssh-server-bootsrap.py` file as
    a job (it runs `databricks server` command that's implemented in the
    follow up PR)
    - Get server metadata by sending a driver-proxy request to the known
    port, it will return a user name
    - Spawn `ssh client` with the right User and a ProxyCommand that
    executed `ssh connect --proxy --metadata`
    - New instance of the `connect` command can now start a proxy over
    websocket connection backed by Driver Proxy API
    
    Overview of the Proxy logic:
    - The main interesting part is that the proxy does automatic re-connects
    every 30 minutes (configurable) to avoid auth token expiration problems.
    In all the tricky places (mostly related to concurrency and locks) there
    are comments explaining the logic.
    
    The final follow up PR:
    - https://github.com/databricks/cli/pull/3475
    
    
    ## Tests
    Manual and unit

--- libs/ssh/keys.go
+++ libs/ssh/keys.go
@@ -1,9 +1,19 @@
 package ssh
 
 import (
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
 	"os"
 	"path/filepath"
+	"runtime"
+	"strings"
+
+	"github.com/databricks/cli/libs/cmdio"
+	"golang.org/x/crypto/ssh"
 )
 
 // We use different client keys for each cluster as a good practice for better isolation and control.
@@ -16,4 +26,105 @@ func getLocalSSHKeyPath(clusterID, keysDir string) (string, error) {
 		keysDir = filepath.Join(homeDir, ".databricks", "ssh-tunnel-keys")
 	}
 	return filepath.Join(keysDir, clusterID), nil
+}
+
+func generateSSHKeyPair() ([]byte, []byte, error) {
+	privateKey, err := rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to generate private key: %w", err)
+	}
+
+	privateKeyPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
+	})
+
+	publicKey, err := ssh.NewPublicKey(&privateKey.PublicKey)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to generate public key: %w", err)
+	}
+
+	publicKeyBytes := ssh.MarshalAuthorizedKey(publicKey)
+
+	return privateKeyPEM, publicKeyBytes, nil
+}
+
+func saveSSHKeyPair(keyPath string, privateKeyBytes, publicKeyBytes []byte) error {
+	err := os.RemoveAll(filepath.Dir(keyPath))
+	if err != nil && !os.IsNotExist(err) {
+		return fmt.Errorf("failed to remove existing key directory: %w", err)
+	}
+
+	if err := os.MkdirAll(filepath.Dir(keyPath), 0o700); err != nil {
+		return fmt.Errorf("failed to create directory for key: %w", err)
+	}
+
+	if err := os.WriteFile(keyPath, privateKeyBytes, 0o600); err != nil {
+		return fmt.Errorf("failed to write private key to file: %w", err)
+	}
+
+	if err := os.WriteFile(keyPath+".pub", publicKeyBytes, 0o644); err != nil {
+		return fmt.Errorf("failed to write public key to file: %w", err)
+	}
+
+	return nil
+}
+
+func checkSSHKeyPairPermissions(keyPath string) error {
+	dir := filepath.Dir(keyPath)
+	dirInfo, err := os.Stat(dir)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return fmt.Errorf("key directory does not exist: %s", dir)
+		} else {
+			return fmt.Errorf("failed to stat key directory: %w", err)
+		}
+	}
+	if runtime.GOOS != "windows" && dirInfo.Mode().Perm() != 0o700 {
+		return fmt.Errorf("key directory permissions are not set to 0700, current permissions: %o", dirInfo.Mode().Perm())
+	}
+
+	info, err := os.Stat(keyPath)
+	if err != nil {
+		return fmt.Errorf("failed to stat key file: %w", err)
+	}
+
+	if runtime.GOOS != "windows" && info.Mode().Perm() != 0o600 {
+		return fmt.Errorf("private key permissions are not set to 0600, current permissions: %o", info.Mode().Perm())
+	}
+
+	pubKeyPath := keyPath + ".pub"
+	pubInfo, err := os.Stat(pubKeyPath)
+	if err != nil {
+		return fmt.Errorf("failed to stat public key file: %w", err)
+	}
+
+	if runtime.GOOS != "windows" && pubInfo.Mode().Perm() != 0o644 {
+		return fmt.Errorf("public key permissions are not set to 0644, current permissions: %o", pubInfo.Mode().Perm())
+	}
+
+	return nil
+}
+
+func checkAndGenerateSSHKeyPair(ctx context.Context, keyPath string) (string, string, error) {
+	if err := checkSSHKeyPairPermissions(keyPath); err != nil {
+		cmdio.LogString(ctx, "Generating new SSH key pair...")
+
+		privateKeyBytes, publicKeyBytes, err := generateSSHKeyPair()
+		if err != nil {
+			return "", "", err
+		}
+
+		if err := saveSSHKeyPair(keyPath, privateKeyBytes, publicKeyBytes); err != nil {
+			return "", "", err
+		}
+	}
+
+	cmdio.LogString(ctx, "Using SSH key pair at "+keyPath)
+	publicKeyBytes, err := os.ReadFile(keyPath + ".pub")
+	if err != nil {
+		return "", "", fmt.Errorf("failed to read public key: %w", err)
+	}
+
+	return keyPath, strings.TrimSpace(string(publicKeyBytes)), nil
 }
