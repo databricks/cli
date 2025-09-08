3ab37b3b1c60a656a2f730544aa253846988574a
Andrew Nester <andrew.nester@databricks.com>
2025-09-08 14:18:51 +0200

    Added new Lakeflow Pipelines support for bundle generate

--- bundle/generate/pipeline.go
+++ bundle/generate/pipeline.go
@@ -1,6 +1,8 @@
 package generate
 
 import (
+	"strings"
+
 	"github.com/databricks/cli/libs/dyn"
 	"github.com/databricks/cli/libs/dyn/yamlsaver"
 	"github.com/databricks/databricks-sdk-go/service/pipelines"
@@ -8,9 +10,21 @@ import (
 
 var pipelineOrder = yamlsaver.NewOrder([]string{"name", "clusters", "configuration", "libraries"})
 
-func ConvertPipelineToValue(pipeline *pipelines.PipelineSpec) (dyn.Value, error) {
-	value := make(map[string]dyn.Value)
+func ConvertPipelineToValue(pipeline *pipelines.PipelineSpec, rootPath string, remoteRootPath string) (dyn.Value, error) {
+	if pipeline.RootPath != "" {
+		pipeline.RootPath = rootPath
+	}
 
+	if pipeline.Libraries != nil {
+		for i := range pipeline.Libraries {
+			lib := &pipeline.Libraries[i]
+			if lib.Glob != nil {
+				lib.Glob.Include = strings.ReplaceAll(lib.Glob.Include, remoteRootPath, rootPath)
+			}
+		}
+	}
+
+	value := make(map[string]dyn.Value)
 	// We ignore the following fields:
 	// - id: this is a read-only field
 	// - storage: changes to this field are rare because changing the storage recreates pipeline-related resources
