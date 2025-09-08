68ad7cdf056881d39d8ffc841f5486e0efabcbdb
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2025-09-08 15:08:44 +0200

    use bearer token for fingerprint

--- bundle/config/mutator/populate_current_user.go
+++ bundle/config/mutator/populate_current_user.go
@@ -3,15 +3,7 @@ package mutator
 import (
 	"context"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"net/http"
-	"reflect"
-	"unsafe"
-
-	"github.com/databricks/databricks-sdk-go"
-	"github.com/databricks/databricks-sdk-go/client"
-
 	"github.com/databricks/cli/bundle"
 	"github.com/databricks/cli/bundle/config"
 	"github.com/databricks/cli/libs/diag"
@@ -52,7 +44,7 @@ func (m *populateCurrentUser) initializeCache(ctx context.Context, b *bundle.Bun
 
 	m.cache = bundle.NewFileCache(cacheDir)
 
-	fmt.Printf("New cache dir initialized: %s\n", cacheDir)
+	fmt.Printf("[DEBUG antonnek] New cache dir initialized: %s\n", cacheDir)
 
 	return nil
 }
@@ -66,17 +58,59 @@ func (m *populateCurrentUser) Apply(ctx context.Context, b *bundle.Bundle) diag.
 		return nil
 	}
 
-	// Initialize cache for authorization headers
-	if err := m.initializeCache(ctx, b); err != nil {
-		return diag.FromErr(err)
+	err := m.initializeCache(ctx, b)
+	if err != nil {
+		fmt.Printf("[DEBUG antonnek] failed to initialize cache: %v \n", err)
+	}
+	w := b.WorkspaceClient()
+
+	// use bearer token
+	bearerToken := ""
+	tokenSource := w.Config.GetTokenSource()
+	if tokenSource == nil {
+		fmt.Printf("[DEBUG antonnek] token source not found\n")
+	} else {
+		token, err := tokenSource.Token(context.Background())
+		if err != nil {
+			fmt.Printf("[DEBUG antonnek] error reading token source: %v \n", err)
+		}
+		bearerToken = token.AccessToken
 	}
 
-	w := b.WorkspaceClient()
-	d := getDatabricksClient(w)
+	var me *iam.User
+	if bearerToken != "" {
+		fmt.Printf("[DEBUG antonnek] bearer token found: will use that for cache fingerprint\n")
+		fmt.Printf("[DEBUG antonnek] bearer token: %s\n", bearerToken)
+		fingerprint, err := bundle.GenerateFingerprint("auth_header", bearerToken)
+		if err != nil {
+			panic(err)
+		}
+		cachedUserBytes, isCacheHit := m.cache.Read(ctx, fingerprint)
+		if isCacheHit {
+			if err := json.Unmarshal(cachedUserBytes, &me); err == nil {
+				fmt.Printf("[DEBUG antonnek] bearer token found: will use that for cache fingerprint\n")
+			}
+		}
+	}
 
-	me, err := m.getCurrentUserWithAuthTracking(ctx, d)
-	if err != nil {
-		return diag.FromErr(err)
+	if me == nil {
+		currentUser, err := w.CurrentUser.Me(ctx)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+		me = currentUser
+		if bearerToken != "" {
+			userBytes, err := json.Marshal(currentUser)
+			if err != nil {
+				fmt.Printf("[DEBUG antonnek] could not serialize current user information: %v\n", err)
+			}
+			err = m.cache.Store(ctx, bearerToken, userBytes)
+			if err != nil {
+				fmt.Printf("[DEBUG antonnek] could not store user information: %v\n", err)
+			} else {
+				fmt.Printf("[DEBUG antonnek] stored user information in cache!")
+			}
+		}
 	}
 
 	b.Config.Workspace.CurrentUser = &config.User{
@@ -90,89 +124,89 @@ func (m *populateCurrentUser) Apply(ctx context.Context, b *bundle.Bundle) diag.
 	return nil
 }
 
-// getCurrentUserWithAuthTracking makes the CurrentUser.Me method, caches the authorization header and returns result
-func (m *populateCurrentUser) getCurrentUserWithAuthTracking(ctx context.Context, client *client.DatabricksClient) (*iam.User, error) {
-	var user iam.User
-	path := "/api/2.0/preview/scim/v2/Me"
+// getCurrentUserWithAuthTracking calls the CurrentUser.Me method, caches the authorization header and returns result
+//func (m *populateCurrentUser) getCurrentUserWithAuthTracking(ctx context.Context, client *client.DatabricksClient) (*iam.User, error) {
+//	var user iam.User
+//	path := "/api/2.0/preview/scim/v2/Me"
+//
+//	headers := make(map[string]string)
+//	headers["Accept"] = "application/json"
+//
+//	// Visitor to inspect request headers
+//	//headerInspector := func(req *http.Request) error {
+//	//	for name, values := range req.Header {
+//	//		if name != "Authorization" {
+//	//			continue
+//	//		}
+//	//		for _, value := range values {
+//	//			if m.cache != nil {
+//	//				fingerprint, err := bundle.GenerateFingerprint("auth_header", value)
+//	//				if err != nil {
+//	//					panic(err)
+//	//				}
+//	//				cachedUserBytes, isCacheHit := m.cache.Read(ctx, fingerprint)
+//	//				if isCacheHit {
+//	//					var cachedUser iam.User
+//	//					if err := json.Unmarshal(cachedUserBytes, &cachedUser); err == nil {
+//	//						return &cacheHitError{user: &cachedUser}
+//	//					}
+//	//				}
+//	//			}
+//	//			m.lastKnownAuthorizationHeader = value
+//	//		}
+//	//	}
+//	//	return nil
+//	//}
+//
+//	err := client.Do(ctx, http.MethodGet, path, headers, nil, nil, &user)
+//
+//	// Check if we got a cache hit error
+//	var cacheHit *cacheHitError
+//	if err != nil && errors.As(err, &cacheHit) {
+//		return cacheHit.user, nil
+//	}
+//
+//	// Store authorization header in cache
+//	if m.cache != nil && m.lastKnownAuthorizationHeader != "" {
+//		fingerprint, err := bundle.GenerateFingerprint("auth_header", m.lastKnownAuthorizationHeader)
+//		if err != nil {
+//			panic(err)
+//		}
+//
+//		userBytes, err := json.Marshal(&user)
+//		if err != nil {
+//			return nil, err
+//		}
+//
+//		err = m.cache.Store(ctx, fingerprint, userBytes)
+//		if err != nil {
+//			fmt.Printf("cache store error: %s\n", err)
+//		}
+//	}
+//
+//	return &user, err
+//}
 
-	headers := make(map[string]string)
-	headers["Accept"] = "application/json"
+//// TODO: find a way to get the client without using reflection
+//func getDatabricksClient(w *databricks.WorkspaceClient) *client.DatabricksClient {
+//	v := reflect.ValueOf(w.CurrentUser)
+//	// value is a pointer. Keep dereferencing it until we get to the actual value
+//	for v.Kind() == reflect.Ptr {
+//		if v.IsNil() {
+//			panic("nil pointer encountered")
+//		}
+//		v = v.Elem()
+//	}
+//
+//	clientField := v.FieldByName("client")
+//	clientInterface := getUnexportedField(clientField)
+//	client, ok := clientInterface.(*client.DatabricksClient)
+//	if !ok {
+//		panic("client is not a client.DatabricksClient")
+//	}
+//	return client
+//}
 
-	// Visitor to inspect request headers
-	headerInspector := func(req *http.Request) error {
-		for name, values := range req.Header {
-			if name != "Authorization" {
-				continue
-			}
-			for _, value := range values {
-				if m.cache != nil {
-					fingerprint, err := bundle.GenerateFingerprint("auth_header", value)
-					if err != nil {
-						panic(err)
-					}
-					cachedUserBytes, isCacheHit := m.cache.Read(ctx, fingerprint)
-					if isCacheHit {
-						var cachedUser iam.User
-						if err := json.Unmarshal(cachedUserBytes, &cachedUser); err == nil {
-							return &cacheHitError{user: &cachedUser}
-						}
-					}
-				}
-				m.lastKnownAuthorizationHeader = value
-			}
-		}
-		return nil
-	}
-
-	err := client.Do(ctx, http.MethodGet, path, headers, nil, nil, &user, headerInspector)
-
-	// Check if we got a cache hit error
-	var cacheHit *cacheHitError
-	if err != nil && errors.As(err, &cacheHit) {
-		return cacheHit.user, nil
-	}
-
-	// Store authorization header in cache
-	if m.cache != nil && m.lastKnownAuthorizationHeader != "" {
-		fingerprint, err := bundle.GenerateFingerprint("auth_header", m.lastKnownAuthorizationHeader)
-		if err != nil {
-			panic(err)
-		}
-
-		userBytes, err := json.Marshal(&user)
-		if err != nil {
-			return nil, err
-		}
-
-		err = m.cache.Store(ctx, fingerprint, userBytes)
-		if err != nil {
-			fmt.Printf("cache store error: %s\n", err)
-		}
-	}
-
-	return &user, err
-}
-
-// TODO: find a way to get the client without using reflection
-func getDatabricksClient(w *databricks.WorkspaceClient) *client.DatabricksClient {
-	v := reflect.ValueOf(w.CurrentUser)
-	// value is a pointer. Keep dereferencing it until we get to the actual value
-	for v.Kind() == reflect.Ptr {
-		if v.IsNil() {
-			panic("nil pointer encountered")
-		}
-		v = v.Elem()
-	}
-
-	clientField := v.FieldByName("client")
-	clientInterface := getUnexportedField(clientField)
-	client, ok := clientInterface.(*client.DatabricksClient)
-	if !ok {
-		panic("client is not a client.DatabricksClient")
-	}
-	return client
-}
-
-func getUnexportedField(field reflect.Value) any {
-	return reflect.NewAt(field.Type(), unsafe.Pointer(field.UnsafeAddr())).Elem().Interface()
-}
+//func getUnexportedField(field reflect.Value) any {
+//	return reflect.NewAt(field.Type(), unsafe.Pointer(field.UnsafeAddr())).Elem().Interface()
+//}
