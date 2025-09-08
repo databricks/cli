109d45361daf1c01bcd2e687350e60cfc8b96d4d
Andrew Nester <andrew.nester@databricks.com>
2025-09-08 14:43:14 +0200

    fixes

--- bundle/config/resources/lifecycle.go
+++ bundle/config/resources/lifecycle.go
@@ -1,5 +1,8 @@
 package resources
 
+// Lifecycle is a struct that contains the lifecycle settings for a resource.
+// It controls the behavior of the resource when it is deployed or destroyed.
 type Lifecycle struct {
+	// Lifecycle setting to prevent the resource from being destroyed.
 	PreventDestroy bool `json:"prevent_destroy,omitempty"`
 }
