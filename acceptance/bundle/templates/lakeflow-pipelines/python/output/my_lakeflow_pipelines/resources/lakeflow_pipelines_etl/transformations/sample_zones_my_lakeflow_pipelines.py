fbb161778129d0ce1e9794a6a3d764b3e6f44229
Lennart Kats <lennart.kats@databricks.com>
2025-09-16 11:32:42 +0200

    Minor fixes

--- acceptance/bundle/templates/lakeflow-pipelines/python/output/my_lakeflow_pipelines/resources/lakeflow_pipelines_etl/transformations/sample_zones_my_lakeflow_pipelines.py
+++ acceptance/bundle/templates/lakeflow-pipelines/python/output/my_lakeflow_pipelines/resources/lakeflow_pipelines_etl/transformations/sample_zones_my_lakeflow_pipelines.py
@@ -8,10 +8,6 @@ from pyspark.sql.functions import col, sum
 
 
 @dlt.table
-def sample_zones_sample():
+def sample_zones_my_lakeflow_pipelines():
     # Read from the "sample_trips" table, then sum all the fares
-    return (
-        spark.read.table(f"sample_trips_my_lakeflow_pipelines")
-        .groupBy(col("pickup_zip"))
-        .agg(sum("fare_amount").alias("total_fare"))
-    )
+    return spark.read.table(f"sample_trips_my_lakeflow_pipelines").groupBy(col("pickup_zip")).agg(sum("fare_amount").alias("total_fare"))
