bfea3fe95f1fc3b8d5923335b782642b19a0541d
Denis Bilenko <denis.bilenko@databricks.com>
2025-09-17 15:48:05 +0200

    claen up

--- libs/structs/structpath/pkg.go
+++ libs/structs/structpath/pkg.go
@@ -3,7 +3,7 @@ package structpath
 /*
 PathNode is a type to represent paths in Go structs that supports fields, map and index keys as well as wildcards for those.
 
-It can represent map keys accurately and can be used to serialized and deserialize path without information loss as it's intended for serialized deployment plan.
+It can represent map keys accurately and can be used to serialized and deserialize path without information loss.
 
 The map keys are encoded in single quotes: tags['name']. Single quote can escaped by placing two single quotes: tags[''''] (map key is one single quote).
 
