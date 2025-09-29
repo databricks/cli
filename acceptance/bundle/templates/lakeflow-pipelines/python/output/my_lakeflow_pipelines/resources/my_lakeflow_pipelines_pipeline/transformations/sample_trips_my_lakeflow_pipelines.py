80f9e56041294d885d8bf08c2947fe45ddf31901
Lennart Kats <lennart.kats@databricks.com>
2025-09-29 13:19:00 +0200

    Format files

--- acceptance/bundle/templates/lakeflow-pipelines/python/output/my_lakeflow_pipelines/resources/my_lakeflow_pipelines_pipeline/transformations/sample_trips_my_lakeflow_pipelines.py
+++ acceptance/bundle/templates/lakeflow-pipelines/python/output/my_lakeflow_pipelines/resources/my_lakeflow_pipelines_pipeline/transformations/sample_trips_my_lakeflow_pipelines.py
@@ -10,4 +10,6 @@ from utilities import utils
 
 @dlt.table
 def sample_trips_my_lakeflow_pipelines():
-    return spark.read.table("samples.nyctaxi.trips").withColumn("trip_distance_km", utils.distance_km(col("trip_distance")))
+    return spark.read.table("samples.nyctaxi.trips").withColumn(
+        "trip_distance_km", utils.distance_km(col("trip_distance"))
+    )
