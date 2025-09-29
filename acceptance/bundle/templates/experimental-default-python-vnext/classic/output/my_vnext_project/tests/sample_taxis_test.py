e1cf153bc47dddf14fee30d9749b6acc13c35abd
Lennart Kats <lennart.kats@databricks.com>
2025-09-29 09:04:06 +0200

    Remove tests for now

--- acceptance/bundle/templates/experimental-default-python-vnext/classic/output/my_vnext_project/tests/sample_taxis_test.py
+++
@@ -1,8 +0,0 @@
-from databricks.sdk.runtime import spark
-from pyspark.sql import DataFrame
-from my_vnext_project import taxis
-
-
-def test_find_all_taxis():
-    results = taxis.find_all_taxis()
-    assert results.count() > 5
