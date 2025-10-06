9e0761546fa0433ba2294f3d2edc14b661d85a3d
Denis Bilenko <denis.bilenko@databricks.com>
2025-10-06 13:24:55 +0200

    remove naked return

--- libs/databrickscfg/profile/file.go
+++ libs/databrickscfg/profile/file.go
@@ -89,7 +89,7 @@ func (f FileProfilerImpl) LoadProfiles(ctx context.Context, fn ProfileMatchFunct
 		}
 	}
 
-	return
+	return profiles, err
 }
 
 func ProfileCompletion(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
