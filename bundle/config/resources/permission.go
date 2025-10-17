276ffde84551e9b6b282cc5daff41b495ad52d82
Denis Bilenko <denis.bilenko@databricks.com>
2025-10-17 19:45:37 +0200

    add IPermission

--- bundle/config/resources/permission.go
+++ bundle/config/resources/permission.go
@@ -26,3 +26,45 @@ func (p Permission) String() string {
 
 	return "level: " + p.Level
 }
+
+type IPermission interface {
+	GetLevel() string
+	GetUserName() string
+	GetServicePrincipalName() string
+	GetGroupName() string
+}
+
+type (
+	JobPermissionLevel      string
+	PipelinePermissionLevel string
+)
+
+// JobPermission holds the permission level setting for a single principal.
+// Multiple of these can be defined on any job.
+type JobPermission struct {
+	Level JobPermissionLevel `json:"level"`
+
+	UserName             string `json:"user_name,omitempty"`
+	ServicePrincipalName string `json:"service_principal_name,omitempty"`
+	GroupName            string `json:"group_name,omitempty"`
+}
+
+// PipelinePermission holds the permission level setting for a single principal.
+// Multiple of these can be defined on any pipeline.
+type PipelinePermission struct {
+	Level PipelinePermissionLevel `json:"level"`
+
+	UserName             string `json:"user_name,omitempty"`
+	ServicePrincipalName string `json:"service_principal_name,omitempty"`
+	GroupName            string `json:"group_name,omitempty"`
+}
+
+func (p JobPermission) GetLevel() string                { return string(p.Level) }
+func (p JobPermission) GetUserName() string             { return p.UserName }
+func (p JobPermission) GetServicePrincipalName() string { return p.ServicePrincipalName }
+func (p JobPermission) GetGroupName() string            { return p.GroupName }
+
+func (p PipelinePermission) GetLevel() string                { return string(p.Level) }
+func (p PipelinePermission) GetUserName() string             { return p.UserName }
+func (p PipelinePermission) GetServicePrincipalName() string { return p.ServicePrincipalName }
+func (p PipelinePermission) GetGroupName() string            { return p.GroupName }
