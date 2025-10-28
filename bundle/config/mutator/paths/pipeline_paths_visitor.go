6576d8e76f85aadeb096e438a39c78abca20a80b
Andrew Nester <andrew.nester.dev@gmail.com>
2025-10-28 15:17:14 +0100

    Allow pipelines reference environment dependencies outside sync root (#3816)
    ## Changes
    Check that pipeline can reference environment dependencies outside sync
    root
    
    <!-- If your PR needs to be included in the release notes for next
    release,
    add a separate entry in NEXT_CHANGELOG.md as part of your PR. -->

--- bundle/config/mutator/paths/pipeline_paths_visitor.go
+++ bundle/config/mutator/paths/pipeline_paths_visitor.go
@@ -14,14 +14,42 @@ type pipelineRewritePattern struct {
 	skipRewrite func(string) bool
 }
 
-func pipelineRewritePatterns() []pipelineRewritePattern {
-	// Base pattern to match all libraries in all pipelines.
-	base := dyn.NewPattern(
-		dyn.Key("resources"),
-		dyn.Key("pipelines"),
-		dyn.AnyKey(),
-	)
+// Base pattern to match all libraries in all pipelines.
+var base = dyn.NewPattern(
+	dyn.Key("resources"),
+	dyn.Key("pipelines"),
+	dyn.AnyKey(),
+)
 
+func pipelineRewritePatterns() []pipelineRewritePattern {
+	// Compile list of configuration paths to rewrite.
+	allPatterns := []pipelineRewritePattern{
+		{
+			pattern:     base.Append(dyn.Key("libraries"), dyn.AnyIndex(), dyn.Key("notebook"), dyn.Key("path")),
+			mode:        TranslateModeNotebook,
+			skipRewrite: noSkipRewrite,
+		},
+		{
+			pattern:     base.Append(dyn.Key("libraries"), dyn.AnyIndex(), dyn.Key("file"), dyn.Key("path")),
+			mode:        TranslateModeFile,
+			skipRewrite: noSkipRewrite,
+		},
+		{
+			pattern:     base.Append(dyn.Key("libraries"), dyn.AnyIndex(), dyn.Key("glob"), dyn.Key("include")),
+			mode:        TranslateModeGlob,
+			skipRewrite: noSkipRewrite,
+		},
+		{
+			pattern:     base.Append(dyn.Key("root_path")),
+			mode:        TranslateModeDirectory,
+			skipRewrite: noSkipRewrite,
+		},
+	}
+
+	return allPatterns
+}
+
+func pipelineLibrariesRewritePatterns() []pipelineRewritePattern {
 	pipelineEnvironmentsPatterns := []pipelineRewritePattern{
 		{
 			pattern: dyn.NewPattern(
@@ -57,33 +85,7 @@ func pipelineRewritePatterns() []pipelineRewritePattern {
 		},
 	}
 
-	// Compile list of configuration paths to rewrite.
-	allPatterns := []pipelineRewritePattern{
-		{
-			pattern:     base.Append(dyn.Key("libraries"), dyn.AnyIndex(), dyn.Key("notebook"), dyn.Key("path")),
-			mode:        TranslateModeNotebook,
-			skipRewrite: noSkipRewrite,
-		},
-		{
-			pattern:     base.Append(dyn.Key("libraries"), dyn.AnyIndex(), dyn.Key("file"), dyn.Key("path")),
-			mode:        TranslateModeFile,
-			skipRewrite: noSkipRewrite,
-		},
-		{
-			pattern:     base.Append(dyn.Key("libraries"), dyn.AnyIndex(), dyn.Key("glob"), dyn.Key("include")),
-			mode:        TranslateModeGlob,
-			skipRewrite: noSkipRewrite,
-		},
-		{
-			pattern:     base.Append(dyn.Key("root_path")),
-			mode:        TranslateModeDirectory,
-			skipRewrite: noSkipRewrite,
-		},
-	}
-
-	allPatterns = append(allPatterns, pipelineEnvironmentsPatterns...)
-	allPatterns = append(allPatterns, pipelineEnvironmentsPatternsWithPipFlags...)
-	return allPatterns
+	return append(pipelineEnvironmentsPatterns, pipelineEnvironmentsPatternsWithPipFlags...)
 }
 
 func VisitPipelinePaths(value dyn.Value, fn VisitFunc) (dyn.Value, error) {
@@ -91,6 +93,30 @@ func VisitPipelinePaths(value dyn.Value, fn VisitFunc) (dyn.Value, error) {
 	newValue := value
 
 	for _, rewritePattern := range pipelineRewritePatterns() {
+		newValue, err = dyn.MapByPattern(newValue, rewritePattern.pattern, func(p dyn.Path, v dyn.Value) (dyn.Value, error) {
+			sv, ok := v.AsString()
+			if !ok {
+				return v, nil
+			}
+			if rewritePattern.skipRewrite(sv) {
+				return v, nil
+			}
+
+			return fn(p, rewritePattern.mode, v)
+		})
+		if err != nil {
+			return dyn.InvalidValue, err
+		}
+	}
+
+	return newValue, nil
+}
+
+func VisitPipelineLibrariesPaths(value dyn.Value, fn VisitFunc) (dyn.Value, error) {
+	var err error
+	newValue := value
+
+	for _, rewritePattern := range pipelineLibrariesRewritePatterns() {
 		newValue, err = dyn.MapByPattern(newValue, rewritePattern.pattern, func(p dyn.Path, v dyn.Value) (dyn.Value, error) {
 			sv, ok := v.AsString()
 			if !ok {
