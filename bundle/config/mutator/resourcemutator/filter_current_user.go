databricks/cli

commit 9a1cc33aac6077fa937fbb49e572bf7aa3c37293

Ensure owner/mgmt permission for current user (#3780)
## Changes
Instead of removing current user from permissions and relying on
terraform to add it back, we'll instead add IS_OWNER/CAN_MANAGE
ourselves.

Previous attempt to remove this mutator completely
https://github.com/databricks/cli/pull/3688 failed because backend
complains about "ambiguous" permissions when both CAN_MANAGE and
IS_OWNER are present. Thus we do additional transformation here: we
upgrade CAN_MANAGE to IS_OWNER if we can.

Don't apply this logic to secret scopes resource as it's not implemented
via databricks_permissions resource in terraform and does not have
IS_OWNER/CAN_MANAGE insertion logic. This means we no longer filter out
current user permissions from secret scopes resources.

## Why
- Enables direct implementation which will not do any transformations,
just use whatever in the config. With this PR, the request payload will
match terraform's.
- Final permissions visible in 'bundle validate -o json'.

## Tests

https://github.com/databricks/cli/pull/3781

bundle/config/mutator/resourcemutator/filter_current_user.go
@@ -2,85 +2,149 @@ package resourcemutator
 
 import (
 	"context"
+	"slices"
 
 	"github.com/databricks/cli/bundle"
 	"github.com/databricks/cli/libs/diag"
 	"github.com/databricks/cli/libs/dyn"
+	"github.com/databricks/cli/libs/iamutil"
 )
 
-type filterCurrentUser struct{}
+const (
+	isOwner   = "IS_OWNER"
+	canManage = "CAN_MANAGE"
+)
 
-// The databricks terraform provider does not allow changing the permissions of
-// current user. The current user is implied to be the owner of all deployed resources.
-// This mutator removes the current user from the permissions of all resources.
-func FilterCurrentUser() bundle.Mutator {
-	return &filterCurrentUser{}
+// This defines which permissions are considered management permissions.
+// Current user must have one management permission of themselves; if they don't have any,
+// we'll add the first in slice;
+// if they end up having both CAN_MANAGE and IS_OWNER, backend may fail with
+//
+//	Error: cannot create permissions: Permissions being set for UserName([USERNAME]) are ambiguous
+//
+// Since terraform adds IS_OWNER permission when there is not one, regardless of CAN_MANAGE presence,
+// the above error can occur.
+// We thus add another bit of logic: we upgrade CAN_MANAGE to IS_OWNER when we can.
+var managementPermissions = map[string][]string{
+	"jobs":           {isOwner, canManage},
+	"pipelines":      {isOwner, canManage},
+	"sql_warehouses": {isOwner, canManage},
+
+	// nil means "do nothing"
+	"secret_scopes": nil,
 }
 
-func (m *filterCurrentUser) Name() string {
-	return "FilterCurrentUserFromPermissions"
+var defaultManagementPermissions = []string{canManage}
+
+type ensureOwnerPermissions struct{}
+
+// This mutator ensures the current user has the correct permissions for deployed resources.
+func EnsureOwnerPermissions() bundle.Mutator {
+	return &ensureOwnerPermissions{}
 }
 
-func filter(currentUser string) dyn.WalkValueFunc {
+func (m *ensureOwnerPermissions) Name() string {
+	return "EnsureOwnerPermissions"
+}
+
+func ensureCurrentUserPermission(currentUser string) dyn.MapFunc {
 	return func(p dyn.Path, v dyn.Value) (dyn.Value, error) {
-		// Permissions are defined at top level of a resource. We can skip walking
-		// after a depth of 4.
-		// [resource_type].[resource_name].[permissions].[array_index]
-		// Example: pipelines.foo.permissions.0
-		if len(p) > 4 {
-			return v, dyn.ErrSkip
-		}
-
-		// We can skip walking at a depth of 3 if the key is not "permissions".
-		// Example: pipelines.foo.libraries
-		if len(p) == 3 && p[2] != dyn.Key("permissions") {
-			return v, dyn.ErrSkip
-		}
-
-		// We want to be at the level of an individual permission to check it's
-		// user_name and service_principal_name fields.
-		if len(p) != 4 || p[2] != dyn.Key("permissions") {
+		// Extract resource type from path: resources.<resource_type>.<resource_name>.permissions
+		if len(p) != 4 || p[0].Key() != "resources" || p[3].Key() != "permissions" {
 			return v, nil
 		}
 
-		// Filter if the user_name matches the current user
-		userName, ok := v.Get("user_name").AsString()
-		if ok && userName == currentUser {
-			return v, dyn.ErrDrop
+		resourceType := p[1].Key()
+
+		// Determine the required permission level
+		mgmtPerms, ok := managementPermissions[resourceType]
+		if !ok {
+			mgmtPerms = defaultManagementPermissions
 		}
 
-		// Filter if the service_principal_name matches the current user
-		servicePrincipalName, ok := v.Get("service_principal_name").AsString()
-		if ok && servicePrincipalName == currentUser {
-			return v, dyn.ErrDrop
+		if len(mgmtPerms) == 0 {
+			return v, nil
 		}
 
-		return v, nil
+		// Process permissions array
+		return processPermissions(v, currentUser, mgmtPerms)
 	}
 }
 
-func (m *filterCurrentUser) Apply(ctx context.Context, b *bundle.Bundle) diag.Diagnostics {
+func readUser(v dyn.Value) string {
+	userName, _ := dyn.GetValue(v, "user_name").AsString()
+	if userName != "" {
+		return userName
+	}
+	servicePrincipalName, _ := dyn.GetValue(v, "service_principal_name").AsString()
+	return servicePrincipalName
+}
+
+func processPermissions(permissions dyn.Value, currentUser string, mgmtPerms []string) (dyn.Value, error) {
+	priorityPermission := mgmtPerms[0]
+	permissionToUpgrade := -1
+
+	permissionArray, ok := permissions.AsSequence()
+	if !ok {
+		return permissions, nil
+	}
+
+	for ind, permission := range permissionArray {
+		level, ok := dyn.GetValue(permission, "level").AsString()
+		if !ok {
+			continue
+		}
+		user := readUser(permission)
+		if user == "" || user != currentUser {
+			continue
+		}
+		if level == priorityPermission {
+			// already have required permission (IS_OWNER)
+			return permissions, nil
+		}
+		if slices.Contains(mgmtPerms, level) {
+			// has management permission that needs to be upgraded to IS_OWNER
+			// continue the loop; if we is IS_OWNER we will still bail
+			permissionToUpgrade = ind
+		}
+	}
+
+	if permissionToUpgrade >= 0 {
+		v, _ := dyn.Set(permissionArray[permissionToUpgrade], "level", dyn.V(priorityPermission))
+		permissionArray[permissionToUpgrade] = v
+	} else {
+		permissionArray = append(permissionArray, createPermission(currentUser, priorityPermission))
+	}
+
+	return dyn.V(permissionArray), nil
+}
+
+func createPermission(user, level string) dyn.Value {
+	permission := map[string]dyn.Value{
+		"level": dyn.V(level),
+	}
+
+	// Determine if currentUser is a service principal or user
+	if iamutil.IsServicePrincipalName(user) {
+		permission["service_principal_name"] = dyn.V(user)
+	} else {
+		permission["user_name"] = dyn.V(user)
+	}
+
+	return dyn.V(permission)
+}
+
+func (m *ensureOwnerPermissions) Apply(ctx context.Context, b *bundle.Bundle) diag.Diagnostics {
 	currentUser := b.Config.Workspace.CurrentUser.UserName
 
 	err := b.Config.Mutate(func(v dyn.Value) (dyn.Value, error) {
-		rv, err := dyn.Get(v, "resources")
-		if err != nil {
-			// If the resources key is not found, we can skip this mutator.
-			if dyn.IsNoSuchKeyError(err) {
-				return v, nil
-			}
-
-			return dyn.InvalidValue, err
-		}
-
-		// Walk the resources and filter out the current user from the permissions
-		nv, err := dyn.Walk(rv, filter(currentUser))
-		if err != nil {
-			return dyn.InvalidValue, err
-		}
-
-		// Set the resources with the filtered permissions back into the bundle
-		return dyn.Set(v, "resources", nv)
+		// Use MapByPattern to directly process permissions arrays
+		return dyn.MapByPattern(v, dyn.NewPattern(
+			dyn.Key("resources"),
+			dyn.AnyKey(),
+			dyn.AnyKey(),
+			dyn.Key("permissions"),
+		), ensureCurrentUserPermission(currentUser))
 	})
 
 	return diag.FromErr(err)

acceptance/bundle/resources/permissions/jobs/current_can_manage/databricks.yml
@@ -14,6 +14,9 @@ resources:
           group_name: data-team
         - level: CAN_MANAGE
           service_principal_name: f37d18cd-98a8-4db5-8112-12dd0a6bfe38
-        # job with explicit CAN_MANAGE matching username, will be filtered out by DABs; terraform will add IS_OWNER
+        # job with explicit CAN_MANAGE matching username, we will upgrade CAN_MANAGE to IS_OWNER
+        # If we don't, then terraform will add IS_OWNER and we will end up with both CAN_MANAGE and IS_OWNER
+        # which can lead to
+        #    Error: cannot create permissions: Permissions being set for UserName([USERNAME]) are ambiguous
         - level: CAN_MANAGE
           user_name: tester@databricks.com

acceptance/bundle/resources/secret_scopes/permissions/databricks.yml.tmpl
@@ -14,7 +14,7 @@ resources:
           level: MANAGE
 
 permissions:
-  - user_name: $CURRENT_USER_NAME # this will be omitted by `FilterCurrentUser`
+  - user_name: $CURRENT_USER_NAME
     level: CAN_MANAGE
   - group_name: users
     level: CAN_VIEW # this is mapped to READ

bundle/config/mutator/resourcemutator/filter_current_user_test.go
@@ -1,266 +1,0 @@
-package resourcemutator
-
-import (
-	"context"
-	"testing"
-
-	"github.com/databricks/cli/bundle/permissions"
-
-	"github.com/databricks/cli/bundle"
-	"github.com/databricks/cli/bundle/config"
-	"github.com/databricks/cli/bundle/config/resources"
-	"github.com/databricks/databricks-sdk-go/service/iam"
-	"github.com/databricks/databricks-sdk-go/service/jobs"
-	"github.com/stretchr/testify/assert"
-)
-
-var jobAlice = resources.JobPermission{
-	Level:    "CAN_MANAGE",
-	UserName: "alice@databricks.com",
-}
-
-var jobBob = resources.JobPermission{
-	Level:    "CAN_VIEW",
-	UserName: "bob@databricks.com",
-}
-
-var jobRobot = resources.JobPermission{
-	Level:                "CAN_MANAGE_RUN",
-	ServicePrincipalName: "i-Robot",
-}
-
-var pipelineAlice = resources.PipelinePermission{
-	Level:    permissions.CAN_MANAGE,
-	UserName: "alice@databricks.com",
-}
-
-var pipelineBob = resources.PipelinePermission{
-	Level:    permissions.CAN_VIEW,
-	UserName: "bob@databricks.com",
-}
-
-var pipelineRobot = resources.PipelinePermission{
-	Level:                permissions.CAN_RUN,
-	ServicePrincipalName: "i-Robot",
-}
-
-var experimentAlice = resources.MlflowExperimentPermission{
-	Level:    permissions.CAN_MANAGE,
-	UserName: "alice@databricks.com",
-}
-
-var experimentBob = resources.MlflowExperimentPermission{
-	Level:    permissions.CAN_VIEW,
-	UserName: "bob@databricks.com",
-}
-
-var experimentRobot = resources.MlflowExperimentPermission{
-	Level:                permissions.CAN_RUN,
-	ServicePrincipalName: "i-Robot",
-}
-
-var modelAlice = resources.MlflowModelPermission{
-	Level:    permissions.CAN_MANAGE,
-	UserName: "alice@databricks.com",
-}
-
-var modelBob = resources.MlflowModelPermission{
-	Level:    permissions.CAN_VIEW,
-	UserName: "bob@databricks.com",
-}
-
-var modelRobot = resources.MlflowModelPermission{
-	Level:                permissions.CAN_RUN,
-	ServicePrincipalName: "i-Robot",
-}
-
-var endpointAlice = resources.ModelServingEndpointPermission{
-	Level:    permissions.CAN_MANAGE,
-	UserName: "alice@databricks.com",
-}
-
-var endpointBob = resources.ModelServingEndpointPermission{
-	Level:    permissions.CAN_VIEW,
-	UserName: "bob@databricks.com",
-}
-
-var endpointRobot = resources.ModelServingEndpointPermission{
-	Level:                permissions.CAN_RUN,
-	ServicePrincipalName: "i-Robot",
-}
-
-func testFixture(userName string) *bundle.Bundle {
-	jobPermissions := []resources.JobPermission{
-		jobAlice,
-		jobBob,
-		jobRobot,
-	}
-
-	pipelinePermissions := []resources.PipelinePermission{
-		pipelineAlice,
-		pipelineBob,
-		pipelineRobot,
-	}
-
-	experimentPermissions := []resources.MlflowExperimentPermission{
-		experimentAlice,
-		experimentBob,
-		experimentRobot,
-	}
-
-	modelPermissions := []resources.MlflowModelPermission{
-		modelAlice,
-		modelBob,
-		modelRobot,
-	}
-
-	endpointPermissions := []resources.ModelServingEndpointPermission{
-		endpointAlice,
-		endpointBob,
-		endpointRobot,
-	}
-
-	return &bundle.Bundle{
-		Config: config.Root{
-			Workspace: config.Workspace{
-				CurrentUser: &config.User{
-					User: &iam.User{
-						UserName: userName,
-					},
-				},
-			},
-			Resources: config.Resources{
-				Jobs: map[string]*resources.Job{
-					"job1": {
-						JobSettings: jobs.JobSettings{
-							Name: "job1",
-						},
-						Permissions: jobPermissions,
-					},
-					"job2": {
-						JobSettings: jobs.JobSettings{
-							Name: "job2",
-						},
-						Permissions: jobPermissions,
-					},
-				},
-				Pipelines: map[string]*resources.Pipeline{
-					"pipeline1": {
-						Permissions: pipelinePermissions,
-					},
-				},
-				Experiments: map[string]*resources.MlflowExperiment{
-					"experiment1": {
-						Permissions: experimentPermissions,
-					},
-				},
-				Models: map[string]*resources.MlflowModel{
-					"model1": {
-						Permissions: modelPermissions,
-					},
-				},
-				ModelServingEndpoints: map[string]*resources.ModelServingEndpoint{
-					"endpoint1": {
-						Permissions: endpointPermissions,
-					},
-				},
-				RegisteredModels: map[string]*resources.RegisteredModel{
-					"registered_model1": {
-						Grants: []resources.Grant{
-							{
-								Principal: "abc",
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-}
-
-func TestFilterCurrentUser(t *testing.T) {
-	b := testFixture("alice@databricks.com")
-
-	diags := bundle.Apply(context.Background(), b, FilterCurrentUser())
-	assert.NoError(t, diags.Error())
-
-	// Assert current user is filtered out.
-	assert.Len(t, b.Config.Resources.Jobs["job1"].Permissions, 2)
-	assert.Contains(t, b.Config.Resources.Jobs["job1"].Permissions, jobRobot)
-	assert.Contains(t, b.Config.Resources.Jobs["job1"].Permissions, jobBob)
-
-	assert.Len(t, b.Config.Resources.Jobs["job2"].Permissions, 2)
-	assert.Contains(t, b.Config.Resources.Jobs["job2"].Permissions, jobRobot)
-	assert.Contains(t, b.Config.Resources.Jobs["job2"].Permissions, jobBob)
-
-	assert.Len(t, b.Config.Resources.Pipelines["pipeline1"].Permissions, 2)
-	assert.Contains(t, b.Config.Resources.Pipelines["pipeline1"].Permissions, pipelineRobot)
-	assert.Contains(t, b.Config.Resources.Pipelines["pipeline1"].Permissions, pipelineBob)
-
-	assert.Len(t, b.Config.Resources.Experiments["experiment1"].Permissions, 2)
-	assert.Contains(t, b.Config.Resources.Experiments["experiment1"].Permissions, experimentRobot)
-	assert.Contains(t, b.Config.Resources.Experiments["experiment1"].Permissions, experimentBob)
-
-	assert.Len(t, b.Config.Resources.Models["model1"].Permissions, 2)
-	assert.Contains(t, b.Config.Resources.Models["model1"].Permissions, modelRobot)
-	assert.Contains(t, b.Config.Resources.Models["model1"].Permissions, modelBob)
-
-	assert.Len(t, b.Config.Resources.ModelServingEndpoints["endpoint1"].Permissions, 2)
-	assert.Contains(t, b.Config.Resources.ModelServingEndpoints["endpoint1"].Permissions, endpointRobot)
-	assert.Contains(t, b.Config.Resources.ModelServingEndpoints["endpoint1"].Permissions, endpointBob)
-
-	// Assert there's no change to the grant.
-	assert.Len(t, b.Config.Resources.RegisteredModels["registered_model1"].Grants, 1)
-}
-
-func TestFilterCurrentServicePrincipal(t *testing.T) {
-	b := testFixture("i-Robot")
-
-	diags := bundle.Apply(context.Background(), b, FilterCurrentUser())
-	assert.NoError(t, diags.Error())
-
-	// Assert current user is filtered out.
-	assert.Len(t, b.Config.Resources.Jobs["job1"].Permissions, 2)
-	assert.Contains(t, b.Config.Resources.Jobs["job1"].Permissions, jobAlice)
-	assert.Contains(t, b.Config.Resources.Jobs["job1"].Permissions, jobBob)
-
-	assert.Len(t, b.Config.Resources.Jobs["job2"].Permissions, 2)
-	assert.Contains(t, b.Config.Resources.Jobs["job2"].Permissions, jobAlice)
-	assert.Contains(t, b.Config.Resources.Jobs["job2"].Permissions, jobBob)
-
-	assert.Len(t, b.Config.Resources.Pipelines["pipeline1"].Permissions, 2)
-	assert.Contains(t, b.Config.Resources.Pipelines["pipeline1"].Permissions, pipelineAlice)
-	assert.Contains(t, b.Config.Resources.Pipelines["pipeline1"].Permissions, pipelineBob)
-
-	assert.Len(t, b.Config.Resources.Experiments["experiment1"].Permissions, 2)
-	assert.Contains(t, b.Config.Resources.Experiments["experiment1"].Permissions, experimentAlice)
-	assert.Contains(t, b.Config.Resources.Experiments["experiment1"].Permissions, experimentBob)
-
-	assert.Len(t, b.Config.Resources.Models["model1"].Permissions, 2)
-	assert.Contains(t, b.Config.Resources.Models["model1"].Permissions, modelAlice)
-	assert.Contains(t, b.Config.Resources.Models["model1"].Permissions, modelBob)
-
-	assert.Len(t, b.Config.Resources.ModelServingEndpoints["endpoint1"].Permissions, 2)
-	assert.Contains(t, b.Config.Resources.ModelServingEndpoints["endpoint1"].Permissions, endpointAlice)
-	assert.Contains(t, b.Config.Resources.ModelServingEndpoints["endpoint1"].Permissions, endpointBob)
-
-	// Assert there's no change to the grant.
-	assert.Len(t, b.Config.Resources.RegisteredModels["registered_model1"].Grants, 1)
-}
-
-func TestFilterCurrentUserDoesNotErrorWhenNoResources(t *testing.T) {
-	b := &bundle.Bundle{
-		Config: config.Root{
-			Workspace: config.Workspace{
-				CurrentUser: &config.User{
-					User: &iam.User{
-						UserName: "abc",
-					},
-				},
-			},
-		},
-	}
-
-	diags := bundle.Apply(context.Background(), b, FilterCurrentUser())
-	assert.NoError(t, diags.Error())
-}

bundle/config/mutator/resourcemutator/resource_mutator.go
@@ -118,9 +118,8 @@ func applyInitializeMutators(ctx context.Context, b *bundle.Bundle) {
 		ApplyBundlePermissions(),
 
 		// Reads (typed): b.Config.Workspace.CurrentUser.UserName (gets current user name)
-		// Updates (dynamic): resources.*.*.permissions (removes permissions entries where user_name or service_principal_name matches current user)
-		// Removes the current user from all resource permissions as the Terraform provider implicitly grants ownership
-		FilterCurrentUser(),
+		// Updates (dynamic): resources.*.*.permissions
+		EnsureOwnerPermissions(),
 	)
 }
 

libs/dyn/visit_get.go
@@ -10,6 +10,12 @@ func Get(v Value, path string) (Value, error) {
 	return GetByPath(v, p)
 }
 
+// GetValue returns the value without the error (InvalidValue indicates that error did happen)
+func GetValue(v Value, path string) Value {
+	val, _ := Get(v, path)
+	return val
+}
+
 // GetByPath returns the value inside the specified value at the specified path.
 // If the path doesn't exist, it returns InvalidValue and an error.
 func GetByPath(v Value, p Path) (Value, error) {

libs/iamutil/service_principal.go
@@ -11,6 +11,10 @@ import (
 // slow for our purposes. And the "users" and "service principals get" APIs
 // only allow access by workspace admins.
 func IsServicePrincipal(user *iam.User) bool {
-	_, err := uuid.Parse(user.UserName)
+	return IsServicePrincipalName(user.UserName)
+}
+
+func IsServicePrincipalName(userName string) bool {
+	_, err := uuid.Parse(userName)
 	return err == nil
 }

commit fb3b2700ae01cbad2d4cd845b545403510d3f856

Fix permissions logic not to add 2nd IS_OWNER (#3850)
## Changes
When processing permissions, do not add 2nd IS_OWNER for current user.

Bug introduced in https://github.com/databricks/cli/pull/3780

## Why
It was always the intention.

https://github.com/databricks/cli/issues/3849

## Tests
Existing tests.

bundle/config/mutator/resourcemutator/filter_current_user.go
@@ -2,7 +2,6 @@ package resourcemutator
 
 import (
 	"context"
-	"slices"
 
 	"github.com/databricks/cli/bundle"
 	"github.com/databricks/cli/libs/diag"
@@ -15,26 +14,26 @@ const (
 	canManage = "CAN_MANAGE"
 )
 
-// This defines which permissions are considered management permissions.
-// Current user must have one management permission of themselves; if they don't have any,
-// we'll add the first in slice;
-// if they end up having both CAN_MANAGE and IS_OWNER, backend may fail with
-//
-//	Error: cannot create permissions: Permissions being set for UserName([USERNAME]) are ambiguous
-//
-// Since terraform adds IS_OWNER permission when there is not one, regardless of CAN_MANAGE presence,
-// the above error can occur.
-// We thus add another bit of logic: we upgrade CAN_MANAGE to IS_OWNER when we can.
-var managementPermissions = map[string][]string{
-	"jobs":           {isOwner, canManage},
-	"pipelines":      {isOwner, canManage},
-	"sql_warehouses": {isOwner, canManage},
-
-	// nil means "do nothing"
-	"secret_scopes": nil,
+// Which resources support IS_OWNER permission.
+var hasIsOwner = map[string]bool{
+	"jobs":           true,
+	"pipelines":      true,
+	"sql_warehouses": true,
 }
 
-var defaultManagementPermissions = []string{canManage}
+var ignoredResources = map[string]bool{
+	"secret_scopes": true,
+}
+
+// When processing permissions, we need to implement these constraints:
+// 1. There should be no more than one IS_OWNER settings for a given resource.
+// 2. We should automatically add IS_OWNER for current user (for resources that support it) or CAN_MANAGE permission.
+//    Terraform will do this, so doing this early makes request equal between terraform and direct.
+// 3. We prefer to add IS_OWNER, unless there already is one, in which case we fallback to CAN_MANAGE.
+// 4. Current user cannot have both CAN_MANAGE and IS_OWNER, we've seen backend failing with
+//    "Error: cannot create permissions: Permissions being set for UserName([USERNAME]) are ambiguous"
+//    Since terraform adds IS_OWNER permission when there is not one, regardless of CAN_MANAGE presence,
+//    he above error can occur. We thus add another bit of logic: we upgrade CAN_MANAGE to IS_OWNER when we can.
 
 type ensureOwnerPermissions struct{}
 
@@ -55,19 +54,11 @@ func ensureCurrentUserPermission(currentUser string) dyn.MapFunc {
 		}
 
 		resourceType := p[1].Key()
-
-		// Determine the required permission level
-		mgmtPerms, ok := managementPermissions[resourceType]
-		if !ok {
-			mgmtPerms = defaultManagementPermissions
-		}
-
-		if len(mgmtPerms) == 0 {
+		if ignoredResources[resourceType] {
 			return v, nil
 		}
 
-		// Process permissions array
-		return processPermissions(v, currentUser, mgmtPerms)
+		return processPermissions(v, currentUser, resourceType)
 	}
 }
 
@@ -80,9 +71,10 @@ func readUser(v dyn.Value) string {
 	return servicePrincipalName
 }
 
-func processPermissions(permissions dyn.Value, currentUser string, mgmtPerms []string) (dyn.Value, error) {
-	priorityPermission := mgmtPerms[0]
-	permissionToUpgrade := -1
+func processPermissions(permissions dyn.Value, currentUser, resourceType string) (dyn.Value, error) {
+	currentUserHasIsOwner := false
+	currentUserIndCanManage := -1
+	canAddIsOwner := hasIsOwner[resourceType]
 
 	permissionArray, ok := permissions.AsSequence()
 	if !ok {
@@ -95,25 +87,35 @@ func processPermissions(permissions dyn.Value, currentUser string, mgmtPerms []s
 			continue
 		}
 		user := readUser(permission)
-		if user == "" || user != currentUser {
-			continue
+		if level == isOwner {
+			canAddIsOwner = false
+			if user == currentUser {
+				currentUserHasIsOwner = true
+			}
 		}
-		if level == priorityPermission {
-			// already have required permission (IS_OWNER)
-			return permissions, nil
-		}
-		if slices.Contains(mgmtPerms, level) {
-			// has management permission that needs to be upgraded to IS_OWNER
-			// continue the loop; if we is IS_OWNER we will still bail
-			permissionToUpgrade = ind
+		if user == currentUser && level == canManage {
+			currentUserIndCanManage = ind
 		}
 	}
 
-	if permissionToUpgrade >= 0 {
-		v, _ := dyn.Set(permissionArray[permissionToUpgrade], "level", dyn.V(priorityPermission))
-		permissionArray[permissionToUpgrade] = v
-	} else {
-		permissionArray = append(permissionArray, createPermission(currentUser, priorityPermission))
+	if currentUserHasIsOwner {
+		return dyn.V(permissionArray), nil
+	}
+
+	if canAddIsOwner {
+		if currentUserIndCanManage >= 0 {
+			// Upgrade current user's CAN_MANAGE to IS_OWNER. We do this because terraform will add IS_OWNER if it does not see one
+			// and that may confuse backend. We can stop doing it when removed terraform.
+			v, _ := dyn.Set(permissionArray[currentUserIndCanManage], "level", dyn.V(isOwner))
+			permissionArray[currentUserIndCanManage] = v
+		} else {
+			permissionArray = append(permissionArray, createPermission(currentUser, isOwner))
+		}
+		return dyn.V(permissionArray), nil
+	}
+
+	if currentUserIndCanManage < 0 {
+		permissionArray = append(permissionArray, createPermission(currentUser, canManage))
 	}
 
 	return dyn.V(permissionArray), nil