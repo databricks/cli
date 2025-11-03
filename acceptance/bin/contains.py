33e57c1306930d375e3b69e6ec38153a9735f697
Denis Bilenko <denis.bilenko@databricks.com>
2025-11-03 11:24:56 +0100

    acc: add negative test to contains.py

--- acceptance/bin/contains.py
+++ acceptance/bin/contains.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python3
 import sys
 
-targets = sys.argv[1:]
-assert targets
+must_find = []
+must_not_find = []
+for arg in sys.argv[1:]:
+    if arg.startswith("!"):
+        must_not_find.append(arg[1:])
+    else:
+        must_find.append(arg)
+
 found = set()
 
 for line in sys.stdin:
     sys.stdout.write(line)
-    for t in targets:
+    for t in must_find:
         if t in line:
             found.add(t)
+    for t in must_not_find:
+        if t in line:
+            sys.stderr.write(f"contains error: {t!r} was not expected\n")
 
 sys.stdout.flush()
 
-not_found = set(targets) - found
+not_found = set(must_find) - found
 for item in sorted(not_found):
     sys.stderr.write(f"contains error: {item!r} not found in the output.\n")
