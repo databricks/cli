8126230761c89347ea3dff228867b2462e6d5169
shreyas-goenka <88374338+shreyas-goenka@users.noreply.github.com>
2025-11-05 17:38:13 +0100

    Add validation and translation for served_models and served_entities in serving endpoints (#3880)
    ## Changes && Why
    `served_models` was deprecated and the fields in it were moved to
    `served_endpoints`. The GET API response for a model serving endpoint
    returns with both `served_models` and `served_entities` even if one is
    set on the client side (not AIP complaint).
    
    In order to simplify the direct deployment logic, this PR translates the
    `served_models` config to `served_entities`, so that we can keep the
    core diff logic in direct deployment simple.
    
    Note: This is not a breaking change. TF also had the same logic with a
    conflicts_with annotation for `served_models` and `served_entities`
    
    
    ## Tests
    New acceptance tests. Manually confirmed that a deployment succeeds with
    no issues.

--- NEXT_CHANGELOG.md
+++ NEXT_CHANGELOG.md
@@ -9,5 +9,6 @@
 ### Dependency updates
 
 ### Bundles
+* Add validation that served_models and served_entities are not used at the same time. Add client side translation logic. ([#3880](https://github.com/databricks/cli/pull/3880))
 
 ### API Changes
---
+++ bundle/config/mutator/resourcemutator/model_serving_endpoint_fixups.go
@@ -0,0 +1,89 @@
+package resourcemutator
+
+import (
+	"context"
+
+	"github.com/databricks/cli/bundle"
+	"github.com/databricks/cli/libs/diag"
+	"github.com/databricks/cli/libs/dyn"
+	"github.com/databricks/cli/libs/utils"
+	"github.com/databricks/databricks-sdk-go/service/serving"
+)
+
+type modelServingEndpointFixups struct{}
+
+func ModelServingEndpointFixups() bundle.Mutator {
+	return &modelServingEndpointFixups{}
+}
+
+func (m *modelServingEndpointFixups) Name() string {
+	return "ModelServingEndpointFixups"
+}
+
+func servedModelToServedEntity(model serving.ServedModelInput) serving.ServedEntityInput {
+	return serving.ServedEntityInput{
+		// served_models does not support ExternalModel, so we set ExternalModel to nil
+		ExternalModel: nil,
+
+		EntityName:                model.ModelName,
+		EntityVersion:             model.ModelVersion,
+		EnvironmentVars:           model.EnvironmentVars,
+		InstanceProfileArn:        model.InstanceProfileArn,
+		MaxProvisionedThroughput:  model.MaxProvisionedThroughput,
+		MinProvisionedThroughput:  model.MinProvisionedThroughput,
+		Name:                      model.Name,
+		ProvisionedModelUnits:     model.ProvisionedModelUnits,
+		ScaleToZeroEnabled:        model.ScaleToZeroEnabled,
+		WorkloadSize:              model.WorkloadSize,
+		WorkloadType:              serving.ServingModelWorkloadType(model.WorkloadType),
+		MaxProvisionedConcurrency: model.MaxProvisionedConcurrency,
+		MinProvisionedConcurrency: model.MinProvisionedConcurrency,
+		ForceSendFields:           utils.FilterFields[serving.ServedEntityInput](model.ForceSendFields),
+	}
+}
+
+func (m *modelServingEndpointFixups) Apply(ctx context.Context, b *bundle.Bundle) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	for key, endpoint := range b.Config.Resources.ModelServingEndpoints {
+		if endpoint == nil || endpoint.Config == nil {
+			continue
+		}
+
+		// Validate that both ServedModels and ServedEntities are not used at the same time.
+		if len(endpoint.Config.ServedModels) > 0 && len(endpoint.Config.ServedEntities) > 0 {
+			diags = diags.Append(diag.Diagnostic{
+				Severity: diag.Error,
+				Summary:  "Cannot use both served_models and served_entities",
+				Detail:   "Model serving endpoint cannot specify both served_models and served_entities at the same time.",
+				Locations: []dyn.Location{
+					b.Config.GetLocation("resources.model_serving_endpoints." + key),
+				},
+			})
+			continue
+		}
+
+		// Convert ServedModels to ServedEntities if specified. ServedModels is a deprecated field, and the service recommends using ServedEntities instead.
+		// We perform this translation here so that the deployment plan only has to detect served_entities and can ignore served_models.
+		if len(endpoint.Config.ServedModels) > 0 {
+			// Add warning recommending served_entities
+			diags = diags.Append(diag.Diagnostic{
+				Severity: diag.Warning,
+				Summary:  "Using served_models is deprecated",
+				Detail:   "The served_models field is deprecated. Please use served_entities instead.",
+				Locations: []dyn.Location{
+					b.Config.GetLocation("resources.model_serving_endpoints." + key + ".config.served_models"),
+				},
+			})
+
+			endpoint.Config.ServedEntities = make([]serving.ServedEntityInput, len(endpoint.Config.ServedModels))
+			for i, model := range endpoint.Config.ServedModels {
+				endpoint.Config.ServedEntities[i] = servedModelToServedEntity(model)
+			}
+			// Clear ServedModels after conversion
+			endpoint.Config.ServedModels = nil
+		}
+	}
+
+	return diags
+}
