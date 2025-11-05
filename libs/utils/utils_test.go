34cf396c767a5ad56583d1327bfe1ac74d111c63
Shreyas Goenka <shreyas.goenka@databricks.com>
2025-11-05 17:21:50 +0100

    move test

---
+++ libs/utils/utils_test.go
@@ -0,0 +1,15 @@
+package utils
+
+import (
+	"testing"
+
+	"github.com/databricks/databricks-sdk-go/service/catalog"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFilterFields(t *testing.T) {
+	fields := []string{"Comment", "NewName", "Owner", "Name", "NotExistingField"}
+	result := FilterFields[catalog.UpdateVolumeRequestContent](fields, "NewName", "Owner")
+	expected := []string{"Comment", "Name"}
+	assert.Equal(t, expected, result)
+}
