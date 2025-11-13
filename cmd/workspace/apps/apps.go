ffe6e3de1f26d28031cad5561c785451236e9444
Eric Feunekes <eric.feunekes@pwc.com>
2025-11-13 10:54:12 -0400

    Register apps logs command in apps.go

--- cmd/workspace/apps/apps.go
+++ cmd/workspace/apps/apps.go
@@ -49,6 +49,7 @@ func New() *cobra.Command {
 	cmd.AddCommand(newStop())
 	cmd.AddCommand(newUpdate())
 	cmd.AddCommand(newUpdatePermissions())
+	cmd.AddCommand(newLogsCommand())
 
 	// Apply optional overrides to this command.
 	for _, fn := range cmdOverrides {
