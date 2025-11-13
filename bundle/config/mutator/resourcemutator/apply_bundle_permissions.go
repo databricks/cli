4f15f593dc52051131412e72aef7dbc8f6fe5d8c
Denis Bilenko <denis.bilenko@databricks.com>
2025-11-13 13:02:55 +0100

    fixes

--- bundle/config/mutator/resourcemutator/apply_bundle_permissions.go
+++ bundle/config/mutator/resourcemutator/apply_bundle_permissions.go
@@ -15,8 +15,6 @@ import (
 	"github.com/databricks/cli/libs/dyn/convert"
 )
 
-var unsupportedResources = []string{"volumes", "schemas", "quality_monitors", "registered_models", "database_catalogs", "synced_database_tables"}
-
 var (
 	allowedLevels = []string{permissions.CAN_MANAGE, permissions.CAN_VIEW, permissions.CAN_RUN}
 	levelsMap     = map[string](map[string]string){
