4d986cb270eb775d53f705d8b653a8a085416fed
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2025-12-08 21:48:24 +0100

    Upgrade Terraform provider to v1.99.0 (#4113)
    ## Changes
    
    See
    https://github.com/databricks/terraform-provider-databricks/releases/tag/v1.99.0
    
    ## Why
    
    Nominal upgrade to match the SDK version.

---
+++ bundle/internal/tf/schema/resource_workspace_entity_tag_assignment.go
@@ -0,0 +1,10 @@
+// Generated from Databricks Terraform provider schema. DO NOT EDIT.
+
+package schema
+
+type ResourceWorkspaceEntityTagAssignment struct {
+	EntityId   string `json:"entity_id"`
+	EntityType string `json:"entity_type"`
+	TagKey     string `json:"tag_key"`
+	TagValue   string `json:"tag_value,omitempty"`
+}
