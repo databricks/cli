f5113cef37756a8bb2b532b6445acc39d012ae0a
Ilya Kuznetsov <ilya.kuznetsov@databricks.com>
2026-01-02 17:32:23 +0100

    Make variable name private

--- bundle/env/experimental_yaml_sync.go
+++ bundle/env/experimental_yaml_sync.go
@@ -4,12 +4,12 @@ import "context"
 
 // ExperimentalYamlSyncVariable names the environment variable that holds the flag whether
 // experimental YAML sync is enabled.
-const ExperimentalYamlSyncVariable = "DATABRICKS_BUNDLE_ENABLE_EXPERIMENTAL_YAML_SYNC"
+const experimentalYamlSyncVariable = "DATABRICKS_BUNDLE_ENABLE_EXPERIMENTAL_YAML_SYNC"
 
 // ExperimentalYamlSync returns the environment variable that holds the flag whether
 // experimental YAML sync is enabled.
 func ExperimentalYamlSync(ctx context.Context) (string, bool) {
 	return get(ctx, []string{
-		ExperimentalYamlSyncVariable,
+		experimentalYamlSyncVariable,
 	})
 }
