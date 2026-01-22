5926f6ebd74ccdf8f2b0a2645084d53426d6a644
Andrew Nester <andrew.nester.dev@gmail.com>
2026-01-22 12:00:16 +0100

    Upgrade TF provider to 1.104.0 (#4354)
    ## Changes
    Upgrade TF provider to 1.104.0
    
    
    <!-- If your PR needs to be included in the release notes for next
    release,
    add a separate entry in NEXT_CHANGELOG.md as part of your PR. -->

--- bundle/internal/tf/schema/resource_postgres_branch.go
+++ bundle/internal/tf/schema/resource_postgres_branch.go
@@ -3,16 +3,19 @@
 package schema
 
 type ResourcePostgresBranchSpec struct {
-	Default          bool   `json:"default,omitempty"`
+	ExpireTime       string `json:"expire_time,omitempty"`
 	IsProtected      bool   `json:"is_protected,omitempty"`
+	NoExpiry         bool   `json:"no_expiry,omitempty"`
 	SourceBranch     string `json:"source_branch,omitempty"`
 	SourceBranchLsn  string `json:"source_branch_lsn,omitempty"`
 	SourceBranchTime string `json:"source_branch_time,omitempty"`
+	Ttl              string `json:"ttl,omitempty"`
 }
 
 type ResourcePostgresBranchStatus struct {
 	CurrentState     string `json:"current_state,omitempty"`
 	Default          bool   `json:"default,omitempty"`
+	ExpireTime       string `json:"expire_time,omitempty"`
 	IsProtected      bool   `json:"is_protected,omitempty"`
 	LogicalSizeBytes int    `json:"logical_size_bytes,omitempty"`
 	PendingState     string `json:"pending_state,omitempty"`
@@ -23,7 +26,7 @@ type ResourcePostgresBranchStatus struct {
 }
 
 type ResourcePostgresBranch struct {
-	BranchId   string                        `json:"branch_id,omitempty"`
+	BranchId   string                        `json:"branch_id"`
 	CreateTime string                        `json:"create_time,omitempty"`
 	Name       string                        `json:"name,omitempty"`
 	Parent     string                        `json:"parent"`
