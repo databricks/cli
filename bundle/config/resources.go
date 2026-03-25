0d8d10a12bb10a56f072d27c7a72f626beb074e6
Jan Rose <jan.rose@databricks.com>
2026-03-25 14:25:09 +0100

    Add bundle/config/resources

--- bundle/config/resources.go
+++ bundle/config/resources.go
@@ -35,6 +35,7 @@ type Resources struct {
 	PostgresProjects      map[string]*resources.PostgresProject      `json:"postgres_projects,omitempty"`
 	PostgresBranches      map[string]*resources.PostgresBranch       `json:"postgres_branches,omitempty"`
 	PostgresEndpoints     map[string]*resources.PostgresEndpoint     `json:"postgres_endpoints,omitempty"`
+	VectorSearchEndpoints map[string]*resources.VectorSearchEndpoint `json:"vector_search_endpoints,omitempty"`
 }
 
 type ConfigResource interface {
@@ -108,6 +109,7 @@ func (r *Resources) AllResources() []ResourceGroup {
 		collectResourceMap(descriptions["postgres_projects"], r.PostgresProjects),
 		collectResourceMap(descriptions["postgres_branches"], r.PostgresBranches),
 		collectResourceMap(descriptions["postgres_endpoints"], r.PostgresEndpoints),
+		collectResourceMap(descriptions["vector_search_endpoints"], r.VectorSearchEndpoints),
 	}
 }
 
@@ -162,5 +164,6 @@ func SupportedResources() map[string]resources.ResourceDescription {
 		"postgres_projects":       (&resources.PostgresProject{}).ResourceDescription(),
 		"postgres_branches":       (&resources.PostgresBranch{}).ResourceDescription(),
 		"postgres_endpoints":      (&resources.PostgresEndpoint{}).ResourceDescription(),
+		"vector_search_endpoints": (&resources.VectorSearchEndpoint{}).ResourceDescription(),
 	}
 }
---
+++ bundle/config/resources/vector_search_endpoint.go
@@ -0,0 +1,58 @@
+package resources
+
+import (
+	"context"
+	"net/url"
+
+	"github.com/databricks/cli/libs/log"
+	"github.com/databricks/databricks-sdk-go"
+	"github.com/databricks/databricks-sdk-go/marshal"
+	"github.com/databricks/databricks-sdk-go/service/vectorsearch"
+)
+
+type VectorSearchEndpoint struct {
+	BaseResource
+	vectorsearch.CreateEndpoint
+}
+
+func (e *VectorSearchEndpoint) UnmarshalJSON(b []byte) error {
+	return marshal.Unmarshal(b, e)
+}
+
+func (e VectorSearchEndpoint) MarshalJSON() ([]byte, error) {
+	return marshal.Marshal(e)
+}
+
+func (e *VectorSearchEndpoint) Exists(ctx context.Context, w *databricks.WorkspaceClient, name string) (bool, error) {
+	_, err := w.VectorSearchEndpoints.GetEndpoint(ctx, vectorsearch.GetEndpointRequest{EndpointName: name})
+	if err != nil {
+		log.Debugf(ctx, "vector search endpoint %s does not exist", name)
+		return false, err
+	}
+	return true, nil
+}
+
+func (e *VectorSearchEndpoint) ResourceDescription() ResourceDescription {
+	return ResourceDescription{
+		SingularName:  "vector_search_endpoint",
+		PluralName:    "vector_search_endpoints",
+		SingularTitle: "Vector Search Endpoint",
+		PluralTitle:   "Vector Search Endpoints",
+	}
+}
+
+func (e *VectorSearchEndpoint) InitializeURL(baseURL url.URL) {
+	if e.ID == "" {
+		return
+	}
+	baseURL.Path = "compute/vector-search/" + e.ID
+	e.URL = baseURL.String()
+}
+
+func (e *VectorSearchEndpoint) GetName() string {
+	return e.Name
+}
+
+func (e *VectorSearchEndpoint) GetURL() string {
+	return e.URL
+}
