1c84f9f1ae5feae229afdbe6126dc332289dda0a
Andrew Nester <andrew.nester.dev@gmail.com>
2026-04-08 17:11:52 +0200

    Use filter option with service principal API when lookup variable used (#4913)
    ## Changes
    Use filter option with service principal API when lookup variable used
    
    ## Why
    Previously we were listing all SPs in the workspace which can be very
    slow. Using filter option in API allows to filter out the result
    quicker/
    
    ## Tests
    Updated unit test
    
    <!-- If your PR needs to be included in the release notes for next
    release,
    add a separate entry in NEXT_CHANGELOG.md as part of your PR. -->
    
    ---------
    
    Co-authored-by: simon <4305831+simonfaltum@users.noreply.github.com>

--- bundle/config/variable/resolve_service_principal.go
+++ bundle/config/variable/resolve_service_principal.go
@@ -2,8 +2,11 @@ package variable
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/databricks/databricks-sdk-go"
+	"github.com/databricks/databricks-sdk-go/listing"
+	"github.com/databricks/databricks-sdk-go/service/iam"
 )
 
 type resolveServicePrincipal struct {
@@ -12,11 +15,23 @@ type resolveServicePrincipal struct {
 
 func (l resolveServicePrincipal) Resolve(ctx context.Context, w *databricks.WorkspaceClient) (string, error) {
 	//nolint:staticcheck // this API is deprecated but we still need use it as there is no replacement yet.
-	entity, err := w.ServicePrincipals.GetByDisplayName(ctx, l.name)
+	it := w.ServicePrincipalsV2.List(ctx, iam.ListServicePrincipalsRequest{
+		Filter: fmt.Sprintf("displayName eq '%s'", l.name),
+	})
+
+	servicePrincipals, err := listing.ToSliceN(ctx, it, 1)
 	if err != nil {
 		return "", err
 	}
-	return entity.ApplicationId, nil
+	if len(servicePrincipals) == 0 {
+		return "", fmt.Errorf("service principal named %q does not exist", l.name)
+	}
+
+	if len(servicePrincipals) > 1 {
+		return "", fmt.Errorf("multiple service principals found with display name %q", l.name)
+	}
+
+	return servicePrincipals[0].ApplicationId, nil
 }
 
 func (l resolveServicePrincipal) String() string {
