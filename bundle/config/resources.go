e816436b6f4b05ea377c29f12e218237bf18e163
Andrew Nester <andrew.nester@databricks.com>
2026-08-10 10:56:01 +0200

    fmt

--- bundle/config/resources.go
+++ bundle/config/resources.go
@@ -203,6 +203,6 @@ func SupportedResources() map[string]resources.ResourceDescription {
 		"vector_search_endpoints":      (&resources.VectorSearchEndpoint{}).ResourceDescription(),
 		"vector_search_indexes":        (&resources.VectorSearchIndex{}).ResourceDescription(),
 		"internal_immutable_snapshots": (&resources.Snapshot{}).ResourceDescription(),
-    "secrets":                      (&resources.Secret{}).ResourceDescription(),
+		"secrets":                      (&resources.Secret{}).ResourceDescription(),
 	}
 }
