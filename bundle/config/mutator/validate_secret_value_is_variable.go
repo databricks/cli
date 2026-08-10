ac36b76ab9b48e7851a7c2ee194c9d1bba1fbb58
Andrew Nester <andrew.nester@databricks.com>
2026-08-10 14:02:13 +0200

    fix

--- bundle/config/mutator/validate_secret_value_is_variable.go
+++ bundle/config/mutator/validate_secret_value_is_variable.go
@@ -30,8 +30,8 @@ func (v *validateSecretValueIsVariable) Apply(ctx context.Context, b *bundle.Bun
 		if dyn.IsNoSuchKeyError(err) {
 			diags = append(diags, diag.Diagnostic{
 				Severity:  diag.Error,
-				Summary:   "Secret value must be a non-empty string",
-				Detail:    fmt.Sprintf(`The secret value for "%s" must be a non-empty string.`, key),
+				Summary:   "Secret value must be a string",
+				Detail:    fmt.Sprintf(`The secret value for "%s" must be a string.`, key),
 				Locations: val.Locations(),
 				Paths:     []dyn.Path{p},
 			})
@@ -42,16 +42,6 @@ func (v *validateSecretValueIsVariable) Apply(ctx context.Context, b *bundle.Bun
 		}
 
 		valueStr := val.MustString()
-		if valueStr == "" {
-			diags = append(diags, diag.Diagnostic{
-				Severity:  diag.Error,
-				Summary:   "Secret value must be a non-empty string",
-				Detail:    fmt.Sprintf(`The secret value for "%s" must be a non-empty string.`, key),
-				Locations: val.Locations(),
-				Paths:     []dyn.Path{p},
-			})
-			continue
-		}
 
 		// Value must be a variable reference to prevent leaking secrets in config files
 		if !dynvar.IsPureVariableReference(valueStr) {
