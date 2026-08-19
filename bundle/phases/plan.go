58392c9b331450ed46eeaac71b264f464d94c0c3
Rada Kamysheva <rada.kamysheva@databricks.com>
2026-08-19 14:50:14 +0000

    acc: cover on_file_change globs, errors, and appear/disappear

--- bundle/phases/plan.go
+++ bundle/phases/plan.go
@@ -21,8 +21,7 @@ import (
 // PreDeployChecks is common set of mutators between "bundle plan" and "bundle deploy".
 // Note, it is not run in "bundle migrate" so it must not modify the config
 func PreDeployChecks(ctx context.Context, b *bundle.Bundle, isPlan bool, engine engine.EngineType) {
-	bundle.ApplySeqContext(
-		ctx, b,
+	bundle.ApplySeqContext(ctx, b,
 		terraform.CheckDashboardsModifiedRemotely(isPlan, engine),
 		resourcemutator.SecretScopeFixups(engine),
 		deploy.StatePull(),
@@ -30,7 +29,6 @@ func PreDeployChecks(ctx context.Context, b *bundle.Bundle, isPlan bool, engine 
 		mutator.ValidateDirectOnlyResources(engine),
 		mutator.ValidateLifecycleStarted(engine),
 		mutator.ValidateCascadeOnDestroy(engine),
-		mutator.ResolveJobRunFileTriggers(),
 		statemgmt.CheckRunningResource(engine),
 	)
 }
