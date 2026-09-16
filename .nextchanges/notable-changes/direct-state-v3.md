4af91675f7fd8682ffc4a37ce439c93a17e42f33
shreyas-goenka <88374338+shreyas-goenka@users.noreply.github.com>
2026-09-16 15:32:38 +0200

    Bump direct deployment state version to 3 (#6713)
    ## Changes
    Update the state version for direct to 3. Also adds tests to ensure all
    permutations of features in state are handled correctly by old and new
    CLI versions.

---
+++ .nextchanges/notable-changes/direct-state-v3.md
@@ -0,0 +1,1 @@
+* Bump the direct deployment state version to 3. Clients older than v1.8.0 will reject bundles deployed with this release. ([#6713](https://github.com/databricks/cli/pull/6713))
--- acceptance/acceptance_test.go
+++ acceptance/acceptance_test.go
@@ -400,6 +400,10 @@ func testAccept(t *testing.T, inprocessMode bool, selectedTests []string, skipTo
 		t.Setenv("CLI_293", cli293Path)
 		repls.SetPath(cli293Path, "[CLI_293]")
 
+		cli18Path := DownloadCLI(t, buildDir, "1.8.0")
+		t.Setenv("CLI_1_8", cli18Path)
+		repls.SetPath(cli18Path, "[CLI_1_8]")
+
 		// CLI version that predates the hashed_fields feature. Used by acceptance/bundle/hashing
 		cli1161Path := DownloadCLI(t, buildDir, "1.16.1")
 		t.Setenv("CLI_1_16_1", cli1161Path)
